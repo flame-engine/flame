5a1f41f7f62763978e59ecc1dc7161b378ca1cd2
feroult <feroult@gmail.com>
2017-11-23 08:33:39 -0200

    move gesture to util

--- lib/util.dart
+++ lib/util.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:ui';
 import 'dart:ui' as ui show TextStyle;
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -29,13 +30,23 @@ class Util {
 
   Paragraph text(String text,
       {double fontSize = 24.0,
-      Color color = Colors.white,
-      fontFamily: 'Arial',
-      double maxWidth = 180.0}) {
+        Color color = Colors.white,
+        fontFamily: 'Arial',
+        double maxWidth = 180.0}) {
     ParagraphBuilder paragraph = new ParagraphBuilder(new ParagraphStyle());
     paragraph.pushStyle(new ui.TextStyle(
         color: color, fontSize: fontSize, fontFamily: fontFamily));
     paragraph.addText(text);
-    return paragraph.build()..layout(new ParagraphConstraints(width: maxWidth));
+    return paragraph.build()
+      ..layout(new ParagraphConstraints(width: maxWidth));
   }
+
+  void addGestureRecognizer(GestureRecognizer recognizer) {
+    GestureBinding.instance.pointerRouter.addGlobalRoute((PointerEvent e) {
+      if (e is PointerDownEvent) {
+        recognizer.addPointer(e);
+      }
+    });
+  }
+
 }
