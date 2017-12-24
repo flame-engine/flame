d138edd6114fd34837ce2f0cea59494c696faafd
Luan Nico <luanpotter27@gmail.com>
2017-12-24 16:16:29 -0200

    extracting animation from component, fix rotate around center bug, add width and height to PositionComponent

--- lib/components/animation_component.dart
+++ lib/components/animation_component.dart
@@ -1,41 +1,44 @@
 import 'dart:ui';
 
 import 'component.dart';
+import 'animation.dart';
 import '../sprite.dart';
 
 class AnimationComponent extends PositionComponent {
 
-  double width, height;
+  Animation animation;
 
-  List<Sprite> sprites;
-  double stepTime = 0.1;
-  double lifeTime = 0.0;
+  AnimationComponent(double width, double height, this.animation) {
+    this.width = width;
+    this.height = height;
+  }
 
-  AnimationComponent.spriteList(this.width, this.height, this.sprites, { this.stepTime, this.lifeTime });
+  AnimationComponent.sequenced(width, height, String imagePath, int amount, { double textureX = 0.0, double textureY = 0.0, double textureWidth = -1.0, double textureHeight = -1.0}) {
+    this.width = width;
+    this.height = height;
 
-  AnimationComponent.sequenced(this.width, this.height, String imagePath, int amount, { double textureX = 0.0, double textureY = 0.0, double textureWidth = -1.0, double textureHeight = -1.0}) {
     if (textureWidth == -1) {
       textureWidth = this.width;
     }
     if (textureHeight == -1) {
       textureHeight = this.height;
     }
-    sprites = new List<Sprite>(amount);
+
+    animation = new Animation();
+    animation.sprites = new List<Sprite>(amount);
     for (var i = 0; i < amount; i++) {
-      sprites[i] = new Sprite(imagePath, x: textureX + i*textureWidth, y: textureY, width: textureWidth, height: textureHeight);
+      animation.sprites[i] = new Sprite(imagePath, x: textureX + i*textureWidth, y: textureY, width: textureWidth, height: textureHeight);
     }
   }
 
   @override
   void render(Canvas canvas) {
     prepareCanvas(canvas);
-
-    int i = (lifeTime / stepTime).round();
-    sprites[i % sprites.length].render(canvas, width, height);
+    animation.getSprite().render(canvas, width, height);
   }
 
   @override
   void update(double t) {
-    this.lifeTime += t;
+    animation.update(t);
   }
 }
