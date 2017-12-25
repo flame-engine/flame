0d514f0e795059baf5096b15a07eec2ff04a8185
Luan Nico <luanpotter27@gmail.com>
2017-12-25 10:00:02 -0200

    adding methods to position

--- lib/position.dart
+++ lib/position.dart
@@ -1,7 +1,25 @@
+import 'dart:math' as math;
+
 class Position {
   double x, y;
 
   Position(this.x, this.y);
 
   Position.empty() : this(0.0, 0.0);
+
+  Position add(Position other) {
+    this.x += other.x;
+    this.y += other.y;
+    return this;
+  }
+
+  Position opposite() {
+    this.x *= -1;
+    this.y *= -1;
+    return this;
+  }
+
+  double length() {
+    return math.sqrt(math.pow(this.x, 2) + math.pow(this.y, 2));
+  }
 }
