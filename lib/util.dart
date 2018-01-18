922f71a1d047b7095ee0eac50650e72d05539e9e
Luan Nico <luanpotter27@gmail.com>
2018-01-18 12:47:51 -0200

    removed old api parameter for text, changed default color to black

--- lib/util.dart
+++ lib/util.dart
@@ -29,7 +29,7 @@ class Util {
     });
   }
 
-  material.TextPainter text(String text, { fontSize = 24.0, color = material.Colors.white, fontFamily: 'Arial', maxWidth = 180.0, textAlign: TextAlign.left, textDirection: TextDirection.ltr }) {
+  material.TextPainter text(String text, { fontSize = 24.0, color = material.Colors.black, fontFamily: 'Arial', textAlign: TextAlign.left, textDirection: TextDirection.ltr }) {
     material.TextStyle style = new material.TextStyle(color: color, fontSize: fontSize, fontFamily: fontFamily);
     material.TextSpan span = new material.TextSpan(style: style, text: text);
     material.TextPainter tp = new material.TextPainter(text: span, textAlign: textAlign, textDirection: textDirection);
