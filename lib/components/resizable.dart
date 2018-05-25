6f8c10c95b93489b8abd70ba9896769a3ccc517f
Luan Nico <luanpotter27@gmail.com>
2018-05-24 22:19:41 -0300

    adding a resizable mixin

---
+++ lib/components/resizable.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+/// Useful mixin to add to your components if you want to hold a reference to the current screen size.
+///
+/// This mixin implements the resize method in order to hold an updated reference to the current screen [size].
+/// Also, it updates its [children], if any.
+class Resizable {
+  /// This is the current updated screen size.
+  Size size;
+
+  /// Implementation provided by this mixin to the resize hook.
+  resize(Size size) {
+    this.size = size;
+    children().where((e) => e != null).forEach((e) => e.resize(size));
+  }
+
+  /// Overwrite this to add children to this [Resizable].
+  ///
+  /// If a [Resizable] has children, its children as resized as well.
+  List<Resizable> children() => [];
+}
