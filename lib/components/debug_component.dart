f03e55876b655a230598ed38e0caa6eb74c332e5
Luan Nico <luanpotter27@gmail.com>
2018-05-24 22:19:14 -0300

    adding debug component

---
+++ lib/components/debug_component.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'component.dart';
+import 'package:flutter/painting.dart';
+
+/// This is a debug component that draws a rect on its position.
+///
+/// You can add it to your game to find out the position of something that's not normally rendered (like a hitbox or point o interaction).
+/// It will render a rectangle of ([width], [height]) on the position([x], [y]), of color [color].
+class DebugComponent extends PositionComponent {
+  /// The color of the rectangle (defaults to magenta).
+  Color color = const Color(0xFFFF00FF);
+
+  /// The actual paint used; by default it paints with stroke only and [color].
+  Paint get paint => new Paint()
+    ..color = color
+    ..style = PaintingStyle.stroke;
+
+  /// Don't do anything (change as desired)
+  void update(double t) {}
+
+  /// Renders the recatangle
+  void render(Canvas c) {
+    prepareCanvas(c);
+    c.drawRect(new Rect.fromLTWH(0.0, 0.0, width, height), paint);
+  }
+
+  /// Don't do anything (change as desired)
+  void resize(Size size) {}
+
+  bool loaded() => true;
+  bool destroy() => false;
+  bool isHud() => false;
+}
