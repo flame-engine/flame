e0eb074330644a2668c227cb990b2be8d1ce1da5
feroult <feroult@gmail.com>
2018-12-29 19:31:49 -0200

    tiled init

--- lib/images.dart
+++ lib/images.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui';
-import 'dart:async';
+
+import 'package:flame/flame.dart';
 
 class Images {
   Map<String, Image> loadedFiles = {};
@@ -18,15 +19,16 @@ class Images {
     return Future.wait(fileNames.map(load));
   }
 
-  Future<Image> load(String fileName) async {
-    if (!loadedFiles.containsKey(fileName)) {
-      loadedFiles[fileName] = await _fetchToMemory(fileName);
+  Future<Image> load(String fileName, {String prefix = 'images/'}) async {
+    var path = prefix + fileName;
+    if (!loadedFiles.containsKey(path)) {
+      loadedFiles[path] = await _fetchToMemory(path);
     }
-    return loadedFiles[fileName];
+    return loadedFiles[path];
   }
 
   Future<Image> _fetchToMemory(String name) async {
-    ByteData data = await rootBundle.load('assets/images/' + name);
+    ByteData data = await Flame.bundle.load('assets/' + name);
     Uint8List bytes = new Uint8List.view(data.buffer);
     Completer<Image> completer = new Completer();
     decodeImageFromList(bytes, (image) => completer.complete(image));
