381323fed5b24e5d850b664b991444a3dbf629de
feroult <feroult@gmail.com>
2018-12-30 18:52:17 -0200

    tiled

--- lib/tiled/tiled_component.dart
+++ lib/tiled/tiled_component.dart
@@ -5,12 +5,13 @@ import 'package:flame/flame.dart';
 import 'package:flutter/material.dart' show Colors;
 import 'package:tmx/tmx.dart' show TileMap, TileMapParser;
 
-
 class TiledComponent extends Component {
   String filename;
   TileMap map;
   Image image;
   Future future;
+
+  static Paint paint = new Paint()..color = Colors.white;
 
   TiledComponent(this.filename) {
     this.future = _load();
@@ -18,26 +19,33 @@ class TiledComponent extends Component {
 
   Future _load() async {
     this.map = await _loadMap();
-    this.image = await Flame.images
-        .load(map.tilesets[0].images[0].source);
+    this.image = await Flame.images.load(map.tilesets[0].image.source);
   }
 
   Future<TileMap> _loadMap() {
-    return Flame.bundle
-        .loadString('assets/tiles/' + filename)
-        .then((contents) {
+    return Flame.bundle.loadString('assets/tiles/' + filename).then((contents) {
       var parser = new TileMapParser();
       return parser.parse(contents);
     });
   }
 
+  bool loaded() => image != null;
+
   @override
   void render(Canvas c) {
-    if (image == null) {
+    if (!loaded()) {
       return;
     }
-    Paint paint = new Paint()..color = Colors.white;
-    c.drawImage(image, Offset(0, 0), paint);
+
+    map.layers[0].tiles.forEach((tile) {
+      var rect = tile.computeDrawRect();
+      var src = Rect.fromLTWH(rect.left.toDouble(), rect.top.toDouble(),
+          rect.width.toDouble(), rect.height.toDouble());
+      var dst = Rect.fromLTWH(tile.x.toDouble(), tile.y.toDouble(),
+          rect.width.toDouble(), rect.height.toDouble());
+      c.drawImageRect(image, src, dst, paint);
+    });
+    // c.drawImage(image, Offset(0, 110), paint);
   }
 
   @override
