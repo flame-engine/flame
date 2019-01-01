b1f101105395ef75eed2b1fd89bdea6dfcbc53eb
feroult <feroult@gmail.com>
2019-01-01 20:15:44 -0200

    tiled

--- test/components/tiled_test.dart
+++ test/components/tiled_test.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flame/components/tiled_component.dart';
 import 'package:flame/flame.dart';
-import 'package:flame/tiled/tiled_component.dart';
 import 'package:flutter/services.dart' show CachingAssetBundle;
 import 'package:test/test.dart';
 
@@ -18,11 +18,11 @@ void main() {
 class TestAssetBundle extends CachingAssetBundle {
   @override
   Future<ByteData> load(String key) async =>
-      new File('test/tiled/assets/map-level1.png')
+      new File('assets/map-level1.png')
           .readAsBytes()
           .then((bytes) => ByteData.view(Uint8List.fromList(bytes).buffer));
 
   @override
   Future<String> loadString(String key, {bool cache = true}) =>
-      new File('test/tiled/assets/map.tmx').readAsString();
+      new File('assets/map.tmx').readAsString();
 }
