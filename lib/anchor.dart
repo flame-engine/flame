b788bf9389f0575c9c528bae01b22b303a44aaf5
Luan Nico <luanpotter27@gmail.com>
2019-01-01 22:45:37 -0200

    creating new anchor class to be used by other classes

---
+++ lib/anchor.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'position.dart';
+
+class Anchor {
+  static const Anchor topLeft = Anchor(Offset(0.0, 0.0));
+  static const Anchor topCenter = Anchor(Offset(0.5, 0.0));
+  static const Anchor topRight = Anchor(Offset(1.0, 0.0));
+  static const Anchor centerLeft = Anchor(Offset(0.0, 0.5));
+  static const Anchor center = Anchor(Offset(0.5, 0.5));
+  static const Anchor centerRight = Anchor(Offset(1.0, 0.5));
+  static const Anchor bottomLeft = Anchor(Offset(0.0, 1.0));
+  static const Anchor bottomCenter = Anchor(Offset(0.5, 1.0));
+  static const Anchor bottomRight = Anchor(Offset(1.0, 1.0));
+
+  final Offset relativePosition;
+
+  const Anchor(this.relativePosition);
+
+  Position translate(Position p, Position size) {
+    return p.clone().minus(new Position(size.x * relativePosition.dx, size.y * relativePosition.dy));
+  }
+}
