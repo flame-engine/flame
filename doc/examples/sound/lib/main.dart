89e26e232940fa865d84e31f3589268d802d91a3
Luan Nico <luanpotter27@gmail.com>
2019-04-20 20:28:15 -0300

    fixing examples

--- doc/examples/sound/lib/main.dart
+++ doc/examples/sound/lib/main.dart
@@ -7,32 +7,31 @@ void main() => runApp(MyGame().widget);
 
 class Ball extends PositionComponent {
   final Size gameSize;
-  final paint = Paint()..color = Color(0xFFFFFFFF);
+  final paint = Paint()..color = const Color(0xFFFFFFFF);
 
   bool foward = true;
 
   Ball(this.gameSize);
 
+  @override
   void render(Canvas c) {
-    c.drawOval(this.toRect(), paint);
+    c.drawOval(toRect(), paint);
   }
 
+  @override
   void update(double delta) {
-    if (foward) {
-      this.x += 100 * delta;
-    } else {
-      this.x -= 100 * delta;
-    }
+    x += (foward ? 1 : -1) * 100 * delta;
 
-    if (this.x <= 0 || this.x + this.width >= gameSize.width) {
-      if (foward)
-        this.x = gameSize.width - this.width - 1;
-      else
-        this.x = 1;
+    if (x <= 0 || x + width >= gameSize.width) {
+      if (foward) {
+        x = gameSize.width - width - 1;
+      } else {
+        x = 1;
+      }
 
       foward = !foward;
-      print("boin");
-      Flame.audio.play("boin.mp3", volume: 1.2);
+      print('boin');
+      Flame.audio.play('boin.mp3', volume: 1.2);
     }
   }
 }
@@ -42,12 +41,12 @@ class MyGame extends BaseGame {
     _start();
   }
 
-  _start() async {
-    Size size = await Flame.util.initialDimensions();
+  void _start() async {
+    final Size size = await Flame.util.initialDimensions();
 
     Flame.audio.disableLog();
-    Flame.audio.load("boin.mp3");
-    Flame.audio.loop("music.mp3", volume: 0.4);
+    Flame.audio.load('boin.mp3');
+    Flame.audio.loop('music.mp3', volume: 0.4);
 
     add(Ball(size)
       ..y = (size.height / 2) - 50
