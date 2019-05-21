dd0de2e751fd7e4f3978ad9f0a780bbc8f58d53e
Erick Zanardo <erickzanardoo@gmail.com>
2019-05-21 12:20:05 -0300

    Adding go desktop example

---
+++ doc/examples/go_desktop/lib/main_desktop.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
+
+import 'package:flutter/material.dart';
+
+import './game.dart';
+
+void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  runApp(MyGame().widget);
+}
