63f6ea0cbe6d7224d7ff5f4d2a1449f0836ad537
Erick Zanardo <erickzanardoo@gmail.com>
2019-05-22 09:24:25 -0300

    Linting files

--- doc/examples/go_desktop/lib/game.dart
+++ doc/examples/go_desktop/lib/game.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 class MyGame extends BaseGame {
-
   static final Paint paint = Paint()..color = Color(0xFFFFFFFF);
 
   var movingLeft = false;
--- doc/examples/go_desktop/lib/main_desktop.dart
+++ doc/examples/go_desktop/lib/main_desktop.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
 
 import 'package:flutter/material.dart';
 
