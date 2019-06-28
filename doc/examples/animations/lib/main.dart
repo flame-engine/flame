1b23ccf9f7d2db397be683e012cc6f03a66570d9
Erick Zanardo <erickzanardoo@gmail.com>
2019-06-28 18:11:38 -0300

    Adding destoryOnFinish flag to AnimationComponent

--- doc/examples/animations/lib/main.dart
+++ doc/examples/animations/lib/main.dart
@@ -1,21 +1,37 @@
+import 'package:flutter/gestures.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/animation.dart' as flame_animation;
 import 'package:flame/components/animation_component.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyGame().widget);
+void main() {
+  final game = MyGame();
+  runApp(game.widget);
+  Flame.util.addGestureRecognizer(TapGestureRecognizer()
+      ..onTapDown = (TapDownDetails evt) {
+        game.addAnimation();
+      });
+}
 
 class MyGame extends BaseGame {
+  final animation = flame_animation.Animation.sequenced('chopper.png', 4,
+      textureWidth: 48, textureHeight: 48, stepTime: 0.15);
+
   MyGame() {
     _start();
   }
 
+  void addAnimation() {
+    final animationComponent = AnimationComponent(100, 100, animation, destroyOnFinish: true);
+    animationComponent.x = size.width / 2 - 50;
+    animationComponent.y = 200;
+
+    add(animationComponent);
+  }
+
   void _start() async {
     final Size size = await Flame.util.initialDimensions();
-
-    final animation = flame_animation.Animation.sequenced('chopper.png', 4,
-        textureWidth: 48, textureHeight: 48, stepTime: 0.15);
 
     final animationComponent = AnimationComponent(100, 100, animation);
     animationComponent.x = size.width / 2 - 100;
