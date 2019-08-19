ef7e2303cebf88f8ba9dc1f2715816c056df8f66
Erick Zanardo <erickzanardoo@gmail.com>
2019-08-19 15:50:09 -0300

    Formating

--- lib/components/events/gestures.dart
+++ lib/components/events/gestures.dart
@@ -2,7 +2,7 @@ import '../component.dart';
 import 'package:flutter/gestures.dart';
 
 mixin Tapeable on PositionComponent {
-    void onTapCancel() {}
-    void onTapDown(TapDownDetails details) {}
-    void onTapUp(TapUpDetails details) {}
+  void onTapCancel() {}
+  void onTapDown(TapDownDetails details) {}
+  void onTapUp(TapUpDetails details) {}
 }
