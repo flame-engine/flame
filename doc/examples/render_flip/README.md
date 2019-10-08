b967c383faf964f7a9c636ab9ac862da09cbf055
kurtome <kurt@melby.me>
2019-10-07 20:47:53 -0400

    adding example and only calculating flip if needed

---
+++ doc/examples/render_flip/README.md
@@ -0,0 +1,4 @@
+# render_flip
+
+A Flame game showcasing how to use render flipping on a `PositionComponent`
+to control the rendered direction.
---
+++ doc/examples/render_flip/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/animation.dart' as flame_animation;
+import 'package:flame/components/animation_component.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  final Size size = await Flame.util.initialDimensions();
+  final game = MyGame(size);
+  runApp(game.widget);
+}
+
+class MyGame extends BaseGame {
+  final animation = flame_animation.Animation.sequenced('chopper.png', 4,
+      textureWidth: 48, textureHeight: 48, stepTime: 0.15);
+
+  AnimationComponent buildAnimation() {
+    final ac = AnimationComponent(100, 100, animation);
+    ac.x = size.width / 2 - ac.width / 2;
+    return ac;
+  }
+
+  MyGame(Size screenSize) {
+    size = screenSize;
+
+    final regular = buildAnimation();
+    regular.y = 100;
+    add(regular);
+
+    final flipX = buildAnimation();
+    flipX.y = 300;
+    flipX.renderFlipX = true;
+    add(flipX);
+
+    final flipY = buildAnimation();
+    flipY.y = 500;
+    flipY.renderFlipY = true;
+    add(flipY);
+  }
+}
