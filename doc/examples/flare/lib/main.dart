33b457b5a5f213b082a9b1ba0c478bbb7c81f4f4
Luan Nico <luanpotter27@gmail.com>
2019-10-14 18:28:20 -0400

    uppercasing colors

--- doc/examples/flare/lib/main.dart
+++ doc/examples/flare/lib/main.dart
@@ -23,7 +23,7 @@ class MyGame extends BaseGame {
   final TextConfig fpsTextConfig =
       const TextConfig(color: const Color(0xFFFFFFFF));
 
-  final paint = Paint()..color = const Color(0xFFe5e5e5e5);
+  final paint = Paint()..color = const Color(0xFFE5E5E5E5);
   final List<String> _animations = ["Stand", "Wave", "Jump", "Dance"];
   int _currentAnimation = 0;
 
