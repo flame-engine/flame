dfe8c9a526723d09db0954cfbf06317fa6fd9a4d
Erick Zanardo <erickzanardoo@gmail.com>
2020-04-19 15:06:54 -0300

    Adding SpriteButton

---
+++ doc/examples/widgets/lib/generated_plugin_registrant.dart
@@ -0,0 +1,16 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:audioplayers/audioplayers_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  AudioplayersPlugin.registerWith(registry.registrarFor(AudioplayersPlugin));
+  registry.registerMessageHandler();
+}
