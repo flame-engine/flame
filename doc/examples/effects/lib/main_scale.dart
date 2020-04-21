ed11b5c4f1036a8810da5e725351b60a4d07168a
Erick Zanardo <erickzanardoo@gmail.com>
2020-04-21 10:14:31 -0300

    Ensuring final position and size for MoveEffect and ScaleEffect

--- doc/examples/effects/lib/main_scale.dart
+++ doc/examples/effects/lib/main_scale.dart
@@ -14,7 +14,7 @@ class MyGame extends BaseGame with TapDetector {
     add(square = Square()
       ..anchor = Anchor.center
       ..x = 200
-      ..y = 400);
+      ..y = 200);
   }
 
   @override
