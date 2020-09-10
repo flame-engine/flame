1c6aa34885b598cd96e12922e49503432f944c8c
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2020-09-10 21:46:50 +0200

    No need for named imports in VectorUtil

--- lib/vector.dart
+++ lib/vector.dart
@@ -2,8 +2,8 @@ library flame.vector_math;
 
 export 'package:vector_math/vector_math_64.dart' show Vector2;
 
-import 'dart:math' as math;
-import 'dart:ui' as ui;
+import 'dart:math';
+import 'dart:ui';
 import 'package:vector_math/vector_math_64.dart';
 
 class VectorUtil {
@@ -12,36 +12,36 @@ class VectorUtil {
   /// Internal representation is still using double, the conversion is made in the constructor only.
   static Vector2 fromInts(int x, int y) => Vector2(x.toDouble(), y.toDouble());
 
-  /// Creates a [Vector2] using an [ui.Offset]
-  static Vector2 fromOffset(ui.Offset offset) => Vector2(offset.dx, offset.dy);
+  /// Creates a [Vector2] using an [Offset]
+  static Vector2 fromOffset(Offset offset) => Vector2(offset.dx, offset.dy);
 
-  /// Creates a [Vector2] using an [ui.Size]
-  static Vector2 fromSize(ui.Size size) => Vector2(size.width, size.height);
+  /// Creates a [Vector2] using an [Size]
+  static Vector2 fromSize(Size size) => Vector2(size.width, size.height);
 
   /// Creates a [Vector2] using a [math.Point]
-  static Vector2 fromPoint(math.Point point) => Vector2(point.x, point.y);
+  static Vector2 fromPoint(Point point) => Vector2(point.x, point.y);
 
-  /// Creates an [ui.Offset] from a [Vector2]
-  static ui.Offset toOffset(Vector2 v) => ui.Offset(v.x, v.y);
+  /// Creates an [Offset] from a [Vector2]
+  static Offset toOffset(Vector2 v) => Offset(v.x, v.y);
 
-  /// Creates an [ui.Size] from a [Vector2]
-  static ui.Size toSize(Vector2 v) => ui.Size(v.x, v.y);
+  /// Creates an [Size] from a [Vector2]
+  static Size toSize(Vector2 v) => Size(v.x, v.y);
 
   /// Creates a [math.Point] from a [Vector2]
-  static math.Point toPoint(Vector2 v) => math.Point(v.x, v.y);
+  static Point toPoint(Vector2 v) => Point(v.x, v.y);
 
-  /// Creates a [ui.Rect] from two [Vector2]
-  static ui.Rect rectFrom(Vector2 topLeft, Vector2 size) {
-    return ui.Rect.fromLTWH(topLeft.x, topLeft.y, size.x, size.y);
+  /// Creates a [Rect] from two [Vector2]
+  static Rect rectFrom(Vector2 topLeft, Vector2 size) {
+    return Rect.fromLTWH(topLeft.x, topLeft.y, size.x, size.y);
   }
 
-  /// Creates bounds in from of a [ui.Rect] from a list of [Vector2]
-  static ui.Rect bounds(List<Vector2> pts) {
-    final double minx = pts.map((e) => e.x).reduce(math.min);
-    final double maxx = pts.map((e) => e.x).reduce(math.max);
-    final double miny = pts.map((e) => e.y).reduce(math.min);
-    final double maxy = pts.map((e) => e.y).reduce(math.max);
-    return ui.Rect.fromPoints(ui.Offset(minx, miny), ui.Offset(maxx, maxy));
+  /// Creates bounds in from of a [Rect] from a list of [Vector2]
+  static Rect bounds(List<Vector2> pts) {
+    final double minx = pts.map((e) => e.x).reduce(min);
+    final double maxx = pts.map((e) => e.x).reduce(max);
+    final double miny = pts.map((e) => e.y).reduce(min);
+    final double maxy = pts.map((e) => e.y).reduce(max);
+    return Rect.fromPoints(Offset(minx, miny), Offset(maxx, maxy));
   }
 
   /// Linearly interpolate between two vectors.
@@ -51,16 +51,18 @@ class VectorUtil {
 
   /// Rotates the gives [Vector2] with [angle] in radians
   static void rotate(Vector2 v, double angle) {
-    final double sin = math.sin(angle);
-    final double cos = math.cos(angle);
-    v.setValues(v.x * cos - v.y * sin, v.x * sin + v.y * cos);
+    v.setValues(
+        v.x * cos(angle) - v.y * sin(angle),
+        v.x * sin(angle) + v.y * cos(angle),
+    );
   }
 
   /// Returns a new [Vector2] that is [v] rotated with [angle] in radians
   static Vector2 rotated(Vector2 v, double angle) {
-    final double sin = math.sin(angle);
-    final double cos = math.cos(angle);
-    return Vector2(v.x * cos - v.y * sin, v.x * sin + v.y * cos);
+    return Vector2(
+        v.x * cos(angle) - v.y * sin(angle),
+        v.x * sin(angle) + v.y * cos(angle),
+    );
   }
 
   /// Returns a new vector with the [length] scaled to [newLength], without changing direction.
