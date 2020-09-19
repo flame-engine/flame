096a151e1e388799148b343848400b56ce849d67
Luan Nico <luanpotter27@gmail.com>
2020-09-19 11:07:04 -0400

    Add docs and comments

--- lib/components/isometric_tile_map_component.dart
+++ lib/components/isometric_tile_map_component.dart
@@ -6,16 +6,34 @@ import '../flame.dart';
 import '../position.dart';
 import '../sprite.dart';
 
+/// This represents an isometric tileset to be used in a tilemap.
+///
+/// It's basically a grid of squares, each square has a tile, in order.
+/// The block ids are calculated going row per row, left to right, top to
+/// bottom.
+///
+/// This class will cache the usage of sprites to improve performance.
 class IsometricTileset {
+  /// The image for this tileset.
   final Image tileset;
+
+  /// The size of each square block within the image.
+  ///
+  /// The image width and height must be multiples of this number.
   final int size;
 
   final Map<int, Sprite> _spriteCache = {};
 
   IsometricTileset(this.tileset, this.size);
 
+  /// Compute the number of columns the image has
+  /// by using the image width and tile width.
   int get cols => tileset.width ~/ size;
 
+  /// Get a sprite to render one specific tile given its id.
+  ///
+  /// The ids are assigned left to right, top to bottom, row per row.
+  /// The returned sprite will be cached, so don't modify it!
   Sprite getTile(int tileId) {
     return _spriteCache[tileId] ??= _computeTile(tileId);
   }
@@ -27,27 +45,44 @@ class IsometricTileset {
     return Sprite.fromImage(tileset, x: s * i, y: s * j, width: s, height: s);
   }
 
+  /// Load a tileset based on a file name.
   static Future<IsometricTileset> load(String fileName, int size) async {
     final image = await Flame.images.load(fileName);
     return IsometricTileset(image, size);
   }
 }
 
+/// This is just a pair of int, int.
+///
+/// Represents a position in a matrix, or in this case, on the tilemap.
 class Block {
+  /// x and y coordinates on the matrix
   int x, y;
+
   Block(this.x, this.y);
 
   @override
   String toString() => '($x, $y)';
 }
 
+/// This component renders a tilemap, represented by an int matrix, given a
+/// tileset, in witch the integers are the block ids.
+///
+/// It can change the scale of each block by using the optional destTileSize
+/// property.
 class IsometricTileMapComponent extends PositionComponent {
+  /// This is the tileset that will be used to render this map.
   IsometricTileset tileset;
+
+  /// The positions of each block will be placed respecting this matrix.
   List<List<int>> matrix;
+
+  /// Optionally provide a new tile size to render it scaled.
   int destTileSize;
 
   IsometricTileMapComponent(this.tileset, this.matrix, {this.destTileSize});
 
+  /// This is the size the tiles will be drawn (either original or overwritten).
   int get effectiveTileSize => destTileSize ?? tileset.size;
 
   @override
@@ -68,6 +103,9 @@ class IsometricTileMapComponent extends PositionComponent {
     });
   }
 
+  /// Get the position in witch a block must be in the isometric space.
+  ///
+  /// This does not include the (x,y) PositionComponent offset!
   Position getBlockPosition(Block block) {
     return getBlockPositionInts(block.x, block.y);
   }
@@ -77,18 +115,23 @@ class IsometricTileMapComponent extends PositionComponent {
     return cartToIso(Position(i * s, j * s)).minus(Position(s, 0));
   }
 
+  /// Converts a coordinate from the isometric space to the cartesian space.
   Position isoToCart(Position p) {
     final x = (2 * p.y + p.x) / 2;
     final y = (2 * p.y - p.x) / 2;
     return Position(x, y);
   }
 
+  /// Converts a coordinate from the cartesian space to the isometric space.
   Position cartToIso(Position p) {
     final x = p.x - p.y;
     final y = (p.x + p.y) / 2;
     return Position(x, y);
   }
 
+  /// Get what block is at isometric position p.
+  ///
+  /// This can be used to handle clicks or hovers.
   Block getBlock(Position p) {
     final s = effectiveTileSize.toDouble() / 2;
     final cart = isoToCart(p.clone().minus(toPosition()));
@@ -97,6 +140,7 @@ class IsometricTileMapComponent extends PositionComponent {
     return Block(px, py);
   }
 
+  /// Return whether the matrix contains a block in its bounds.
   bool containsBlock(Block block) {
     return block.x >= 0 &&
         block.x < matrix.length &&
