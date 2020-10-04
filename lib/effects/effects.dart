18048b16a37fdc21b3e6ed5d5812c7c33a2fcc06
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2020-10-04 21:55:29 +0200

    Do a full reset on all infinite effects

--- lib/effects/effects.dart
+++ lib/effects/effects.dart
@@ -60,7 +60,7 @@ abstract class PositionComponentEffect {
     if (isAlternating) {
       curveDirection = isMax() ? -1 : (isMin() ? 1 : curveDirection);
     } else if (isInfinite && isMax()) {
-      currentTime = 0.0;
+      reset();
     }
     final driftMultiplier = (isAlternating && isMax() ? 2 : 1) * curveDirection;
     if (!hasFinished()) {
