8128cc8e69f60e87d56911dc6bda499492297eb8
Luan Nico <luanpotter27@gmail.com>
2020-10-10 00:33:26 -0400

    Removing audio stuff in favor of flame_audio

--- lib/flame.dart
+++ lib/flame.dart
@@ -12,28 +12,21 @@ import 'util.dart';
 
 /// This class holds static references to some useful objects to use in your game.
 ///
-/// You can access shared instances of [AudioCache], [Images] and [Util].
+/// You can access shared instances of [AssetsCache], [Images] and [Util].
 /// Most games should need only one instance of each, and should use this class to manage that reference.
 class Flame {
   // Flame asset bundle, defaults to root
   static AssetBundle _bundle;
   static AssetBundle get bundle => _bundle == null ? rootBundle : _bundle;
 
-  /// Access a shared instance of the [FlameAudio] class.
-  static FlameAudio audio = FlameAudio();
-
-  /// Access a shared instance of the [Bgm] class.
-  static Bgm _bgm;
-  static Bgm get bgm => _bgm ??= Bgm();
+  /// Access a shard instance of [AssetsCache] class.
+  static AssetsCache assets = AssetsCache();
 
   /// Access a shared instance of the [Images] class.
   static Images images = Images();
 
   /// Access a shared instance of the [Util] class.
   static Util util = Util();
-
-  /// Access a shard instance of [AssetsCache] class.
-  static AssetsCache assets = AssetsCache();
 
   static Future<void> init(
       {AssetBundle bundle,
