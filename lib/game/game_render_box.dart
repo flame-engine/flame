69aaf90353c3330443b5d5d17dd4fc2ae41c5802
Gilbert Jolly <gilbertj99@gmail.com>
2020-12-03 09:33:24 +0100

    Add GameRenderBox.computeDryLayout implementation.
    This fixes error in flutter (Channel dev, 1.25.0-4.0.pre, on Mac OS X 10.15.4) Running on OSX
    
    Exception:
    
    ════════ Exception caught by rendering library ═════════════════════════════════════════════════════
    The following assertion was thrown during performResize():
    The GameRenderBox class does not implement "computeDryLayout".
    
    If you are not writing your own RenderBox subclass, then this is not
    your fault.

--- lib/game/game_render_box.dart
+++ lib/game/game_render_box.dart
@@ -81,4 +81,9 @@ class GameRenderBox extends RenderBox with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     game.lifecycleStateChange(state);
   }
+
+  @override
+  Size computeDryLayout(BoxConstraints constraints) {
+    return constraints.biggest;
+  }
 }
