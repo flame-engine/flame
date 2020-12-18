57eb0500d81eb37a4688b420a54cd296900dda99
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2020-12-18 23:32:05 +0100

    All children don't need preparation

--- lib/components/base_component.dart
+++ lib/components/base_component.dart
@@ -114,17 +114,12 @@ abstract class BaseComponent extends Component {
   List<ComponentEffect> get effects => _effectsHandler.effects;
 
   /// Uses the game passed in, or uses the game from [HasGameRef] otherwise,
-
   /// to prepare the child component before it is added to the list of children.
   /// Note that this component needs to be added to the game first if
   /// [this.gameRef] should be used to prepare the child.
   /// For children that don't need preparation from the game instance can
   /// disregard both the options given above.
   Future<void> addChild(Component child, {Game gameRef}) async {
-    assert(
-      gameRef != null || (this as HasGameRef).gameRef != null,
-      "Need gameRef either as an argument or from the HasGameRef mixin",
-    );
     gameRef ??= (this as HasGameRef).gameRef;
     if (gameRef is BaseGame) {
       gameRef.prepare(child);
