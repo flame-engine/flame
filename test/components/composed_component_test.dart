a7c1941c853b4eeaed482fe1cb80f5062814c9c3
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2021-01-18 21:18:18 +0100

    Remove unused imports

--- test/components/composed_component_test.dart
+++ test/components/composed_component_test.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 import 'package:test/test.dart';
 
 import '../util/mock_canvas.dart';
