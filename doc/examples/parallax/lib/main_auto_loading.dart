718379ae8901e848d13c59370501a0c4318d610e
Erick <erickzanardoo@gmail.com>
2021-01-27 09:13:56 -0300

    Adapting ParallaxComponent constructors to other components (#642)
    * Adapating ParallaxComponent constructors to other components
    
    * Removing wrongly commited folder
    
    * Update doc/components.md
    
    Co-authored-by: Jochum van der Ploeg <jochum@vdploeg.net>
    
    * Update doc/components.md
    
    Co-authored-by: Jochum van der Ploeg <jochum@vdploeg.net>
    
    Co-authored-by: Jochum van der Ploeg <jochum@vdploeg.net>

---
+++ doc/examples/parallax/lib/main_auto_loading.dart
@@ -0,0 +1,39 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/parallax.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+  runApp(
+    GameWidget(
+      game: MyGame(),
+    ),
+  );
+}
+
+class MyGame extends BaseGame {
+  @override
+  Future<void> onLoad() async {
+    add(MyParallaxComponent());
+  }
+}
+
+class MyParallaxComponent extends ParallaxComponent with HasGameRef<MyGame> {
+  @override
+  Future<void> onLoad() async {
+    parallax = await gameRef.loadParallax(
+      [
+        'bg.png',
+        'mountain-far.png',
+        'mountains.png',
+        'trees.png',
+        'foreground-trees.png',
+      ],
+      baseVelocity: Vector2(20, 0),
+      velocityMultiplierDelta: Vector2(1.8, 1.0),
+    );
+  }
+}
