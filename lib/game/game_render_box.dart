c8ace4fbdb6ca43983fc1509781d6f66a44cd993
Luan Nico <luanpotter27@gmail.com>
2021-02-15 19:33:23 -0500

    Fix from previous PR

--- lib/game/game_render_box.dart
+++ lib/game/game_render_box.dart
@@ -77,10 +77,13 @@ class GameRenderBox extends RenderBox with WidgetsBindingObserver {
     WidgetsBinding.instance.removeObserver(this);
   }
 
-  // TODO(luan): this was added to flutter beta but pub is still using an old version to lint
-  // in this state it will work for all flutter channels until stable catches up
-  // ignore: annotate_overrides
+  @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     game.lifecycleStateChange(state);
   }
+
+  // TODO(luan): this was added to flutter beta but pub is still using an old version to lint
+  // in this state it will work for all flutter channels until stable catches up
+  // ignore: annotate_overrides
+  Size computeDryLayout(BoxConstraints constraints) => constraints.biggest;
 }
