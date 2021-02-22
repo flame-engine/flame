e013ac6109a7cbea73c11e39ae8a0b0ae7f5b078
Jochum van der Ploeg <jochum@vdploeg.net>
2021-02-22 19:11:44 +0100

    Updated analyze after merge

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+flutter pub get
+
 # We need to run pubget on all the examples
 for f in $(find doc/examples -name 'pubspec.yaml'); do
   d=$(dirname $f)
@@ -8,10 +10,17 @@ for f in $(find doc/examples -name 'pubspec.yaml'); do
 done
 
 cd .
-result=$(flutter analyze --pub .)
-if ! echo "$result" | grep -q "No issues found!"; then
+flutter pub get
+result=$(flutter pub run dart_code_metrics:metrics .)
+if [ "$result" != "" ]; then
+  echo "flutter dart code metrics issues: $1"
   echo "$result"
   exit 1
+fi
+result=$(flutter analyze .)
+if ! echo "$result" | grep -q "No issues found!"; then
+  echo "$result"
+  echo "flutter analyze issue: $1"
 fi
 
 exit 0
