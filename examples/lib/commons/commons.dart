d3a31698c8e740068871509911a5c78e10e5fa13
Luan Nico <luanpotter27@gmail.com>
2021-03-10 21:11:14 -0500

    Address suggestions

--- examples/lib/commons/commons.dart
+++ examples/lib/commons/commons.dart
@@ -1,6 +1,6 @@
 String baseLink(String path) {
   const _basePath =
-      'https://github.com/flame-engine/flame_example/blob/master/lib/stories/';
+      'https://github.com/flame-engine/flame/blob/master/examples/lib/stories/';
 
   return '$_basePath$path';
 }
