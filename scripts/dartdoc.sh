a3616556decf7e844bf888217963f2634eebec2d
Jochum van der Ploeg <jochum@vdploeg.net>
2021-03-22 18:26:24 +0100

    Upgraded scripts to support multi projects

--- scripts/dartdoc.sh
+++ scripts/dartdoc.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
-cd packages/flame
-flutter pub get
-flutter pub run dartdoc --no-auto-include-dependencies --quiet
+function run_dartdoc() {
+  result=$(flutter pub get 2>&1) # Sadly a pub get can block up our actions as it will retry forever if a package is not found, but this should atleast report everything else.
+  if [ $? -ne 0 ]; then
+    echo "flutter pub get failed:"
+    echo "$result"
+    exit 1
+  fi
+
+  flutter pub run dartdoc --no-auto-include-dependencies --quiet
+#   result=$(flutter analyze .)
+#   if ! echo "$result" | grep -q "No issues found!"; then
+#     echo "$result"
+#     echo "flutter analyze issue:"
+#     exit 1
+#   fi
+}
+
+echo "Starting Flame Dartdoc"
+echo "----------------------"
+for file in $(find ./packages -maxdepth 2 -type f -name "pubspec.yaml"); do
+  dir=$(dirname $file)
+  cd $dir
+  echo "Generating dartdoc $dir"
+  run_dartdoc
+  dartdoc_result=$?
+  if [ $dartdoc_result -ne 0 ]; then
+    exit $dartdoc_result
+  fi
+  cd $(cd -)
+done
+
+exit 0
