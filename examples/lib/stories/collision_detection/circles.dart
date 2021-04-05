50be22b075c5a1ff36d713c9f06dddd2f485e506
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2021-04-05 12:54:27 +0200

    Remove ! from gameRef

--- examples/lib/stories/collision_detection/circles.dart
+++ examples/lib/stories/collision_detection/circles.dart
@@ -26,7 +26,7 @@ class MyCollidable extends PositionComponent
 
   @override
   Future<void> onLoad() async {
-    final center = gameRef!.size / 2;
+    final center = gameRef.size / 2;
     velocity = (center - position).normalized() * 150;
   }
 
