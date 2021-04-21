989947174e77717f08b4f8b2cb5f23e0707dfee0
Luan Nico <luanpotter27@gmail.com>
2021-04-21 11:21:57 -0400

    Bring back renderRect to Sprite (#768)

--- packages/flame/lib/src/sprite.dart
+++ packages/flame/lib/src/sprite.dart
@@ -8,6 +8,14 @@ import 'extensions/vector2.dart';
 import 'flame.dart';
 import 'palette.dart';
 
+/// A [Sprite] is a region of an [Image] that can be rendered in the Canvas.
+///
+/// It might represent the entire image or be one of the pieces a spritesheet is composed of.
+/// It holds a reference to the source image from which the region is extracted, and the [src]
+/// rectangle is the portion inside that image that is to be rendered (not to be confused with the
+/// `dest` rect, which is where in the Canvas the sprite is rendered).
+/// It also has a [paint] field that can be overwritten to apply a tint to this [Sprite] (default is
+/// white, meaning no tint).
 class Sprite {
   Paint paint = BasicPalette.white.paint();
   Image image;
@@ -54,12 +62,32 @@ class Sprite {
     src = (position ?? Vector2.zero()).toPositionedRect(srcSize);
   }
 
-  /// Renders this sprite onto the canvas.
+  /// Same as [render], but takes both the position and the size as a single
+  /// [Rect].
   ///
-  /// * position: x,y coordinates where it will be drawn; default to origin.
-  /// * size: width/height dimensions; it can be bigger or smaller than the original size -- but it defaults to the original texture size.
-  /// * overridePaint: paint to use. You can also change the paint on your Sprite instance. Default is white.
-  /// * anchor: where in the sprite the x/y coordinates refer to; defaults to topLeft.
+  /// **Note**: only use this if you are already using [Rect]'s to represent both the position and
+  /// dimension of your [Sprite]. If you are using [Vector2]s, prefer the other method.
+  void renderRect(
+    Canvas canvas,
+    Rect rect, {
+    Paint? overridePaint,
+  }) {
+    render(
+      canvas,
+      position: rect.topLeft.toVector2(),
+      size: rect.size.toVector2(),
+      overridePaint: overridePaint,
+    );
+  }
+
+  /// Renders this sprite onto the [canvas].
+  ///
+  /// * [position]: x,y coordinates where it will be drawn; default to origin.
+  /// * [size]: width/height dimensions; it can be bigger or smaller than the original size -- but it
+  ///   defaults to the original texture size.
+  /// * [anchor]: where in the sprite the x/y coordinates refer to; defaults to topLeft.
+  /// * [overridePaint]: paint to use. You can also change the paint on your Sprite instance. Default
+  ///   is white.
   void render(
     Canvas canvas, {
     Vector2? position,
