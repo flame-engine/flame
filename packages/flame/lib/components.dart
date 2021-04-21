ad36b14fa082e7fe9895dc612915f64e4efaf79e
Erick <erickzanardoo@gmail.com>
2021-04-21 11:30:48 -0300

    Adding SpriteAnimationGroupComponent (#766)
    * Adding SpriteAnimationGroupComponent
    
    * adding tests
    
    * Changing ci to use flutter stable
    
    * Addressing Spydon's comments
    
    * reverting analysis_options.yml
    
    * linting

--- packages/flame/lib/components.dart
+++ packages/flame/lib/components.dart
@@ -15,6 +15,7 @@ export 'src/components/parallax_component.dart';
 export 'src/components/particle_component.dart';
 export 'src/components/position_component.dart';
 export 'src/components/sprite_animation_component.dart';
+export 'src/components/sprite_animation_group_component.dart';
 export 'src/components/sprite_batch_component.dart';
 export 'src/components/sprite_component.dart';
 export 'src/components/text_box_component.dart';
