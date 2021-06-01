69eb646489d9e533cc91c41cd5d66e42f19ffe39
Luan Nico <luanpotter27@gmail.com>
2021-06-01 12:45:37 -0400

    Update base_component.dart

--- packages/flame/lib/src/components/base_component.dart
+++ packages/flame/lib/src/components/base_component.dart
@@ -154,8 +154,11 @@ abstract class BaseComponent extends Component {
     if (this is HasGameRef) {
       final c = this as HasGameRef;
       gameRef ??= c.hasGameRef ? c.gameRef : null;
-    } else if (gameRef == null && isMounted) {
-      throw 'Parent was already added to Game and has no HasGameRef; in this case, gameRef is mandatory.';
+    } else if (gameRef == null) {
+      assert(
+        !isMounted,
+        'Parent was already added to Game and has no HasGameRef; in this case, gameRef is mandatory.',
+      );
     }
     if (gameRef is BaseGame) {
       gameRef.prepare(child);
