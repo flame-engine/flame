72303ab04b3c5f8a7f5beeca324b0f58f0e74fa3
Jochum van der Ploeg <jochum@vdploeg.net>
2021-07-07 00:51:12 +0200

    Update example

--- packages/flame_oxygen/example/lib/main.dart
+++ packages/flame_oxygen/example/lib/main.dart
@@ -1,18 +1,90 @@
 import 'package:flame/game.dart';
 import 'package:flame_oxygen/flame_oxygen.dart';
 import 'package:flutter/material.dart';
+import 'package:oxygen/oxygen.dart';
 
 void main() {
   runApp(GameWidget(game: ExampleGame()));
 }
 
-class ExampleGame extends OxygenGame {
+class SpriteSystem extends BaseSystem {
+  @override
+  List<Filter<Component>> get filters => [Has<SpriteComponent>()];
+
+  @override
+  void renderEntity(Canvas canvas, Entity entity) {
+    final size = entity.get<SizeComponent>()!.size;
+    final sprite = entity.get<SpriteComponent>()?.sprite;
+
+    sprite?.render(canvas, size: size);
+  }
+}
+
+class DebugSystem extends BaseSystem {
+  final debugPaint = Paint()
+    ..color = Colors.green
+    ..style = PaintingStyle.stroke;
+
+  final textPainter = TextPaint(
+    config: const TextPaintConfig(
+      color: Colors.green,
+      fontSize: 10,
+    ),
+  );
+
+  @override
+  List<Filter<Component>> get filters => [];
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    textPainter.copyWith((config) => config.withFontSize(16)).render(
+          canvas,
+          [
+            'FPS: ${(world!.game as FPSCounter).fps()}',
+            'Entities: ${world!.entities.length}',
+          ].join('\n'),
+          Vector2.zero(),
+        );
+  }
+
+  @override
+  void renderEntity(Canvas canvas, Entity entity) {
+    final size = entity.get<SizeComponent>()!.size;
+
+    canvas.drawRect(Vector2.zero() & size, debugPaint);
+
+    textPainter.render(
+      canvas,
+      [
+        'position: ${entity.get<PositionComponent>()!.position}',
+        'size: $size',
+        'angle: ${entity.get<AngleComponent>()?.radians ?? 0}',
+        'anchor: ${entity.get<AnchorComponent>()?.anchor ?? Anchor.topLeft}'
+      ].join('\n'),
+      Vector2(size.x + 2, 0),
+    );
+    textPainter.render(
+      canvas,
+      entity.name ?? '',
+      Vector2(size.x / 2, size.y + 2),
+      anchor: Anchor.topCenter,
+    );
+  }
+}
+
+class ExampleGame extends OxygenGame with FPSCounter {
   @override
   Future<void> init() async {
-    final entity = createEntity(
+    world.registerSystem(DebugSystem());
+    world.registerSystem(SpriteSystem());
+    world.registerSystem(DebugSystem());
+
+    createEntity(
       name: 'Entity 1',
-      position: Vector2.zero(),
+      position: size / 2,
       size: Vector2.all(64),
+      angle: 0,
     )..add<SpriteComponent, SpriteInit>(
         SpriteInit(await loadSprite('pizza.png')),
       );
