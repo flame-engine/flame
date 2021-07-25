94531a8019f50ba7840274f6acc7d139b6ca9c20
Erick Zanardo <erickzanardoo@gmail.com>
2021-07-25 14:46:16 -0300

    some adjustments

--- packages/flame/test/components/mixins/has_paint_test.dart
+++ packages/flame/test/components/mixins/has_paint_test.dart
@@ -13,163 +13,207 @@ void main() {
       expect(comp.paint, comp.getPaint('main'));
     });
 
-    test('paint setter sets the main paint', () {
-      final comp = MyComponent();
+    test(
+      'paint setter sets the main paint',
+      () {
+        final comp = MyComponent();
 
-      const color = Color(0xFFE5E5E5);
-      comp.paint = Paint()..color = color;
+        const color = Color(0xFFE5E5E5);
+        comp.paint = Paint()..color = color;
 
-      expect(comp.getPaint('main').color, color);
-    });
+        expect(comp.getPaint('main').color, color);
+      },
+    );
 
     test(
-        'getPaint throws exception when retrieving a paint that does not exists',
-        () {
-      final comp = MyComponent();
+      'getPaint throws exception when retrieving a paint that does not exists',
+      () {
+        final comp = MyComponent();
 
-      expect(
-        () => comp.getPaint('bla'),
-        throwsArgumentError,
-      );
-    });
-
-    test('setPaint sets a paint', () {
-      final comp = MyComponent();
-
-      const color = Color(0xFFA9A9A9);
-      comp.setPaint('background', Paint()..color = color);
-
-      expect(comp.getPaint('background').color, color);
-    });
-
-    test('deletePaint removes a paint from the map', () {
-      final comp = MyComponent();
-
-      comp.setPaint('background', Paint());
-      comp.deletePaint('background');
-
-      expect(
-        () => comp.getPaint('background'),
-        throwsArgumentError,
-      );
-    });
-
-    test('deletePaint cannot remove the main paint', () {
-      final comp = MyComponent();
-
-      expect(
-        () => comp.deletePaint('main'),
-        throwsArgumentError,
-      );
-    });
+        expect(
+          () => comp.getPaint('bla'),
+          throwsArgumentError,
+        );
+      },
+    );
 
     test(
-        'makeTransparent sets opacity to 0 on the main when paintId is omitted',
-        () {
-      final comp = MyComponent();
-      comp.makeTransparent();
+      'setPaint sets a paint',
+      () {
+        final comp = MyComponent();
 
-      expect(comp.paint.color.opacity, 0);
-    });
+        const color = Color(0xFFA9A9A9);
+        comp.setPaint('background', Paint()..color = color);
 
-    test('makeTransparent sets opacity to 0 on informed paintId', () {
-      final comp = MyComponent();
-      comp.setPaint('background', Paint());
-      comp.makeTransparent(paintId: 'background');
-
-      expect(comp.getPaint('background').color.opacity, 0);
-    });
-
-    test('makeOpaque sets opacity to 1 on the main when paintId is omitted',
-        () {
-      final comp = MyComponent();
-      comp.makeTransparent();
-      comp.makeOpaque();
-
-      expect(comp.paint.color.opacity, 1);
-    });
-
-    test('makeOpaque sets opacity to 1 on informed paintId', () {
-      final comp = MyComponent();
-      comp.setPaint('background', Paint()..color = const Color(0x00E5E5E5));
-      comp.makeOpaque(paintId: 'background');
-
-      expect(comp.getPaint('background').color.opacity, 1);
-    });
-
-    test('setOpacity sets opacity of the main when paintId is omitted', () {
-      final comp = MyComponent();
-      comp.setOpacity(0.2);
-
-      expect(comp.paint.color.opacity, 0.2);
-    });
-
-    test('setOpacity sets opacity of the informed paintId', () {
-      final comp = MyComponent();
-      comp.setPaint('background', Paint());
-      comp.setOpacity(0.2, paintId: 'background');
-
-      expect(comp.getPaint('background').color.opacity, 0.2);
-    });
-
-    test('throws error if opacity is less than 0', () {
-      final comp = MyComponent();
-
-      expect(
-        () => comp.setOpacity(-0.5),
-        throwsArgumentError,
-      );
-    });
-
-    test('throws error if opacity is greater than 1', () {
-      final comp = MyComponent();
-
-      expect(
-        () => comp.setOpacity(1.1),
-        throwsArgumentError,
-      );
-    });
-
-    test('setColor sets color of the main when paintId is omitted', () {
-      final comp = MyComponent();
-      const color = Color(0xFFE5E5E5);
-      comp.setColor(color);
-
-      expect(comp.paint.color, color);
-    });
-
-    test('setOpacity sets opacity of the informed paintId', () {
-      final comp = MyComponent();
-      const color = Color(0xFFE5E5E5);
-      comp.setPaint('background', Paint());
-      comp.setColor(color, paintId: 'background');
-
-      expect(comp.getPaint('background').color, color);
-    });
+        expect(comp.getPaint('background').color, color);
+      },
+    );
 
     test(
-        'tint sets the correct color filter of the main when paintId is omitted',
-        () {
-      final comp = MyComponent();
-      const color = Color(0xFFE5E5E5);
-      comp.tint(color);
+      'deletePaint removes a paint from the map',
+      () {
+        final comp = MyComponent();
 
-      expect(
-        comp.paint.colorFilter,
-        const ColorFilter.mode(color, BlendMode.multiply),
-      );
-    });
+        comp.setPaint('background', Paint());
+        comp.deletePaint('background');
 
-    test('setOpacity sets opacity of the informed paintId', () {
-      final comp = MyComponent();
-      const color = Color(0xFFE5E5E5);
-      comp.setPaint('background', Paint());
-      comp.tint(color, paintId: 'background');
+        expect(
+          () => comp.getPaint('background'),
+          throwsArgumentError,
+        );
+      },
+    );
 
-      expect(
-        comp.getPaint('background').colorFilter,
-        const ColorFilter.mode(color, BlendMode.multiply),
-      );
-    });
+    test(
+      'deletePaint cannot remove the main paint',
+      () {
+        final comp = MyComponent();
+
+        expect(
+          () => comp.deletePaint('main'),
+          throwsArgumentError,
+        );
+      },
+    );
+
+    test(
+      'makeTransparent sets opacity to 0 on the main when paintId is omitted',
+      () {
+        final comp = MyComponent();
+        comp.makeTransparent();
+
+        expect(comp.paint.color.opacity, 0);
+      },
+    );
+
+    test(
+      'makeTransparent sets opacity to 0 on informed paintId',
+      () {
+        final comp = MyComponent();
+        comp.setPaint('background', Paint());
+        comp.makeTransparent(paintId: 'background');
+
+        expect(comp.getPaint('background').color.opacity, 0);
+      },
+    );
+
+    test(
+      'makeOpaque sets opacity to 1 on the main when paintId is omitted',
+      () {
+        final comp = MyComponent();
+        comp.makeTransparent();
+        comp.makeOpaque();
+
+        expect(comp.paint.color.opacity, 1);
+      },
+    );
+
+    test(
+      'makeOpaque sets opacity to 1 on informed paintId',
+      () {
+        final comp = MyComponent();
+        comp.setPaint('background', Paint()..color = const Color(0x00E5E5E5));
+        comp.makeOpaque(paintId: 'background');
+
+        expect(comp.getPaint('background').color.opacity, 1);
+      },
+    );
+
+    test(
+      'setOpacity sets opacity of the main when paintId is omitted',
+      () {
+        final comp = MyComponent();
+        comp.setOpacity(0.2);
+
+        expect(comp.paint.color.opacity, 0.2);
+      },
+    );
+
+    test(
+      'setOpacity sets opacity of the informed paintId',
+      () {
+        final comp = MyComponent();
+        comp.setPaint('background', Paint());
+        comp.setOpacity(0.2, paintId: 'background');
+
+        expect(comp.getPaint('background').color.opacity, 0.2);
+      },
+    );
+
+    test(
+      'throws error if opacity is less than 0',
+      () {
+        final comp = MyComponent();
+
+        expect(
+          () => comp.setOpacity(-0.5),
+          throwsArgumentError,
+        );
+      },
+    );
+
+    test(
+      'throws error if opacity is greater than 1',
+      () {
+        final comp = MyComponent();
+
+        expect(
+          () => comp.setOpacity(1.1),
+          throwsArgumentError,
+        );
+      },
+    );
+
+    test(
+      'setColor sets color of the main when paintId is omitted',
+      () {
+        final comp = MyComponent();
+        const color = Color(0xFFE5E5E5);
+        comp.setColor(color);
+
+        expect(comp.paint.color, color);
+      },
+    );
+
+    test(
+      'setOpacity sets opacity of the informed paintId',
+      () {
+        final comp = MyComponent();
+        const color = Color(0xFFE5E5E5);
+        comp.setPaint('background', Paint());
+        comp.setColor(color, paintId: 'background');
+
+        expect(comp.getPaint('background').color, color);
+      },
+    );
+
+    test(
+      'tint sets the correct color filter of the main when paintId is omitted',
+      () {
+        final comp = MyComponent();
+        const color = Color(0xFFE5E5E5);
+        comp.tint(color);
+
+        expect(
+          comp.paint.colorFilter,
+          const ColorFilter.mode(color, BlendMode.multiply),
+        );
+      },
+    );
+
+    test(
+      'setOpacity sets opacity of the informed paintId',
+      () {
+        final comp = MyComponent();
+        const color = Color(0xFFE5E5E5);
+        comp.setPaint('background', Paint());
+        comp.tint(color, paintId: 'background');
+
+        expect(
+          comp.getPaint('background').colorFilter,
+          const ColorFilter.mode(color, BlendMode.multiply),
+        );
+      },
+    );
   });
 }
--- packages/flame/test/effects/paint_effect_test.dart
+++ packages/flame/test/effects/paint_effect_test.dart
@@ -12,50 +12,56 @@ class MyGame extends BaseGame {}
 void main() {
   group('Paint Effects', () {
     group('OpacityEffect', () {
-      test('Sets the correct opacity on the paint', () {
-        final comp = MyComponent();
-        final game = MyGame();
+      test(
+        'Sets the correct opacity on the paint',
+        () {
+          final comp = MyComponent();
+          final game = MyGame();
 
-        game.onResize(Vector2.all(100));
-        game.add(comp);
-        game.update(0); // Making sure the component was added
+          game.onResize(Vector2.all(100));
+          game.add(comp);
+          game.update(0); // Making sure the component was added
 
-        comp.addEffect(
-          OpacityEffect(
-            opacity: 0,
-            duration: 1,
-          ),
-        );
+          comp.addEffect(
+            OpacityEffect(
+              opacity: 0,
+              duration: 1,
+            ),
+          );
 
-        game.update(0.2);
+          game.update(0.2);
 
-        expect(comp.paint.color.opacity, 0.8);
-      });
+          expect(comp.paint.color.opacity, 0.8);
+        },
+      );
     });
 
     group('ColorEffect', () {
-      test('Sets the correct color filter on the paint', () {
-        final comp = MyComponent();
-        final game = MyGame();
+      test(
+        'Sets the correct color filter on the paint',
+        () {
+          final comp = MyComponent();
+          final game = MyGame();
 
-        game.onResize(Vector2.all(100));
-        game.add(comp);
-        game.update(0); // Making sure the component was added
+          game.onResize(Vector2.all(100));
+          game.add(comp);
+          game.update(0); // Making sure the component was added
 
-        comp.addEffect(
-          ColorEffect(
+          comp.addEffect(
+            ColorEffect(
               color: const Color(0xFF000000),
               duration: 1,
-          ),
-        );
+            ),
+          );
 
-        game.update(0.5);
+          game.update(0.5);
 
-        expect(
+          expect(
             comp.paint.colorFilter,
             const ColorFilter.mode(Color(0xFF7F7F7F), BlendMode.multiply),
-        );
-      });
+          );
+        },
+      );
     });
   });
 }
