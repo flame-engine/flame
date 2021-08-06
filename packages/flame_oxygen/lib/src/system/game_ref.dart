f9ad75f5a9203e493a89944a57df1dabc2bb0811
Jochum van der Ploeg <jochum@vdploeg.net>
2021-08-06 11:33:33 +0200

    Fixed GameRef

--- packages/flame_oxygen/lib/src/system/game_ref.dart
+++ packages/flame_oxygen/lib/src/system/game_ref.dart
@@ -1,6 +1,10 @@
 part of flame_oxygen.system;
 
-mixin GameRef<T extends Game> on System {
-  /// The [Game] this system belongs to.
-  T? get game => world?.game;
+mixin GameRef<T extends OxygenGame> on System {
+  /// The world this system belongs to.
+  @override
+  FlameWorld? get world => super.world as FlameWorld?;
+
+  /// The [T] this system belongs to.
+  T? get game => world?.game as T?;
 }
