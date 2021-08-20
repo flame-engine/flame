755c6243e615bf8269be5e46731a770f1e09a661
Pasha Stetsenko <pasha@h2o.ai>
2021-08-19 23:06:34 -0700

    continue work on MockCanvas'

--- packages/flame/lib/src/test_helpers/mock_canvas.dart
+++ packages/flame/lib/src/test_helpers/mock_canvas.dart
@@ -1,9 +1,9 @@
 import 'dart:typed_data';
 import 'dart:ui';
 
-import 'package:flame/src/game/transform2d.dart';
 import 'package:test/fake.dart';
 import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 class MockCanvas extends Fake implements Canvas {
   int saveCount = 1;
@@ -82,37 +82,133 @@ class MockCanvas extends Fake implements Canvas {
 }
 
 
-class MokkCanvas extends Fake implements Canvas {
+class MokkCanvas extends Fake implements Canvas, Matcher {
   MokkCanvas()
     : _commands = [];
 
   final List<_CanvasCommand> _commands;
 
   @override
-  void translate(double dx, double dy) {
-    _currentTransform.translate(dx, dy);
+  bool matches(covariant MokkCanvas item, Map matchState) {
+    if (_commands.length != item._commands.length) {
+      return _fail('Canvas contains ${_commands.length} commands, but '
+                   '${item._commands.length} expected', matchState);
+    }
+    for (var i = 0; i < _commands.length; i++) {
+      final cmd1 = _commands[i];
+      final cmd2 = item._commands[i];
+      if (cmd1.runtimeType != cmd2.runtimeType) {
+        return _fail('Mismatched canvas commands at index $i: the actual '
+                     'command is ${cmd1.runtimeType}, while the expected '
+                     'was ${cmd2.runtimeType}', matchState);
+      }
+      if (!cmd1.matches(cmd2, matchState)) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  _Transform2DCanvasCommand get _currentTransform {
-    if (_commands.isNotEmpty && _commands.last is _Transform2DCanvasCommand) {
-      return _commands.last as _Transform2DCanvasCommand;
+  //#region Canvas API
+
+  @override
+  void transform(Float64List matrix4) {
+    _lastTransform.transform(matrix4);
+  }
+
+  @override
+  void translate(double dx, double dy) {
+    _lastTransform.translate(dx, dy);
+  }
+
+  @override
+  void rotate(double angle) {
+    _lastTransform.rotate(angle);
+  }
+
+  @override
+  void scale(double sx, [double? sy]) {
+    sy ??= sx;
+    _lastTransform.scale(sx, sy);
+  }
+
+  @override
+  void drawRect(Rect rect, [Paint? paint]) {
+    _commands.add(_RectCommand(rect, paint));
+  }
+
+  @override
+  void drawRRect(RRect rrect, [Paint? paint]) {
+    _commands.add(_RRectCommand(rrect, paint));
+  }
+
+  @override
+  void drawLine(Offset p1, Offset p2, [Paint? paint]) {
+    _commands.add(_LineCommand(p1, p2, paint));
+  }
+
+  //#endregion
+
+  //#region Private helpers
+
+  _TransformCanvasCommand get _lastTransform {
+    if (_commands.isNotEmpty && _commands.last is _TransformCanvasCommand) {
+      return _commands.last as _TransformCanvasCommand;
     }
-    final transform2d = _Transform2DCanvasCommand();
+    final transform2d = _TransformCanvasCommand();
     _commands.add(transform2d);
     return transform2d;
   }
+
+  bool _fail(String reason, Map state) {
+    state['description'] = reason;
+    return false;
+  }
+
+  //#endregion
 }
 
 abstract class _CanvasCommand {
-
+  bool matches(covariant _CanvasCommand cmd, Map matchState);
 }
 
-class _Transform2DCanvasCommand extends _CanvasCommand {
-  _Transform2DCanvasCommand()
-    : _transform = Transform2D();
+class _TransformCanvasCommand extends _CanvasCommand {
+  _TransformCanvasCommand()
+    : _transform = Matrix4.identity();
 
-  final Transform2D _transform;
+  final Matrix4 _transform;
 
-  void translate(double dx, double dy) => _transform.position += Vector2(dx, dy);
-  void rotate(double angle) => _transform.angle += angle;
+  void transform(Float64List matrix) => _transform.multiply(Matrix4.fromFloat64List(matrix));
+  void translate(double dx, double dy) => _transform.translate(dx, dy);
+  void rotate(double angle) => _transform.rotateZ(angle);
+  void scale(double sx, double sy) => _transform.scale(sx, sy, 1);
+
+  @override
+  bool matches(_TransformCanvasCommand cmd, Map state) {
+    final storage1 = _transform.storage;
+    final storage2 = cmd._transform.storage;
+    for (var i = 0; i < 16; i++) {
+
+    }
+    return true;
+  }
+}
+
+class _LineCommand extends _CanvasCommand {
+  _LineCommand(this.p1, this.p2, this.paint);
+  final Offset p1;
+  final Offset p2;
+  final Paint? paint;
+}
+
+class _RectCommand extends _CanvasCommand {
+  _RectCommand(this.rect, this.paint);
+  final Rect rect;
+  final Paint? paint;
+}
+
+class _RRectCommand extends _CanvasCommand {
+  _RRectCommand(this.rrect, this.paint);
+  final RRect rrect;
+  final Paint? paint;
 }
