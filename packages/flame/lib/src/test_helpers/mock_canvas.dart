ff5ae7c3b1a49d5bd5af1e9e78b7005630df5bf4
Lukas Klingsbo (spydon) <me@lukas.fyi>
2021-09-08 19:48:08 +0200

    Fix analyze errors

--- packages/flame/lib/src/test_helpers/mock_canvas.dart
+++ packages/flame/lib/src/test_helpers/mock_canvas.dart
@@ -4,6 +4,8 @@ import 'dart:ui';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test/fake.dart';
+;
+;
 
 import 'assertion_mode.dart';
 import 'canvas_commands/cliprect_command.dart';
