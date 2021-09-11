ff27db538da31fde95e29759685fb20d5b565f0f
Luan Nico <luanpotter27@gmail.com>
2021-09-11 17:55:20 -0400

    Fix new lint

--- packages/flame_svg/lib/svg.dart
+++ packages/flame_svg/lib/svg.dart
@@ -1,11 +1,11 @@
 import 'dart:ui';
-import 'package:flame/components.dart';
-import 'package:flame/game.dart';
+
 import 'package:flame/assets.dart';
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-import 'package:flame/flame.dart';
 
 class Svg {
   DrawableRoot svgRoot;
