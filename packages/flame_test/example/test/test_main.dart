a96ead1ee1f722b1be6b4f526ef11aec670a4e15
Lukas Klingsbo (spydon) <me@lukas.fyi>
2021-09-16 15:56:51 +0200

    Fix analyze warnings

--- packages/flame_test/example/test/test_main.dart
+++ packages/flame_test/example/test/test_main.dart
@@ -6,17 +6,17 @@ import 'package:test/test.dart';
 void main() {
   group('flame_test', () {
     test('can test vector2', () {
-      final MyVectorChanger changer = MyVectorChanger();
-      final Vector2 vector = Vector2.all(1.0);
-      final Vector2 changedVector = changer.addOne(vector);
+      final changer = MyVectorChanger();
+      final vector = Vector2.all(1.0);
+      final changedVector = changer.addOne(vector);
       expectVector2(vector + Vector2.all(1.0), changedVector);
       expectVector2(vector + Vector2.all(1.1), changedVector, epsilon: 0.2);
     });
 
     test('can test double', () {
-      final MyDoubleChanger changer = MyDoubleChanger();
-      final double one = 1.0;
-      final double two = changer.addOne(one);
+      final changer = MyDoubleChanger();
+      const one = 1.0;
+      final two = changer.addOne(one);
       expectDouble(one + 1.0, two);
       expectDouble(one + 1.1, two, epsilon: 0.11);
     });
