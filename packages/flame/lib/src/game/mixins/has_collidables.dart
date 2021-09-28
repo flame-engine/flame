3cbdd682c5e38b7bd5b6a2ef34da1dc64db43dc6
Luan Nico <luanpotter27@gmail.com>
2021-09-28 12:28:11 -0400

    .

---
+++ packages/flame/lib/src/game/mixins/has_collidables.dart
@@ -0,0 +1,28 @@
+import '../../../components.dart';
+import '../../../game.dart';
+import '../../components/mixins/collidable.dart';
+import '../../geometry/collision_detection.dart';
+
+/// Keeps track of all the [Collidable]s in the component tree and initiates
+/// collision detection every tick.
+mixin HasCollidables on FlameGame {
+  final List<Collidable> collidables = [];
+
+  @override
+  void prepareComponent(Component component) {
+    super.prepareComponent(component);
+    if (component is Collidable) {
+      collidables.add(component);
+    }
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    handleCollidables();
+  }
+
+  void handleCollidables() {
+    collisionDetection(collidables);
+  }
+}
