8c9ec98a2143796ac0297e67a6c87346acd5029d
Pasha Stetsenko <stpasha@google.com>
2021-09-28 22:36:08 -0700

    Move config files into their own directory

--- doc/_conf/conf.py
+++ doc/_conf/conf.py
@@ -3,6 +3,8 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os, sys
+sys.path.append(os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -11,7 +13,8 @@ project = 'Flame'
 copyright = '2021, Blue Fire Team'
 author = 'Blue Fire Team'
 
-root_doc = "README"
+root_doc = "README"   #os.path.abspath("../README")
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -37,7 +40,7 @@ myst_enable_extensions = [
 myst_heading_anchors = 2
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['../_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -56,7 +59,7 @@ html_theme_options = {
     "repository_url": "https://github.com/flame-engine/flame",
     "use_edit_page_button": True,
     "repository_branch": "main",
-    "path_to_docs": "docs",
+    "path_to_docs": "doc",
 }
 
 # Style for syntax highlighting
@@ -65,7 +68,7 @@ pygments_style = 'monokai'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../_static']
 
 
 def setup(app):
