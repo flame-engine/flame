551af19f078a310b3b3bc23daa7c6ccd548864b9
Erick Zanardo <erickzanardoo@gmail.com>
2021-10-09 14:41:23 -0300

    fixing and improving the text a little bit

--- tutorials/space_shooter/lib/steps/1_getting_started/1_flame_game/tutorial.dart
+++ tutorials/space_shooter/lib/steps/1_getting_started/1_flame_game/tutorial.dart
@@ -2,13 +2,13 @@ const tutorial = ['''
 # Getting Started
 
 This tutorial will guide you on the development of a full Flame game, starting from the ground,
-step by step. By the end of it, you will have build a classic Space Shooter game, featuring
-animations, gesture, mouse and keyboard controls, collision detections and so on.
+step by step. By the end of it, you will have built a classic Space Shooter game, featuring
+animations, input using gestures, mouse and keyboard controls, collision detections and so on.
 
-This first part will introduce you to: 
+This first part will introduce you to:
  - `FlameGame`: The base class for games using the Flame Component system.
- - `GameWidget`: The Widget that will insert your game into the Flutter widget tree
- - `PositionComponent`: One of the most basic Flame components which is a component that has both
+ - `GameWidget`: The Widget that will insert your game into the Flutter widget tree.
+ - `PositionComponent`: One of the most basic Flame components which is holds both
 a position and dimensions in the game space.
 
 Lets start by creating our game class and the `GameWidget` that will run it.
@@ -26,13 +26,13 @@ void main() {
 }
 ```
 ''','''
-That is it! If you run this, you will only see an empty black screen for now, but now we have the
-bare-bones needed to start implementing our game.
+That is it! If you run this, you will only see an empty black screen for now, from that, we can
+start implementing our game.
 
-Next, we can add our player component, to do so, we will create a new class based on Flame's
-`PositionComponent`, this component is the base for all components that has a postion and dimension
-on the game. For now, our component will render a white square, such component could be implemented
-as the follow:
+Next, lets create our player component, to do so, we will create a new class based on Flame's
+`PositionComponent`, this component is the base for all components that has a position and a dimension
+on the game screen. For now, our component will only render a white square, such component could be
+implemented as the following:
 ''','''```
 import 'package:flame/components.dart';
 
@@ -44,9 +44,9 @@ class Player extends PositionComponent {
 }
 ```
 ''','''
-Now we need to add our new component to our game. Any component addition on game start up should be
-done on the `onLoad` method, so lets override `FlameGame.onLoad` and add our logic, the modified
-source will like the follow:
+Now lets add our new component to our game. Any component addition on game startup should be done
+on the `onLoad` method, so lets override `FlameGame.onLoad` and add our logic there, the modified
+code will like the following:
 ''','''```
 class SpaceShooterGame extends FlameGame {
   @override
@@ -64,16 +64,17 @@ class SpaceShooterGame extends FlameGame {
   }
 }
 ```''','''
-If you run this, you will now see a white retangle being rendered in the center of the screen, a
-couple of points worth commenting at this point:
+If you run this, you will now see a white rectangle being rendered in the center of the screen.
+
+A couple of points worth commenting at this point:
 
  - Always call `await super.onLoad()` as the first instruction of your custom `onLoad` method.
- - `size` is a `Vector2` variable from game and it holds the current dimension of the game area,
-where `x` is the horizontal dimension, or the width, and `y` the vertical, or the height.
- - By the default, Flame follows Flutter canvas anchor, which means that (0, 0) is anchored on the
-top left of the canvas, so the game and all components uses that same anchor by default, we can
-change this by changing our component `anchor` attribute to center, which will make our life way
-easier to put the component on the center of the screen.
+ - `size` is a `Vector2` variable from the game class and it holds the current dimension of the game
+area, where `x` is the horizontal dimension, or the width, and `y` the vertical, or the height.
+ - By default, Flame follows Flutter's canvas anchoring, which means that (0, 0) is anchored on the
+top left corner of the canvas, so the game and all components uses that same anchor by default, we
+can change this by changing our component's `anchor` attribute to `Anchor.center`, which will make
+our life way easier to center the component on the screen.
 
 And that is it for this first part, on this first step we learned the basics on how to create a
 game class, insert it into the Flutter widget tree, and render a simple component.
--- tutorials/space_shooter/lib/steps/1_getting_started/2_input_and_graphics/tutorial.dart
+++ tutorials/space_shooter/lib/steps/1_getting_started/2_input_and_graphics/tutorial.dart
@@ -1,15 +1,15 @@
 const tutorial = ['''
 # Controlling the player and adding some graphics
 ''','''
-Now that the bases for our game and a component for our player, lets add some interactive to it,
-lets begin that by allowing the player to be controlled by the mouse/gestures.
+Now that the base for our game and we have a component for our player, lets add some interactive to
+it, lets begin that by allowing the player to be controlled by the mouse/touch gestures.
 
-There are a couple of ways of doing that on Flame, for this step we will do that by using one of
+There are a couple of ways of doing that on Flame, for this tutorial we will do that by using one of
 Flame's gestures detectors: `PanDetector`.
 
-This detector will make our game class receive pan (or drag) events, to use it, it as simple as
-adding the `PanDetector` mixin to our game class and override its listeners methods, for our game
-we will use the `onPadUpdate` method, our game class will look like the following:
+This detector will make our game class receive pan (or drag) events, to do so, we just need to
+add the `PanDetector` mixin to our game class and override its listeners methods, for our game
+we will use the `onPadUpdate` method, our updated code will look like the following:
 ''','''```
 import 'package:flame/input.dart';
 
@@ -29,9 +29,8 @@ class SpaceShooterGame extends FlameGame with PanDetector {
 At this point our game should be receiving all the pan updates inputs, but we are not doing
 anything with those events.
 
-We need now a way to move our player, we can simply do that by saving our `Player` component to a
-variable inside our game class, and a method `move` to our `Player` and just connect them, we can
-implement that with something like this:
+We need now a way to move our player, that can be achieved by simply saving our `Player` component to a
+variable inside our game class, add a method `move` to our `Player` and just connect them:
 ''','''```
 class Player extends PositionComponent {
   @override
@@ -67,19 +66,20 @@ class SpaceShooterGame extends FlameGame with PanDetector {
   }
 }
 ```''','''
-That is it! is you drag the screen, the player should follow your movement and we have just
-implemented our first interactive game!
+That is it! If you drag the screen, the player should follow your movement and we have just
+implemented our very first interactive game!
 
-Before we move to our next step, lets replace that boring white rectangle to a some cool graphics.
+Before we move to our next step, lets replace that boring white rectangle for a some cool graphics.
 
 Flame provides many classes to help us with graphical rendering, for this step we are going to use
 the `Sprite` class.
 
-`Sprite` are used in Flame to render static images, or portions of it on the game. To render a
-`Sprite` inside a `FlameGame`, we can use the `SpriteComponent` class.
+`Sprite`s are used in Flame to render static images, or portions of it in the game. To render a
+`Sprite` inside a `FlameGame`, we should use the `SpriteComponent` class, which wraps the `Sprite`
+features into a component.
 
-So lets refactor our current implementation, lets replace our inheritance from `PositionComponent`
-to `SpriteComponent` and load a sprite. Our code will look like the follow now:
+So lets refactor our current implementation, first we can replace our inheritance from
+`PositionComponent` to `SpriteComponent` and load the sprite:
 ''','''```
 class Player extends SpriteComponent {
   void move(Vector2 delta) {
@@ -115,7 +115,7 @@ class SpaceShooterGame extends FlameGame with PanDetector {
 And now, you should see a small blue spaceship on the screen!
 
 A couple of notes worth mentioning:
- - Unlike `PositionComponent`, `SpriteComponent` has an implementation from the `render` method
+ - Unlike `PositionComponent`, `SpriteComponent` has an implementation for the `render` method
 that actually renders something, so we don't need to override it anymore.
  - `FlameGame` has a couple of methods for loading assets, like `loadSprite`, those methods are
 quite handful, because when used, `FlameGame` will take care of cleaning any cache when the game
@@ -123,16 +123,17 @@ is removed from the Flutter widget tree.
 
 Before we close this step, there is one small improvement that we can do. Right now, we are loading
 the sprite and passing it to our component. For now, this may seen fine, but imagine a game with
-dozen of components, if the game is respnsible for loading their assets, our code can get a mess
-quite fast.
+dozen of components, if the game is responsible for loading assets for all coponents, our code can
+get a mess quite fast.
 
-Just like `FlameGame`, components also have a `onLoad` method that can be overridden to initialize
-components. But before we implement our player own load method, note that we use a attribute and
-the `loadSprite` method, which belongs to the `FlameGame`.
+Just like `FlameGame`, components also have an `onLoad` method that can be overridden to do
+initializations. But before we implement our player's own load method, note that we use an attribute
+and the `loadSprite` method from the `FlameGame` class.
 
-That is not a problem, everytime our component needs to access things on its game class, we can mix
-our component with the `HasGameRef` mixin, that will add a new variable to our component called
-`gameRef` which will point to the game where the component is running. Now, lets refactor our game:
+That is not a problem, everytime our component needs to access things from its game class, we can
+mix our component with the `HasGameRef` mixin, that will add a new variable to our component called
+`gameRef` which will point to the game instance where the component is running. Now, lets refactor
+our game a little bit:
 ''','''```
 class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
   @override
@@ -173,6 +174,6 @@ class SpaceShooterGame extends FlameGame with PanDetector {
 }
 ```''','''
 If you run the game now, you will not notice any visual differences, but now we have a more
-scalable structure to grow our game on.
+scalable structure to grow our game on. And that closes this step!
 '''
 ];
