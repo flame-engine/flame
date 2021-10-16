b1ca0b5c6c8fa65722a1f6a57d84b3330080ff4e
Pasha Stetsenko <stpasha@google.com>
2021-10-16 12:47:50 -0700

    Simpler css injection

--- doc/_sphinx/conf.py
+++ doc/_sphinx/conf.py
@@ -68,8 +68,4 @@ pygments_style = 'monokai'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['images', 'styles']
-
-
-def setup(app):
-    """Add functions to the Sphinx setup."""
-    app.add_css_file("custom.css")
+html_css_files = ["custom.css"]
