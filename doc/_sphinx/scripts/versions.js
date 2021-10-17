e85a5e11d794a93f306b387dbfc7cca4a6bc47f7
Pasha Stetsenko <stpasha@google.com>
2021-10-17 00:21:03 -0700

    Auto-detect current docs version

--- doc/_sphinx/scripts/versions.js
+++ doc/_sphinx/scripts/versions.js
@@ -6,11 +6,24 @@ window.versions = [
     "1.0.0-releasecandidate.14",
 ];
 
-$(function() {
+$(function() {  // Will be executed when the DOM finishes loading
+
+// First, detect the doc version of the current page. This can be done by
+// looking at the URL, which is supposed to be of the form
+// `https://docs.flame-engine.org/${version}/...`. Thus, the first part in the
+// path is presumed to be the version.
+var this_version = '--';
+if (location.host == 'docs.flame-engine.org') {
+  var parts = location.pathname.split('/');
+  if (parts.length >= 2 && parts[0] == '') {
+    this_version = parts[1];
+  }
+}
+
 var version_buttons = "";
 for (var version of window.versions) {
   var classes = "btn btn-secondary topbarbtn";
-  if (location.href.includes('/' + version + '/')) {
+  if (version == this_version) {
     classes += " selected";
   }
   version_buttons +=
@@ -20,7 +33,8 @@ for (var version of window.versions) {
 $('div.topbar-main').append(
   '<div class="dropdown-buttons-trigger" id="versions-menu">' +
     '<button class="btn btn-secondary topbarbtn">' +
-      '<span class="tag">version:</span> <span class="version-id">main</span>' +
+      '<span class="tag">version:</span> ' +
+      '<span class="version-id">' + this_version + '</span>' +
     '</button>' +
     '<div class="dropdown-buttons">' +
       version_buttons +
