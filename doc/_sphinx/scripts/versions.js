83b8e58679673082a590e78d1b363f0cdd9d31d3
Pasha Stetsenko <stpasha@google.com>
2021-10-18 23:06:54 -0700

    Use let/const instead of var, and single quotes

--- doc/_sphinx/scripts/versions.js
+++ doc/_sphinx/scripts/versions.js
@@ -1,9 +1,9 @@
 
 // List all published documentation versions here
 window.versions = [
-    "main",
-    "1.0.0-releasecandidate.15",
-    "1.0.0-releasecandidate.14",
+    'main',
+    '1.0.0-releasecandidate.15',
+    '1.0.0-releasecandidate.14',
 ];
 
 $(function() {  // Will be executed when the DOM finishes loading
@@ -12,19 +12,19 @@ $(function() {  // Will be executed when the DOM finishes loading
 // looking at the URL, which is supposed to be of the form
 // `https://docs.flame-engine.org/${version}/...`. Thus, the first part in the
 // path is presumed to be the version.
-var this_version = '--';
+let this_version = '--';
 if (location.host == 'docs.flame-engine.org') {
-  var parts = location.pathname.split('/');
+  const parts = location.pathname.split('/');
   if (parts.length >= 2 && parts[0] == '') {
     this_version = parts[1];
   }
 }
 
-var version_buttons = "";
-for (var version of window.versions) {
-  var classes = "btn btn-secondary topbarbtn";
+let version_buttons = '';
+for (const version of window.versions) {
+  let classes = 'btn btn-secondary topbarbtn';
   if (version == this_version) {
-    classes += " selected";
+    classes += ' selected';
   }
   version_buttons +=
     `<a href="/${version}/index.html"><button class="${classes}">${version}</button></a>`;
