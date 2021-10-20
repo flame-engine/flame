a456781b5096a4c7c400528227978cd27a1df56b
Pasha Stetsenko <stpasha@google.com>
2021-10-19 23:03:15 -0700

    expand rotate effect

--- packages/flame/lib/src/effects_new/rotate_effect.dart
+++ packages/flame/lib/src/effects_new/rotate_effect.dart
@@ -1,22 +1,42 @@
 import 'flame_animation_controller.dart';
 import 'transform2d_effect.dart';
 
-/// Rotate a component by a specified `angle` relative to its orientation
-/// at the onset of the effect.
+/// Rotate a component around its anchor.
+///
+/// Two constructors are provided: [RotateEffect.by] will rotate the target by
+/// the specified `angle` relative to its orientation at the onset of the
+/// effect; at the same time [RotateEffect.to] will rotate the target from its
+/// current orientation at the onset of the effect to the fixed direction
+/// specified by the `angle`.
 ///
 /// This effect applies incremental changes to the component's angle, and
 /// requires that any other effect or update logic applied to the same component
 /// also used incremental updates.
 class RotateEffect extends Transform2DEffect {
-  RotateEffect({
-    required double angle,
+  RotateEffect.by(double angle, {
     required FlameAnimationController controller,
   })  : _angle = angle,
+        _relative = true,
         _lastProgress = 0,
         super(controller: controller);
 
-  final double _angle;
+  RotateEffect.to(double angle, {
+    required FlameAnimationController controller,
+  })  : _angle = angle,
+        _relative = false,
+        _lastProgress = 0,
+        super(controller: controller);
+
+  final bool _relative;
+  double _angle;
   double _lastProgress;
+
+  @override
+  void onStart() {
+    if (!_relative) {
+      _angle -= target.angle;
+    }
+  }
 
   @override
   void apply(double progress) {
