6070f38718f4522f49e065b0745fed6f2d77d5dc
Lukas Klingsbo <me@lukas.fyi>
2021-10-22 12:15:31 +0200

    Move hoverable tests to flameTest

--- packages/flame/test/components/hoverable_test.dart
+++ packages/flame/test/components/hoverable_test.dart
@@ -45,135 +45,143 @@ class NonPropagatingComponent extends HoverableComponent {
 
 void main() {
   group('hoverable test', () {
-    test('make sure they cannot be added to invalid games', () async {
-      final game1 = _GameWithHoverables();
-      game1.onGameResize(Vector2.all(100));
-      // should be ok
-      await game1.add(HoverableComponent());
+    flameTest<_GameWithHoverables>(
+      'make sure they cannot be added to invalid games',
+      createGame: () => _GameWithHoverables(),
+      verify: (game) async {
+        // should be ok
+        await game.add(HoverableComponent());
 
-      final game2 = _GameWithoutHoverables();
-      game2.onGameResize(Vector2.all(100));
+        final game2 = _GameWithoutHoverables();
+        game2.onGameResize(Vector2.all(100));
 
-      const message =
-          'Hoverable Components can only be added to a FlameGame with '
-          'HasHoverableComponents';
+        const message =
+            'Hoverable Components can only be added to a FlameGame with '
+            'HasHoverableComponents';
 
-      expect(
-        () => game2.add(HoverableComponent()),
-        throwsA(
-          predicate(
-            (e) => e is AssertionError && e.message == message,
+        expect(
+          () => game2.add(HoverableComponent()),
+          throwsA(
+            predicate(
+              (e) => e is AssertionError && e.message == message,
+            ),
           ),
-        ),
-      );
-    });
-    test('single component', () async {
-      final game = _GameWithHoverables();
-      game.onGameResize(Vector2.all(100));
-
-      final c = HoverableComponent()
-        ..position = Vector2(10, 20)
-        ..size = Vector2(3, 3);
-      await game.add(c);
-      game.update(0);
-
-      expect(c.isHovered, false);
-      expect(c.enterCount, 0);
-      expect(c.leaveCount, 0);
-
-      _triggerMouseMove(game, 0, 0);
-      expect(c.isHovered, false);
-      expect(c.enterCount, 0);
-      expect(c.leaveCount, 0);
-
-      _triggerMouseMove(game, 11, 0);
-      expect(c.isHovered, false);
-      expect(c.enterCount, 0);
-      expect(c.leaveCount, 0);
-
-      _triggerMouseMove(game, 11, 21); // enter!
-      expect(c.isHovered, true);
-      expect(c.enterCount, 1);
-      expect(c.leaveCount, 0);
-
-      _triggerMouseMove(game, 12, 22); // still inside
-      expect(c.isHovered, true);
-      expect(c.enterCount, 1);
-      expect(c.leaveCount, 0);
-
-      _triggerMouseMove(game, 11, 25); // leave
-      expect(c.isHovered, false);
-      expect(c.enterCount, 1);
-      expect(c.leaveCount, 1);
-
-      _triggerMouseMove(game, 11, 21); // enter again
-      expect(c.isHovered, true);
-      expect(c.enterCount, 2);
-      expect(c.leaveCount, 1);
-    });
-    test('camera is respected', () async {
-      final game = _GameWithHoverables();
-      game.onGameResize(Vector2.all(100));
-
-      final c = HoverableComponent()
-        ..position = Vector2(10, 20)
-        ..size = Vector2(3, 3);
-      await game.add(c);
-      game.update(0);
-
-      // component is now at the corner of the screen
-      game.camera.snapTo(Vector2(10, 20));
-
-      _triggerMouseMove(game, 11, 21);
-      expect(c.isHovered, false);
-      _triggerMouseMove(game, 11, 1);
-      expect(c.isHovered, false);
-      _triggerMouseMove(game, 1, 1);
-      expect(c.isHovered, true);
-      _triggerMouseMove(game, 5, 1);
-      expect(c.isHovered, false);
-    });
-    test('multiple components', () async {
-      final game = _GameWithHoverables();
-      game.onGameResize(Vector2.all(100));
-
-      final a = HoverableComponent()
-        ..position = Vector2(10, 0)
-        ..size = Vector2(2, 20);
-      final b = HoverableComponent()
-        ..position = Vector2(10, 10)
-        ..size = Vector2(2, 2);
-      final c = HoverableComponent()
-        ..position = Vector2(0, 7)
-        ..size = Vector2(20, 2);
-      await game.add(a);
-      await game.add(b);
-      await game.add(c);
-      game.update(0);
-
-      _triggerMouseMove(game, 0, 0);
-      expect(a.isHovered, false);
-      expect(b.isHovered, false);
-      expect(c.isHovered, false);
-
-      _triggerMouseMove(game, 10, 10);
-      expect(a.isHovered, true);
-      expect(b.isHovered, true);
-      expect(c.isHovered, false);
-
-      _triggerMouseMove(game, 11, 8);
-      expect(a.isHovered, true);
-      expect(b.isHovered, false);
-      expect(c.isHovered, true);
-
-      _triggerMouseMove(game, 11, 6);
-      expect(a.isHovered, true);
-      expect(b.isHovered, false);
-      expect(c.isHovered, false);
-    });
+        );
+      },
+    );
 
     flameTest<_GameWithHoverables>(
-      'events does not propagate to parent when child returns false',
+      'single component',
+      createGame: () => _GameWithHoverables(),
+      verify: (game) async {
+        final c = HoverableComponent()
+          ..position = Vector2(10, 20)
+          ..size = Vector2(3, 3);
+        await game.add(c);
+        game.update(0);
+
+        expect(c.isHovered, false);
+        expect(c.enterCount, 0);
+        expect(c.leaveCount, 0);
+
+        _triggerMouseMove(game, 0, 0);
+        expect(c.isHovered, false);
+        expect(c.enterCount, 0);
+        expect(c.leaveCount, 0);
+
+        _triggerMouseMove(game, 11, 0);
+        expect(c.isHovered, false);
+        expect(c.enterCount, 0);
+        expect(c.leaveCount, 0);
+
+        _triggerMouseMove(game, 11, 21); // enter!
+        expect(c.isHovered, true);
+        expect(c.enterCount, 1);
+        expect(c.leaveCount, 0);
+
+        _triggerMouseMove(game, 12, 22); // still inside
+        expect(c.isHovered, true);
+        expect(c.enterCount, 1);
+        expect(c.leaveCount, 0);
+
+        _triggerMouseMove(game, 11, 25); // leave
+        expect(c.isHovered, false);
+        expect(c.enterCount, 1);
+        expect(c.leaveCount, 1);
+
+        _triggerMouseMove(game, 11, 21); // enter again
+        expect(c.isHovered, true);
+        expect(c.enterCount, 2);
+        expect(c.leaveCount, 1);
+      },
+    );
+
+    flameTest<_GameWithHoverables>(
+      'camera is respected',
+      createGame: () => _GameWithHoverables(),
+      verify: (game) async {
+        final c = HoverableComponent()
+          ..position = Vector2(10, 20)
+          ..size = Vector2(3, 3);
+        await game.add(c);
+        game.update(0);
+
+        // component is now at the corner of the screen
+        game.camera.snapTo(Vector2(10, 20));
+
+        _triggerMouseMove(game, 11, 21);
+        expect(c.isHovered, false);
+        _triggerMouseMove(game, 11, 1);
+        expect(c.isHovered, false);
+        _triggerMouseMove(game, 1, 1);
+        expect(c.isHovered, true);
+        _triggerMouseMove(game, 5, 1);
+        expect(c.isHovered, false);
+      },
+    );
+
+    flameTest<_GameWithHoverables>(
+      'multiple components',
+      createGame: () => _GameWithHoverables(),
+      verify: (game) async {
+        final a = HoverableComponent()
+          ..position = Vector2(10, 0)
+          ..size = Vector2(2, 20);
+        final b = HoverableComponent()
+          ..position = Vector2(10, 10)
+          ..size = Vector2(2, 2);
+        final c = HoverableComponent()
+          ..position = Vector2(0, 7)
+          ..size = Vector2(20, 2);
+        await game.add(a);
+        await game.add(b);
+        await game.add(c);
+        game.update(0);
+
+        _triggerMouseMove(game, 0, 0);
+        expect(a.isHovered, false);
+        expect(b.isHovered, false);
+        expect(c.isHovered, false);
+
+        _triggerMouseMove(game, 10, 10);
+        expect(a.isHovered, true);
+        expect(b.isHovered, true);
+        expect(c.isHovered, false);
+
+        _triggerMouseMove(game, 11, 8);
+        expect(a.isHovered, true);
+        expect(b.isHovered, false);
+        expect(c.isHovered, true);
+
+        _triggerMouseMove(game, 11, 6);
+        expect(a.isHovered, true);
+        expect(b.isHovered, false);
+        expect(c.isHovered, false);
+      },
+    );
+
+    flameTest<_GameWithHoverables>(
+      'composed components',
       createGame: () => _GameWithHoverables(),
       verify: (game) async {
         final parent = HoverableComponent()
@@ -197,45 +205,6 @@ void main() {
         expect(parent.leaveCount, 0);
       },
     );
-
-    test('composed components', () async {
-      final game = _GameWithHoverables();
-      game.onGameResize(Vector2.all(100));
-
-      final a = HoverableComponent()
-        ..position = Vector2(10, 0)
-        ..size = Vector2(2, 20);
-      final b = HoverableComponent()
-        ..position = Vector2(10, 10)
-        ..size = Vector2(2, 2);
-      final c = HoverableComponent()
-        ..position = Vector2(0, 7)
-        ..size = Vector2(20, 2);
-      await game.add(a);
-      await game.add(b);
-      await game.add(c);
-      game.update(0);
-
-      _triggerMouseMove(game, 0, 0);
-      expect(a.isHovered, false);
-      expect(b.isHovered, false);
-      expect(c.isHovered, false);
-
-      _triggerMouseMove(game, 10, 10);
-      expect(a.isHovered, true);
-      expect(b.isHovered, true);
-      expect(c.isHovered, false);
-
-      _triggerMouseMove(game, 11, 8);
-      expect(a.isHovered, true);
-      expect(b.isHovered, false);
-      expect(c.isHovered, true);
-
-      _triggerMouseMove(game, 11, 6);
-      expect(a.isHovered, true);
-      expect(b.isHovered, false);
-      expect(c.isHovered, false);
-    });
   });
 }
 
