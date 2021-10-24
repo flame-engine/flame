5bc74e10ed3c8706801d9cddb81a489de309701e
Pasha Stetsenko <stpasha@google.com>
2021-10-24 16:42:00 -0700

    Adding tests for removeOnFinish

--- packages/flame/test/effects2/effect_test.dart
+++ packages/flame/test/effects2/effect_test.dart
@@ -1,6 +1,8 @@
-
 import 'package:flame/effects.dart';
+import 'package:flame/src/components/component.dart';
 import 'package:flame/src/effects2/effect.dart';
+import 'package:flame/src/game/flame_game.dart';
+import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class MyEffect extends Effect {
@@ -59,5 +61,77 @@ void main() {
       effect.update(5);
       expect(effect.x, closeTo(1, 1e-15));
     });
+
+    flameTest<FlameGame>(
+      'removeOnFinish = true',
+      createGame: () => FlameGame(),
+      verify: (game) {
+        final obj = Component();
+        game.add(obj);
+        final effect = MyEffect(StandardAnimationController(duration: 1));
+        obj.add(effect);
+        game.update(0);
+        expect(obj.children.length, 1);
+
+        expect(effect.removeOnFinish, true);
+        expect(effect.isMounted, true);
+        game.update(1);
+
+        expect(effect.controller.completed, true);
+        game.update(0);
+        expect(effect.isMounted, false);
+        expect(obj.children.length, 0);
+      },
+    );
+
+    flameTest<FlameGame>(
+      'removeOnFinish = false',
+      createGame: () => FlameGame(),
+      verify: (game) {
+        final obj = Component();
+        game.add(obj);
+        final effect = MyEffect(StandardAnimationController(duration: 1));
+        effect.removeOnFinish = false;
+        obj.add(effect);
+        game.update(0);
+        expect(obj.children.length, 1);
+
+        expect(effect.removeOnFinish, false);
+        expect(effect.isMounted, true);
+
+        // After the effect completes, it still remains mounted
+        game.update(1);
+        expect(effect.x, 1);
+        expect(effect.controller.completed, true);
+        game.update(0);
+        expect(effect.isMounted, true);
+        expect(obj.children.length, 1);
+
+        // Even as more time is passing, the effect remains mounted and in
+        // the completed state
+        game.update(10);
+        expect(effect.x, 1);
+        expect(effect.isMounted, true);
+        expect(effect.controller.completed, true);
+
+        // However, once the effect is reset, it goes to its initial state
+        effect.reset();
+        expect(effect.x, -1);
+        expect(effect.controller.completed, false);
+        expect(effect.controller.started, false);
+
+        game.update(0.5);
+        expect(effect.x, 0.5);
+        expect(effect.controller.started, true);
+        expect(effect.controller.completed, false);
+
+        // Now the effect completes once again, but still remains mounted
+        game.update(0.5);
+        expect(effect.controller.completed, true);
+        expect(effect.x, 1);
+        game.update(0);
+        expect(effect.isMounted, true);
+      },
+    );
   });
 }
