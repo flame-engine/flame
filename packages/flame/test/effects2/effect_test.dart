20bd648b71870533299827b96bca65dc875109c1
Pasha Stetsenko <stpasha@google.com>
2021-10-31 06:43:32 -0700

    Added Transform2DEffect class (#1052)
    * Added FlameAnimationController class
    
    * Added MainAnimationController class
    
    * Update doc comments
    
    * formatting
    
    * rename MainAnimationController
    
    * Added tests for StandardAnimationController
    
    * Added more tests
    
    * comment
    
    * Added changelog note
    
    * Export StandardAnimationController
    
    * formatting
    
    * Use a default for 'curve' parameter
    
    * rename onsetDelay -> startDelay
    
    * Added Transofm2DEffect
    
    * Added EffectComponent
    
    * Added .transform getter
    
    * formatting
    
    * Rename EffectComponent -> Effect
    
    * Add documentation for the Effect class
    
    * minor
    
    * Added a test for Effect class
    
    * Adding tests for removeOnFinish
    
    * Adding tests for onStart and onFinish
    
    * Also check the effect after reset
    
    * Fix-up merge
    
    * formatting
    
    * added doc-comments
    
    * changelog note
    
    * Added test for transform2DEffect
    
    * Adjusted comments
    
    * Make PositionComponent._transform public
    
    * change changelog

---
+++ packages/flame/test/effects2/effect_test.dart
@@ -0,0 +1,200 @@
+import 'package:flame/src/components/component.dart';
+import 'package:flame/src/effects2/effect.dart';
+import 'package:flame/src/effects2/effect_controller.dart';
+import 'package:flame/src/effects2/standard_effect_controller.dart';
+import 'package:flame/src/game/flame_game.dart';
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+typedef VoidCallback = void Function();
+
+class MyEffect extends Effect {
+  MyEffect(EffectController controller) : super(controller);
+
+  double x = -1;
+  VoidCallback? onStartCallback;
+  VoidCallback? onFinishCallback;
+
+  @override
+  void apply(double progress) {
+    x = progress;
+  }
+
+  @override
+  void reset() {
+    super.reset();
+    x = -1;
+  }
+
+  @override
+  void onStart() {
+    super.onStart();
+    onStartCallback?.call();
+  }
+
+  @override
+  void onFinish() {
+    super.onFinish();
+    onFinishCallback?.call();
+  }
+}
+
+void main() {
+  group('Effect', () {
+    test('Pause & Resume', () {
+      final effect = MyEffect(StandardEffectController(duration: 10));
+      expect(effect.x, -1);
+      expect(effect.isPaused, false);
+
+      effect.update(0);
+      expect(effect.x, 0);
+
+      effect.update(1);
+      expect(effect.x, closeTo(0.1, 1e-15));
+
+      effect.update(2);
+      expect(effect.x, closeTo(0.3, 1e-15));
+
+      effect.pause();
+      effect.update(5);
+      effect.update(2);
+      expect(effect.x, closeTo(0.3, 1e-15));
+
+      effect.resume();
+      effect.update(1);
+      expect(effect.x, closeTo(0.4, 1e-15));
+
+      effect.pause();
+      effect.update(1000);
+      expect(effect.isPaused, true);
+
+      effect.reset();
+      expect(effect.isPaused, false);
+      expect(effect.x, -1);
+
+      effect.update(5);
+      expect(effect.x, closeTo(0.5, 1e-15));
+
+      effect.update(5);
+      expect(effect.x, closeTo(1, 1e-15));
+    });
+
+    flameTest<FlameGame>(
+      'removeOnFinish = true',
+      createGame: () => FlameGame(),
+      verify: (game) {
+        final obj = Component();
+        game.add(obj);
+        final effect = MyEffect(StandardEffectController(duration: 1));
+        obj.add(effect);
+        game.update(0);
+        expect(obj.children.length, 1);
+
+        expect(effect.removeOnFinish, true);
+        expect(effect.isMounted, true);
+        game.update(1);
+
+        expect(effect.controller.completed, true);
+        game.update(0);
+        expect(effect.isMounted, false);
+        expect(obj.children.length, 0);
+      },
+    );
+
+    flameTest<FlameGame>(
+      'removeOnFinish = false',
+      createGame: () => FlameGame(),
+      verify: (game) {
+        final obj = Component();
+        game.add(obj);
+        final effect = MyEffect(StandardEffectController(duration: 1));
+        effect.removeOnFinish = false;
+        obj.add(effect);
+        game.update(0);
+        expect(obj.children.length, 1);
+
+        expect(effect.removeOnFinish, false);
+        expect(effect.isMounted, true);
+
+        // After the effect completes, it still remains mounted
+        game.update(1);
+        expect(effect.x, 1);
+        expect(effect.controller.completed, true);
+        game.update(0);
+        expect(effect.isMounted, true);
+        expect(obj.children.length, 1);
+
+        // Even as more time is passing, the effect remains mounted and in
+        // the completed state
+        game.update(10);
+        expect(effect.x, 1);
+        expect(effect.isMounted, true);
+        expect(effect.controller.completed, true);
+
+        // However, once the effect is reset, it goes to its initial state
+        effect.reset();
+        expect(effect.x, -1);
+        expect(effect.controller.completed, false);
+        expect(effect.controller.started, false);
+
+        game.update(0.5);
+        expect(effect.x, 0.5);
+        expect(effect.controller.started, true);
+        expect(effect.controller.completed, false);
+
+        // Now the effect completes once again, but still remains mounted
+        game.update(0.5);
+        expect(effect.controller.completed, true);
+        expect(effect.x, 1);
+        game.update(0);
+        expect(effect.isMounted, true);
+      },
+    );
+
+    test('onStart & onFinish', () {
+      var nStarted = 0;
+      var nFinished = 0;
+      final effect = MyEffect(StandardEffectController(duration: 1))
+        ..onStartCallback = () {
+          nStarted++;
+        }
+        ..onFinishCallback = () {
+          nFinished++;
+        };
+
+      effect.update(0);
+      expect(effect.controller.started, true);
+      expect(effect.x, 0);
+      expect(nStarted, 1);
+      expect(nFinished, 0);
+
+      effect.update(0.5);
+      expect(effect.controller.started, true);
+      expect(effect.x, 0.5);
+      expect(nStarted, 1);
+      expect(nFinished, 0);
+
+      effect.update(0.5);
+      expect(effect.controller.started, true);
+      expect(effect.controller.completed, true);
+      expect(effect.x, 1);
+      expect(nStarted, 1);
+      expect(nFinished, 1);
+
+      // As more time passes, `onStart` and `onFinish` are no longer called
+      effect.update(0.5);
+      expect(effect.x, 1);
+      expect(nStarted, 1);
+      expect(nFinished, 1);
+
+      // However, if we reset the effect, the callbacks will be invoked again
+      effect.reset();
+      effect.update(0);
+      expect(nStarted, 2);
+      expect(nFinished, 1);
+      effect.update(1);
+      expect(nStarted, 2);
+      expect(nFinished, 2);
+    });
+  });
+}
---
+++ packages/flame/test/effects2/transform2d_effect_test.dart
@@ -0,0 +1,39 @@
+import 'package:flame/src/components/position_component.dart';
+import 'package:flame/src/effects2/effect_controller.dart';
+import 'package:flame/src/effects2/standard_effect_controller.dart';
+import 'package:flame/src/effects2/transform2d_effect.dart';
+import 'package:flame/src/game/flame_game.dart';
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MyEffect extends Transform2DEffect {
+  MyEffect(EffectController controller) : super(controller);
+
+  @override
+  void apply(double progress) {}
+}
+
+void main() {
+  group('Transform2DEffect', () {
+    flameTest<FlameGame>(
+      'onMount',
+      createGame: () => FlameGame(),
+      verify: (game) {
+        final obj = PositionComponent();
+        game.add(obj);
+        game.update(0);
+
+        final effect = MyEffect(StandardEffectController(duration: 1));
+        obj.add(effect);
+        game.update(0);
+        expect(effect.target, obj.transform);
+
+        final effect2 = MyEffect(StandardEffectController(duration: 1));
+        expect(
+          () async => game.add(effect2),
+          throwsA(isA<UnsupportedError>()),
+        );
+      },
+    );
+  });
+}
