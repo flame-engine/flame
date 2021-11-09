30b5b333976e2b096394b0b6d9af42d6a4ff94b9
Pasha Stetsenko <stpasha@google.com>
2021-11-08 18:30:06 -0800

    Added @mustCallSuper for preRender() / postRender()

--- packages/flame_forge2d/lib/body_component.dart
+++ packages/flame_forge2d/lib/body_component.dart
@@ -53,6 +53,7 @@ abstract class BodyComponent<T extends Forge2DGame> extends Component
   final Matrix4 _flipYTransform = Matrix4.identity()..scale(1.0, -1.0);
   double? _lastAngle;
 
+  @mustCallSuper
   @override
   void preRender(Canvas canvas) {
     if (_transform.m14 != body.position.x ||
@@ -67,6 +68,7 @@ abstract class BodyComponent<T extends Forge2DGame> extends Component
     canvas.transform(_transform.storage);
   }
 
+  @mustCallSuper
   @override
   void postRender(Canvas canvas) {
     canvas.restore();
