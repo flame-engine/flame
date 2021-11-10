7c2f2fd6dcc0617dc34fffe895bf8aa711fc125d
Lukas Klingsbo (spydon) <me@lukas.fyi>
2021-11-10 21:57:06 +0100

    Doesn't need super.render

--- packages/flame/lib/src/components/shape_component.dart
+++ packages/flame/lib/src/components/shape_component.dart
@@ -11,9 +11,6 @@ import '../extensions/vector2.dart';
 /// A [ShapeComponent] is a [Shape] wrapped in a [PositionComponent] so that it
 /// can be added to a component tree and take the camera and viewport into
 /// consideration when rendering.
-///
-/// Once you have added a shape in the [ShapeComponent] you should only edit
-/// [position], [size] and [angle] of the [ShapeComponent], not of the [Shape].
 class ShapeComponent extends PositionComponent with HasHitboxes {
   final HitboxShape shape;
   Paint paint;
