950a7d6152d38369cc8de93a9d7992e5c3ec8312
Luan Nico <luanpotter27@gmail.com>
2021-11-16 09:36:23 -0500

    Improvements to isometric tilemaps (#1109)
    * Improvements to isometric tilemaps
    
    * Changelog
    
    * Add test
    
    * const block
    
    * Fix example
    
    * Update packages/flame/lib/src/components/isometric_tile_map_component.dart
    
    Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>
    
    * Update packages/flame/lib/src/components/isometric_tile_map_component.dart
    
    Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>
    
    * Update packages/flame/lib/src/components/isometric_tile_map_component.dart
    
    Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>

---
+++ packages/flame/test/components/isometric_tile_map_component_test.dart
@@ -0,0 +1,28 @@
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class _MockSpriteSheet extends Mock implements SpriteSheet {}
+
+void main() {
+  group('isometric tile map', () {
+    test('conversions work back and forth', () {
+      final c = IsometricTileMapComponent(
+        _MockSpriteSheet(),
+        [
+          [1, 1],
+          [1, 1],
+        ],
+        destTileSize: Vector2.all(32.0),
+        tileHeight: 8.0,
+      );
+
+      const blocks = [Block(0, 0), Block(1, 1), Block(-1, 0), Block(2, -10)];
+      for (final block in blocks) {
+        expect(c.getBlockRenderedAt(c.getBlockRenderPosition(block)), block);
+        expect(c.getBlock(c.getBlockCenterPosition(block)), block);
+      }
+    });
+  });
+}
