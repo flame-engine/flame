9ebddbc8c78b420903be52e83e073ec3d1c56c8c
Pasha Stetsenko <stpasha@google.com>
2021-11-21 18:04:56 -0800

    CurvedEffectController and its reverse

---
+++ packages/flame/lib/src/effects2/controllers/curved_effect_controller.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/animation.dart';
+
+import 'duration_effect_controller.dart';
+
+/// A controller that grows non-linearly from 0 to 1 following the provided
+/// [curve]. The [duration] cannot be 0.
+class CurvedEffectController extends DurationEffectController {
+  CurvedEffectController({
+    required double duration,
+    required Curve curve,
+  }) : assert(duration > 0, 'Duration must be positive: $duration'),
+       _curve = curve,
+       super(duration);
+
+  Curve get curve => _curve;
+  final Curve _curve;
+
+  @override
+  double get progress => _curve.transform(timer / duration);
+}
