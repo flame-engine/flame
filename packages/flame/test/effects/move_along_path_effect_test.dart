58ec6a020fff28cf0affb89fb45bfbef9c178bba
Pasha Stetsenko <stpasha@google.com>
2021-12-06 16:59:05 -0800

    added test

--- packages/flame/test/effects/move_along_path_effect_test.dart
+++ packages/flame/test/effects/move_along_path_effect_test.dart
@@ -38,22 +38,64 @@ void main() {
     });
 
     test('absolute path', () {
-      final game = FlameGame() ..onGameResize(Vector2(100, 100));
-      final component = PositionComponent() ..position = Vector2(17, -5);
+      final game = FlameGame()..onGameResize(Vector2(100, 100));
+      final component = PositionComponent()..position = Vector2(17, -5);
       game.add(component);
       game.update(0);
 
       component.add(
         MoveAlongPathEffect(
-          Path() ..moveTo(1000, 300) ..lineTo(1200, 500),
+          Path()
+            ..moveTo(1000, 300)
+            ..lineTo(1200, 500),
           EffectController(duration: 1),
           absolute: true,
         ),
       );
       game.update(0);
       for (var i = 0; i < 10; i++) {
-        expect(component.position.x, closeTo(1000 + 200*(i/10), 1e-10));
-        expect(component.position.y, closeTo(300 + 200*(i/10), 1e-10));
+        expect(component.position.x, closeTo(1000 + 200 * (i / 10), 1e-10));
+        expect(component.position.y, closeTo(300 + 200 * (i / 10), 1e-10));
+        game.update(0.1);
+      }
+    });
+
+    test('absolute oriented path', () {
+      final game = FlameGame()..onGameResize(Vector2(100, 100));
+      final component = PositionComponent(
+        position: Vector2(17, -5),
+        angle: -30.5,
+      );
+      game.add(component);
+      game.update(0);
+
+      component.add(
+        MoveAlongPathEffect(
+          Path() // pythagorean triangle, perimeter=600
+            ..moveTo(200, 200)
+            ..lineTo(290, 80)
+            ..lineTo(450, 200)
+            ..lineTo(200, 200),
+          EffectController(duration: 6),
+          absolute: true,
+          oriented: true,
+        ),
+      );
+      game.update(0);
+      for (var i = 0; i < 60; i++) {
+        if (i <= 15) {
+          expect(component.position.x, closeTo(200 + 6 * i, 1e-10));
+          expect(component.position.y, closeTo(200 - 8 * i, 1e-10));
+          expect(component.angle, closeTo(-asin(0.8), 1e-7));
+        } else if (i <= 35) {
+          expect(component.position.x, closeTo(290 + 8 * (i - 15), 1e-10));
+          expect(component.position.y, closeTo(80 + 6 * (i - 15), 1e-10));
+          expect(component.angle, closeTo(asin(0.6), 1e-7));
+        } else {
+          expect(component.position.x, closeTo(450 - 10 * (i - 35), 1e-10));
+          expect(component.position.y, closeTo(200, 1e-10));
+          expect(component.angle, closeTo(pi, 1e-7));
+        }
         game.update(0.1);
       }
     });
