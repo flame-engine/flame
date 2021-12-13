c33b04f9b0555fcb68ef515fbcf8bcadf21f3f51
Pasha Stetsenko <stpasha@google.com>
2021-12-13 14:09:53 -0800

    Add export..show in effects.dart

--- packages/flame/lib/effects.dart
+++ packages/flame/lib/effects.dart
@@ -1,24 +1,34 @@
-export 'src/effects/color_effect.dart';
-export 'src/effects/component_effect.dart';
-export 'src/effects/controllers/curved_effect_controller.dart';
-export 'src/effects/controllers/delayed_effect_controller.dart';
-export 'src/effects/controllers/duration_effect_controller.dart';
-export 'src/effects/controllers/effect_controller.dart';
-export 'src/effects/controllers/infinite_effect_controller.dart';
-export 'src/effects/controllers/linear_effect_controller.dart';
-export 'src/effects/controllers/pause_effect_controller.dart';
-export 'src/effects/controllers/random_effect_controller.dart';
-export 'src/effects/controllers/repeated_effect_controller.dart';
-export 'src/effects/controllers/reverse_curved_effect_controller.dart';
-export 'src/effects/controllers/reverse_linear_effect_controller.dart';
-export 'src/effects/controllers/sequence_effect_controller.dart';
-export 'src/effects/effect.dart';
-export 'src/effects/move_along_path_effect.dart';
-export 'src/effects/move_effect.dart';
-export 'src/effects/opacity_effect.dart';
-export 'src/effects/remove_effect.dart';
-export 'src/effects/rotate_effect.dart';
-export 'src/effects/scale_effect.dart';
-export 'src/effects/size_effect.dart';
-export 'src/effects/transform2d_effect.dart';
-export 'src/extensions/vector2.dart';
+export 'src/effects/color_effect.dart' show ColorEffect;
+export 'src/effects/component_effect.dart' show ComponentEffect;
+export 'src/effects/controllers/curved_effect_controller.dart'
+    show CurvedEffectController;
+export 'src/effects/controllers/delayed_effect_controller.dart'
+    show DelayedEffectController;
+export 'src/effects/controllers/duration_effect_controller.dart'
+    show DurationEffectController;
+export 'src/effects/controllers/effect_controller.dart' show EffectController;
+export 'src/effects/controllers/infinite_effect_controller.dart'
+    show InfiniteEffectController;
+export 'src/effects/controllers/linear_effect_controller.dart'
+    show LinearEffectController;
+export 'src/effects/controllers/pause_effect_controller.dart'
+    show PauseEffectController;
+export 'src/effects/controllers/random_effect_controller.dart'
+    show RandomEffectController, RandomVariable;
+export 'src/effects/controllers/repeated_effect_controller.dart'
+    show RepeatedEffectController;
+export 'src/effects/controllers/reverse_curved_effect_controller.dart'
+    show ReverseCurvedEffectController;
+export 'src/effects/controllers/reverse_linear_effect_controller.dart'
+    show ReverseLinearEffectController;
+export 'src/effects/controllers/sequence_effect_controller.dart'
+    show SequenceEffectController;
+export 'src/effects/effect.dart' show Effect;
+export 'src/effects/move_along_path_effect.dart' show MoveAlongPathEffect;
+export 'src/effects/move_effect.dart' show MoveEffect;
+export 'src/effects/opacity_effect.dart' show OpacityEffect;
+export 'src/effects/remove_effect.dart' show RemoveEffect;
+export 'src/effects/rotate_effect.dart' show RotateEffect;
+export 'src/effects/scale_effect.dart' show ScaleEffect;
+export 'src/effects/size_effect.dart' show SizeEffect;
+export 'src/effects/transform2d_effect.dart' show Transform2DEffect;
