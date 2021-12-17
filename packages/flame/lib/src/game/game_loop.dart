1f3b9953133de11987ce2889ac7fe6d667c00d28
Pasha Stetsenko <stpasha@google.com>
2021-12-17 10:16:04 -0800

    GameLoop.previous is now private

--- packages/flame/lib/src/game/game_loop.dart
+++ packages/flame/lib/src/game/game_loop.dart
@@ -2,17 +2,17 @@ import 'package:flutter/scheduler.dart';
 
 class GameLoop {
   void Function(double dt) callback;
-  Duration previous = Duration.zero;
-  late Ticker _ticker;
+  Duration _previous = Duration.zero;
+  late final Ticker _ticker;
 
   GameLoop(this.callback) {
     _ticker = Ticker(_tick);
   }
 
   void _tick(Duration timestamp) {
-    final durationDelta = timestamp - previous;
+    final durationDelta = timestamp - _previous;
     final dt = durationDelta.inMicroseconds / Duration.microsecondsPerSecond;
-    previous = timestamp;
+    _previous = timestamp;
     callback(dt);
   }
 
@@ -24,12 +24,15 @@ class GameLoop {
 
   void stop() {
     _ticker.stop();
-    previous = Duration.zero;
+    _previous = Duration.zero;
   }
 
   void dispose() {
     _ticker.dispose();
   }
+
+  @Deprecated('Internal variable')
+  Duration get previous => _previous;
 
   @Deprecated('Use stop() instead')
   void pause() => stop();
