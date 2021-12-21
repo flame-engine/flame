8938e08b8d4c896c01e67c5a4381c7979bf02009
Ryuzaki <ufrshubham@gmail.com>
2021-12-21 18:21:06 +0530

    Fix trailing comma warning

--- packages/flame_tiled/lib/src/tiled_component.dart
+++ packages/flame_tiled/lib/src/tiled_component.dart
@@ -23,8 +23,11 @@ class TiledComponent extends Component {
   }
 
   /// Loads a [TiledComponent] from a file.
-  static Future<TiledComponent> load(String fileName, Vector2 destTileSize,
-      {int? priority}) async {
+  static Future<TiledComponent> load(
+    String fileName,
+    Vector2 destTileSize, {
+    int? priority,
+  }) async {
     return TiledComponent(
       await RenderableTiledMap.fromFile(fileName, destTileSize),
       priority: priority,
