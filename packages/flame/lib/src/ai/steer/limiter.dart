d69d95638b2d6f4608df67ddc81b8dcc79152904
Pasha Stetsenko <stpasha@google.com>
2021-12-22 20:27:12 -0800

    added Location class

--- packages/flame/lib/src/ai/steer/limiter.dart
+++ packages/flame/lib/src/ai/steer/limiter.dart
@@ -1,7 +1,6 @@
 // Copyright 2021 Blue Fire team, MIT license
 //
-// Original license notice:
-// -----------------------------------------------------------------------------
+// -- Original license notice: -------------------------------------------------
 // Copyright 2014 See AUTHORS file.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
---
+++ packages/flame/lib/src/ai/utils/location.dart
@@ -0,0 +1,45 @@
+// Copyright 2021 Blue Fire team, MIT license
+//
+// -- Original license notice: -------------------------------------------------
+// Copyright 2014 See AUTHORS file.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// -----------------------------------------------------------------------------
+// TRANSLATED INTO DART from original $GDXAI/utils/Location.java
+// -----------------------------------------------------------------------------
+import 'dart:math' as math;
+import 'package:vector_math/vector_math_64.dart';
+
+/// The [Location] interface represents any game object having a position and
+/// an orientation.
+abstract class Location {
+  /// Vector indicating the position of this location.
+  Vector2 get position;
+
+  /// Float value indicating the orientation of this location. The orientation
+  /// is the angle in radians representing the direction that this location is
+  /// facing.
+  double get orientation;
+
+  /// Returns the angle in radians pointing along the specified vector.
+  double vectorToAngle(Vector2 vector) {
+    return math.atan2(-vector.x, vector.y);
+  }
+
+  /// Returns the unit vector in the direction of the specified angle
+  /// expressed in radians. This method must be consistent with
+  /// `vector2Angle()`.
+  Vector2 angleToVector(double angle) {
+    return Vector2(-math.sin(angle), math.cos(angle));
+  }
+}
