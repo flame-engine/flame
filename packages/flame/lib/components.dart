6444bdd83395e9ef923a84e2e751ea7bb59d1d0f
Pasha Stetsenko <stpasha@google.com>
2021-12-30 00:03:50 -0800

    unexport SteeravkeComponent

--- packages/flame/lib/components.dart
+++ packages/flame/lib/components.dart
@@ -23,7 +23,6 @@ export 'src/components/sprite_animation_group_component.dart';
 export 'src/components/sprite_batch_component.dart';
 export 'src/components/sprite_component.dart';
 export 'src/components/sprite_group_component.dart';
-export 'src/components/steerable_component.dart';
 export 'src/components/text_box_component.dart';
 export 'src/components/text_component.dart';
 export 'src/components/timer_component.dart';
