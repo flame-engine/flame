9530aaddc5d4ca256879964fd5d64864987cea8d
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2022-01-07 16:05:14 +0100

    Fix analyze issues

--- examples/lib/stories/effects/dual_effect_removal_example.dart
+++ examples/lib/stories/effects/dual_effect_removal_example.dart
@@ -8,63 +8,61 @@ import 'package:flutter/material.dart';
 class DualEffectRemovalExample extends FlameGame with TapDetector {
   static const String description = '''
     In this example we show how a dual effect can be used and removed.
-    To remove an effect just tap anywhere on the screen.
-    The 1st tap will remove the OpacityEffect and the 2nd tap removes the ColorEffect.
+    To remove an effect, tap anywhere on the screen and the first tap will
+    remove the OpacityEffect and the second tap removes the ColorEffect.
+    In this example, when an effect is removed the component is reset to
+    the state (the part of the state that was affected by the running effect)
+    that it had before the effect started running.
   ''';
 
-  late final SpriteComponent mySprite;
-  late  ColorEffect colorEffect;
+  late ColorEffect colorEffect;
   late OpacityEffect opacityEffect;
   
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
-    Sprite flameSprite = await loadSprite('flame.png');
-    Vector2 spriteSize = flameSprite.srcSize;
-    Vector2 spritePostion = Vector2(50,50);
-    mySprite = SpriteComponent(
-        sprite: flameSprite, position: spritePostion, size: spriteSize);
+    final mySprite = SpriteComponent(
+        sprite: await loadSprite('flame.png'), 
+        position: Vector2(50,50), 
+    );
     add(mySprite);
 
-    EffectController controller1 = EffectController(
+    final colorController = EffectController(
       duration: 2,
       reverseDuration: 2,
       infinite: true,
     );
     colorEffect = ColorEffect(
         Colors.blue,
-        const Offset(
-          0.0,
-          0.8,
-        ),
-        controller1);
+        const Offset(0.0, 0.8),
+        colorController,
+    );
     mySprite.add(colorEffect);
 
-    EffectController controller2 = EffectController(
+    EffectController opacityController = EffectController(
       duration: 1,
       reverseDuration: 1,
       infinite: true,
     );
-    opacityEffect = OpacityEffect.fadeOut(controller2);
+    opacityEffect = OpacityEffect.fadeOut(opacityController);
     mySprite.add(opacityEffect);
   }
   
-///apply(0) sends the animation to its initial starting state. if this isnt called, the fx could be removed at its presently animating state, and then that look
-/// of the sprite cant be changed. Hence when you want an effect to be removed and the sprite must go back to exactly how it looked, prior to the effect, you must call apply(0)
-///befor you call on the removeFromParent()
   @override
   void onTap() {
+    // apply(0) sends the animation to its initial starting state. 
+    // If this isn't called, the effect would be removed and leave the
+    // component at its current state.
+    // Hence when you want an effect to be removed and the component to go back
+    // to how it looked prior to the effect, you must call apply(0) before you
+    // call removeFromParent().
     if (opacityEffect.isMounted) {
       opacityEffect.apply(0);
       opacityEffect.removeFromParent();
-    }
-    else if(colorEffect.isMounted){
+    } else if(colorEffect.isMounted) {
       colorEffect.apply(0);
       colorEffect.removeFromParent();
     }
   }
-
-
-
 }
