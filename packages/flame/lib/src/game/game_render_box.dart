52ba58d45da0972c49ab8440fade7dcd206ebcc0
Pasha Stetsenko <stpasha@google.com>
2022-01-11 23:38:37 -0800

    refactor: Resize logic in GameRenderBox

--- packages/flame/lib/src/game/game_render_box.dart
+++ packages/flame/lib/src/game/game_render_box.dart
@@ -19,10 +19,10 @@ class GameRenderBox extends RenderBox with WidgetsBindingObserver {
   bool get isRepaintBoundary => true;
 
   @override
-  void performResize() {
-    super.performResize();
-    game.onGameResize(constraints.biggest.toVector2());
-  }
+  bool get sizedByParent => true;
+
+  @override
+  Size computeDryLayout(BoxConstraints constraints) => constraints.biggest;
 
   @override
   void attach(PipelineOwner owner) {
@@ -59,11 +59,6 @@ class GameRenderBox extends RenderBox with WidgetsBindingObserver {
   }
 
   @override
-  void performLayout() {
-    size = constraints.biggest;
-  }
-
-  @override
   void paint(PaintingContext context, Offset offset) {
     context.canvas.save();
     context.canvas.translate(offset.dx, offset.dy);
@@ -83,7 +78,4 @@ class GameRenderBox extends RenderBox with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     game.lifecycleStateChange(state);
   }
-
-  @override
-  Size computeDryLayout(BoxConstraints constraints) => constraints.biggest;
 }
