eb10b59698639d64e2e6b27f7de4de148786017d
Erick Zanardo <erickzanardoo@gmail.com>
2022-01-23 14:54:33 -0300

    feat: adding default constructor on SvgComponent

--- packages/flame_svg/lib/svg_component.dart
+++ packages/flame_svg/lib/svg_component.dart
@@ -7,18 +7,26 @@ import './svg.dart';
 /// Wraps [Svg] in a Flame component.
 class SvgComponent extends PositionComponent {
   /// The wrapped instance of [Svg].
-  Svg svg;
+  final Svg? svg;
 
-  /// Creates an [SvgComponent] from an [Svg] instance.
-  SvgComponent.fromSvg(
-    this.svg, {
+  /// Creates an [SvgComponent]
+  SvgComponent({
+    this.svg,
     Vector2? position,
     Vector2? size,
     int? priority,
   }) : super(position: position, size: size, priority: priority);
 
+  /// Creates an [SvgComponent] from an [Svg] instance.
+  SvgComponent.fromSvg(
+    Svg svg, {
+    Vector2? position,
+    Vector2? size,
+    int? priority,
+  }) : this(svg: svg, position: position, size: size, priority: priority);
+
   @override
   void render(Canvas canvas) {
-    svg.render(canvas, size);
+    svg?.render(canvas, size);
   }
 }
