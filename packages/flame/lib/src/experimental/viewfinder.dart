0388b09c82a252c31c24a2a4bb0750ed8fa992b9
Pasha Stetsenko <stpasha@google.com>
2022-02-04 03:06:41 -0800

    wip on Viewfinder

--- packages/flame/lib/src/experimental/viewfinder.dart
+++ packages/flame/lib/src/experimental/viewfinder.dart
@@ -1,9 +1,23 @@
 
 import 'dart:ui';
 
+import 'package:vector_math/vector_math_64.dart';
+
 import '../components/component.dart';
+import '../game/transform2d.dart';
 
 class Viewfinder extends Component {
+
+  final Transform2D _transform = Transform2D();
+
+  Vector2 get position => _transform.position;
+  set position(Vector2 value) => _transform.position = value;
+
+  double get zoom => _transform.scale.x;
+  set zoom(double value) => _transform.scale = Vector2.all(value);
+
+  double get angle => _transform.angle;
+  set angle(double value) => _transform.angle;
 
   @override
   void renderTree(Canvas canvas) {}
