c9978fdd4b424ae3ea197d1471fe6dcf6fe1b136
Pasha Stetsenko <stpasha@google.com>
2022-02-05 01:19:49 -0800

    docs for world

--- packages/flame/lib/src/experimental/world.dart
+++ packages/flame/lib/src/experimental/world.dart
@@ -5,12 +5,16 @@ import 'package:meta/meta.dart';
 import '../components/component.dart';
 import 'camera.dart';
 
+/// The root component for all game world elements.
+///
+/// The primary feature of this component is that it disables regular rendering,
+/// and allows itself to be rendered through a [Camera2] only. The updates
+/// proceed through the world tree normally.
 class World extends Component {
-  // World may only be rendered through a camera, so regular [renderTree] is
-  // disabled.
   @override
   void renderTree(Canvas canvas) {}
 
+  /// Internal rendering method invoked by the [Camera2].
   @internal
   void renderFromCamera(Canvas canvas) {
     assert(Camera2.currentCamera != null);
