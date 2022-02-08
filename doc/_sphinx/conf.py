2963bcbaa272d9cae2132ce2146f12d53ce33b31
Pasha Stetsenko <stpasha@google.com>
2022-02-07 17:48:52 -0800

    more style fixes

--- doc/_sphinx/conf.py
+++ doc/_sphinx/conf.py
@@ -59,7 +59,7 @@ pygments_style = 'monokai'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['images', 'styles', 'scripts', 'theme', 'fonts']
+html_static_path = ['images', 'styles', 'scripts', 'theme']
 html_css_files = ['custom.css']
 html_js_files = ['versions.js']
 
