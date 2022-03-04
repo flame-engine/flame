101732e0c89386fc8ab5058e6127f9c2ce77862a
Pasha Stetsenko <stpasha@google.com>
2022-03-03 22:36:09 -0800

    Added doc for the experimental.dart file

--- packages/flame/lib/experimental.dart
+++ packages/flame/lib/experimental.dart
@@ -1,3 +1,15 @@
+/// Classes and components in this sub-module are considered experimental,
+/// that is, their API may still be incomplete and subject to change at a more
+/// rapid pace than the rest of the Flame code.
+///
+/// However, do not feel discouraged to use this functionality: on the contrary,
+/// consider this as a way to help the Flame community by beta-testing new
+/// components.
+///
+/// After the components lived here for some time, and when we gain more
+/// confidence in their robustness, they will be moved out into the main Flame
+/// library.
+
 export 'src/experimental/camera_component.dart' show CameraComponent;
 export 'src/experimental/circular_viewport.dart' show CircularViewport;
 export 'src/experimental/fixed_aspect_ratio_viewport.dart'
