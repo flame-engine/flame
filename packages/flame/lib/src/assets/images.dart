4ddd90aafc40a3f5ce3d9b181a66369436de3c9c
Jochum van der Ploeg <jochum@vdploeg.net>
2022-03-18 17:17:41 +0100

    feat: Deprecating Rect methods (#1455)
    * feat: Deprecating Rect methods
    
    * Fixed analyze problems
    
    Co-authored-by: Erick <erickzanardoo@gmail.com>

--- packages/flame/lib/src/assets/images.dart
+++ packages/flame/lib/src/assets/images.dart
@@ -118,7 +118,7 @@ class Images {
   /// [runAsWeb] to `true`. Keep in mind that it is slightly slower than the
   /// native [ui.decodeImageFromPixels]. By default it is set to [kIsWeb].
   @Deprecated(
-    'Use Image.fromPixels() instead. This function will be removed in 1.1.0',
+    'Use Image.fromPixels() instead. This function will be removed in 1.2.0',
   )
   Future<Image> decodeImageFromPixels(
     Uint8List pixels,
