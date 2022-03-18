651b6556c79ec96b06382bf212646e4657c5e84d
Pasha Stetsenko <stpasha@google.com>
2022-03-17 18:59:26 -0700

    fix analysis warnings

--- packages/flame/test/effects/move_effect_test.dart
+++ packages/flame/test/effects/move_effect_test.dart
@@ -3,9 +3,6 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
 import 'package:flame/game.dart';
-import 'package:flame/src/effects/controllers/linear_effect_controller.dart';
-import 'package:flame/src/effects/move_effect.dart';
-import 'package:flame/src/effects/provider_interfaces.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
