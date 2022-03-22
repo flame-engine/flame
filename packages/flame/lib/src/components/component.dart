080aef931e4bbee63a8657da79ce8e6b38782d78
Lukas Klingsbo <me@lukas.fyi>
2022-03-22 22:51:56 +0100

    fix: Remove generics from ancestors

--- packages/flame/lib/src/components/component.dart
+++ packages/flame/lib/src/components/component.dart
@@ -321,12 +321,10 @@ class Component {
   /// An iterator producing this component's parent, then its parent's parent,
   /// then the great-grand-parent, and so on, until it reaches a component
   /// without a parent.
-  Iterable<T> ancestors<T extends Component>({bool includeSelf = false}) sync* {
+  Iterable<Component> ancestors({bool includeSelf = false}) sync* {
     var current = includeSelf ? this : parent;
     while (current != null) {
-      if (current is T) {
-        yield current;
-      }
+      yield current;
       current = current.parent;
     }
   }
