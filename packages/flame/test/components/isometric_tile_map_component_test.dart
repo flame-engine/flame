616a5226adbd7a5ec6620816a2accd0f4cdb0b18
Ratatosk <SEFUNCTeamRatatosk@skandia.se>
2022-04-06 11:38:53 +0200

    Fix mocks

--- packages/flame/test/components/isometric_tile_map_component_test.dart
+++ packages/flame/test/components/isometric_tile_map_component_test.dart
@@ -1,10 +1,28 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-class _MockSpriteSheet extends Mock implements SpriteSheet {}
+class MockImage extends Mock implements Image {
+  MockImage(this.size);
+
+  final Vector2 size;
+  @override
+  int get width => size.x.toInt();
+  @override
+  int get height => size.y.toInt();
+}
+
+class _MockSpriteSheet extends Mock implements SpriteSheet {
+  _MockSpriteSheet(this.tileSize);
+
+  final Vector2 tileSize;
+  @override
+  Image get image => MockImage(tileSize);
+}
 
 void main() {
   group('isometric tile map', () {
@@ -12,11 +30,13 @@ void main() {
       [1, 1],
       [1, 1],
     ];
+
     test('conversions work back and forth', () {
+      final tileSize = Vector2.all(32.0);
       final c = IsometricTileMapComponent(
-        _MockSpriteSheet(),
+        _MockSpriteSheet(tileSize),
         map,
-        destTileSize: Vector2.all(32.0),
+        destTileSize: tileSize,
         tileHeight: 8.0,
       );
 
@@ -26,11 +46,13 @@ void main() {
         expect(c.getBlock(c.getBlockCenterPosition(block)), block);
       }
     });
+
     test('center position', () {
+      final tileSize = Vector2.all(32.0);
       final c = IsometricTileMapComponent(
-        _MockSpriteSheet(),
+        _MockSpriteSheet(tileSize),
         map,
-        destTileSize: Vector2.all(32.0),
+        destTileSize: tileSize,
         tileHeight: 8.0,
       );
 
@@ -38,10 +60,11 @@ void main() {
     });
 
     test('height scaling', () {
+      final tileSize = Vector2(156, 181);
       final c = IsometricTileMapComponent(
-        _MockSpriteSheet(),
+        _MockSpriteSheet(tileSize),
         map,
-        destTileSize: Vector2(156, 181),
+        destTileSize: tileSize,
       );
 
       //expect the block to be directly below
