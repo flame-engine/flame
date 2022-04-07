47f3ffe6d8caf3b8f7a5eda5f04ef3a663b57507
Pasha Stetsenko <stpasha@google.com>
2022-04-07 14:00:04 -0700

    rename file

--- packages/flame/test/sprite_test.dart
+++ packages/flame/test/sprite_test.dart
@@ -6,7 +6,7 @@ import 'package:flame_test/flame_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '_resources/images.dart';
+import '_resources/load_image.dart';
 
 void main() {
   group('Sprite', () {
