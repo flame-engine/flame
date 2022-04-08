7eafa76397cafcfd8be5dd19ee564431065e674c
Pasha Stetsenko <stpasha@google.com>
2022-04-08 13:15:46 -0700

    docs for MoveByEffect

--- packages/flame/lib/src/effects/move_by_effect.dart
+++ packages/flame/lib/src/effects/move_by_effect.dart
@@ -4,6 +4,19 @@ import 'controllers/effect_controller.dart';
 import 'move_effect.dart';
 import 'provider_interfaces.dart';
 
+/// A [MoveEffect] that moves its target by the specified offset vector.
+///
+/// This effect will move its target in a straight line to a new position that
+/// is at an `offset` from the target's position at the start of the effect.
+///
+/// The `controller` can be used to change the timing of the movement: when the
+/// move starts, how fast it is, whether the motion is uniform or not, and so
+/// on. Refer to [EffectController] for details.
+///
+/// This effect applies incremental changes to the target's position, which
+/// allows it to be combined with other [MoveEffect]s. When several
+/// [MoveByEffect]s are applied to the same target simultaneously, the target
+/// is moved by the vector sum of offsets from all effects.
 class MoveByEffect extends MoveEffect {
   MoveByEffect(
     Vector2 offset,
