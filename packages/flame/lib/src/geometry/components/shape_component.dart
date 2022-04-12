78cee042c9aafd6f3c3b92d9a23c98a57e42ba56
Pasha Stetsenko <stpasha@google.com>
2022-04-12 15:56:08 -0700

    Expand ShapeComponent

--- packages/flame/lib/src/geometry/components/shape_component.dart
+++ packages/flame/lib/src/geometry/components/shape_component.dart
@@ -6,9 +6,29 @@ import '../../components/position_component.dart';
 import '../../extensions/vector2.dart';
 import '../shapes/shape.dart';
 
+/// A component that renders a simple geometric shape.
+///
+/// The shape is drawn using one or more [paints] provided in the constructor.
+/// Several paints can be used, for example, to draw the shape with a background
+/// color, a border, a shadow, etc.
 class ShapeComponent extends PositionComponent {
+  /// Constructs a [ShapeComponent] from the given [_shape].
+  ///
+  /// Furthermore, the ShapeComponent requires a list of paints for rendering,
+  /// so either the parameter [paint] or [paints] must be provided (but not
+  /// both).
+  ///
+  /// The placement of the ShapeComponent can either be specified explicitly,
+  /// or it will be derived from the current location of the [_shape].
+  /// Specifically, if [anchor] is not provided, it will be placed into the
+  /// shape's `.center`. If [position] is not specified, then it will be derived
+  /// from the shape's current location.
+  ///
+  /// The [_shape] passed into the constructor will be moved in such a way as to
+  /// make the local coordinate system defined by the [PositionComponent]
+  /// coincide with the local coordinate system of the [Shape].
   ShapeComponent(
-    this.shape, {
+    this._shape, {
     Paint? paint,
     List<Paint>? paints,
     Iterable<Component>? children,
@@ -20,8 +40,8 @@ class ShapeComponent extends PositionComponent {
           (paints == null) != (paint == null),
           'Either parameter `paint` or `paints` is required, but not both',
         ),
-        paints = paints ?? [paint!],
-        _path = shape.asPath(),
+        _paints = paints ?? <Paint>[paint!],
+        _path = _shape.asPath(),
         super(
           children: children,
           position: position,
@@ -29,29 +49,37 @@ class ShapeComponent extends PositionComponent {
           angle: angle,
           anchor: anchor,
         ) {
-    final aabb = shape.aabb;
-    if (!aabb.min.isZero()) {
-      shape.move(-aabb.min);
-      this.position.add(aabb.min);
-    }
+    final aabb = _shape.aabb;
     size = aabb.max - aabb.min;
     if (anchor == null) {
-      final center = shape.center;
+      final center = _shape.center;
       this.anchor = Anchor(
-          size.x == 0? 0.5 : center.x/size.x,
-          size.y == 0? 0.5 : center.y/size.y,
+        size.x == 0 ? 0.5 : (center.x - aabb.min.x) / size.x,
+        size.y == 0 ? 0.5 : (center.y - aabb.min.y) / size.y,
       );
+    }
+    if (position == null) {
+      this.position.x = aabb.min.x + size.x * this.anchor.x;
+      this.position.x = aabb.min.y + size.y * this.anchor.y;
+    }
+    if (!aabb.min.isZero()) {
+      _shape.move(-aabb.min);
     }
   }
 
-  final Shape shape;
-  final List<Paint> paints;
+  final Shape _shape;
   final Path _path;
+
+  /// The list of paints for drawing the shape.
+  List<Paint> get paints => _paints;
+  final List<Paint> _paints;
 
   @override
   void render(Canvas canvas) {
-    for (final paint in paints) {
+    for (final paint in _paints) {
       canvas.drawPath(_path, paint);
     }
   }
+
+  bool containsLocalPoint(Vector2 point) => _shape.containsPoint(point);
 }
