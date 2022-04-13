0232ea771696aba36a305f9057b86b0c37b08c12
Pasha Stetsenko <stpasha@google.com>
2022-04-12 21:54:28 -0700

    clean up Circle

--- packages/flame/lib/src/experimental/geometry/shapes/circle.dart
+++ packages/flame/lib/src/experimental/geometry/shapes/circle.dart
@@ -6,14 +6,26 @@ import '../../../extensions/vector2.dart';
 import '../../../game/transform2d.dart';
 import 'shape.dart';
 
-/// The circle with a given [center] and [radius].
+/// The circle with a given [center] and a [radius].
 class Circle extends Shape {
-  Circle(Vector2 center, this.radius)
-      : _center = center.clone(),
-        assert(radius > 0, 'Radius must be positive: $radius');
+  Circle(Vector2 center, double radius)
+      : assert(radius > 0, 'Radius must be positive: $radius'),
+        _center = center.clone(),
+        _radius = radius;
 
+  @override
+  Vector2 get center => _center;
   final Vector2 _center;
-  final double radius;
+
+  double get radius => _radius;
+  final double _radius;
+
+  @override
+  Aabb2 get aabb => _aabb ??= _calculateAabb();
+  Aabb2? _aabb;
+  Aabb2 _calculateAabb() {
+    return Aabb2.centerAndHalfExtents(_center, Vector2.all(_radius));
+  }
 
   @override
   bool get isClosed => true;
@@ -22,30 +34,24 @@ class Circle extends Shape {
   bool get isConvex => true;
 
   @override
-  Vector2 get center => _center;
-
-  @override
-  double get perimeter => radius * tau;
-
-  @override
-  Aabb2 get aabb => Aabb2.centerAndHalfExtents(_center, Vector2.all(radius));
+  double get perimeter => _radius * tau;
 
   @override
   Path asPath() {
     final center = _center.toOffset();
-    return Path()..addOval(Rect.fromCircle(center: center, radius: radius));
+    return Path()..addOval(Rect.fromCircle(center: center, radius: _radius));
   }
 
   @override
   bool containsPoint(Vector2 point) {
-    return (point - _center).length2 <= radius * radius;
+    return (point - _center).length2 <= _radius * _radius;
   }
 
   @override
   Shape project(Transform2D transform) {
     if (transform.isTranslation) {
       final newCenter = transform.localToGlobal(_center);
-      return Circle(newCenter, radius);
+      return Circle(newCenter, _radius);
     }
     throw UnimplementedError();
   }
@@ -53,10 +59,11 @@ class Circle extends Shape {
   @override
   void move(Vector2 offset) {
     _center.add(offset);
+    _aabb = null;
   }
 
   @override
-  String toString() => 'Circle([${center.x}, ${center.y}], $radius)';
+  String toString() => 'Circle([${_center.x}, ${_center.y}], $_radius)';
 }
 
 @internal
