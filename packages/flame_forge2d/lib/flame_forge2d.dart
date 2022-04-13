94a452e84c6eace72c653e004ab8476be0aea04b
alestiago <dev@alestiago.com>
2022-04-14 01:59:54 +0200

    refactor: reordered exports

--- packages/flame_forge2d/lib/flame_forge2d.dart
+++ packages/flame_forge2d/lib/flame_forge2d.dart
@@ -3,6 +3,6 @@ library flame_forge2d;
 export 'package:forge2d/forge2d.dart';
 
 export 'body_component.dart';
-export 'contact_callbacks.dart';
 export 'forge2d_camera.dart';
 export 'forge2d_game.dart';
+export 'world_contact_listener.dart';
