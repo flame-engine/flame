d75bcd071fc684be4ab804f23a50028721e0b61f
alestiago <dev@alestiago.com>
2022-04-14 01:14:55 +0200

    feat: simplified ContactCallback process

--- packages/flame_forge2d/lib/contact_callbacks.dart
+++ packages/flame_forge2d/lib/contact_callbacks.dart
@@ -2,92 +2,64 @@ import 'package:forge2d/forge2d.dart';
 
 import 'body_component.dart';
 
-class ContactTypes<T1, T2> {
-  // If o1 is, or inherits from, T1 or T2
-  bool has(Object o1) => o1 is T1 || o1 is T2;
-  bool hasOne(Object o1, Object o2) => has(o1) || has(o2);
-
-  // Only makes sense to call with objects that you know is in [T1, T2]
-  bool inOrder(Object o1, Object o2) => o1 is T1 && o2 is T2;
-
-  // Remember that this is not symmetric, it checks if the types in `o1` and
-  // `o2` are the same or inherits from the types in `other`
-  bool match(Object o1, Object o2) =>
-      (o1 is T1 && o2 is T2) || (o2 is T1 && o1 is T2);
-}
-
-typedef ContactCallbackFun = void Function(Object a, Object b, Contact contact);
-
-abstract class ContactCallback<Type1, Type2> {
-  ContactTypes<Type1, Type2> types = ContactTypes<Type1, Type2>();
-
-  void begin(Type1 a, Type2 b, Contact contact) {}
-  void end(Type1 a, Type2 b, Contact contact) {}
-  void preSolve(Type1 a, Type2 b, Contact contact, Manifold oldManifold) {}
-  void postSolve(Type1 a, Type2 b, Contact contact, ContactImpulse impulse) {}
+/// Listens to contacts events.
+mixin ContactCallback on BodyComponent {
+  void begin(Contact contact) {}
+  void end(Contact contact) {}
+  void preSolve(Contact contact, Manifold oldManifold) {}
+  void postSolve(Contact contact, ContactImpulse contactImpulse) {}
 }
 
 class ContactCallbacks extends ContactListener {
-  final List<ContactCallback> _callbacks = [];
+  @override
+  void beginContact(Contact contact) {
+    final userDataA = contact.fixtureA.body.userData;
+    final userDataB = contact.fixtureB.body.userData;
 
-  void register(ContactCallback callback) {
-    _callbacks.add(callback);
-  }
-
-  void deregister(ContactCallback callback) {
-    _callbacks.remove(callback);
-  }
-
-  void clear() {
-    _callbacks.clear();
-  }
-
-  void _maybeCallback(
-    Contact contact,
-    ContactCallback callback,
-    ContactCallbackFun f,
-  ) {
-    final a = contact.fixtureA.body.userData;
-    final b = contact.fixtureB.body.userData;
-    final wanted = callback.types;
-
-    if (a == null || b == null) {
-      return;
+    if (userDataA is ContactCallback) {
+      userDataA.begin(contact);
     }
-
-    if (wanted.match(a, b) ||
-        (wanted.has(BodyComponent) && wanted.hasOne(a, b))) {
-      wanted.inOrder(a, b) ? f(a, b, contact) : f(b, a, contact);
+    if (userDataB is ContactCallback) {
+      userDataB.begin(contact);
     }
   }
 
   @override
-  void beginContact(Contact contact) =>
-      _callbacks.forEach((c) => _maybeCallback(contact, c, c.begin));
+  void endContact(Contact contact) {
+    final userDataA = contact.fixtureA.body.userData;
+    final userDataB = contact.fixtureB.body.userData;
 
-  @override
-  void endContact(Contact contact) =>
-      _callbacks.forEach((c) => _maybeCallback(contact, c, c.end));
+    if (userDataA is ContactCallback) {
+      userDataA.end(contact);
+    }
+    if (userDataB is ContactCallback) {
+      userDataB.end(contact);
+    }
+  }
 
   @override
   void preSolve(Contact contact, Manifold oldManifold) {
-    _callbacks.forEach((c) {
-      void preSolveAux(Object a, Object b, Contact contact) {
-        c.preSolve(a, b, contact, oldManifold);
-      }
+    final userDataA = contact.fixtureA.body.userData;
+    final userDataB = contact.fixtureB.body.userData;
 
-      _maybeCallback(contact, c, preSolveAux);
-    });
+    if (userDataA is ContactCallback) {
+      userDataA.preSolve(contact, oldManifold);
+    }
+    if (userDataB is ContactCallback) {
+      userDataB.preSolve(contact, oldManifold);
+    }
   }
 
   @override
-  void postSolve(Contact contact, ContactImpulse impulse) {
-    _callbacks.forEach((c) {
-      void postSolveAux(Object a, Object b, Contact contact) {
-        c.postSolve(a, b, contact, impulse);
-      }
+  void postSolve(Contact contact, ContactImpulse contactImpulse) {
+    final userDataA = contact.fixtureA.body.userData;
+    final userDataB = contact.fixtureB.body.userData;
 
-      _maybeCallback(contact, c, postSolveAux);
-    });
+    if (userDataA is ContactCallback) {
+      userDataA.postSolve(contact, contactImpulse);
+    }
+    if (userDataB is ContactCallback) {
+      userDataB.postSolve(contact, contactImpulse);
+    }
   }
 }
--- packages/flame_forge2d/lib/forge2d_game.dart
+++ packages/flame_forge2d/lib/forge2d_game.dart
@@ -1,21 +1,10 @@
 import 'package:flame/game.dart';
-import 'package:forge2d/forge2d.dart' hide Timer;
+import 'package:forge2d/forge2d.dart';
 
 import 'contact_callbacks.dart';
 import 'forge2d_camera.dart';
 
 class Forge2DGame extends FlameGame {
-  static final Vector2 defaultGravity = Vector2(0, -10.0);
-  static const double defaultZoom = 10.0;
-
-  final World world;
-
-  final ContactCallbacks _contactCallbacks = ContactCallbacks();
-
-  /// The camera translates the coordinate space after the viewport is applied.
-  @override
-  Forge2DCamera get camera => super.camera as Forge2DCamera;
-
   Forge2DGame({
     Vector2? gravity,
     double zoom = defaultZoom,
@@ -23,25 +12,23 @@ class Forge2DGame extends FlameGame {
   })  : world = World(gravity ?? defaultGravity),
         super(camera: camera ?? Forge2DCamera()) {
     this.camera.zoom = zoom;
-    world.setContactListener(_contactCallbacks);
+    world.setContactListener(ContactCallbacks());
   }
+
+  static final Vector2 defaultGravity = Vector2(0, -10.0);
+
+  static const double defaultZoom = 10.0;
+
+  final World world;
+
+  /// The camera translates the coordinate space after the viewport is applied.
+  @override
+  Forge2DCamera get camera => super.camera as Forge2DCamera;
 
   @override
   void update(double dt) {
     super.update(dt);
     world.stepDt(dt);
-  }
-
-  void addContactCallback(ContactCallback callback) {
-    _contactCallbacks.register(callback);
-  }
-
-  void removeContactCallback(ContactCallback callback) {
-    _contactCallbacks.deregister(callback);
-  }
-
-  void clearContactCallbacks() {
-    _contactCallbacks.clear();
   }
 
   Vector2 worldToScreen(Vector2 position) {
