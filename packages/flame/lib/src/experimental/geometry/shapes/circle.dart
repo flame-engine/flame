d02c50cc304988f8473f1cdcfccf2ec62db2ebe8
Pasha Stetsenko <stpasha@google.com>
2022-04-12 21:42:29 -0700

    Circle is now mutable

--- packages/flame/lib/src/experimental/geometry/shapes/circle.dart
+++ packages/flame/lib/src/experimental/geometry/shapes/circle.dart
@@ -7,7 +7,6 @@ import '../../../game/transform2d.dart';
 import 'shape.dart';
 
 /// The circle with a given [center] and [radius].
-@immutable
 class Circle extends Shape {
   Circle(Vector2 center, this.radius)
       : _center = center.clone(),
@@ -51,13 +50,6 @@ class Circle extends Shape {
     }
     throw UnimplementedError();
   }
-
-  @override
-  bool operator ==(Object other) =>
-      other is Circle && _center == other._center && radius == other.radius;
-
-  @override
-  int get hashCode => hashValues(_center, radius);
 
   @override
   String toString() => 'Circle([${center.x}, ${center.y}], $radius)';
