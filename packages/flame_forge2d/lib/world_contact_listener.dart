ca4dc0a7424a90fd367de1e4f81907f62b3dbcc2
alestiago <dev@alestiago.com>
2022-04-14 02:19:39 +0200

    docs: improved documentation

--- packages/flame_forge2d/lib/world_contact_listener.dart
+++ packages/flame_forge2d/lib/world_contact_listener.dart
@@ -1,4 +1,4 @@
-import 'package:forge2d/forge2d.dart';
+import 'flame_forge2d.dart';
 
 /// Listens to the entire [World] contacts events.
 ///
@@ -10,10 +10,27 @@ import 'package:forge2d/forge2d.dart';
 /// of this will be called to when any [Body]'s fixture contacts another
 /// [Fixture].
 ///
+/// For example:
+/// ```dart
+// class Foo extends BodyComponent with ContactListener {
+///   @override
+///   Body createBody() {
+///     return world.createBody(
+///       BodyDef(userData: this),
+///     )..createFixture(
+///         FixtureDef(CircleShape()..radius = 5),
+///       );
+///  }
+/// }
+/// ```
+///
 /// If instead you wish to be more specific and only trigger contact events
 /// when a specific [Body]'s fixture contacts with another [Fixture] you can
 /// set the fixture `userData` to a [ContactListener].
-// TODO(alestiago): Make this class protected to the package.
+///
+/// The described behaviour is a simple out of the box solution to propagate
+/// contact events. If you wish to implement your own logic you can subclass
+/// [ContactListener] and provide it to your [Forge2DGame].
 class WorldContactListener extends ContactListener {
   Iterable<ContactListener> _contactListeners(Contact contact) => {
         contact.bodyA.userData,
