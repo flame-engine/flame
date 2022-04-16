6667794668cde54e42374637b0f8dfaf2d40550d
alestiago <dev@alestiago.com>
2022-04-16 21:45:05 +0100

    refactor: implemented SpriteAnimationController

--- packages/flame/lib/src/widgets/animation_widget.dart
+++ packages/flame/lib/src/widgets/animation_widget.dart
@@ -2,169 +2,62 @@ import 'dart:math';
 
 import 'package:flutter/material.dart' hide Animation;
 
-import '../anchor.dart';
-import '../assets/images.dart';
-import '../sprite_animation.dart';
-import 'base_future_builder.dart';
+import '../../components.dart';
 import 'sprite_painter.dart';
 
 export '../sprite_animation.dart';
 
-/// A [StatelessWidget] that renders a [SpriteAnimation]
+/// {@template flame.widgets.sprite_animation_widget}
+/// A [StatelessWidget] that renders a [SpriteAnimation].
+/// {@endtemplate}
 class SpriteAnimationWidget extends StatelessWidget {
-  /// The positioning [Anchor]
+  /// {@macro flame.widgets.sprite_animation_widget}
+  const SpriteAnimationWidget({
+    required this.controller,
+    this.anchor = Anchor.topLeft,
+    Key? key,
+  }) : super(key: key);
+
+  /// The positioning [Anchor].
   final Anchor anchor;
 
-  /// Should the animation be playing or not
-  final bool playing;
-
-  final Future<SpriteAnimation> Function() _animationFuture;
-
-  /// A builder function that is called if the loading fails
-  final WidgetBuilder? errorBuilder;
-
-  /// A builder function that is called while the loading is on the way
-  final WidgetBuilder? loadingBuilder;
-
-  SpriteAnimationWidget({
-    required SpriteAnimation animation,
-    this.playing = true,
-    this.anchor = Anchor.topLeft,
-    this.errorBuilder,
-    this.loadingBuilder,
-    Key? key,
-  })  : _animationFuture = (() => Future.value(animation)),
-        super(key: key);
-
-  SpriteAnimationWidget.asset({
-    required String path,
-    required SpriteAnimationData data,
-    Images? images,
-    this.playing = true,
-    this.anchor = Anchor.topLeft,
-    this.errorBuilder,
-    this.loadingBuilder,
-    Key? key,
-  })  : _animationFuture = (() => SpriteAnimation.load(
-              path,
-              data,
-              images: images,
-            )),
-        super(key: key);
+  final SpriteAnimationController controller;
 
   @override
   Widget build(BuildContext context) {
-    return BaseFutureBuilder<SpriteAnimation>(
-      futureBuilder: _animationFuture,
-      builder: (_, spriteAnimation) {
-        return _SpriteAnimationWidget(
-          animation: spriteAnimation,
-          anchor: anchor,
-          playing: playing,
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (_, __) {
+        return CustomPaint(
+          painter: SpritePainter(
+            controller.animation.getSprite(),
+            anchor,
+          ),
         );
       },
-      errorBuilder: errorBuilder,
-      loadingBuilder: loadingBuilder,
     );
   }
 }
 
-/// A [StatefulWidget] that render a [SpriteAnimation].
-class _SpriteAnimationWidget extends StatefulWidget {
-  /// The [SpriteAnimation] to be rendered
+class SpriteAnimationController extends AnimationController {
+  SpriteAnimationController({
+    required TickerProvider vsync,
+    required this.animation,
+  }) : super(vsync: vsync) {
+    duration = Duration(seconds: animation.totalDuration().ceil());
+  }
+
   final SpriteAnimation animation;
 
-  /// The positioning [Anchor]
-  final Anchor anchor;
-
-  /// Should the [animation] be playing or not
-  final bool playing;
-
-  const _SpriteAnimationWidget({
-    required this.animation,
-    this.playing = true,
-    this.anchor = Anchor.topLeft,
-  });
-
-  @override
-  State createState() => _SpriteAnimationWidgetState();
-}
-
-class _SpriteAnimationWidgetState extends State<_SpriteAnimationWidget>
-    with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
   double? _lastUpdated;
 
   @override
-  void initState() {
-    super.initState();
-    widget.animation.onComplete = _pauseAnimation;
-    _setupController();
-    if (widget.playing) {
-      _initAnimation();
-    }
-  }
+  void notifyListeners() {
+    super.notifyListeners();
 
-  @override
-  void didUpdateWidget(_SpriteAnimationWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    if (oldWidget.animation != widget.animation) {
-      oldWidget.animation.onComplete = null;
-      _setupController();
-    }
-
-    if (widget.playing) {
-      _initAnimation();
-    } else {
-      _pauseAnimation();
-    }
-  }
-
-  void _initAnimation() {
-    setState(() {
-      widget.animation.reset();
-      _lastUpdated = DateTime.now().millisecond.toDouble();
-      _controller?.repeat(
-        // Approximately 60 fps
-        period: const Duration(milliseconds: 16),
-      );
-    });
-  }
-
-  void _setupController() {
-    _controller?.dispose();
-
-    _controller = AnimationController(vsync: this)
-      ..addListener(() {
-        final now = DateTime.now().millisecond.toDouble();
-
-        final dt = max(0, (now - (_lastUpdated ?? 0)) / 1000).toDouble();
-        widget.animation.update(dt);
-
-        setState(() => _lastUpdated = now);
-      });
-  }
-
-  void _pauseAnimation() {
-    setState(() => _controller?.stop());
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext ctx) {
-    return Container(
-      child: CustomPaint(
-        painter: SpritePainter(
-          widget.animation.getSprite(),
-          widget.anchor,
-        ),
-      ),
-    );
+    final now = DateTime.now().millisecond.toDouble();
+    final dt = max(0, (now - (_lastUpdated ?? 0)) / 1000).toDouble();
+    animation.update(dt);
+    _lastUpdated = now;
   }
 }
