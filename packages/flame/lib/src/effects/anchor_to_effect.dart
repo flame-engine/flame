7dc6a56c99c027daa410ad9669a7afe2904b00fa
Pasha Stetsenko <stpasha@google.com>
2022-04-20 23:50:33 -0700

    Added AnchorEffect class

--- packages/flame/lib/src/effects/anchor_to_effect.dart
+++ packages/flame/lib/src/effects/anchor_to_effect.dart
@@ -1,10 +1,8 @@
 import 'package:vector_math/vector_math_64.dart';
 
 import '../anchor.dart';
+import 'anchor_effect.dart';
 import 'controllers/effect_controller.dart';
-import 'effect.dart';
-import 'effect_target.dart';
-import 'measurable_effect.dart';
 import 'provider_interfaces.dart';
 
 /// An effect that moves the target's anchor to the specified value.
@@ -12,17 +10,13 @@ import 'provider_interfaces.dart';
 /// The anchor will move in a straight line from the anchor's value at the start
 /// of the effect towards the provided target. The timing of the move is
 /// governed by the [controller].
-class AnchorToEffect extends Effect
-    with EffectTarget<AnchorProvider>
-    implements MeasurableEffect {
+class AnchorToEffect extends AnchorEffect {
   AnchorToEffect(
     Anchor destination,
     EffectController controller, {
     AnchorProvider? target,
   })  : _destination = destination,
-        super(controller) {
-    this.target = target;
-  }
+        super(controller, target);
 
   final Anchor _destination;
   late Vector2 _offset;
