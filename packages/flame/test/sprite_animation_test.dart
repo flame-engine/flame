c8804959248f56807769e6048778936d1915fed6
oloshe <jacob.j@qq.com>
2022-04-27 11:07:48 +0800

    test: add SpriteAnimationData test

--- packages/flame/test/sprite_animation_test.dart
+++ packages/flame/test/sprite_animation_test.dart
@@ -1,3 +1,4 @@
+import 'package:flame/extensions.dart';
 import 'package:flame/src/sprite_animation.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -183,6 +184,47 @@ void main() {
         expectLater(animation.completed, doesNotComplete);
       },
     );
+  });
+
+  group('SpriteAnimationData', () {
+    test(
+      'Throw assertion error when amountPerRow is greater than amount',
+      () {
+        expect(
+          () => SpriteAnimationData.variable(
+            amount: 5,
+            stepTimes: List.filled(5, 0.1),
+            textureSize: Vector2(50, 50),
+            amountPerRow: 10,
+          ),
+          failsAssert(),
+        );
+      },
+    );
+
+    test('new a SpriteAnimationData using range constructor', () {
+      final animationData = SpriteAnimationData.range(
+        start: 6,
+        end: 11,
+        amount: 18,
+        stepTimes: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
+        amountPerRow: 6,
+        textureSize: Vector2(1, 1),
+      );
+      expect(animationData.frames.length, 6);
+      expect(
+        animationData.frames.map((f) => f.stepTime),
+        [0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
+      );
+      expect(animationData.frames.map((f) => f.srcPosition), [
+        Vector2(0, 1),
+        Vector2(1, 1),
+        Vector2(2, 1),
+        Vector2(3, 1),
+        Vector2(4, 1),
+        Vector2(5, 1),
+      ]);
+    });
   });
 }
 
