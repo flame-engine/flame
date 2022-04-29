9c18900ec133a60086f87d804dec76603d1dc4da
Lukas Klingsbo <me@lukas.fyi>
2022-04-29 08:27:00 +0200

    Update main with link

--- packages/flame_forge2d/example/lib/main.dart
+++ packages/flame_forge2d/example/lib/main.dart
@@ -1,3 +1,7 @@
 void main() async {
   // There will be a new example in here after Google I/O, stay tuned!
+  // If you want to see the previous examples, go to the flame_forge2d section
+  // of https://examples.flame-engine.org
+  // The sourcecode lives here:
+  // https://github.com/flame-engine/flame/tree/main/examples/lib/stories/bridge_libraries/forge2d
 }
