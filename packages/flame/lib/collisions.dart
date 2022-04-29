7877579868041f4844ebae885da559097b7aa8a5
Lukas Klingsbo <me@lukas.fyi>
2022-04-29 16:09:48 +0200

    fix: Expose `CompositeHitbox` (#1589)

--- packages/flame/lib/collisions.dart
+++ packages/flame/lib/collisions.dart
@@ -3,6 +3,7 @@ export 'src/collisions/collision_callbacks.dart';
 export 'src/collisions/collision_detection.dart';
 export 'src/collisions/collision_passthrough.dart';
 export 'src/collisions/hitboxes/circle_hitbox.dart';
+export 'src/collisions/hitboxes/composite_hitbox.dart';
 export 'src/collisions/hitboxes/hitbox.dart';
 export 'src/collisions/hitboxes/polygon_hitbox.dart';
 export 'src/collisions/hitboxes/rectangle_hitbox.dart';
