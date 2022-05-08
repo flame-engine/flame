7cc0e07c4e670d04800199efe5754983f7b45058
Pasha Stetsenko <stpasha@google.com>
2022-05-08 00:15:57 -0700

    Added golden test

--- packages/flame/test/experimental/fixed_size_viewport_test.dart
+++ packages/flame/test/experimental/fixed_size_viewport_test.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
 import 'package:flame/experimental.dart';
+import 'package:flame/game.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:vector_math/vector_math_64.dart';
 
 void main() {
   group('FixedSizeViewport', () {
@@ -43,5 +46,41 @@ void main() {
       expect(viewport.containsLocalPoint(Vector2(300, 100)), false);
     });
 
+    FlameTester(() => FlameGame()).testGameWidget(
+      'Clipping behavior',
+      setUp: (game, tester) async {
+        final world = World();
+        final camera = CameraComponent(
+          world: world,
+          viewport: FixedSizeViewport(500, 200),
+        )
+          ..viewport.position = Vector2(400, 300)
+          ..viewfinder.position = Vector2.zero();
+        world.add(
+          CircleComponent(
+            position: Vector2.zero(),
+            radius: 200,
+            anchor: Anchor.center,
+            paint: Paint()..color = const Color(0x4400ffff),
+          ),
+        );
+        world.addAll([
+          for (var i = -6; i <= 6; i++)
+            CircleComponent(
+              position: Vector2(i * 60.0 - i * i.abs() * 3, 0),
+              radius: 25 - i.abs() * 3,
+              anchor: Anchor.center,
+            )
+        ]);
+        game.addAll([world, camera]);
+        await game.ready();
+      },
+      verify: (game, tester) async {
+        await expectLater(
+          find.byGame<FlameGame>(),
+          matchesGoldenFile('../_goldens/fixed_size_viewport_test_1.png'),
+        );
+      },
+    );
   });
 }
