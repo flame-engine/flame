c38e73d4daf90e98ffe6008bd1088755965390b0
Pasha Stetsenko <stpasha@google.com>
2022-05-13 12:29:00 -0700

    added docs

--- packages/flame_test/lib/src/test_golden.dart
+++ packages/flame_test/lib/src/test_golden.dart
@@ -4,7 +4,25 @@ import 'package:meta/meta.dart';
 
 /// Test that a game renders correctly.
 ///
+/// The way golden tests work is as follows: you set up a scene in [testBody],
+/// then the test framework renders your game widget into an image, and compares
+/// that image against stored [goldenFile]. The test passes if two images are
+/// identical, or fails if the images differ even in a single pixel.
 ///
+/// The term _golden file_ refers to the true rendering of a given game scene,
+/// captured at the creation of the test. In order to create a golden file, you
+/// first specify its desired name in the [goldenFile] parameter, and then run
+/// the tests using the command
+/// ```
+/// flutter test --update-goldens
+/// ```
+///
+/// The [testBody] is given a `game` parameter (which is by default a new
+/// [FlameGame] instance, but you can also supply your own [game] object), and
+/// is expected to set up a scene for rendering. Usually this involves adding
+/// necessary game components, and possibly advancing the game clock. As a
+/// convenience, we will run `await game.ready()` before rendering, to ensure
+/// that all components that might be pending are properly mounted.
 @isTest
 void testGolden(
   String testName,
