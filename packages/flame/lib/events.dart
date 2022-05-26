cbb903da4d9178199343d4b7ee220eae5eec44f5
Pasha Stetsenko <stpasha@google.com>
2022-05-26 11:18:27 -0700

    added events.dart

---
+++ packages/flame/lib/events.dart
@@ -0,0 +1,7 @@
+export 'src/events/component_mixins/tap_callbacks.dart' show TapCallbacks;
+export 'src/events/flame_game_mixins/has_tappable_components.dart'
+    show HasTappableComponents, HasLegacyTappables;
+export 'src/events/interfaces/multi_tap_listener.dart' show MultiTapListener;
+export 'src/events/messages/tap_cancel_event.dart' show TapCancelEvent;
+export 'src/events/messages/tap_down_event.dart' show TapDownEvent;
+export 'src/events/messages/tap_up_event.dart' show TapUpEvent;
