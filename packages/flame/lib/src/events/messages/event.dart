544dafa75b3071d99bda479538e7280d3bc93bf8
Pasha Stetsenko <stpasha@google.com>
2022-05-26 10:42:23 -0700

    docs for Event and PositionEvent

--- packages/flame/lib/src/events/messages/event.dart
+++ packages/flame/lib/src/events/messages/event.dart
@@ -1,19 +1,37 @@
 import 'package:flame/src/components/component.dart';
+import 'package:meta/meta.dart';
 
-class Event {
+/// Base class for a variety of input events, such as tap events, drag events,
+/// keyboard events, etc.
+///
+/// This base class offers only simple common functionality; please see the
+/// concrete [Event] subclasses for the information about each individual event
+/// and the circumstances when they occur.
+abstract class Event {
+  /// Flag that can be used to indicate that the event was handled by one of the
+  /// components.
+  ///
+  /// This flag is neither set nor read by Flame. Instead, it can be set by the
+  /// user in a component that handles an event, and then read by the user in a
+  /// different component, or at the root Game level.
   bool handled = false;
 
+  /// If this flag is false (default), the event will be delivered to the first
+  /// component that can handle it. If that component sets this flag to true,
+  /// the event will propagate further down the component tree to other eligible
+  /// components.
   bool continuePropagation = false;
 
-  void deliverToComponents<T extends Component, E extends Event>(
-    Component root,
-    void Function(T, E) handler,
+  @internal
+  void deliverToComponents<T extends Component>(
+    Component rootComponent,
+    void Function(T component) eventHandler,
   ) {
-    assert(this is E);
-    for (final child
-        in root.descendants(reversed: true, includeSelf: true).whereType<T>()) {
+    for (final child in rootComponent
+        .descendants(reversed: true, includeSelf: true)
+        .whereType<T>()) {
       continuePropagation = false;
-      handler(child, this as E);
+      eventHandler(child);
       if (!continuePropagation) {
         break;
       }
