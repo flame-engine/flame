c5ace9403fe421ddcdce832c7852992ff6d1b761
Hwan-seok <tttkhs@naver.com>
2022-05-27 02:44:29 +0900

    Add tests

--- packages/flame/test/game/active_overlays_test.dart
+++ packages/flame/test/game/active_overlays_test.dart
@@ -19,6 +19,70 @@ void main() {
       });
     });
 
+    group('addAll', () {
+      test('can add multiple overlays at once', () {
+        final overlays = FlameGame().overlays;
+        overlays.addAll(['test', 'test2']);
+        expect(overlays.isActive('test'), true);
+        expect(overlays.isActive('test2'), true);
+      });
+
+      test('only refreshes gameWidget once if added', () {
+        final game = FlameGame();
+        var widgetRefreshedCount = 0;
+        game.addGameStateListener(() => widgetRefreshedCount++);
+
+        game.overlays.addAll(['test', 'test2']);
+
+        expect(widgetRefreshedCount, 1);
+      });
+
+      test('does not refreshes gameWidget when not changed', () {
+        final game = FlameGame();
+        var widgetRefreshedCount = 0;
+        game.overlays.addAll(['test', 'test2']);
+        game.addGameStateListener(() => widgetRefreshedCount++);
+
+        game.overlays.addAll(['test', 'test2']);
+
+        expect(widgetRefreshedCount, 0);
+      });
+    });
+
+    group('removeAll', () {
+      test('can remove multiple overlays at once', () {
+        final overlays = FlameGame().overlays;
+        overlays.addAll(['test', 'test2']);
+
+        overlays.removeAll(['test', 'test2']);
+
+        expect(overlays.isActive('test'), false);
+        expect(overlays.isActive('test2'), false);
+      });
+
+      test('only refreshes gameWidget once if removed', () {
+        final game = FlameGame();
+        var widgetRefreshedCount = 0;
+        game.overlays.addAll(['test', 'test2']);
+        game.addGameStateListener(() => widgetRefreshedCount++);
+
+        game.overlays.removeAll(['test', 'test2']);
+
+        expect(widgetRefreshedCount, 1);
+      });
+
+      test('does not refreshes gameWidget when not changed', () {
+        final game = FlameGame();
+        var widgetRefreshedCount = 0;
+        game.overlays.removeAll(['test', 'test2']);
+        game.addGameStateListener(() => widgetRefreshedCount++);
+
+        game.overlays.removeAll(['test', 'test2']);
+
+        expect(widgetRefreshedCount, 0);
+      });
+    });
+
     group('remove', () {
       test('can remove an overlay', () {
         final overlays = FlameGame().overlays;
