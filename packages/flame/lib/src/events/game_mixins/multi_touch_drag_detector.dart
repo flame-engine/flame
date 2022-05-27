e9fb51902d4e823145d758bb6bd6af5ac4568111
Pasha Stetsenko <stpasha@google.com>
2022-05-27 16:29:11 -0700

    docs for MultiTouchDragDetector

--- packages/flame/lib/src/events/game_mixins/multi_touch_drag_detector.dart
+++ packages/flame/lib/src/events/game_mixins/multi_touch_drag_detector.dart
@@ -1,8 +1,22 @@
 import 'package:flame/src/events/interfaces/multi_drag_listener.dart';
 import 'package:flame/src/game/mixins/game.dart';
+import 'package:flame/src/game/mixins/has_draggables.dart';
 import 'package:flame/src/gestures/events.dart';
 import 'package:flutter/gestures.dart';
 
+/// Mixin that can be added to a [Game] allowing it to receive drag events.
+///
+/// The user can override one of the callback methods
+///  - [onDragStart]
+///  - [onDragUpdate]
+///  - [onDragEnd]
+///  - [onDragCancel]
+/// in order to respond to each corresponding event. Those events whose methods
+/// are not overridden are ignored.
+///
+/// See [MultiDragGestureRecognizer] for the description of each individual
+/// event. If your game is derived from the FlameGame class, consider using the
+/// [HasDraggables] mixin instead.
 mixin MultiTouchDragDetector on Game implements MultiDragListener {
   void onDragStart(int pointerId, DragStartInfo info) {}
   void onDragUpdate(int pointerId, DragUpdateInfo info) {}
