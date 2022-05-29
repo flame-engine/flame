7c1f6c46043ec3b4f2ce9575cb9a7e4b80f40b37
Pasha Stetsenko <stpasha@google.com>
2022-05-29 15:16:03 -0700

    try to update a test

--- packages/flame/test/components/text_box_component_test.dart
+++ packages/flame/test/components/text_box_component_test.dart
@@ -79,40 +79,40 @@ void main() {
         game.addAll([
           _FramedTextBox(
             text: 'I strike quickly, being moved.',
-            position: Vector2(10, 10),
+            position: Vector2(10.5, 10.5),
             size: Vector2(390, 100),
             align: Anchor.topLeft,
           ),
           _FramedTextBox(
             text: 'But thou art not quickly moved to strike.',
-            position: Vector2(10, 120),
+            position: Vector2(10.5, 120.5),
             size: Vector2(390, 115),
             align: Anchor.topCenter,
           ),
           _FramedTextBox(
             text: 'A dog of the house of Montague moves me.',
-            position: Vector2(10, 245),
+            position: Vector2(10.5, 245.5),
             size: Vector2(390, 115),
             align: Anchor.topRight,
           ),
           _FramedTextBox(
             text: 'To move is to stir, and to be valiant is to stand. '
                 'Therefore, if thou art moved, thou runn‘st away.',
-            position: Vector2(10, 370),
+            position: Vector2(10.5, 370.5),
             size: Vector2(390, 225),
             align: Anchor.bottomRight,
           ),
           _FramedTextBox(
             text: 'A dog of that house shall move me to stand. I will take '
                 'the wall of any man or maid of Montague‘s.',
-            position: Vector2(410, 10),
+            position: Vector2(410.5, 10.5),
             size: Vector2(380, 300),
             align: Anchor.center,
           ),
           _FramedTextBox(
             text: 'That shows thee a weak slave; for the weakest goes to the '
                 'wall.',
-            position: Vector2(410, 320),
+            position: Vector2(410.5, 320.5),
             size: Vector2(380, 270),
             align: Anchor.centerRight,
           ),
