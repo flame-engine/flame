c833b3f25a5e0598a5cbb6ed6fb1166ecbcfed7a
Pasha Stetsenko <stpasha@google.com>
2022-05-31 11:18:28 -0700

    space text element

--- packages/flame/lib/src/text/inline/space_text_element.dart
+++ packages/flame/lib/src/text/inline/space_text_element.dart
@@ -4,25 +4,59 @@ import 'package:flame/src/text/inline/text_element.dart';
 import 'package:flame/src/text/line_metrics.dart';
 import 'package:flame/src/text/text_line.dart';
 
-class SpaceTextElement extends TextElement {
-  @override
-  bool get isLaidOut => throw UnimplementedError();
+/// Text element representing a single space between words.
+///
+/// The space is treated specially in several regards:
+/// - it doesn't render anything;
+/// - the space is allowed to "overshoot" the right boundary during layout;
+/// - the space can be stretched when justifying text.
+class SpaceTextElement extends TextElement implements TextLine {
+  SpaceTextElement({
+    required double width,
+    required double height,
+    required double baseline,
+  }) : metrics = LineMetrics(
+    left: 0,
+    right: width,
+    top: -baseline,
+    bottom: height - baseline,
+    baseline: 0,
+  );
 
   @override
-  TextLine get lastLine => throw UnimplementedError();
+  final LineMetrics metrics;
+
+  @override
+  bool get isLaidOut => true;
+
+  @override
+  TextLine get lastLine => this;
+
+  @override
+  Iterable<TextLine> get lines => [this];
 
   @override
   LayoutResult layOutNextLine(LineMetrics bounds) {
-    throw UnimplementedError();
+    assert(metrics.left == 0 && metrics.baseline == 0);
+    final x0 = bounds.left;
+    final y0 = bounds.baseline;
+    metrics.left = x0;
+    metrics.right += x0;
+    metrics.baseline = y0;
+    metrics.top += y0;
+    metrics.bottom += y0;
+    return LayoutResult.done;
   }
-
-  @override
-  Iterable<TextLine> get lines => throw UnimplementedError();
 
   @override
   void render(Canvas canvas) {}
 
   @override
   void resetLayout() {
+    metrics.right -= metrics.left;
+    metrics.top -= metrics.baseline;
+    metrics.bottom -= metrics.baseline;
+    metrics.left = 0;
+    metrics.baseline = 0;
   }
 }
