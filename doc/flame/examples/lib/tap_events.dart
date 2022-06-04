82f5c0df4d82f68f9d18109265162c8d937c1a41
Pasha Stetsenko <stpasha@google.com>
2022-06-04 01:19:40 -0700

    docs wip

--- doc/flame/examples/lib/tap_events.dart
+++ doc/flame/examples/lib/tap_events.dart
@@ -1,8 +1,114 @@
+import 'dart:math';
 import 'dart:ui';
 
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
 
-class TapEventsGame extends FlameGame {
+class TapEventsGame extends FlameGame with HasTappableComponents {
   @override
-  Color backgroundColor() => Color(0xFFccddff);
+  Future<void> onLoad() async {
+    add(TapTarget());
+  }
+}
+
+class TapTarget extends PositionComponent with TapCallbacks {
+  TapTarget() : super(anchor: Anchor.center);
+
+  late final game = findGame()!;
+  late Rect _rect;
+  late Paint _paint = Paint()..color = const Color(0x448BA8FF);
+  ExpandingCircle? _currentCircle;
+
+  @override
+  void onGameResize(Vector2 canvasSize) {
+    super.onGameResize(canvasSize);
+    size = canvasSize - Vector2(100, 75);
+    position = canvasSize / 2;
+    _rect = size.toRect();
+  }
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(_rect, _paint);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    add(_currentCircle = ExpandingCircle(event.localPosition));
+  }
+
+  @override
+  void onLongTapDown(TapDownEvent event) {
+    _currentCircle!.accent();
+  }
+
+  @override
+  void onTapUp(TapUpEvent event) {
+    _currentCircle!.release();
+    _currentCircle = null;
+  }
+
+  @override
+  void onTapCancel(TapCancelEvent event) {
+    _currentCircle!.cancel();
+    _currentCircle = null;
+  }
+}
+
+class ExpandingCircle extends Component {
+  ExpandingCircle(this._center)
+      : _baseColor =
+            HSLColor.fromAHSL(1, random.nextDouble() * 360, 1, 0.8).toColor();
+
+  final Color _baseColor;
+  final Vector2 _center;
+  double _outerRadius = 0;
+  double _innerRadius = 0;
+  double _accentRadius = -double.infinity;
+  bool _released = false;
+  bool _cancelled = false;
+  late final Paint _paint = Paint()
+    ..style = PaintingStyle.stroke
+    ..color = _baseColor;
+  late final Paint _accentPaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 0
+    ..color = const Color(0xFFFFFFFF);
+
+  static const maxRadius = 150;
+  static Random random = Random();
+
+  double get radius => (_innerRadius + _outerRadius) / 2;
+
+  void release() => _released = true;
+  void cancel() => _cancelled = true;
+  void accent() => _accentRadius = 0;
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawCircle(_center.toOffset(), radius, _paint);
+    if (_accentRadius >= 0) {
+      canvas.drawCircle(_center.toOffset(), _accentRadius, _accentPaint);
+    }
+  }
+
+  @override
+  void update(double dt) {
+    if (_cancelled) {
+      _innerRadius += dt * 50;
+    } else {
+      _outerRadius += dt * 10;
+      _innerRadius += dt * (_released? 10 : 5);
+      _accentRadius += dt * 10;
+    }
+    if (radius >= maxRadius || _innerRadius > _outerRadius) {
+      removeFromParent();
+    } else {
+      final opacity = 1 - radius / maxRadius;
+      _paint.color = _baseColor.withOpacity(opacity);
+      _paint.strokeWidth = _outerRadius - _innerRadius;
+    }
+  }
 }
--- doc/flame/inputs/tap-events.md
+++ doc/flame/inputs/tap-events.md
@@ -5,6 +5,58 @@ This document describes a new experimental API. The more traditional approach
 for handling tap events is described in [](gesture-input.md).
 ```
 
+Tap events are one of the most basic methods of interaction with a Flame game. These events occur
+when the user touches the screen with a finger, or clicks with a mouse, or taps with a stylus.
+
+Multiple tap events can occur at the same time, especially if the user has multiple fingers. Such
+cases will be handled correctly by Flame, and you can even keep track of the events by using the
+events' `pointerId` property.
+
+It takes only a few simple steps to enable these events for your game:
+
+1.  Add the `HasTappableComponents` mixin to your main game class.
+    - This mixin works "as-is", and doesn't need to be configured further.
+2.  For those components that you want to respond to taps, add the `TapCallbacks` mixin.
+    - This mixin adds four overridable methods to your component: `onTapDown`, `onTapUp`,
+      `onTapCancel`, and `onLongTapDown`. By default, each of these methods does nothing, they need
+      to be overridden in order to perform any function.
+    - In addition, the component must implement the `containsLocalPoint()` method -- this method
+      allows Flame to know whether the event occurred within the component or not.
+
+
+## Tap anatomy
+
+### onTapDown
+
+Every tap begins with a "tap down" event, which you receive via the `void onTapDown(TapDownEvent)`
+handler. The event is delivered to the first component located at the point of touch that has the
+`TapCallbacks` mixin. Normally, the event then stops propagation. However, you can force the event
+to also be delivered to the components below by setting `event.continuePropagation` to true.
+
+The `TapDownEvent` object that is passed to the event handler, contains the available information
+about the event. For example, `event.localPosition` will contain the coordinate of the event in the
+current component's local coordinate system, whereas `event.canvasPosition` is in the coordinate
+system of the entire game canvas.
+
+Every component that received an `onTapDown` event will eventually receive either `onTapUp` or
+`onTapCancel` with the same `pointerId`.
+
+
+### onLongTapDown
+
+If the user holds their finger down for some time (as configured by the `.longTapDelay` property
+in `HasTappableComponents`), the "long tap" will be generated. This event invokes the
+`void onLongTapDown(TapDownEvent)` handler on those components that previously received the
+`onTapDown` event.
+
+
+### onTapUp
+
+This event indicates successful completion of the tap sequence. It is guaranteed to only be
+delivered to those components that previously received the `onTapDown` event with the same event id.
+
+
+
 ```{flutter-app}
 :sources: ../flame/examples
 :page: tap_events
