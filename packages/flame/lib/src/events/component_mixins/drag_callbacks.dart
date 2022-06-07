8ecff0c8c0113b1166e18fce62e053189bdd5796
Pasha Stetsenko <stpasha@google.com>
2022-06-06 21:49:51 -0700

    finish DragCallbacks

--- packages/flame/lib/src/events/component_mixins/drag_callbacks.dart
+++ packages/flame/lib/src/events/component_mixins/drag_callbacks.dart
@@ -1,7 +1,23 @@
-
 import 'package:flame/src/components/component.dart';
+import 'package:flame/src/events/flame_game_mixins/has_draggable_components.dart';
+import 'package:flame/src/events/messages/drag_cancel_event.dart';
+import 'package:flame/src/events/messages/drag_end_event.dart';
 import 'package:flame/src/events/messages/drag_start_event.dart';
+import 'package:flame/src/events/messages/drag_update_event.dart';
 
 mixin DragCallbacks on Component {
   void onDragStart(DragStartEvent event);
+  void onDragUpdate(DragUpdateEvent event);
+  void onDragEnd(DragEndEvent event);
+  void onDragCancel(DragCancelEvent event);
+
+  @override
+  void onMount() {
+    super.onMount();
+    assert(
+      findGame()! is HasDraggableComponents,
+      'The components with DragCallbacks can only be added to a FlameGame with '
+      'the HasDraggableComponents mixin',
+    );
+  }
 }
