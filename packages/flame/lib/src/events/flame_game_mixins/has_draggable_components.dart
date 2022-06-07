7743ec6bf1f86cb846fa9b192548b8cef5db2b99
Pasha Stetsenko <stpasha@google.com>
2022-06-06 22:52:21 -0700

    finish onDragUpdate, onDragEnd and onDragCancel

--- packages/flame/lib/src/events/flame_game_mixins/has_draggable_components.dart
+++ packages/flame/lib/src/events/flame_game_mixins/has_draggable_components.dart
@@ -4,29 +4,70 @@ import 'package:flame/src/events/messages/drag_cancel_event.dart';
 import 'package:flame/src/events/messages/drag_end_event.dart';
 import 'package:flame/src/events/messages/drag_start_event.dart';
 import 'package:flame/src/events/messages/drag_update_event.dart';
+import 'package:flame/src/events/tagged_component.dart';
 import 'package:flame/src/game/flame_game.dart';
 import 'package:flutter/gestures.dart';
 import 'package:meta/meta.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 mixin HasDraggableComponents on FlameGame implements MultiDragListener {
+  /// The record of all components currently being touched.
+  final Set<TaggedComponent<DragCallbacks>> _records = {};
+
   @mustCallSuper
   void onDragStart(DragStartEvent event) {
     event.deliverAtPoint(
       rootComponent: this,
       eventHandler: (DragCallbacks component) {
+        _records.add(TaggedComponent(event.pointerId, component));
         component.onDragStart(event);
       },
     );
   }
 
   @mustCallSuper
-  void onDragUpdate(DragUpdateEvent event) {}
+  void onDragUpdate(DragUpdateEvent event) {
+    final updated = <TaggedComponent<DragCallbacks>>{};
+    event.deliverAtPoint(
+      rootComponent: this,
+      eventHandler: (DragCallbacks component) {
+        final record = TaggedComponent(event.pointerId, component);
+        if (_records.contains(record)) {
+          component.onDragUpdate(event);
+          updated.add(record);
+        }
+      },
+    );
+    event.renderingTrace.add(Vector2(double.nan, double.nan));
+    for (final record in _records) {
+      if (record.pointerId == event.pointerId && !updated.contains(record)) {
+        record.component.onDragUpdate(event);
+      }
+    }
+    event.renderingTrace.removeLast();
+  }
 
   @mustCallSuper
-  void onDragEnd(DragEndEvent event) {}
+  void onDragEnd(DragEndEvent event) {
+    _records.removeWhere((record) {
+      if (record.pointerId == event.pointerId) {
+        record.component.onDragEnd(event);
+        return true;
+      }
+      return false;
+    });
+  }
 
   @mustCallSuper
-  void onDragCancel(DragCancelEvent event) {}
+  void onDragCancel(DragCancelEvent event) {
+    _records.removeWhere((record) {
+      if (record.pointerId == event.pointerId) {
+        record.component.onDragCancel(event);
+        return true;
+      }
+      return false;
+    });
+  }
 
   //#region MultiDragListener API
 
