21705287461ea4621f91a88f48e0d48bbca3a8f3
Pasha Stetsenko <stpasha@google.com>
2022-06-07 18:49:34 -0700

    docs for MultiDragListener

--- packages/flame/lib/src/events/interfaces/multi_drag_listener.dart
+++ packages/flame/lib/src/events/interfaces/multi_drag_listener.dart
@@ -1,5 +1,6 @@
+import 'package:flame/src/events/flame_game_mixins/has_draggable_components.dart';
 import 'package:flame/src/events/game_mixins/multi_touch_drag_detector.dart';
-import 'package:flame/src/game/mixins/has_draggables.dart';
+import 'package:flame/src/events/interfaces/multi_tap_listener.dart';
 import 'package:flutter/gestures.dart';
 
 /// Interface that must be implemented by a game in order for it to be eligible
@@ -7,11 +8,30 @@ import 'package:flutter/gestures.dart';
 ///
 /// Instead of implementing this class directly consider using one of the
 /// prebuilt mixins:
-///  - [HasDraggables] for a `FlameGame`
+///  - [HasDraggableComponents] for a `FlameGame`
 ///  - [MultiTouchDragDetector] for a custom `Game`
 abstract class MultiDragListener {
+  /// The beginning of a drag operation.
+  ///
+  /// If the game is not listening to tap events, this event will occur as soon
+  /// as the user touches the screen. If the game uses both a [MultiTapListener]
+  /// and a [MultiDragListener] simultaneously, then this event will fire once
+  /// the user moves their finger away from the point of the initial touch.
   void handleDragStart(int pointerId, DragStartDetails details);
+
+  /// The pointer that was touching the screen has moved.
+  ///
+  /// This event occurs frequently during the drag, allowing you to keep track
+  /// of the position of the point of touch as it moves. This event will only
+  /// fire when the point of touch moves, and not when it stays still.
   void handleDragUpdate(int pointerId, DragUpdateDetails details);
+
+  /// Marks the end of a drag operation.
+  ///
+  /// This event fires when the pointer stops touching the screen. It will fire
+  /// even if the pointer is currently outside of the game widget.
   void handleDragEnd(int pointerId, DragEndDetails details);
+
+  /// (unknown)
   void handleDragCancel(int pointerId);
 }
