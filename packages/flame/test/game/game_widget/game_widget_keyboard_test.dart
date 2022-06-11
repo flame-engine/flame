68e53a08790318d29f577e5e73ad7108d9fd89cd
Pasha Stetsenko <stpasha@google.com>
2022-06-11 15:30:00 -0700

    refactor tests

--- packages/flame/test/game/game_widget/game_widget_keyboard_test.dart
+++ packages/flame/test/game/game_widget/game_widget_keyboard_test.dart
@@ -8,15 +8,12 @@ import 'package:flutter_test/flutter_test.dart';
 class _KeyboardEventsGame extends FlameGame with KeyboardEvents {
   final List<String> keysPressed = [];
 
-  _KeyboardEventsGame();
-
   @override
   KeyEventResult onKeyEvent(
     RawKeyEvent event,
     Set<LogicalKeyboardKey> keysPressed,
   ) {
     this.keysPressed.add(event.character ?? 'none');
-
     return KeyEventResult.handled;
   }
 }
@@ -33,78 +30,27 @@ class _KeyboardHandlerComponent extends Component with KeyboardHandler {
 
 class _HasKeyboardHandlerComponentsGame extends FlameGame
     with HasKeyboardHandlerComponents {
-  _HasKeyboardHandlerComponentsGame();
-
-  late _KeyboardHandlerComponent keyboardHandler;
-
-  @override
-  Future<void> onLoad() async {
-    keyboardHandler = _KeyboardHandlerComponent();
-    add(keyboardHandler);
-  }
-}
-
-class _GamePage extends StatelessWidget {
-  final Widget child;
-
-  const _GamePage({required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Positioned.fill(
-              child: child,
-            ),
-            Positioned(
-              top: 0,
-              right: 0,
-              child: ElevatedButton(
-                child: const Text('Back'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  _HasKeyboardHandlerComponentsGame({super.children});
 }
 
 void main() {
-  final size = Vector2(1.0, 1.0);
-
   testWidgets('adds focus', (tester) async {
     final focusNode = FocusNode();
-
-    final game = _KeyboardEventsGame();
-
     await tester.pumpWidget(
-      _GamePage(
-        child: GameWidget(
-          game: game,
-          focusNode: focusNode,
-        ),
+      GameWidget(
+        game: _KeyboardEventsGame(),
+        focusNode: focusNode,
       ),
     );
+    await tester.pump();
 
     expect(focusNode.hasFocus, true);
   });
 
   testWidgets('game with KeyboardEvents receives keys', (tester) async {
     final game = _KeyboardEventsGame();
-
-    await tester.pumpWidget(
-      _GamePage(
-        child: GameWidget(
-          game: game,
-        ),
-      ),
-    );
+    await tester.pumpWidget(GameWidget(game: game));
+    await tester.pump();
 
     await tester.sendKeyDownEvent(LogicalKeyboardKey.keyA);
     await tester.sendKeyDownEvent(LogicalKeyboardKey.keyB);
@@ -116,26 +62,20 @@ void main() {
   testWidgets(
     'game with HasKeyboardHandlerComponents receives keys',
     (tester) async {
-      final game = _HasKeyboardHandlerComponentsGame();
-
-      await tester.pumpWidget(
-        _GamePage(
-          child: GameWidget(
-            game: game,
-          ),
-        ),
+      final keyboardHandler = _KeyboardHandlerComponent();
+      final game = _HasKeyboardHandlerComponentsGame(
+        children: [keyboardHandler],
       );
 
-      game.onGameResize(size);
-      game.update(0.1);
-
+      await tester.pumpWidget(GameWidget(game: game));
+      await tester.pump();
       await tester.pump();
 
       await tester.sendKeyDownEvent(LogicalKeyboardKey.keyZ);
       await tester.sendKeyDownEvent(LogicalKeyboardKey.keyF);
       await tester.sendKeyDownEvent(LogicalKeyboardKey.keyI);
 
-      expect(game.keyboardHandler.keysPressed, ['z', 'f', 'i']);
+      expect(keyboardHandler.keysPressed, ['z', 'f', 'i']);
     },
   );
 }
