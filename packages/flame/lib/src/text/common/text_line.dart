92b40603d1bf4f537fe791062d71aa2803b6205d
Pasha Stetsenko <stpasha@google.com>
2022-06-10 20:16:04 -0700

    TextLine

---
+++ packages/flame/lib/src/text/common/text_line.dart
@@ -0,0 +1,15 @@
+import 'package:flame/src/text/common/line_metrics.dart';
+import 'package:flame/src/text/inline/text_element.dart';
+
+/// [TextLine] is an abstract class describing a single line (or a fragment of
+/// a line) of a laid-out text.
+///
+/// More specifically, after any [TextElement] has been laid out, its layout
+/// will be described by one or more [TextLine]s.
+abstract class TextLine {
+  /// The dimensions of this line.
+  LineMetrics get metrics;
+
+  /// Move the text within this [TextLine] by the specified offsets [dx], [dy].
+  void translate(double dx, double dy);
+}
