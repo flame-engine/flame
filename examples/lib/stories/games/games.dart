c61756814ee66f3c1b33f0b878274796ea471f71
Lukas Klingsbo <me@lukas.fyi>
2022-06-20 20:16:21 +0200

    Fix formatting

--- examples/lib/stories/games/games.dart
+++ examples/lib/stories/games/games.dart
@@ -5,7 +5,8 @@ import 'package:padracing/padracing_widget.dart';
 import 'package:trex_game/trex_game.dart';
 import 'package:trex_game/trex_widget.dart';
 
-String gamesLink(String game) => 'https://github.com/flame-engine/flame/blob/main/examples/games/$game';
+String gamesLink(String game) =>
+    'https://github.com/flame-engine/flame/blob/main/examples/games/$game';
 
 void addGameStories(Dashbook dashbook) {
   dashbook.storiesOf('Sample Games')
