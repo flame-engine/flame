72e317f898deca52bc7c637ca88d9f11904cd5dc
Lukas Klingsbo <me@lukas.fyi>
2022-06-22 19:46:47 +0200

    fix: Fix broken link for example games code (#1747)

--- examples/lib/stories/games/games.dart
+++ examples/lib/stories/games/games.dart
@@ -1,22 +1,24 @@
 import 'package:dashbook/dashbook.dart';
-import 'package:examples/commons/commons.dart';
 import 'package:padracing/padracing_game.dart';
 import 'package:padracing/padracing_widget.dart';
 import 'package:trex_game/trex_game.dart';
 import 'package:trex_game/trex_widget.dart';
+
+String gamesLink(String game) =>
+    'https://github.com/flame-engine/flame/blob/main/examples/games/$game';
 
 void addGameStories(Dashbook dashbook) {
   dashbook.storiesOf('Sample Games')
     ..add(
       'Padracing',
       (_) => const PadracingWidget(),
-      codeLink: baseLink('games/padracing'),
+      codeLink: gamesLink('padracing'),
       info: PadRacingGame.description,
     )
     ..add(
       'T-Rex',
       (_) => const TRexWidget(),
-      codeLink: baseLink('games/trex'),
+      codeLink: gamesLink('trex'),
       info: TRexGame.description,
     );
 }
