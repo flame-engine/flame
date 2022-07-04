0eadcecc6cbd2ceb4e76b3f7f1ddd1b0c4ed00ff
Pasha Stetsenko <stpasha@google.com>
2022-07-03 23:51:21 -0700

    more tests

--- packages/flame/test/components/navigator_component_test.dart
+++ packages/flame/test/components/navigator_component_test.dart
@@ -17,6 +17,7 @@ void main() {
       game.add(navigator);
       await game.ready();
 
+      expect(navigator.routes.length, 3);
       expect(navigator.currentRoute.name, 'A');
       expect(navigator.currentRoute.children.length, 1);
       expect(navigator.currentRoute.children.first, isA<_ComponentA>());
@@ -26,16 +27,20 @@ void main() {
       expect(navigator.currentRoute.name, 'B');
       expect(navigator.currentRoute.children.length, 1);
       expect(navigator.currentRoute.children.first, isA<_ComponentB>());
+      expect(navigator.stack.length, 2);
 
       navigator.pop();
       await game.ready();
       expect(navigator.currentRoute.name, 'A');
+      expect(navigator.stack.length, 1);
 
       navigator.pushRoute(Route(builder: _ComponentD.new), name: 'Dee');
       await game.ready();
+      expect(navigator.routes.length, 4);
       expect(navigator.currentRoute.name, 'Dee');
       expect(navigator.currentRoute.children.length, 1);
       expect(navigator.currentRoute.children.first, isA<_ComponentD>());
+      expect(navigator.stack.length, 2);
     });
 
     testWithFlameGame('Route factories', (game) async {
@@ -61,6 +66,37 @@ void main() {
       await game.ready();
       expect(navigator.currentRoute.name, 'b/something');
       expect(navigator.currentRoute.children.first, isA<_ComponentB>());
+    });
+
+    testWithFlameGame('push an existing route', (game) async {
+      final navigator = NavigatorComponent(
+        routes: {
+          'A': Route(builder: _ComponentA.new),
+          'B': Route(builder: _ComponentB.new),
+          'C': Route(builder: _ComponentC.new),
+        },
+        initialRoute: 'A',
+      )..addToParent(game);
+      await game.ready();
+
+      navigator.pushNamed('A');
+      await game.ready();
+      expect(navigator.stack.length, 1);
+
+      navigator.pushNamed('B');
+      navigator.pushNamed('C');
+      expect(navigator.stack.length, 3);
+
+      navigator.pushNamed('B');
+      expect(navigator.stack.length, 3);
+      navigator.pushNamed('A');
+      expect(navigator.stack.length, 3);
+
+      await game.ready();
+      expect(navigator.children.length, 3);
+      expect((navigator.children.elementAt(0) as Route).name, 'C');
+      expect((navigator.children.elementAt(1) as Route).name, 'B');
+      expect((navigator.children.elementAt(2) as Route).name, 'A');
     });
 
     testWithFlameGame('onUnknownRoute', (game) async {
@@ -94,6 +130,19 @@ void main() {
                     'Route "hello" could not be resolved by the Navigator',
           ),
         ),
+      );
+    });
+
+    testWithFlameGame('cannot pop last remaining route', (game) async {
+      final navigator = NavigatorComponent(
+        initialRoute: 'home',
+        routes: {'home': Route(builder: _ComponentA.new)},
+      )..addToParent(game);
+      await game.ready();
+
+      expect(
+        navigator.pop,
+        failsAssert('Cannot pop the last route from the Navigator'),
       );
     });
   });
