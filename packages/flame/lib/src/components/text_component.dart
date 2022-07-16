fb26b790a7882caa6882394408989295c1799ea8
Pasha Stetsenko <stpasha@google.com>
2022-07-16 16:32:18 -0700

    minor code cleanup

--- packages/flame/lib/src/components/text_component.dart
+++ packages/flame/lib/src/components/text_component.dart
@@ -1,30 +1,12 @@
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/src/text/inline/text_element.dart';
 import 'package:flame/src/text/text_renderer.dart';
 import 'package:flutter/painting.dart';
 import 'package:meta/meta.dart';
 
 class TextComponent<T extends TextRenderer> extends PositionComponent {
-  String _text;
-  T _textRenderer;
-
-  String get text => _text;
-
-  set text(String text) {
-    if (_text != text) {
-      _text = text;
-      updateBounds();
-    }
-  }
-
-  T get textRenderer => _textRenderer;
-
-  set textRenderer(T textRenderer) {
-    _textRenderer = textRenderer;
-    updateBounds();
-  }
-
   TextComponent({
     String? text,
     T? textRenderer,
@@ -39,6 +21,24 @@ class TextComponent<T extends TextRenderer> extends PositionComponent {
         _textRenderer = textRenderer ?? TextRenderer.createDefault<T>() {
     updateBounds();
   }
+
+  String get text => _text;
+  String _text;
+  set text(String text) {
+    if (_text != text) {
+      _text = text;
+      updateBounds();
+    }
+  }
+
+  T get textRenderer => _textRenderer;
+  T _textRenderer;
+  set textRenderer(T textRenderer) {
+    _textRenderer = textRenderer;
+    updateBounds();
+  }
+
+  TextElement? _textElement;
 
   @internal
   void updateBounds() {
