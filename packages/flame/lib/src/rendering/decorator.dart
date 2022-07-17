f258849a3ada1825242e84ceb0e34cf86471fefd
Pasha Stetsenko <stpasha@google.com>
2022-07-17 15:05:12 -0700

    copied Decorator class

---
+++ packages/flame/lib/src/rendering/decorator.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+/// [Decorator] is an abstract class that encapsulates a particular visual
+/// effect that should apply to drawing commands wrapped by this class.
+///
+/// The simplest way to apply a [Decorator] to a component is to override its
+/// `renderTree` method like this:
+/// ```dart
+/// @override
+/// void renderTree(Canvas canvas) {
+///   decorator.apply(super.renderTree, canvas);
+/// }
+/// ```
+///
+/// The following implementations are available:
+abstract class Decorator {
+  /// Applies visual effect while [draw]ing on the [canvas].
+  ///
+  /// A no-op decorator would simply call `draw(canvas)`. Any other non-trivial
+  /// decorator can transform the canvas before drawing, or perform any other
+  /// adjustment.
+  void apply(void Function(Canvas) draw, Canvas canvas);
+}
