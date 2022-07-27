63f27d6cef11bb02ecab8ad52f06bd661e717c9c
immadisairaj <immadirajendra.sai@gmail.com>
2022-07-28 00:22:55 +0530

    modify methods to reduce lines and make it simple
    - remove unnecessary comments
    - change the method expandFirstOnHome to check and expand in one line
    - remove unnecessary methods

--- doc/_sphinx/scripts/menu-expand.js
+++ doc/_sphinx/scripts/menu-expand.js
@@ -1,6 +1,5 @@
 // Auto expand the first expandable node ("flame") when loaded.
 window.addEventListener('load', (_event) => {
-    // expand first node when on home page
     expandFirstOnHome();
 });
 
@@ -9,61 +8,14 @@ window.addEventListener('load', (_event) => {
  * 
  * If the current page is not the home page, this method does nothing.
  */
-// Two scenarios are possible here:
-// 1. docs.flame-engine.org/ host
-//  - the pathname for homepage can be /{version}/index.html or /{version}/
-// 2. local host
-// - the pathname for homepage will end with /html/index.html
+// When the path name ends with index.html or an empty string, it is home page.
 function expandFirstOnHome() {
     const parts = location.pathname.split('/');
-    const partsLength = parts.length;
-    let canExpandFirst = false;
-    if (location.host === 'docs.flame-engine.org') {
-        if (partsLength == 3 && parts[0] === '' && (parts[2] === '' || parts[2] === 'index.html')) {
-            canExpandFirst = true;
-        }
-    } else {
-        // local
-        if (parts[partsLength-2] == 'html' && parts[partsLength-1] == 'index.html') {
-            canExpandFirst = true;
-        }
-    }
+    const lastPart = parts[parts.length - 1];
+    const isHomePage = (lastPart == '') || (lastPart == 'index.html');
 
-    if (canExpandFirst) {
+    if (isHomePage) {
         // expand the first expandable node in the toctree
-        var menu = document.querySelector(".toctree-l1")
-        expand(menu);
+        $('li.toctree-l1').has('ul').first().addClass('current');
     }
-}
-
-/**
- * Given a Node, if is expandable, it expands unless it is already expanded.
- * 
- * @param {Node} node 
- */
-function expand(node) {
-    if (is_expandable(node) && !is_expanded(node)) {
-        node.classList.add("current");
-    }
-}
-
-/**
- * Returns whether or not the given node is an expandable list.
- * 
- * @param {Node} node 
- * @returns {boolean} true if the node is a toctree that can be expanded, false otherwise.
- */
-function is_expandable(node) {
-    return node.querySelectorAll("li").length > 0
-}
-
-/**
- * Returns whether or not the given expandable node is already expanded.
- * Nodes are considered expandaded if they are 'current'ly selected, so we take advantage of this.
- * 
- * @param {Node} node 
- * @returns {boolean} true if the node is already expanded, false otherwise.
- */
-function is_expanded(node) {
-    return node.classList.contains("current")
 }
