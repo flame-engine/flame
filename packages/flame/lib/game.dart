f1b276e020c6f80a18764e63ffbea21abb52b1f2
Pasha Stetsenko <stpasha@google.com>
2022-08-18 15:24:48 -0700

    feat: Added routes that can return a value (#1848)
    This PR introduces class ValueRoute<T> that can be used together with the RouterComponent in order to create routes that return a value. The most common example of such routes are dialog boxes.

--- packages/flame/lib/game.dart
+++ packages/flame/lib/game.dart
@@ -3,6 +3,7 @@
 export 'src/collisions/has_collision_detection.dart';
 export 'src/components/route.dart' show Route;
 export 'src/components/router_component.dart' show RouterComponent;
+export 'src/components/value_route.dart' show ValueRoute;
 export 'src/extensions/vector2.dart';
 export 'src/game/camera/camera.dart';
 export 'src/game/camera/viewport.dart';
