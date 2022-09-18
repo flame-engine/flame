94a628589981130b4755247cba44acf21a1668e5
Pasha Stetsenko <stpasha@google.com>
2022-09-17 21:47:21 -0700

    GroupTextNode layout

--- packages/flame/lib/src/text/nodes/group_text_node.dart
+++ packages/flame/lib/src/text/nodes/group_text_node.dart
@@ -1,4 +1,5 @@
-
+import 'package:flame/src/text/elements/group_text_element.dart';
+import 'package:flame/src/text/elements/text_element.dart';
 import 'package:flame/src/text/nodes/text_node.dart';
 import 'package:flame/src/text/styles/document_style.dart';
 import 'package:flame/src/text/styles/text_style.dart';
@@ -14,5 +15,50 @@ class GroupTextNode extends TextNode {
   }
 
   @override
-  TextNodeLayoutBuilder get layoutBuilder => throw UnimplementedError();
+  TextNodeLayoutBuilder get layoutBuilder => _GroupTextLayoutBuilder(this);
+}
+
+class _GroupTextLayoutBuilder extends TextNodeLayoutBuilder {
+  _GroupTextLayoutBuilder(this.node);
+
+  final GroupTextNode node;
+  int _currentChildIndex = 0;
+  TextNodeLayoutBuilder? _currentChildBuilder;
+
+  @override
+  bool get isDone => _currentChildIndex == node.children.length;
+
+  @override
+  TextElement? layOutNextLine(double availableWidth) {
+    assert(!isDone);
+    final out = <TextElement>[];
+    var usedWidth = 0.0;
+    while (true) {
+      if (_currentChildBuilder?.isDone ?? false) {
+        _currentChildBuilder = null;
+        _currentChildIndex += 1;
+        if (_currentChildIndex == node.children.length) {
+          break;
+        }
+      }
+      _currentChildBuilder ??= node.children[_currentChildIndex].layoutBuilder;
+
+      final maybeLine =
+          _currentChildBuilder!.layOutNextLine(availableWidth - usedWidth);
+      if (maybeLine == null) {
+        break;
+      } else {
+        maybeLine.translate(usedWidth, 0);
+        out.add(maybeLine);
+        usedWidth += maybeLine.metrics.width;
+      }
+    }
+    if (out.isEmpty) {
+      return null;
+    } else {
+      final element = GroupTextElement(out);
+      element.translate(0, element.metrics.ascent);
+      return element;
+    }
+  }
 }
