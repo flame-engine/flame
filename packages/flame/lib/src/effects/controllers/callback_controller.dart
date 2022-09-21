c06ee259fb29a14b23c1518b7e454e2c0ac5a2b7
Valentin Treu <valley@MacBook-Pro-von-Valentin.local>
2022-09-21 21:07:03 +0200

    Update doc in CallbackController

--- packages/flame/lib/src/effects/controllers/callback_controller.dart
+++ packages/flame/lib/src/effects/controllers/callback_controller.dart
@@ -1,10 +1,15 @@
 import 'dart:ui';
 import 'package:flame/effects.dart';
 
-/// The [callback] will be invoked once the effect's [progress] is reached.
-/// 1.0 to match `duration` or 0.0 to match when the `reverseDuration`
-/// has been reached.
+/// This effect controller invokes the [callback] function and then immediately
+/// finishes. Use it as part of a more complex effect to insert callbacks at
+/// certain parts of that effect.
 class CallbackController extends DurationEffectController {
+  /// Creates a controller that invokes the given [callback] function.
+  /// The [progress] parameter specifies the progress level of the effect at the
+  /// time when the callback is invoked. It is the responsibility of the user to
+  /// ensure that this progress level is contiguous with respect to the progress
+  /// of the overall effect.
   CallbackController(this.callback, {required double progress})
       : _progress = progress,
         super(0.0);
