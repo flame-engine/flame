7587b0d37c042312a1661b2884bc064acd0f7f14
Alexey Volkov <asgalex@gmail.com>
2022-09-22 13:21:25 +0300

    feat: support all tests from collision_callback_test.dart

--- packages/flame/test/collisions/collision_callback_test.dart
+++ packages/flame/test/collisions/collision_callback_test.dart
@@ -7,50 +7,49 @@ import 'collision_test_helpers.dart';
 
 void main() {
   group('Collision callbacks', () {
-    testCollisionDetectionGame('collidable callbacks are called', (game) async {
-      final blockA = TestBlock(
-        Vector2.zero(),
-        Vector2.all(10),
-      );
-      final blockB = TestBlock(
-        Vector2.all(1),
-        Vector2.all(10),
-      );
-      await game.ensureAddAll([blockA, blockB]);
-      game.update(0);
-      expect(blockA.collidingWith(blockB), isTrue);
-      expect(blockB.collidingWith(blockA), isTrue);
-      expect(blockA.activeCollisions.length, 1);
-      expect(blockB.activeCollisions.length, 1);
-      expect(blockA.startCounter, 1);
-      expect(blockB.startCounter, 1);
-      expect(blockA.onCollisionCounter, 1);
-      expect(blockB.onCollisionCounter, 1);
-      expect(blockA.endCounter, 0);
-      expect(blockB.endCounter, 0);
-      game.update(0);
-      expect(blockA.activeCollisions.length, 1);
-      expect(blockB.activeCollisions.length, 1);
-      expect(blockA.startCounter, 1);
-      expect(blockB.startCounter, 1);
-      expect(blockA.onCollisionCounter, 2);
-      expect(blockB.onCollisionCounter, 2);
-      blockB.position = Vector2.all(21);
-      expect(blockA.endCounter, 0);
-      expect(blockB.endCounter, 0);
-      game.update(0);
-      expect(blockA.collidingWith(blockB), isFalse);
-      expect(blockB.collidingWith(blockA), isFalse);
-      expect(blockA.activeCollisions.length, 0);
-      expect(blockB.activeCollisions.length, 0);
-      game.update(0);
-      expect(blockA.endCounter, 1);
-      expect(blockB.endCounter, 1);
-    });
-
-    testCollisionDetectionGame(
-      'collidable callbacks are called when removing a Collidable',
-      (game) async {
+    runTestRegistry({
+      'collidable callbacks are called': (game) async {
+        final blockA = TestBlock(
+          Vector2.zero(),
+          Vector2.all(10),
+        );
+        final blockB = TestBlock(
+          Vector2.all(1),
+          Vector2.all(10),
+        );
+        await game.ensureAddAll([blockA, blockB]);
+        game.update(0);
+        expect(blockA.collidingWith(blockB), isTrue);
+        expect(blockB.collidingWith(blockA), isTrue);
+        expect(blockA.activeCollisions.length, 1);
+        expect(blockB.activeCollisions.length, 1);
+        expect(blockA.startCounter, 1);
+        expect(blockB.startCounter, 1);
+        expect(blockA.onCollisionCounter, 1);
+        expect(blockB.onCollisionCounter, 1);
+        expect(blockA.endCounter, 0);
+        expect(blockB.endCounter, 0);
+        game.update(0);
+        expect(blockA.activeCollisions.length, 1);
+        expect(blockB.activeCollisions.length, 1);
+        expect(blockA.startCounter, 1);
+        expect(blockB.startCounter, 1);
+        expect(blockA.onCollisionCounter, 2);
+        expect(blockB.onCollisionCounter, 2);
+        blockB.position = Vector2.all(21);
+        expect(blockA.endCounter, 0);
+        expect(blockB.endCounter, 0);
+        game.update(0);
+        expect(blockA.collidingWith(blockB), isFalse);
+        expect(blockB.collidingWith(blockA), isFalse);
+        expect(blockA.activeCollisions.length, 0);
+        expect(blockB.activeCollisions.length, 0);
+        game.update(0);
+        expect(blockA.endCounter, 1);
+        expect(blockB.endCounter, 1);
+      },
+      'collidable callbacks are called when removing a Collidable':
+          (game) async {
         final blockA = TestBlock(
           Vector2.zero(),
           Vector2.all(10),
@@ -74,50 +73,48 @@ void main() {
         expect(blockA.activeCollisions.length, 0);
         expect(blockB.activeCollisions.length, 0);
       },
-    );
-
-    testCollisionDetectionGame('hitbox callbacks are called', (game) async {
-      final blockA = TestBlock(
-        Vector2.zero(),
-        Vector2.all(10),
-      );
-      final blockB = TestBlock(
-        Vector2.all(1),
-        Vector2.all(10),
-      );
-      final hitboxA = blockA.hitbox;
-      final hitboxB = blockB.hitbox;
-      await game.ensureAddAll([blockA, blockB]);
-      game.update(0);
-      expect(hitboxA.collidingWith(hitboxB), true);
-      expect(hitboxB.collidingWith(hitboxA), true);
-      expect(hitboxA.activeCollisions.length, 1);
-      expect(hitboxB.activeCollisions.length, 1);
-      expect(hitboxA.startCounter, 1);
-      expect(hitboxB.startCounter, 1);
-      expect(hitboxA.onCollisionCounter, 1);
-      expect(hitboxB.onCollisionCounter, 1);
-      game.update(0);
-      expect(hitboxA.startCounter, 1);
-      expect(hitboxB.startCounter, 1);
-      expect(hitboxA.onCollisionCounter, 2);
-      expect(hitboxB.onCollisionCounter, 2);
-      blockB.position = Vector2.all(21);
-      expect(hitboxA.endCounter, 0);
-      expect(hitboxB.endCounter, 0);
-      game.update(0);
-      expect(hitboxA.collidingWith(hitboxB), false);
-      expect(hitboxB.collidingWith(hitboxA), false);
-      expect(hitboxA.activeCollisions.length, 0);
-      expect(hitboxB.activeCollisions.length, 0);
-      expect(hitboxA.endCounter, 1);
-      expect(hitboxB.endCounter, 1);
+      'hitbox callbacks are called': (game) async {
+        final blockA = TestBlock(
+          Vector2.zero(),
+          Vector2.all(10),
+        );
+        final blockB = TestBlock(
+          Vector2.all(1),
+          Vector2.all(10),
+        );
+        final hitboxA = blockA.hitbox;
+        final hitboxB = blockB.hitbox;
+        await game.ensureAddAll([blockA, blockB]);
+        game.update(0);
+        expect(hitboxA.collidingWith(hitboxB), true);
+        expect(hitboxB.collidingWith(hitboxA), true);
+        expect(hitboxA.activeCollisions.length, 1);
+        expect(hitboxB.activeCollisions.length, 1);
+        expect(hitboxA.startCounter, 1);
+        expect(hitboxB.startCounter, 1);
+        expect(hitboxA.onCollisionCounter, 1);
+        expect(hitboxB.onCollisionCounter, 1);
+        game.update(0);
+        expect(hitboxA.startCounter, 1);
+        expect(hitboxB.startCounter, 1);
+        expect(hitboxA.onCollisionCounter, 2);
+        expect(hitboxB.onCollisionCounter, 2);
+        blockB.position = Vector2.all(21);
+        expect(hitboxA.endCounter, 0);
+        expect(hitboxB.endCounter, 0);
+        game.update(0);
+        expect(hitboxA.collidingWith(hitboxB), false);
+        expect(hitboxB.collidingWith(hitboxA), false);
+        expect(hitboxA.activeCollisions.length, 0);
+        expect(hitboxB.activeCollisions.length, 0);
+        expect(hitboxA.endCounter, 1);
+        expect(hitboxB.endCounter, 1);
+      },
     });
   });
 
-  testCollisionDetectionGame(
-    'hitbox callbacks are called when Collidable is removed',
-    (game) async {
+  runTestRegistry({
+    'hitbox callbacks are called when Collidable is removed': (game) async {
       final blockA = TestBlock(
         Vector2.zero(),
         Vector2.all(10),
@@ -143,11 +140,8 @@ void main() {
       expect(hitboxA.endCounter, 1);
       expect(hitboxB.endCounter, 1);
     },
-  );
-
-  testCollisionDetectionGame(
-    'hitbox end callbacks are called when hitbox is moved away fast',
-    (game) async {
+    'hitbox end callbacks are called when hitbox is moved away fast':
+        (game) async {
       final blockA = TestBlock(
         Vector2.zero(),
         Vector2.all(10),
@@ -173,11 +167,8 @@ void main() {
       expect(hitboxA.endCounter, 1);
       expect(hitboxB.endCounter, 1);
     },
-  );
-
-  testCollisionDetectionGame(
-    'onCollisionEnd is only called when there previously was a collision',
-    (game) async {
+    'onCollisionEnd is only called when there previously was a collision':
+        (game) async {
       final blockA = TestBlock(
         Vector2.zero(),
         Vector2.all(10),
@@ -197,11 +188,7 @@ void main() {
       expect(blockA.endCounter, 0);
       expect(blockB.endCounter, 0);
     },
-  );
-
-  testCollisionDetectionGame(
-    'callbacks are only called once for hitboxes on each other',
-    (game) async {
+    'callbacks are only called once for hitboxes on each other': (game) async {
       final blockA = TestBlock(
         Vector2.zero(),
         Vector2.all(10),
@@ -255,11 +242,8 @@ void main() {
       expect(blockA.endCounter, 1);
       expect(blockB.endCounter, 1);
     },
-  );
-
-  testCollisionDetectionGame(
-    'end and start callbacks are only called once for hitboxes sharing a side',
-    (game) async {
+    'end and start callbacks are only called once for hitboxes sharing a side':
+        (game) async {
       final blockA = TestBlock(
         Vector2.all(10),
         Vector2.all(10),
@@ -313,12 +297,9 @@ void main() {
       expect(blockA.endCounter, 1);
       expect(blockB.endCounter, 1);
     },
-  );
 
-  // Reproduced #1478
-  testCollisionDetectionGame(
-    'collision callbacks with many hitboxes added',
-    (game) async {
+    // Reproduced #1478
+    'collision callbacks with many hitboxes added': (game) async {
       const side = 10.0;
       final player = TestBlock(Vector2.zero(), Vector2.all(side))
         ..anchor = Anchor.center;
@@ -376,12 +357,8 @@ void main() {
       expect(player.onCollisionCounter, 3);
       expect(player.endCounter, 0);
     },
-  );
-
-  // Reproduced #1478
-  testCollisionDetectionGame(
-    'collision callbacks with changed game size',
-    (game) async {
+    // Reproduced #1478
+    'collision callbacks with changed game size': (game) async {
       final block = TestBlock(Vector2.all(20), Vector2.all(10))
         ..anchor = Anchor.center;
       await game.ensureAddAll([ScreenHitbox(), block]);
@@ -409,5 +386,5 @@ void main() {
       expect(block.onCollisionCounter, 2);
       expect(block.endCounter, 1);
     },
-  );
+  });
 }
