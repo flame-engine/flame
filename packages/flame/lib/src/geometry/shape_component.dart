205ac561eef4becd90a0d5dca2301b988b15959f
Lukas Klingsbo <me@lukas.fyi>
2022-09-23 17:01:39 +0200

    feat: Add possibility for solid hitboxes (#1919)
    Currently all hitboxes are hollow, this PR adds the possibility for solid hitboxes.
    When a hitbox is fully enclosed by a solid hitbox the inner hitbox's position will be returned as the intersection point.

--- packages/flame/lib/src/geometry/shape_component.dart
+++ packages/flame/lib/src/geometry/shape_component.dart
@@ -7,8 +7,6 @@ import 'package:flame/components.dart';
 /// center.
 /// A point can be determined to be within of outside of a shape.
 abstract class ShapeComponent extends PositionComponent with HasPaint {
-  bool renderShape = true;
-
   ShapeComponent({
     super.position,
     super.size,
@@ -21,4 +19,18 @@ abstract class ShapeComponent extends PositionComponent with HasPaint {
   }) {
     this.paint = paint ?? this.paint;
   }
+
+  bool renderShape = true;
+
+  /// Whether the shape is solid or hollow.
+  ///
+  /// If it is solid, intersections will occur even if the other component is
+  /// fully enclosed by the other hitbox. The intersection point in such cases
+  /// will be the center of the enclosed [ShapeComponent].
+  /// A hollow shape that is fully enclosed by a solid hitbox will cause an
+  /// intersection result, but not the other way around.
+  ///
+  /// This field is not related to how the shape should be rendered, see
+  /// [Paint.style] for that.
+  bool isSolid = false;
 }
