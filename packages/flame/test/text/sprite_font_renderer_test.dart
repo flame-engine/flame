ce0cedee8b8e5b19613c3b58bf5c5d83bef99dd6
Pasha Stetsenko <stpasha@google.com>
2022-10-02 10:46:30 -0700

    more tests

--- packages/flame/test/text/sprite_font_renderer_test.dart
+++ packages/flame/test/text/sprite_font_renderer_test.dart
@@ -118,6 +118,153 @@ void main() {
       goldenFile: '../_goldens/sprite_font_renderer_2.png',
       size: Vector2(200, 140),
     );
+
+    testGolden(
+      'Render text with large src rectangles',
+      (game) async {
+        final font = SpriteFont(
+          source: await loadImage('sprite_font.png'),
+          size: 18,
+          ascent: 14,
+          glyphs: [
+            Glyph(
+              'a',
+              left: 5,
+              top: 1,
+              width: 10,
+              srcLeft: 0,
+              srcTop: 0,
+              srcRight: 20,
+              srcBottom: 20,
+            ),
+            Glyph(
+              'b',
+              left: 26,
+              top: 1,
+              width: 9,
+              srcLeft: 20,
+              srcTop: 0,
+              srcBottom: 20,
+              srcRight: 40,
+            ),
+            Glyph(
+              'c',
+              left: 46,
+              top: 1,
+              width: 9,
+              srcLeft: 40,
+              srcTop: 0,
+              srcBottom: 20,
+              srcRight: 60,
+            ),
+            Glyph(
+              'd',
+              left: 66,
+              top: 1,
+              width: 9,
+              srcLeft: 60,
+              srcTop: 0,
+              srcBottom: 20,
+              srcRight: 80,
+            ),
+            Glyph(
+              'e',
+              left: 86,
+              top: 1,
+              width: 9,
+              srcLeft: 80,
+              srcTop: 0,
+              srcBottom: 20,
+              srcRight: 100,
+            ),
+            Glyph(
+              'f',
+              left: 51,
+              top: 21,
+              width: 6,
+              srcLeft: 46,
+              srcTop: 20,
+              srcBottom: 40,
+              srcRight: 61,
+            ),
+            Glyph(
+              'g',
+              left: 86,
+              top: 21,
+              width: 9,
+              srcLeft: 80,
+              srcTop: 20,
+              srcBottom: 40,
+              srcRight: 100,
+            ),
+          ],
+        );
+        game.addAll([
+          RectangleComponent(
+            size: Vector2(200, 200),
+            paint: Paint()..color = const Color(0xffd8e1c4),
+          ),
+          TextComponent(
+            text: 'badface',
+            textRenderer: SpriteFontRenderer.fromFont(font),
+            position: Vector2(10, 10),
+          ),
+          TextComponent(
+            text: 'badface',
+            textRenderer: SpriteFontRenderer.fromFont(font, letterSpacing: 11),
+            position: Vector2(10, 40),
+          ),
+        ]);
+      },
+      goldenFile: '../_goldens/sprite_font_renderer_3.png',
+      size: Vector2(200, 70),
+    );
+
+    testGolden(
+      'Render colored text',
+      (game) async {
+        const lines = [
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+          'abcdefghijklmnopqrstuvwxyz',
+        ];
+        final font = SpriteFont(
+          source: await loadImage('alphabet.png'),
+          size: 6,
+          ascent: 6,
+          glyphs: [
+            for (var j = 0; j < lines.length; j++)
+              for (var i = 0; i < lines[j].length; i++)
+                Glyph(lines[j][i], left: i * 6, top: 1 + j * 6)
+          ],
+        );
+        const colors = [
+          Color(0xffff0000),
+          Color(0x66ff0000),
+          Color(0x33ff0000),
+          Color(0x11ff0000),
+          Color(0xffffffff),
+          Color(0xff8c27c9),
+        ];
+        game.addAll([
+          RectangleComponent(
+            size: Vector2(200, 200),
+            paint: Paint()..color = const Color(0xffcfc6e5),
+          ),
+          for (var i = 0; i < colors.length; i++)
+            TextComponent(
+              text: 'pandemonium',
+              textRenderer: SpriteFontRenderer.fromFont(
+                font,
+                scale: 2,
+                color: colors[i],
+              ),
+              position: Vector2(10, 10 + i * 20),
+            ),
+        ]);
+      },
+      goldenFile: '../_goldens/sprite_font_renderer_4.png',
+      size: Vector2(200, 130),
+    );
   });
 }
 
