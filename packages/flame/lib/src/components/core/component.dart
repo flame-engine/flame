d917e3aa30845d187f0e447c348cd06dafd908f5
Lukas Klingsbo <me@lukas.fyi>
2022-10-03 17:23:45 +0200

    Update packages/flame/lib/src/components/core/component.dart

--- packages/flame/lib/src/components/core/component.dart
+++ packages/flame/lib/src/components/core/component.dart
@@ -6,7 +6,7 @@ import 'package:flame/src/cache/value_cache.dart';
 import 'package:flame/src/components/core/component_set.dart';
 import 'package:flame/src/components/core/position_type.dart';
 import 'package:flame/src/components/mixins/coordinate_transform.dart';
-import 'package:flame/src/experimental/has_game_reference.dart';
+import 'package:flame/src/components/mixins/has_game_ref.dart';
 import 'package:flame/src/game/flame_game.dart';
 import 'package:flame/src/game/game.dart';
 import 'package:flame/src/gestures/events.dart';
