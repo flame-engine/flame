ae3d7eac9b037349cab133c0b7555a4448e2e0d0
Munsterlander <munsterlander@users.noreply.github.com>
2022-10-03 11:30:18 -0700

    Updates sequence effect to reset

--- doc/flame/examples/lib/sequence_effect.dart
+++ doc/flame/examples/lib/sequence_effect.dart
@@ -6,19 +6,30 @@ import 'package:flame/game.dart';
 class SequenceEffectGame extends FlameGame with HasTappableComponents {
   @override
   Future<void> onLoad() async {
-    final effect = SequenceEffect([
-      ScaleEffect.by(
-        Vector2.all(1.5),
-        EffectController(duration: 0.2, alternate: true),
-      ),
-      MoveEffect.by(Vector2(30, -50), EffectController(duration: 0.5)),
-    ]);
-
     final flower = Flower(
       size: 40,
       position: canvasSize / 2,
       onTap: (flower) {
-        flower.add(effect);
+        flower.add(
+          SequenceEffect([
+            ScaleEffect.by(
+              Vector2.all(1.5),
+              EffectController(duration: 0.2, alternate: true),
+            ),
+            MoveEffect.by(
+              Vector2(30, -50),
+              EffectController(duration: 0.5),
+            ),
+            MoveEffect.by(
+              Vector2(-30, 50),
+              EffectController(duration: 0.5),
+            ),
+            ScaleEffect.by(
+              Vector2.all(.75),
+              EffectController(duration: 0.2, alternate: true),
+            ),
+          ]),
+        );
       },
     );
     add(flower);
