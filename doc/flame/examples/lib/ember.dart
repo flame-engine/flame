e4854efebbc0ccaecf8c6b2af11a4d0020d82618
Munsterlander <munsterlander@users.noreply.github.com>
2022-10-03 20:41:40 -0700

    Working ember component

--- doc/flame/examples/lib/ember.dart
+++ doc/flame/examples/lib/ember.dart
@@ -1,18 +1,18 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame/effects.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/flame.dart';
-import 'package:flutter/material.dart';
 
-class EmberPlayer extends SpriteAnimationComponent
-    with CollisionCallbacks, Tappable {
+class EmberPlayer extends SpriteAnimationComponent with TapCallbacks {
   EmberPlayer({
+    void Function(EmberPlayer)? onTap,
     required super.position,
-  }) : super(
-          size: Vector2.all(50),
-        );
+    required super.size,
+  })  : _onTap = onTap,
+        super();
 
-  final velocity = Vector2(0, 150);
+  Vector2 velocity = Vector2(0, 0);
+  final void Function(EmberPlayer)? _onTap;
 
   @override
   Future<void> onLoad() async {
@@ -24,29 +24,16 @@ class EmberPlayer extends SpriteAnimationComponent
         stepTime: 0.12,
       ),
     );
-    add(CircleHitbox());
+
+    add(CircleHitbox()..collisionType = CollisionType.active);
   }
 
   @override
   void update(double dt) {
+    position += velocity * dt;
     super.update(dt);
-    //position.y += velocity.y * dt;
   }
 
   @override
-  void onCollisionStart(Set<Vector2> _, PositionComponent other) {
-    super.onCollisionStart(_, other);
-  }
-
-  void fly() {
-    add(
-      MoveByEffect(
-        Vector2(0, -100),
-        EffectController(
-          duration: 0.2,
-          curve: Curves.decelerate,
-        ),
-      ),
-    );
-  }
+  void onTapUp([TapUpEvent? event]) => _onTap?.call(this);
 }
--- doc/flame/flame.md
+++ doc/flame/flame.md
@@ -7,7 +7,7 @@ Router               <router.md>
 Game loop            <game.md>
 Components           <components.md>
 Platforms            <platforms.md>
-Collision detection  <collision_detection.md>
+Collision Detection  <collision_detection.md>
 Effects              <effects.md>
 Camera & Viewport    <camera_and_viewport.md>
 Camera component     <camera_component.md>
