61473e95baa56f65ceb19c8eaae775a84bd4c1fc
sumit7577 <asmitkumarr082@gmail.com>
2022-10-04 15:56:15 +0530

    fix:added toString method to all event handlers and added examples for events

--- tutorials/space_shooter/lib/steps/1_getting_started/1_flame_game/code.dart
+++ tutorials/space_shooter/lib/steps/1_getting_started/1_flame_game/code.dart
@@ -1,10 +1,13 @@
 import 'package:flame/components.dart';
-import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 class Player extends PositionComponent {
   static final _paint = Paint()..color = Colors.white;
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
+  }
 }
 
 class SpaceShooterGame extends FlameGame {
