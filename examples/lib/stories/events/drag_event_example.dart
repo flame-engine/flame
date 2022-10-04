6126c35cb998bed156874510c8c61d13caca9277
sumit7577 <asmitkumarr082@gmail.com>
2022-10-04 15:45:06 +0530

    fix:added toString method to all event handlers

--- examples/lib/stories/events/drag_event_example.dart
+++ examples/lib/stories/events/drag_event_example.dart
@@ -3,8 +3,30 @@ import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-class Player extends PositionComponent {
+class Player extends PositionComponent with DragCallbacks {
   static final _paint = Paint()..color = Colors.white;
+  @override
+  void onDragEnd(DragEndEvent event) {
+    //TODO: implements onDragEnd
+  }
+  @override
+  void onDragCancel(DragCancelEvent event) {
+    // TODO: implement onDragCancel
+  }
+
+  @override
+  void onDragStart(DragStartEvent event) {
+    // TODO: implement onDragStart
+  }
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    // TODO: implement onDragUpdate
+  }
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
+  }
 }
 
 class SpaceShooterGame extends FlameGame {
