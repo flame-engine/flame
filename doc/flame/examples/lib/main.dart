95a35246646c280d0803df417a73d0f970bd6123
Munsterlander <munsterlander@users.noreply.github.com>
2022-10-04 14:13:57 -0700

    docs: Sequence effect (#1993)
    Addresses: #1954
    
    Adds the interactive example. There is an issue with OpacityEffect in a Sequence so it was omitted.

--- doc/flame/examples/lib/main.dart
+++ doc/flame/examples/lib/main.dart
@@ -12,6 +12,7 @@ import 'package:doc_flame_examples/drag_events.dart';
 import 'package:doc_flame_examples/router.dart';
 import 'package:doc_flame_examples/scale_by_effect.dart';
 import 'package:doc_flame_examples/scale_to_effect.dart';
+import 'package:doc_flame_examples/sequence_effect.dart';
 import 'package:doc_flame_examples/tap_events.dart';
 import 'package:doc_flame_examples/value_route.dart';
 import 'package:flame/game.dart';
@@ -23,6 +24,9 @@ void main() {
     page = page.substring(1);
   }
   final routes = <String, Game Function()>{
+    'anchor_by_effect': AnchorByEffectGame.new,
+    'anchor_to_effect': AnchorToEffectGame.new,
+    'collision_detection': CollisionDetectionGame.new,
     'decorator_blur': DecoratorBlurGame.new,
     'decorator_grayscale': DecoratorGrayscaleGame.new,
     'decorator_rotate3d': DecoratorRotate3DGame.new,
@@ -30,13 +34,11 @@ void main() {
     'decorator_tint': DecoratorTintGame.new,
     'drag_events': DragEventsGame.new,
     'router': RouterGame.new,
-    'tap_events': TapEventsGame.new,
-    'value_route': ValueRouteExample.new,
-    'anchor_by_effect': AnchorByEffectGame.new,
-    'anchor_to_effect': AnchorToEffectGame.new,
     'scale_by_effect': ScaleByEffectGame.new,
     'scale_to_effect': ScaleToEffectGame.new,
-    'collision_detection': CollisionDetectionGame.new,
+    'sequence_effect': SequenceEffectGame.new,
+    'tap_events': TapEventsGame.new,
+    'value_route': ValueRouteExample.new,
   };
   final game = routes[page]?.call();
   if (game != null) {
