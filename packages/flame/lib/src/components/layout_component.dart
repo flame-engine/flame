0f6c85367a4db8562a65d672b4c567fd0eccb343
Valentin Treu <valley@MacBook-Pro-von-Valentin.local>
2022-10-05 20:39:34 +0200

    Switching to onChildrenChanged notifier

--- packages/flame/lib/src/components/layout_component.dart
+++ packages/flame/lib/src/components/layout_component.dart
@@ -20,31 +20,9 @@ abstract class LayoutComponent extends PositionComponent with HasGameRef {
   double get gap => _gap;
 
   @override
-  Future<void>? add(Component component) {
-    assert(
-      component is PositionComponent,
-      'The added component has to be a child of PositionComponent',
-    );
-    (component as PositionComponent).size.addListener(layoutChildren);
-
-    component.mounted.then((_) => layoutChildren());
-    return super.add(component);
+  void onChildrenChanged(Component child, ChildrenChangeType changeType) {
+    layoutChildren();
   }
 
-  @override
-  void remove(Component component) {
-    assert(
-      contains(component),
-      'This component is not a child of this class',
-    );
-    (component as PositionComponent).size.removeListener(layoutChildren);
-
-    super.remove(component);
-    // will be corrected when this issue has been resolved:
-    // https://github.com/flame-engine/flame/issues/1956
-    Future.delayed(const Duration(milliseconds: 50), layoutChildren);
-  }
-
-  @protected
   void layoutChildren();
 }
