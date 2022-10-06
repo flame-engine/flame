7d268245001673f0b5a542ebce0b7e57cbf91baf
ayrotideysarkar <ayrotideysarkar@gmail.com>
2022-10-06 13:35:54 +0530

    link rectified

--- examples/lib/stories/input/input.dart
+++ examples/lib/stories/input/input.dart
@@ -97,7 +97,7 @@ void addInputStories(Dashbook dashbook) {
     ..add(
       'Gesture Hitboxes',
       (_) => GameWidget(game: GestureHitboxesExample()),
-      codeLink: baseLink('input/Gesture Hitboxes_example.dart'),
+      codeLink: baseLink('input/gesture_hitboxes_example.dart'),
       info: GestureHitboxesExample.description,
     )
     ..add(
