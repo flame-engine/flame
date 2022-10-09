727359c7c9798b1fb75b5ffc5523a84097f5edb7
2002Bishwajeet <bishwajeet.techmaster@gmail.com>
2022-10-09 19:55:26 +0530

    set anchor to center and position to size/2

--- doc/flame/examples/lib/opacity_effect.dart
+++ doc/flame/examples/lib/opacity_effect.dart
@@ -1,4 +1,5 @@
 import 'package:doc_flame_examples/ember.dart';
+import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
 import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
@@ -8,7 +9,7 @@ class OpacityGame extends FlameGame with HasTappableComponents {
   @override
   Future<void> onLoad() async {
     final ember = EmberPlayer(
-      position: canvasSize / 3,
+      position: size / 2,
       size: size / 4,
       onTap: (ember) {
         if (reset = !reset) {
@@ -17,7 +18,7 @@ class OpacityGame extends FlameGame with HasTappableComponents {
           ember.add(OpacityEffect.to(1.0, EffectController(duration: 0.75)));
         }
       },
-    );
+    )..anchor = Anchor.center;
 
     add(ember);
   }
