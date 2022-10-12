7b03b4d1fd7287e0f904dec64ca835d9c0bff43f
Dipak Prajapati <dipakp2726@gmail.com>
2022-10-12 19:39:05 +0530

    TapDetector mixin tests added

--- packages/flame/test/gestures/detectors_test.dart
+++ packages/flame/test/gestures/detectors_test.dart
@@ -1,5 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -30,6 +32,95 @@ void main() {
       },
     );
   });
+
+  group('TapDetector', () {
+    final tapGame = FlameTester(_TapDetectorGame.new);
+
+    testWithGame<_TapDetectorGame>(
+      'can be Tapped Down',
+      _TapDetectorGame.new,
+      (game) async {
+        await game.ready();
+
+        game.handleTapDown(TapDownDetails());
+
+        expect(game.hasOnTapDown, isTrue);
+      },
+    );
+    testWithGame<_TapDetectorGame>(
+      'can be Tapped Up',
+      _TapDetectorGame.new,
+      (game) async {
+        await game.ready();
+
+        game.handleTapUp(TapUpDetails(kind: PointerDeviceKind.touch));
+
+        expect(game.hasOnTapUp, isTrue);
+      },
+    );
+    testWithGame<_TapDetectorGame>(
+      'can be Tapped Canceled',
+      _TapDetectorGame.new,
+      (game) async {
+        await game.ready();
+
+        game.handleTapDown(TapDownDetails());
+        game.onTapCancel();
+
+        expect(game.hasOnTapCancel, isTrue);
+      },
+    );
+    tapGame.testGameWidget(
+      'can receive taps',
+      verify: (game, tester) async {
+        await tester.tapAt(const Offset(10, 10));
+        expect(game.tapRegistered, isTrue);
+      },
+    );
+    tapGame.testGameWidget(
+      'can receive tapDown',
+      verify: (game, tester) async {
+        await tester.startGesture(const Offset(10, 10));
+        expect(game.hasOnTapDown, isTrue);
+        expect(game.hasOnTapUp, isFalse);
+      },
+    );
+    tapGame.testGameWidget(
+      'can receive tapCancel',
+      verify: (game, tester) async {
+        await tester.dragFrom(const Offset(10, 10), const Offset(20, 10));
+        expect(game.hasOnTapDown, isTrue);
+        expect(game.hasOnTapCancel, isTrue);
+      },
+    );
+  });
+}
+
+class _TapDetectorGame extends FlameGame with TapDetector {
+  bool hasOnTapUp = false;
+  bool hasOnTapDown = false;
+  bool hasOnTapCancel = false;
+  bool tapRegistered = false;
+
+  @override
+  void onTap() {
+    tapRegistered = true;
+  }
+
+  @override
+  void onTapUp(TapUpInfo info) {
+    hasOnTapUp = true;
+  }
+
+  @override
+  void onTapDown(TapDownInfo info) {
+    hasOnTapDown = true;
+  }
+
+  @override
+  void onTapCancel() {
+    hasOnTapCancel = true;
+  }
 }
 
 class _MultiDragPanGame extends FlameGame
