88304cc67eb21b8d74b0380d05b4b8e5e0a2b200
Matt Bennett <m.bennett@xehkode.com>
2022-10-20 23:15:10 +0100

    Renamed comp to component in all HasPaint tests

--- packages/flame/test/components/mixins/has_paint_test.dart
+++ packages/flame/test/components/mixins/has_paint_test.dart
@@ -6,71 +6,77 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('HasPaint', () {
     test('paint returns the default paint', () {
-      final comp = _MyComponent();
+      final component = _MyComponent();
 
-      expect(comp.paint, comp.getPaint());
+      expect(component.paint, component.getPaint());
     });
 
     test(
       'paint setter sets the main paint',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
-        comp.paint = Paint()..color = color;
+        component.paint = Paint()..color = color;
 
-        expect(comp.getPaint().color, color);
+        expect(component.getPaint().color, color);
       },
     );
 
     test('paintLayers defaults to [paint]', () {
-      final comp = _MyComponent();
+      final component = _MyComponent();
 
       const color = Color(0xFFE5E5E5);
-      comp.paint = Paint()..color = color;
+      component.paint = Paint()..color = color;
 
       expect(
-        (comp.paintLayers.first == comp.paint) &&
-            (comp.paintLayers.length == 1),
+        (component.paintLayers.first == component.paint) &&
+            (component.paintLayers.length == 1),
         isTrue,
       );
     });
 
     test('setting paintLayers.first sets paint', () {
-      final comp = _MyComponent();
+      final component = _MyComponent();
 
       const color = Color(0xFFE5E5E5);
-      comp.paintLayers.first = Paint()..color = color;
+      component.paintLayers.first = Paint()..color = color;
 
       expect(
-        (comp.paintLayers.first == comp.paint) &&
-            (comp.paintLayers.length == 1),
+        (component.paintLayers.first == component.paint) &&
+            (component.paintLayers.length == 1),
         isTrue,
       );
     });
 
     test('setting paintLayers.first when length > 1 sets paint', () {
-      final comp = _MyComponent();
+      final component = _MyComponent();
 
       const color = Color(0xFFE5E5E5);
       const secondColor = Color(0xFF123456);
       const thirdColor = Color(0xFFABABAB);
-      comp.paintLayers = [Paint()..color = color, Paint()..color = secondColor];
-      comp.paintLayers.first = Paint()..color = thirdColor;
+      component.paintLayers = [
+        Paint()..color = color,
+        Paint()..color = secondColor
+      ];
+      component.paintLayers.first = Paint()..color = thirdColor;
 
-      expect(comp.paint.color, equals(thirdColor));
+      expect(component.paint.color, equals(thirdColor));
     });
 
     test('setting paint sets paintLayers.first when length > 1', () {
-      final comp = _MyComponent();
+      final component = _MyComponent();
 
       const color = Color(0xFFE5E5E5);
       const secondColor = Color(0xFF123456);
       const thirdColor = Color(0xFFABABAB);
-      comp.paintLayers = [Paint()..color = color, Paint()..color = secondColor];
-      comp.paint = Paint()..color = thirdColor;
+      component.paintLayers = [
+        Paint()..color = color,
+        Paint()..color = secondColor
+      ];
+      component.paint = Paint()..color = thirdColor;
 
-      expect(comp.paintLayers.first.color, equals(thirdColor));
+      expect(component.paintLayers.first.color, equals(thirdColor));
     });
 
     test('paintLayers takes precedence over paint if both set on constructor',
@@ -160,10 +166,10 @@ void main() {
     test(
       'getPaint throws exception when retrieving a paint that does not exist',
       () {
-        final comp = _MyComponentWithType();
+        final component = _MyComponentWithType();
 
         expect(
-          () => comp.getPaint(_MyComponentKeys.background),
+          () => component.getPaint(_MyComponentKeys.background),
           throwsArgumentError,
         );
       },
@@ -172,36 +178,39 @@ void main() {
     test(
       'setPaint sets a paint',
       () {
-        final comp = _MyComponentWithType();
+        final component = _MyComponentWithType();
 
         const color = Color(0xFFA9A9A9);
-        comp.setPaint(_MyComponentKeys.background, Paint()..color = color);
+        component.setPaint(_MyComponentKeys.background, Paint()..color = color);
 
-        expect(comp.getPaint(_MyComponentKeys.background).color, equals(color));
+        expect(
+          component.getPaint(_MyComponentKeys.background).color,
+          equals(color),
+        );
       },
     );
 
     test(
       'deletePaint removes a paint from the map',
       () {
-        final comp = _MyComponentWithType();
+        final component = _MyComponentWithType();
 
-        comp.setPaint(_MyComponentKeys.foreground, Paint());
-        comp.deletePaint(_MyComponentKeys.foreground);
+        component.setPaint(_MyComponentKeys.foreground, Paint());
+        component.deletePaint(_MyComponentKeys.foreground);
 
         expect(
-          () => comp.getPaint(_MyComponentKeys.foreground),
+          () => component.getPaint(_MyComponentKeys.foreground),
           throwsArgumentError,
         );
       },
     );
 
     test('paintLayers set to default', () {
-      final comp = _MyComponent();
+      final component = _MyComponent();
 
       expect(
-        (comp.paintLayers.length == 1) &&
-            (comp.paintLayers[0] == comp.getPaint()),
+        (component.paintLayers.length == 1) &&
+            (component.paintLayers[0] == component.getPaint()),
         isTrue,
       );
     });
@@ -209,79 +218,83 @@ void main() {
     test(
       'append paint to paintLayers',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
-        comp.paintLayers.add(Paint()..color = color);
+        component.paintLayers.add(Paint()..color = color);
 
-        expect(comp.paintLayers[1].color, equals(color));
+        expect(component.paintLayers[1].color, equals(color));
       },
     );
 
     test(
       'append paint to paintLayers using addPaintLayer',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
         const anotherColor = Color(0xFFABABAB);
-        comp.setPaint('test', Paint()..color = color, updatePaintLayers: false);
-        comp.setPaint(
+        component.setPaint(
+          'test',
+          Paint()..color = color,
+          updatePaintLayers: false,
+        );
+        component.setPaint(
           'anotherTest',
           Paint()..color = anotherColor,
           updatePaintLayers: false,
         );
-        comp.addPaintLayer('test');
+        component.addPaintLayer('test');
 
-        expect(comp.paintLayers[1].color, equals(color));
+        expect(component.paintLayers[1].color, equals(color));
       },
     );
 
     test(
       'setPaint changes paint on paintLayers by default',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         final testPaint = Paint()..color = const Color(0xFFE5E5E5);
-        comp.setPaint('test', testPaint);
-        comp.setPaintLayers(['test']);
+        component.setPaint('test', testPaint);
+        component.setPaintLayers(['test']);
 
         const newColor = Color(0xFF123456);
         final newPaint = Paint()..color = newColor;
-        comp.setPaint('test', newPaint);
+        component.setPaint('test', newPaint);
 
-        expect(comp.paintLayers[0].color, equals(newColor));
+        expect(component.paintLayers[0].color, equals(newColor));
       },
     );
 
     test(
       'setPaint does not change paintLayers when updatePaintLayers is false',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const testColor = Color(0xFFE5E5E5);
         final testPaint = Paint()..color = testColor;
-        comp.setPaint('test', testPaint);
-        comp.setPaintLayers(['test']);
+        component.setPaint('test', testPaint);
+        component.setPaintLayers(['test']);
 
         final newPaint = Paint()..color = const Color(0xFF123456);
-        comp.setPaint('test', newPaint, updatePaintLayers: false);
+        component.setPaint('test', newPaint, updatePaintLayers: false);
 
-        expect(comp.paintLayers[0].color, equals(testColor));
+        expect(component.paintLayers[0].color, equals(testColor));
       },
     );
 
     test(
       'deletePaint deletes paint from paintLayers by default',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
-        comp.setPaint('test', Paint()..color = color);
-        comp.addPaintLayer('test');
-        comp.deletePaint('test');
+        component.setPaint('test', Paint()..color = color);
+        component.addPaintLayer('test');
+        component.deletePaint('test');
 
-        expect(comp.paintLayers.length, equals(1));
+        expect(component.paintLayers.length, equals(1));
       },
     );
 
@@ -289,52 +302,52 @@ void main() {
       'deletePaint does not delete paint from paintLayers when '
       'updatePaintLayers is false',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
-        comp.setPaint('test', Paint()..color = color);
-        comp.addPaintLayer('test');
-        comp.deletePaint('test', updatePaintLayers: false);
+        component.setPaint('test', Paint()..color = color);
+        component.addPaintLayer('test');
+        component.deletePaint('test', updatePaintLayers: false);
 
-        expect(comp.paintLayers.length, equals(2));
+        expect(component.paintLayers.length, equals(2));
       },
     );
 
     test(
       'delete paint from paintLayers using removePaintIdFromLayers',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
         const anotherColor = Color(0xFFABABAB);
         const thirdColor = Color(0xFF123456);
-        comp.setPaint('test', Paint()..color = color);
-        comp.setPaint('anotherTest', Paint()..color = anotherColor);
-        comp.setPaint('thirdTest', Paint()..color = thirdColor);
-        comp.setPaintLayers(['test', 'anotherTest', 'thirdTest']);
-        comp.removePaintIdFromLayers('anotherTest');
+        component.setPaint('test', Paint()..color = color);
+        component.setPaint('anotherTest', Paint()..color = anotherColor);
+        component.setPaint('thirdTest', Paint()..color = thirdColor);
+        component.setPaintLayers(['test', 'anotherTest', 'thirdTest']);
+        component.removePaintIdFromLayers('anotherTest');
 
-        expect(comp.paintLayers[1].color, equals(thirdColor));
+        expect(component.paintLayers[1].color, equals(thirdColor));
       },
     );
 
     test(
       'use setPaintLayers to set mutliple paintIds in paintLayers',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
         const anotherColor = Color(0xFFABABAB);
         const thirdColor = Color(0xFF123456);
-        comp.setPaint('test', Paint()..color = color);
-        comp.setPaint('anotherTest', Paint()..color = anotherColor);
-        comp.setPaint('thirdTest', Paint()..color = thirdColor);
+        component.setPaint('test', Paint()..color = color);
+        component.setPaint('anotherTest', Paint()..color = anotherColor);
+        component.setPaint('thirdTest', Paint()..color = thirdColor);
 
-        comp.setPaintLayers(['thirdTest', 'test']);
+        component.setPaintLayers(['thirdTest', 'test']);
 
         expect(
-          (comp.paintLayers[0].color == thirdColor) &&
-              (comp.paintLayers[1].color == color),
+          (component.paintLayers[0].color == thirdColor) &&
+              (component.paintLayers[1].color == color),
           isTrue,
         );
       },
@@ -343,15 +356,15 @@ void main() {
     test(
       'setPaintLayers throws exception if unrecognised paintId',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         const color = Color(0xFFE5E5E5);
-        comp.setPaint('test', Paint()..color = color);
-        comp.setPaint('anotherTest', Paint()..color = color);
-        comp.setPaint('thirdTest', Paint()..color = color);
+        component.setPaint('test', Paint()..color = color);
+        component.setPaint('anotherTest', Paint()..color = color);
+        component.setPaint('thirdTest', Paint()..color = color);
 
         expect(
-          () => comp.setPaintLayers(['thirdTest', 'wrong']),
+          () => component.setPaintLayers(['thirdTest', 'wrong']),
           throwsArgumentError,
         );
       },
@@ -360,77 +373,86 @@ void main() {
     test(
       'makeTransparent sets opacity to 0 on the main when paintId is omitted',
       () {
-        final comp = _MyComponent();
-        comp.makeTransparent();
+        final component = _MyComponent();
+        component.makeTransparent();
 
-        expect(comp.paint.color.opacity, 0);
+        expect(component.paint.color.opacity, 0);
       },
     );
 
     test(
       'makeTransparent sets opacity to 0 on informed paintId',
       () {
-        final comp = _MyComponentWithType();
-        comp.setPaint(_MyComponentKeys.background, Paint());
-        comp.makeTransparent(paintId: _MyComponentKeys.background);
+        final component = _MyComponentWithType();
+        component.setPaint(_MyComponentKeys.background, Paint());
+        component.makeTransparent(paintId: _MyComponentKeys.background);
 
-        expect(comp.getPaint(_MyComponentKeys.background).color.opacity, 0);
+        expect(
+          component.getPaint(_MyComponentKeys.background).color.opacity,
+          0,
+        );
       },
     );
 
     test(
       'makeOpaque sets opacity to 1 on the main when paintId is omitted',
       () {
-        final comp = _MyComponent();
-        comp.makeTransparent();
-        comp.makeOpaque();
+        final component = _MyComponent();
+        component.makeTransparent();
+        component.makeOpaque();
 
-        expect(comp.paint.color.opacity, 1);
+        expect(component.paint.color.opacity, 1);
       },
     );
 
     test(
       'makeOpaque sets opacity to 1 on informed paintId',
       () {
-        final comp = _MyComponentWithType();
-        comp.setPaint(
+        final component = _MyComponentWithType();
+        component.setPaint(
           _MyComponentKeys.background,
           Paint()..color = const Color(0x00E5E5E5),
         );
-        comp.makeOpaque(paintId: _MyComponentKeys.background);
+        component.makeOpaque(paintId: _MyComponentKeys.background);
 
-        expect(comp.getPaint(_MyComponentKeys.background).color.opacity, 1);
+        expect(
+          component.getPaint(_MyComponentKeys.background).color.opacity,
+          1,
+        );
       },
     );
 
     test(
       'setOpacity sets opacity of the main when paintId is omitted',
       () {
-        final comp = _MyComponent();
-        comp.setOpacity(0.2);
+        final component = _MyComponent();
+        component.setOpacity(0.2);
 
-        expect(comp.paint.color.opacity, 0.2);
+        expect(component.paint.color.opacity, 0.2);
       },
     );
 
     test(
       'setOpacity sets opacity of the informed paintId',
       () {
-        final comp = _MyComponentWithType();
-        comp.setPaint(_MyComponentKeys.background, Paint());
-        comp.setOpacity(0.2, paintId: _MyComponentKeys.background);
+        final component = _MyComponentWithType();
+        component.setPaint(_MyComponentKeys.background, Paint());
+        component.setOpacity(0.2, paintId: _MyComponentKeys.background);
 
-        expect(comp.getPaint(_MyComponentKeys.background).color.opacity, 0.2);
+        expect(
+          component.getPaint(_MyComponentKeys.background).color.opacity,
+          0.2,
+        );
       },
     );
 
     test(
       'throws error if opacity is less than 0',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         expect(
-          () => comp.setOpacity(-0.5),
+          () => component.setOpacity(-0.5),
           throwsArgumentError,
         );
       },
@@ -439,10 +461,10 @@ void main() {
     test(
       'throws error if opacity is greater than 1',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
 
         expect(
-          () => comp.setOpacity(1.1),
+          () => component.setOpacity(1.1),
           throwsArgumentError,
         );
       },
@@ -451,35 +473,35 @@ void main() {
     test(
       'setColor sets color of the main when paintId is omitted',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
         const color = Color(0xFFE5E5E5);
-        comp.setColor(color);
+        component.setColor(color);
 
-        expect(comp.paint.color, color);
+        expect(component.paint.color, color);
       },
     );
 
     test(
       'setOpacity sets opacity of the informed paintId',
       () {
-        final comp = _MyComponentWithType();
+        final component = _MyComponentWithType();
         const color = Color(0xFFE5E5E5);
-        comp.setPaint(_MyComponentKeys.background, Paint());
-        comp.setColor(color, paintId: _MyComponentKeys.background);
+        component.setPaint(_MyComponentKeys.background, Paint());
+        component.setColor(color, paintId: _MyComponentKeys.background);
 
-        expect(comp.getPaint(_MyComponentKeys.background).color, color);
+        expect(component.getPaint(_MyComponentKeys.background).color, color);
       },
     );
 
     test(
       'tint sets the correct color filter of the main when paintId is omitted',
       () {
-        final comp = _MyComponent();
+        final component = _MyComponent();
         const color = Color(0xFFE5E5E5);
-        comp.tint(color);
+        component.tint(color);
 
         expect(
-          comp.paint.colorFilter,
+          component.paint.colorFilter,
           const ColorFilter.mode(color, BlendMode.srcATop),
         );
       },
@@ -488,13 +510,13 @@ void main() {
     test(
       'setOpacity sets opacity of the informed paintId',
       () {
-        final comp = _MyComponentWithType();
+        final component = _MyComponentWithType();
         const color = Color(0xFFE5E5E5);
-        comp.setPaint(_MyComponentKeys.background, Paint());
-        comp.tint(color, paintId: _MyComponentKeys.background);
+        component.setPaint(_MyComponentKeys.background, Paint());
+        component.tint(color, paintId: _MyComponentKeys.background);
 
         expect(
-          comp.getPaint(_MyComponentKeys.background).colorFilter,
+          component.getPaint(_MyComponentKeys.background).colorFilter,
           const ColorFilter.mode(color, BlendMode.srcATop),
         );
       },
