c9fc48e70e7866e304214a804f682f1550f28704
Ryuzaki <ufrshubham@gmail.com>
2022-10-21 00:21:16 +0530

    Update example to use `target` `OpacityProvider`

--- doc/flame/examples/lib/opacity_to_effect.dart
+++ doc/flame/examples/lib/opacity_to_effect.dart
@@ -1,4 +1,4 @@
-import 'package:doc_flame_examples/ember.dart';
+import 'package:doc_flame_examples/flower.dart';
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
 import 'package:flame/experimental.dart';
@@ -6,30 +6,41 @@ import 'package:flame/game.dart';
 
 class OpacityToEffectGame extends FlameGame with HasTappableComponents {
   bool reset = false;
+
+  // This reference needs to be stored because every new instance of
+  // OpacityProvider caches the opacity ratios at the time on creation.
+  late OpacityProvider _borderOpacityProvider;
+
   @override
   Future<void> onLoad() async {
-    final ember = EmberPlayer(
+    final flower = Flower(
       position: size / 2,
-      size: size / 4,
-      onTap: (ember) {
-        if (reset = !reset) {
-          ember.add(
-            OpacityEffect.to(
-              0.2,
-              EffectController(duration: 0.75),
-            ),
-          );
-        } else {
-          ember.add(
-            OpacityEffect.to(
-              1.0,
-              EffectController(duration: 0.75),
-            ),
-          );
-        }
-      },
+      size: 60,
+      onTap: _onTap,
     )..anchor = Anchor.center;
 
-    add(ember);
+    _borderOpacityProvider = flower.opacityProviderOfList(
+      paintIds: const [FlowerPaint.paintId1, FlowerPaint.paintId2],
+    );
+
+    add(flower);
+  }
+
+  void _onTap(Flower flower) {
+    if (reset = !reset) {
+      flower.add(
+        OpacityEffect.fadeOut(
+          EffectController(duration: 0.75),
+          target: _borderOpacityProvider,
+        ),
+      );
+    } else {
+      flower.add(
+        OpacityEffect.fadeIn(
+          EffectController(duration: 0.75),
+          target: _borderOpacityProvider,
+        ),
+      );
+    }
   }
 }
