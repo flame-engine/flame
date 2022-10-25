0b6a8398f9092af9cb46e43895469852fc5501e9
idlehart <jocke.sjostedt@gmail.com>
2022-10-25 22:24:24 +0200

    WIP

--- doc/flame/examples/lib/remove_effect.dart
+++ doc/flame/examples/lib/remove_effect.dart
@@ -1,60 +1,35 @@
 import 'package:doc_flame_examples/flower.dart';
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
-import 'package:flame/experimental.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/cupertino.dart';
 
-class RemoveEffectGame extends FlameGame with HasTappableComponents {
+class RemoveEffectGame extends FlameGame with TapDetector {
+  late Flower flower;
+  late TextComponent text;
+  late Timer countDown;
+
   @override
   Future<void> onLoad() async {
-    final flower = Flower(
-      position: size / 2,
-      size: 45,
-      onTap: (flower) {
-        flower.add(
-          RemoveEffect(delay: 3),
-        );
-      },
-    )..anchor = Anchor.center;
-
     add(
-      SpawnBox(
-        onTap: () {
-          if (!children.contains(flower)) {
-            add(
-              flower
-                ..opacity = 0
-                ..add(
-                  OpacityEffect.by(
-                    1,
-                    EffectController(duration: 0.75),
-                  ),
-                ),
-            );
-          }
+      flower = Flower(
+        position: size / 2,
+        size: 45,
+        onTap: (flower) {
+          flower.add(
+            RemoveEffect(delay: 3),
+          );
         },
-      )
-        ..anchor = Anchor.topLeft
-        ..size = size,
+      )..anchor = Anchor.center,
     );
-
-    add(flower);
+    add
+    add(text = TextComponent(text: countDown.current.toString()));
   }
-}
-
-class SpawnBox extends PositionComponent with TapCallbacks {
-  SpawnBox({
-    required VoidCallback onTap,
-  }) : _onTap = onTap;
-
-  final VoidCallback _onTap;
 
   @override
-  void onTapUp([TapUpEvent? event]) => _onTap.call();
-
-  @override
-  void render(Canvas canvas) {
-    canvas.drawRect(const Rect.fromLTRB(5, 5, 5, 5), Paint());
+  void onTap() {
+    if (!children.contains(flower)) {
+      add(flower);
+    }
   }
 }
