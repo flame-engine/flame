084b4c2ba66b75acd369ffd7875d4d85dcf315b9
Pasha Stetsenko <stpasha@google.com>
2022-10-27 22:54:44 -0700

    Add {package} directive

--- doc/_sphinx/conf.py
+++ doc/_sphinx/conf.py
@@ -26,11 +26,13 @@ extensions = [
     'myst_parser',  # Markdown support
     'sphinxcontrib.mermaid',
     'extensions.flutter_app',
+    'extensions.package',
 ]
 
 # Configuration options for MyST:
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
 myst_enable_extensions = [
+    'colon_fence',
     'dollarmath',
     'html_admonition',
     'html_image',
