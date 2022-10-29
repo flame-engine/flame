bc1c7ae2a9411ab815940d5f8410f97f6a811d35
Dipak Prajapati <dipakp2726@gmail.com>
2022-10-29 15:13:26 +0530

    Update packages/flame_spine/lib/flame_spine.dart
    Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>

--- packages/flame_spine/lib/flame_spine.dart
+++ packages/flame_spine/lib/flame_spine.dart
@@ -1,11 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-/// Spine support for the Flame game engine.
 library flame_spine;
 
 export 'src/sekele_render.dart';
