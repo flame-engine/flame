30e26dd5f018bf78c00a7815f319d353ffca4a5d
Dipak <dipakp2726@gmail.com>
2022-10-29 18:29:45 +0530

    test: SpineComponent test added

--- packages/flame_spine/test/src/flame_spine_test.dart
+++ packages/flame_spine/test/src/flame_spine_test.dart
@@ -1,7 +1,51 @@
+import 'package:flame_spine/flame_spine.dart';
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
 void main() {
-  /* group('FlameSpine', () {
-    test('can be instantiated', () {
-      expect((), isNotNull);
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('SpineComponent', () {
+    testWithFlameGame('Can Add to FlameGame', (game) async {
+      final spineComponent = SpineComponent(
+        renderer: SkeletonRender(skeleton: MockAnimation()),
+        size: game.size / 2,
+      );
+
+      game.add(spineComponent);
+      await game.ready();
+
+      expect(spineComponent.parent, game);
     });
-  });*/
+
+    testWithFlameGame('Can play animation', (game) async {
+      final spineComponent = SpineComponent(
+        renderer: SkeletonRender(skeleton: MockAnimation()),
+        size: game.size / 2,
+      );
+
+      game.add(spineComponent);
+      await game.ready();
+
+      spineComponent.renderer.playState = PlayState.playing;
+
+      expect(spineComponent.renderer.playState, PlayState.playing);
+    });
+    testWithFlameGame('Can pause animation', (game) async {
+      final spineComponent = SpineComponent(
+        renderer: SkeletonRender(skeleton: MockAnimation()),
+        size: game.size / 2,
+      );
+
+      game.add(spineComponent);
+      await game.ready();
+
+      spineComponent.renderer.playState = PlayState.paused;
+
+      expect(spineComponent.renderer.playState, PlayState.paused);
+    });
+  });
 }
+
+class MockAnimation extends Mock implements SkeletonAnimation {}
