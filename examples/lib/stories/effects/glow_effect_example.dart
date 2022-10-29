6dd7214f55bb092f2e9f5380e63b4c2e6f1c2c25
Dipak <dipakp2726@gmail.com>
2022-10-29 09:38:04 +0530

    updated examples

--- examples/lib/stories/effects/glow_effect_example.dart
+++ examples/lib/stories/effects/glow_effect_example.dart
@@ -1,3 +1,4 @@
+import 'package:examples/commons/ember.dart';
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
 import 'package:flame/game.dart';
@@ -15,19 +16,39 @@ class GlowEffectExample extends FlameGame with TapDetector {
 
   @override
   Future<void> onLoad() async {
-    final paint2 = Paint()..color = const Color(0xff39FF14);
+    final paint2 = Paint()
+      ..color = const Color(0xff39FF14)
+      ..style = PaintingStyle.stroke;
+
+    add(
+      Ember(
+        position: Vector2(280, 280),
+        size: Vector2.all(100),
+      )..add(
+          RectangleComponent(paint: paint2, size: Vector2.all(100))
+            ..add(
+              GlowEffect(
+                30.0,
+                EffectController(
+                  duration: 3.5,
+                  infinite: true,
+                ),
+              ),
+            ),
+        ),
+    );
 
     add(
       CircleComponent(
         radius: 50,
-        position: Vector2(280, 280),
+        position: Vector2(300, 400),
         paint: paint2,
       )..add(
           GlowEffect(
             10.0,
             EffectController(
               duration: 3,
-              reverseDuration: 1.5,
+              // reverseDuration: 1.5,
               infinite: true,
             ),
           ),
