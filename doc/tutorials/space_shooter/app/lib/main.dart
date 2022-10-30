c64d5ae64b1431880d90991844eb783aefaa0131
Munsterlander <munsterlander@users.noreply.github.com>
2022-10-30 10:27:22 -0700

    Fixes bad embedded code

--- doc/tutorials/space_shooter/app/lib/main.dart
+++ doc/tutorials/space_shooter/app/lib/main.dart
@@ -1,14 +1,15 @@
 import 'dart:html'; // ignore: avoid_web_libraries_in_flutter
 
 import 'package:flutter/widgets.dart';
-import 'package:tutorials_space_shooter/step_1/main.dart' as step1;
-import 'package:tutorials_space_shooter/step_2/main.dart' as step2;
+import 'package:tutorials_space_shooter/step1/main.dart' as step1;
+import 'package:tutorials_space_shooter/step2/main.dart' as step2;
 
 void main() {
   var page = window.location.search ?? '';
   if (page.startsWith('?')) {
     page = page.substring(1);
   }
+
   switch (page) {
     case 'step1':
       step1.main();
