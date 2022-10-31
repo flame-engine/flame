7188a46e831387fe6e3d93c19ce516e79ccb9745
Munsterlander <munsterlander@users.noreply.github.com>
2022-10-30 17:53:32 -0700

    Organized and added routes

--- doc/flame/examples/lib/main.dart
+++ doc/flame/examples/lib/main.dart
@@ -29,6 +29,8 @@ import 'package:doc_flame_examples/scale_to_effect.dart';
 import 'package:doc_flame_examples/sequence_effect.dart';
 import 'package:doc_flame_examples/size_by_effect.dart';
 import 'package:doc_flame_examples/size_to_effect.dart';
+import 'package:doc_flame_examples/snippet_hud_controls.dart';
+import 'package:doc_flame_examples/snippet_physics_arc.dart';
 import 'package:doc_flame_examples/tap_events.dart';
 import 'package:doc_flame_examples/value_route.dart';
 import 'package:flame/game.dart';
@@ -43,18 +45,24 @@ void main() {
     'anchor_by_effect': AnchorByEffectGame.new,
     'anchor_to_effect': AnchorToEffectGame.new,
     'collision_detection': CollisionDetectionGame.new,
+    'color_effect': ColorEffectExample.new,
     'decorator_blur': DecoratorBlurGame.new,
     'decorator_grayscale': DecoratorGrayscaleGame.new,
     'decorator_rotate3d': DecoratorRotate3DGame.new,
     'decorator_shadow3d': DecoratorShadowGame.new,
     'decorator_tint': DecoratorTintGame.new,
     'drag_events': DragEventsGame.new,
+    'glow_effect': GlowEffectExample.new,
     'opacity_to_effect': OpacityToEffectGame.new,
     'opacity_effect_with_target': OpacityEffectWithTargetGame.new,
     'opacity_by_effect': OpacityByEffectGame.new,
     'move_along_path_effect': MoveAlongPathEffectGame.new,
     'move_by_effect': MoveByEffectGame.new,
     'move_to_effect': MoveToEffectGame.new,
+    'ray_cast': RayCastExample.new,
+    'ray_trace': RayTraceExample.new,
+    'remove_effect': RemoveEffectGame.new,
+    'rive_example': RiveExampleGame.new,
     'rotate_by_effect': RotateByEffectGame.new,
     'rotate_to_effect': RotateToEffectGame.new,
     'router': RouterGame.new,
@@ -62,15 +70,11 @@ void main() {
     'scale_to_effect': ScaleToEffectGame.new,
     'size_by_effect': SizeByEffectGame.new,
     'size_to_effect': SizeToEffectGame.new,
+    'snippet_hud_controls': SnippetHudControlsGame.new,
+    'snippet_physics_arc': SnippetPhysicsArcGame.new,
     'sequence_effect': SequenceEffectGame.new,
     'tap_events': TapEventsGame.new,
     'value_route': ValueRouteExample.new,
-    'rive_example': RiveExampleGame.new,
-    'ray_cast': RayCastExample.new,
-    'ray_trace': RayTraceExample.new,
-    'glow_effect': GlowEffectExample.new,
-    'remove_effect': RemoveEffectGame.new,
-    'color_effect': ColorEffectExample.new,
   };
   final game = routes[page]?.call();
   if (game != null) {
