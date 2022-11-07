f90849b462ebbb81497ef2f59bca3c153f2fd146
Dipak <dipakp2726@gmail.com>
2022-11-07 15:35:10 +0530

    chore :cleanup

--- packages/flame_lottie/test/src/flame_lottie_test.dart
+++ packages/flame_lottie/test/src/flame_lottie_test.dart
@@ -1,9 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
 
 import 'package:flame_lottie/flame_lottie.dart';
 // ignore_for_file: prefer_const_constructors
