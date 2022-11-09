ecd92d08cc27f3c22b3038d4323ff4c64a97c87a
Tobias Zuber <zuber.tobi@gmail.com>
2022-11-09 23:30:16 +0100

    add dashbook story for flame_lottie

---
+++ examples/lib/stories/bridge_libraries/flame_lottie/lottie_animation_example.dart
@@ -0,0 +1,25 @@
+import 'package:flame/game.dart';
+
+import 'package:flame_lottie/flame_lottie.dart';
+
+class LottieAnimationExample extends FlameGame {
+  static const String description = '''
+    This example shows how to load an Lottie animation. It is configured to 
+    contiously loop the animation and restart once its done.
+  ''';
+
+  @override
+  Future onLoad() async {
+    final asset =
+        await loadLottie(Lottie.asset('assets/images/lottieLogo.json'));
+
+    add(
+      LottieComponent(
+        composition: asset,
+        repeating: true,
+      ),
+    );
+
+    return super.onLoad();
+  }
+}
