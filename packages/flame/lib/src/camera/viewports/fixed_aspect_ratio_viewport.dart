6d46657a69bc3447e02b516ae242f551787fdd79
Pasha Stetsenko <stpasha@google.com>
2022-11-19 17:53:49 -0800

    Update FixedAspectRatioViewport

--- packages/flame/lib/src/camera/viewports/fixed_aspect_ratio_viewport.dart
+++ packages/flame/lib/src/camera/viewports/fixed_aspect_ratio_viewport.dart
@@ -1,22 +1,33 @@
 import 'dart:ui';
 
+import 'package:flame/extensions.dart';
 import 'package:flame/src/camera/viewport.dart';
 import 'package:vector_math/vector_math_64.dart';
 
+/// [FixedAspectRatioViewport] is a rectangular viewport which auto-expands to
+/// take as much space as possible within the canvas, while maintaining a fixed
+/// aspect ratio.
+///
+/// This viewport will automatically adjust its size and position when the
+/// game canvas changes in size.
 class FixedAspectRatioViewport extends Viewport {
   FixedAspectRatioViewport({
     required this.aspectRatio,
     super.children,
-  }) : assert(aspectRatio > 0);
+  })  : _canvasSize = Vector2.zero(),
+        assert(aspectRatio > 0);
 
+  /// The ratio of width to height of the viewport.
   final double aspectRatio;
+
+  final Vector2 _canvasSize;
   Rect _clipRect = Rect.zero;
 
   @override
   void onGameResize(Vector2 canvasSize) {
     super.onGameResize(canvasSize);
-    position = canvasSize / 2;
-    size = canvasSize;
+    _canvasSize.setFrom(canvasSize);
+    onViewportResize();
   }
 
   @override
@@ -24,20 +35,24 @@ class FixedAspectRatioViewport extends Viewport {
 
   @override
   bool containsLocalPoint(Vector2 point) {
-    return point.x.abs() <= size.x / 2 && point.y.abs() <= size.y / 2;
+    final x = point.x;
+    final y = point.y;
+    return x >= 0 && y >= 0 && x <= size.x && y <= size.y;
   }
 
   @override
   void onViewportResize() {
-    final desiredWidth = size.y * aspectRatio;
-    if (desiredWidth > size.x) {
-      size.y = size.x / aspectRatio;
+    final availableWidth = _canvasSize.x;
+    final availableHeight = _canvasSize.y;
+    if (availableHeight * aspectRatio > availableWidth) {
+      size.x = availableWidth;
+      size.y = availableWidth / aspectRatio;
     } else {
-      size.x = desiredWidth;
+      size.x = availableHeight * aspectRatio;
+      size.y = availableHeight;
     }
-
-    final x = size.x / 2;
-    final y = size.y / 2;
-    _clipRect = Rect.fromLTRB(-x, -y, x, y);
+    position.x = (availableWidth - size.x) / 2 + anchor.x * size.x;
+    position.y = (availableHeight - size.y) / 2 + anchor.y * size.y;
+    _clipRect = Rect.fromLTRB(0, 0, size.x, size.y);
   }
 }
