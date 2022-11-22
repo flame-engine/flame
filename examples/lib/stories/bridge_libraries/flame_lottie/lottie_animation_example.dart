aae97e7f21921228c50883577d2f18e1ffc99c26
tozu <zuber.tobi@gmail.com>
2022-11-22 15:13:09 +0100

    Update examples/lib/stories/bridge_libraries/flame_lottie/lottie_animation_example.dart
    Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>

--- examples/lib/stories/bridge_libraries/flame_lottie/lottie_animation_example.dart
+++ examples/lib/stories/bridge_libraries/flame_lottie/lottie_animation_example.dart
@@ -4,7 +4,7 @@ import 'package:flame_lottie/flame_lottie.dart';
 
 class LottieAnimationExample extends FlameGame {
   static const String description = '''
-    This example shows how to load an Lottie animation. It is configured to 
+    This example shows how to load a Lottie animation. It is configured to 
     continuously loop the animation and restart once its done.
   ''';
 
