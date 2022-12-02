534df83a16e0b0195ee6ffb8f2bbafd7bed364ea
Lukas Klingsbo <me@lukas.fyi>
2022-12-02 22:04:45 +0100

    fix: Re-use paint object in ImageParticle

--- packages/flame/lib/src/particles/image_particle.dart
+++ packages/flame/lib/src/particles/image_particle.dart
@@ -8,15 +8,17 @@ import 'package:flame/src/particles/particle.dart';
 class ImageParticle extends Particle {
   /// dart.ui [Image] to draw
   Image image;
+  Paint paint;
 
   late Rect src;
   late Rect dest;
 
   ImageParticle({
     required this.image,
+    Paint? paint,
     Vector2? size,
     super.lifespan,
-  }) {
+  }) : paint = paint ?? Paint() {
     final srcWidth = image.width.toDouble();
     final srcHeight = image.height.toDouble();
     final destWidth = size?.x ?? srcWidth;
@@ -29,6 +31,6 @@ class ImageParticle extends Particle {
 
   @override
   void render(Canvas canvas) {
-    canvas.drawImageRect(image, src, dest, Paint());
+    canvas.drawImageRect(image, src, dest, paint);
   }
 }
