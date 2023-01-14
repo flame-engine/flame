0bc905cc515001875e955a0f50e1aca27a48aa81
Pasha Stetsenko <stpasha@google.com>
2023-01-13 20:30:37 -0800

    few missing doc-comments

--- packages/flame_jenny/jenny/lib/src/character.dart
+++ packages/flame_jenny/jenny/lib/src/character.dart
@@ -4,13 +4,17 @@
 class Character {
   Character(this.name, {List<String>? aliases}) : aliases = aliases ?? [];
 
+  Map<String, dynamic>? _data;
+
   /// The canonical name of the character
   final String name;
 
   /// The list of aliases
   final List<String> aliases;
-  Map<String, dynamic>? _data;
 
+  /// Additional information associated with this character.
+  ///
+  /// You can store any key-value pairs here that you want, or nothing at all.
   Map<String, dynamic> get data => _data ??= <String, dynamic>{};
 
   @override
