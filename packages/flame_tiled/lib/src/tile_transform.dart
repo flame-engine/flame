34c3b6bdc4c570f4e8641b11b94efe19bdd1ef32
Hwanseok Barth Kang <tttkhs@naver.com>
2023-02-06 05:24:01 +0900

    feat: Add ability to opt-out flip (#2316)
    As I mentioned on #2310 (comment),
    it's inevitable to create an atlas that has 2x width from the original one to support flip, it could bring some limitations. So it would be great to opt out the flip.
    
    Here's the reason why this is needed.
    Flip may impact performance if the composed tileset(AKA texture) is big.
    The software tiled has shortcuts to go back by cmd(ctrl)-z, rotating by z, and flipping horizontally by x. Imagine that you clicked a tile to the wrong place, you may press cmd-z and so on. In this situation, you can also accidentally press z or x, and as a result, the tile flips! It is fine with some complicated or flip(rotate)-consiousable tiles. But some things like the floor or sky are hard to recognize. I even made a program for my design team to pick out coordinates of where the flipped tiles exist but often there are flips. So, providing a way by simple boolean argument may prevent this.

--- packages/flame_tiled/lib/src/tile_transform.dart
+++ packages/flame_tiled/lib/src/tile_transform.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flame/sprite.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flame_tiled/src/mutable_transform.dart';
 import 'package:meta/meta.dart';
 
@@ -9,7 +10,7 @@ import 'package:meta/meta.dart';
 class TileTransform {
   final Rect source;
   final MutableRSTransform transform;
-  final bool flip;
+  final SimpleFlips flip;
   final SpriteBatch batch;
 
   TileTransform(
