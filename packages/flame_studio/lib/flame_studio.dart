e695278e7e97484cc39ee521d709ff2e1be310bf
Pasha Stetsenko <stpasha@gmail.com>
2023-02-09 20:45:12 -0800

    clean up exports

--- packages/flame_studio/lib/flame_studio.dart
+++ packages/flame_studio/lib/flame_studio.dart
@@ -1,3 +1,1 @@
 export 'src/run_flame_studio.dart' show runFlameStudio;
-export 'src/widgets/flame_studio.dart' show FlameStudio;
-export 'src/widgets/toolbar/flame_studio_toolbar.dart' show FlameStudioToolbar;
