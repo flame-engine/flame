11d122b3ff047578f1effb6c3355bc92a97675e3
Pasha Stetsenko <stpasha@gmail.com>
2023-02-09 20:44:33 -0800

    remove static lists

--- packages/flame_studio/lib/src/widgets/toolbar/flame_studio_toolbar.dart
+++ packages/flame_studio/lib/src/widgets/toolbar/flame_studio_toolbar.dart
@@ -9,20 +9,13 @@ final toolbarHeightProvider = Provider((ref) => 28.0);
 class FlameStudioToolbar extends ConsumerWidget {
   const FlameStudioToolbar({super.key});
 
-  /// List of widgets to show on the left side of the toolbar.
-  static final List<Widget> left = [];
-
-  /// List of widgets to show in the center of the toolbar.
-  static final List<Widget> middle = [
-    const StartButton(),
-    const PauseButton(),
-  ];
-
-  /// List of widgets to show on the right side of the toolbar.
-  static final List<Widget> right = [];
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    const middleButtons = <Widget>[
+      StartButton(),
+      PauseButton()
+    ];
+
     final height = ref.watch(toolbarHeightProvider);
     final gap = height * 0.10;
 
@@ -50,25 +43,12 @@ class FlameStudioToolbar extends ConsumerWidget {
             padding: EdgeInsets.fromLTRB(gap * 2, gap, gap * 10, gap * 2),
             child: Image.asset('logo_flame.png', fit: BoxFit.scaleDown),
           ),
-          for (final button in left) _ButtonWithPadding(button, gap),
           Expanded(child: Container()),
-          for (final button in middle) _ButtonWithPadding(button, gap),
+          for (final button in middleButtons)
+            Padding(padding: EdgeInsets.all(gap), child: button),
           Expanded(child: Container()),
-          for (final button in right) _ButtonWithPadding(button, gap),
         ],
       ),
     );
-  }
-}
-
-class _ButtonWithPadding extends StatelessWidget {
-  const _ButtonWithPadding(this.button, this.padding);
-
-  final Widget button;
-  final double padding;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(padding), child: button);
   }
 }
