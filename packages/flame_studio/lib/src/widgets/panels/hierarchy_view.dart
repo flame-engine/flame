6c10f5460b3c8f9c77154cbd2d9f6bf4a28ea4a2
Pasha Stetsenko <stpasha@gmail.com>
2023-02-11 15:09:59 -0800

    initial version

--- packages/flame_studio/lib/src/widgets/panels/hierarchy_view.dart
+++ packages/flame_studio/lib/src/widgets/panels/hierarchy_view.dart
@@ -1,5 +1,4 @@
-import 'package:flame/game.dart';
-import 'package:flame_studio/src/core/game_controller.dart';
+import 'package:flame_studio/src/core/component_tree.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,25 +7,38 @@ class HierarchyView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final gameController = ref.watch(gameControllerProvider);
-    final game = gameController.game as FlameGame?;
-    if (game == null) {
+    final componentTree = ref.watch(componentTreeProvider);
+    if (componentTree == null) {
       return Container();
     }
 
+    final listItems = <Widget>[];
+    final controller = ref.read(componentTreeProvider.notifier);
+    componentTree.traverse((node, depth, parent, index) {
+      listItems.add(_buildItem(node, depth, controller));
+      return false;
+    });
+
     return ListView(
       padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        for (final component in game.children)
-          Container(
-            child: Text(
-              '${component.runtimeType}',
-              style: const TextStyle(
-                color: Color(0xffffffff),
-              ),
-            ),
-          ),
-      ],
+      children: listItems,
+    );
+  }
+
+  Widget _buildItem(Node node, int depth, ComponentTreeNotifier controller) {
+    final pre = node.isExpanded
+        ? '[-]'
+        : node.isCollapsed
+            ? '[+]'
+            : '[ ]';
+    return GestureDetector(
+      onTap: () => controller.toggle(node.component),
+      child: Text(
+        '$pre ${'..' * depth}${node.name}',
+        style: const TextStyle(
+          color: Color(0xffffffff),
+        ),
+      ),
     );
   }
 }
