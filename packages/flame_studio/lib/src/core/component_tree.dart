8ed3cce9c8345332da2be12e60437828b2351dd3
Pasha Stetsenko <stpasha@gmail.com>
2023-02-12 20:40:44 -0800

    simplify ComponentTree provider

--- packages/flame_studio/lib/src/core/component_tree.dart
+++ packages/flame_studio/lib/src/core/component_tree.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_studio/src/core/game_controller.dart';
@@ -7,130 +6,64 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final componentTreeProvider =
-    StateNotifierProvider<ComponentTreeNotifier, Node?>((ref) {
+    StateNotifierProvider<ComponentTreeObserver, ComponentTreeState>((ref) {
   final gameState = ref.watch(gameControllerProvider);
-  return ComponentTreeNotifier(gameState.game as FlameGame?);
+  return ComponentTreeObserver(gameState.game as FlameGame?);
 });
 
 @immutable
-class Node {
-  Node._({required this.component, this.expandable = false, this.children})
-      : name = component.runtimeType.toString();
+class ComponentTreeNode {
+  const ComponentTreeNode._(this.component, this.children);
 
-  factory Node.fromComponent(
-    Component component, {
-    Node? oldNode,
-    bool? expanded,
-  }) {
-    assert(oldNode == null || oldNode.component == component);
-    List<Node>? childrenNodes;
-    if ((expanded ?? oldNode?.isExpanded ?? false) && component.hasChildren) {
-      childrenNodes = [
-        for (final child in component.children)
-          Node.fromComponent(
-            child,
-            oldNode: oldNode?.children
-                ?.firstWhereOrNull((node) => node.component == child),
-          )
-      ];
-    }
-    return Node._(
-      component: component,
-      expandable: component.hasChildren,
-      children: childrenNodes,
+  factory ComponentTreeNode.fromComponent(Component component) {
+    return ComponentTreeNode._(
+      component,
+      component.hasChildren
+          ? [
+              for (final child in component.children)
+                ComponentTreeNode.fromComponent(child)
+            ]
+          : null,
     );
   }
 
   final Component component;
-  final String name;
-  final bool expandable;
-  final List<Node>? children;
-  bool get isExpanded => children != null;
-  bool get isCollapsed => expandable && !isExpanded;
-
-  /// Traverses the tree in depth-first order, calling [visitor] for each node.
-  void traverse(_VisitorFn visitor) {
-    final stop = visitor(this, 0, null, -1);
-    if (!stop) {
-      _traverseInner(1, visitor);
-    }
-  }
-
-  bool _traverseInner(int depth, _VisitorFn visitor) {
-    if (children != null) {
-      var i = 0;
-      for (final node in children!) {
-        final stop = visitor(node, depth, this, i) ||
-            node._traverseInner(depth + 1, visitor);
-        if (stop) {
-          return true;
-        }
-        i++;
-      }
-    }
-    return false;
-  }
+  final List<ComponentTreeNode>? children;
+  String get name => component.runtimeType.toString();
 
   @override
   bool operator ==(Object other) =>
-      other is Node &&
+      other is ComponentTreeNode &&
       component == other.component &&
-      name == other.name &&
-      expandable == other.expandable &&
       listEquals(children, other.children);
 
   @override
-  int get hashCode => Object.hash(component, name, expandable, children);
+  int get hashCode => Object.hash(component, children);
 }
 
-/// The visitor function receives four parameters: the `node` itself, its
-/// `depth` within the tree, the node's `parent` if it exists, and its
-/// `index` within the parent's list of children. The function may return
-/// `true` to indicate that the traversal should stop, or false to continue
-/// tree traversal.
-typedef _VisitorFn = bool Function(
-  Node node,
-  int depth,
-  Node? parent,
-  int index,
-);
+@immutable
+class ComponentTreeState {
+  ComponentTreeState(Component? rootComponent)
+      : root = rootComponent == null
+            ? null
+            : ComponentTreeNode.fromComponent(rootComponent);
 
-class ComponentTreeNotifier extends StateNotifier<Node?> {
-  ComponentTreeNotifier(Component? root)
-      : super(root == null ? null : Node.fromComponent(root)) {
-    if (root != null) {
+  final ComponentTreeNode? root;
+
+  @override
+  bool operator ==(Object other) =>
+      other is ComponentTreeState && root == other.root;
+
+  @override
+  int get hashCode => root?.hashCode ?? 0;
+}
+
+class ComponentTreeObserver extends StateNotifier<ComponentTreeState> {
+  ComponentTreeObserver(Component? rootComponent)
+      : super(ComponentTreeState(rootComponent)) {
+    if (rootComponent != null) {
       _refresh(Duration.zero);
     }
-  }
-
-  void toggle(Component component) => _toggle(component);
-
-  void expand(Component component) => _toggle(component, expand: true);
-
-  void collapse(Component component) => _toggle(component, expand: false);
-
-  void _toggle(Component component, {bool? expand}) {
-    if (state!.component == component) {
-      state = Node.fromComponent(
-        component,
-        oldNode: state,
-        expanded: expand ?? !state!.isExpanded,
-      );
-      return;
-    }
-    final newTree = Node.fromComponent(state!.component, oldNode: state);
-    newTree.traverse((node, level, parent, index) {
-      if (node.component == component) {
-        parent!.children![index] = Node.fromComponent(
-          component,
-          oldNode: node,
-          expanded: expand ?? !node.isExpanded,
-        );
-        return true;
-      }
-      return false;
-    });
-    state = newTree;
   }
 
   static Duration refreshFrequency = const Duration(milliseconds: 300);
@@ -139,9 +72,9 @@ class ComponentTreeNotifier extends StateNotifier<Node?> {
   void _refresh(Duration timeDelta) {
     _timeSinceLastRefresh += timeDelta;
     if (_timeSinceLastRefresh >= refreshFrequency) {
-      final newTree = Node.fromComponent(state!.component, oldNode: state);
-      if (newTree != state) {
-        state = newTree;
+      final newState = ComponentTreeState(state.root!.component);
+      if (newState != state) {
+        state = newState;
       }
       _timeSinceLastRefresh = Duration.zero;
     }
--- packages/flame_studio/lib/src/widgets/panels/hierarchy_view.dart
+++ packages/flame_studio/lib/src/widgets/panels/hierarchy_view.dart
@@ -1,23 +1,28 @@
+import 'package:flame/components.dart';
 import 'package:flame_studio/src/core/component_tree.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HierarchyView extends ConsumerWidget {
+class HierarchyView extends ConsumerStatefulWidget {
   const HierarchyView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final componentTree = ref.watch(componentTreeProvider);
+  HierarchyViewState createState() => HierarchyViewState();
+}
+
+class HierarchyViewState extends ConsumerState<ConsumerStatefulWidget> {
+  Component? selectedComponent;
+  Set<Component> expandedComponents = {};
+
+  @override
+  Widget build(BuildContext context) {
+    final componentTree = ref.watch(componentTreeProvider).root;
     if (componentTree == null) {
       return Container();
     }
 
     final listItems = <Widget>[];
-    final controller = ref.read(componentTreeProvider.notifier);
-    componentTree.traverse((node, depth, parent, index) {
-      listItems.add(_buildItem(node, depth, controller));
-      return false;
-    });
+    _buildList(componentTree, 0, listItems);
 
     return ListView(
       padding: const EdgeInsets.all(8),
@@ -25,14 +30,19 @@ class HierarchyView extends ConsumerWidget {
     );
   }
 
-  Widget _buildItem(Node node, int depth, ComponentTreeNotifier controller) {
-    final pre = node.isExpanded
-        ? '[-]'
-        : node.isCollapsed
-            ? '[+]'
-            : '[ ]';
+  void _buildList(ComponentTreeNode node, int depth, List<Widget> out) {
+    out.add(_buildItem(node, depth));
+    if (expandedComponents.contains(node.component) && node.children != null) {
+      for (final childNode in node.children!) {
+        _buildList(childNode, depth + 1, out);
+      }
+    }
+  }
+
+  Widget _buildItem(ComponentTreeNode node, int depth) {
+    final pre = '[-]';
     return GestureDetector(
-      onTap: () => controller.toggle(node.component),
+      onTap: () => _toggle(node.component),
       child: Text(
         '$pre ${'..' * depth}${node.name}',
         style: const TextStyle(
@@ -40,5 +50,15 @@ class HierarchyView extends ConsumerWidget {
         ),
       ),
     );
+  }
+
+  void _toggle(Component component) {
+    setState(() {
+      if (expandedComponents.contains(component)) {
+        expandedComponents.remove(component);
+      } else {
+        expandedComponents.add(component);
+      }
+    });
   }
 }
