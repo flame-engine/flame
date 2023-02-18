3c442928981cfffa93a478530321b2197217de7f
Norberto Martín Afonso <normaafo@gmail.com>
2023-02-18 13:57:22 +0000

    Update rive package to 0.10.2

--- packages/flame_rive/lib/src/rive_component.dart
+++ packages/flame_rive/lib/src/rive_component.dart
@@ -5,10 +5,8 @@ import 'dart:ui' as ui;
 import 'package:flame/components.dart';
 import 'package:flutter/rendering.dart';
 import 'package:rive/rive.dart';
-// ignore_for_file: implementation_imports
-import 'package:rive/src/rive_core/math/aabb.dart';
-import 'package:rive/src/rive_core/math/mat2d.dart';
-import 'package:rive/src/rive_core/math/vec2d.dart';
+
+import 'package:rive/math.dart';
 
 class RiveComponent extends PositionComponent {
   final Artboard artboard;
