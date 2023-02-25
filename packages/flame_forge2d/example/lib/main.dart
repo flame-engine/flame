2fa43a82b846b7c5a9c80853d518a9f27589dd3c
Eugene Kleshnin <eukleshnin@gmail.com>
2023-02-25 22:32:17 +0000

    added basic Forge2D example

--- packages/flame_forge2d/example/lib/main.dart
+++ packages/flame_forge2d/example/lib/main.dart
@@ -1,26 +1,88 @@
 import 'dart:html';
 
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_forge2d_example/joints/constant_volume_joint.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  var page = window.location.search ?? '';
-  if (page.startsWith('?')) {
-    page = page.substring(1);
+  runApp(GameWidget(game: Forge2DExample()));
+}
+
+class Forge2DExample extends Forge2DGame with HasTappables {
+  @override
+  Future<void> onLoad() async {
+    add(Ball(size / 2));
+    addAll(createBoundaries());
+
+    return super.onLoad();
   }
-  final routes = <String, Game Function()>{
-    'constant_volume_joint': ConstantVolumeJointExample.new,
-  };
-  final game = routes[page]?.call();
-  if (game != null) {
-    runApp(GameWidget(game: game));
-  } else {
-    runApp(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: Text('Error: unknown page name "$page"'),
-      ),
+
+  List<Component> createBoundaries() {
+    final topLeft = Vector2.zero();
+    final bottomRight = screenToWorld(camera.viewport.effectiveSize);
+    final topRight = Vector2(bottomRight.x, topLeft.y);
+    final bottomLeft = Vector2(topLeft.x, bottomRight.y);
+
+    return [
+      Wall(topLeft, topRight),
+      Wall(topRight, bottomRight),
+      Wall(bottomLeft, bottomRight),
+      Wall(topLeft, bottomLeft)
+    ];
+  }
+}
+
+class Ball extends BodyComponent with Tappable {
+  final Vector2 _position;
+
+  Ball(this._position);
+
+  @override
+  Body createBody() {
+    final shape = CircleShape();
+    shape.radius = 5;
+
+    final fixtureDef = FixtureDef(
+      shape,
+      restitution: 0.8,
+      density: 1.0,
+      friction: 0.4,
     );
+
+    final bodyDef = BodyDef(
+      userData: this,
+      angularDamping: 0.8,
+      position: _position,
+      type: BodyType.dynamic,
+    );
+
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
+
+  @override
+  bool onTapDown(_) {
+    body.applyLinearImpulse(Vector2.random() * 5000);
+    return false;
+  }
+}
+
+class Wall extends BodyComponent {
+  final Vector2 _start;
+  final Vector2 _end;
+
+  Wall(this._start, this._end);
+
+  @override
+  Body createBody() {
+    final shape = EdgeShape()..set(_start, _end);
+    final fixtureDef = FixtureDef(shape, friction: 0.3);
+    final bodyDef = BodyDef(
+      userData: this,
+      position: Vector2.zero(),
+    );
+
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
