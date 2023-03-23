547248cb8a0e4fdedb4a030d83dd55451e2138cb
Eugene Kleshnin <eukleshnin@gmail.com>
2023-03-23 23:13:12 +0000

    fix format

--- examples/lib/stories/bridge_libraries/forge2d/joints/motor_joint.dart
+++ examples/lib/stories/bridge_libraries/forge2d/joints/motor_joint.dart
@@ -25,10 +25,11 @@ class MotorJointExample extends Forge2DGame with TapDetector, HasDraggables {
     super.onLoad();
 
     final box = Box(
-        startPosition: size / 2,
-        width: 2,
-        height: 1,
-        bodyType: BodyType.static,);
+      startPosition: size / 2,
+      width: 2,
+      height: 1,
+      bodyType: BodyType.static,
+    );
     add(box);
 
     ball = Ball(Vector2(size.x / 2, size.y / 2 - 5));
