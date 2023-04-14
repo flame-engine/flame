5855508e83a7f82447c127bbd57178aea82df011
projectitis <peter@projectitis.com>
2023-04-14 13:51:20 +1200

    Named constructor. Added tests and example

--- packages/flame/lib/src/nine_tile_box.dart
+++ packages/flame/lib/src/nine_tile_box.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flame/src/extensions/vector2.dart';
 import 'package:flame/src/palette.dart';
 import 'package:flame/src/sprite.dart';
+import 'package:meta/meta.dart';
 
 /// This allows you to create a rectangle textured with a 9-sliced image.
 ///
@@ -24,7 +25,9 @@ class NineTileBox {
   /// (optionally used to scale the src image).
   late int destTileSize;
 
-  late Rect _center;
+  @visibleForTesting
+  late Rect center;
+  
   late final Rect _dst;
 
   /// Creates a nine-box instance.
@@ -41,70 +44,101 @@ class NineTileBox {
       : tileSize = tileSize ?? sprite.src.width ~/ 3 {
     this.destTileSize = destTileSize ?? this.tileSize;
     final centerEdge = this.tileSize.toDouble();
-    _center = Rect.fromLTWH(centerEdge, centerEdge, centerEdge, centerEdge);
+    center = Rect.fromLTWH(centerEdge, centerEdge, centerEdge, centerEdge);
     _dst = Rect.fromLTWH(0, 0, this.destTileSize * 3, this.destTileSize * 3);
   }
 
+  /// Creates a nine-box instance with the specified grid size
+  ///
+  /// The outside columns and rows can be specified with fixed width or height.
+  /// The center column and row take up the remaining space.
+  NineTileBox.withGrid(this.sprite, {int leftColumnWidth = 0,
+      int rightColumnWidth = 0, int topRowHeight = 0,
+      int bottomRowHeight = 0,}) : tileSize = sprite.src.width ~/ 3 {
+    destTileSize = tileSize;
+    final centerEdge = tileSize.toDouble();
+    center = Rect.fromLTWH(0, 0, sprite.src.width, sprite.src.height);
+    _dst = Rect.fromLTWH(0, 0, sprite.src.width, sprite.src.height);
+    setGrid(
+      leftColumnWidth: leftColumnWidth,
+      rightColumnWidth: rightColumnWidth,
+      topRowHeight: topRowHeight,
+      bottomRowHeight: bottomRowHeight,
+    );
+  }
+
   /// Set different sizes for each of the fixed size rows and columns
-  void setGrid({int? leftCol, int? rightCol, int? topRow, int? bottomRow}) {
-    if (leftCol != null && rightCol != null) {
+  ///
+  /// The outside columns and rows can be specified with fixed width or height.
+  /// The center column and row take up the remaining space. Any values that are
+  /// not specified will remain unchanged.
+  void setGrid({int? leftColumnWidth, int? rightColumnWidth,
+      int? topRowHeight, int? bottomRowHeight,}) {
+    if (leftColumnWidth != null && rightColumnWidth != null) {
       assert(
-        leftCol + rightCol <= sprite.src.width,
-        'The left and right columns ($leftCol + $rightCol) do not fit in the width of the sprite (${sprite.src.width.round()})',
+        leftColumnWidth + rightColumnWidth <= sprite.src.width,
+        'The left and right columns ($leftColumnWidth + $rightColumnWidth) do not fit in the width of the sprite (${sprite.src.width.round()})',
       );
-    } else if (leftCol != null) {
+    } else if (leftColumnWidth != null) {
       assert(
-        leftCol <= _center.right,
-        'The left column ($leftCol) is too large (max ${_center.right.round()})',
+        leftColumnWidth <= center.right,
+        'The left column ($leftColumnWidth) is too large (max ${center.right.round()})',
       );
-    } else if (rightCol != null) {
+    } else if (rightColumnWidth != null) {
       assert(
-        rightCol + _center.left <= sprite.src.width,
-        'The right column ($rightCol) is too large (max ${(sprite.src.width - _center.left).round()})',
+        rightColumnWidth + center.left <= sprite.src.width,
+        'The right column ($rightColumnWidth) is too large (max ${(sprite.src.width - center.left).round()})',
       );
     }
-    if (topRow != null && bottomRow != null) {
+    if (topRowHeight != null && bottomRowHeight != null) {
       assert(
-        topRow + bottomRow <= sprite.src.height,
-        'The top and bottom rows ($topRow + $bottomRow) do not fit in the height of the sprite (${sprite.src.height.round()})',
+        topRowHeight + bottomRowHeight <= sprite.src.height,
+        'The top and bottom rows ($topRowHeight + $bottomRowHeight) do not fit in the height of the sprite (${sprite.src.height.round()})',
       );
-    } else if (topRow != null) {
+    } else if (topRowHeight != null) {
       assert(
-        topRow <= _center.bottom,
-        'The top row ($topRow) is too large (max ${_center.bottom.round()})',
+        topRowHeight <= center.bottom,
+        'The top row ($topRowHeight) is too large (max ${center.bottom.round()})',
       );
-    } else if (bottomRow != null) {
+    } else if (bottomRowHeight != null) {
       assert(
-        bottomRow + _center.top <= sprite.src.height,
-        'The bottom row ($bottomRow) is too large (max ${(sprite.src.height - _center.top).round()})',
+        bottomRowHeight + center.top <= sprite.src.height,
+        'The bottom row ($bottomRowHeight) is too large (max ${(sprite.src.height - center.top).round()})',
       );
     }
 
-    leftCol ??= _center.left.round();
-    topRow ??= _center.top.round();
-    if (rightCol == null) {
-      rightCol = _center.right.round();
+    final left = leftColumnWidth ?? center.left.round();
+    final top = topRowHeight ?? center.top.round();
+    late final int right;
+    if (rightColumnWidth == null) {
+      right = center.right.round();
     } else {
-      rightCol = sprite.src.width.round() - rightCol;
+      right = sprite.src.width.round() - rightColumnWidth;
     }
-    if (bottomRow == null) {
-      bottomRow = _center.bottom.round();
+    late final int bottom;
+    if (bottomRowHeight == null) {
+      bottom = center.bottom.round();
     } else {
-      bottomRow = sprite.src.height.round() - bottomRow;
+      bottom = sprite.src.height.round() - bottomRowHeight;
     }
-    _center = Rect.fromLTRB(leftCol.toDouble(), topRow.toDouble(), rightCol.toDouble(), bottomRow.toDouble());
+    center = Rect.fromLTRB(
+      left.toDouble(),
+      top.toDouble(),
+      right.toDouble(),
+      bottom.toDouble(),
+    );
   }
 
   /// Renders this nine box with the dimensions provided by [dst].
   void drawRect(Canvas c, [Rect? dst]) {
-    c.drawImageNine(sprite.image, _center, dst ?? _dst, _whitePaint);
+    c.drawImageNine(sprite.image, center, dst ?? _dst, _whitePaint);
   }
 
   /// Renders this nine box as a rectangle at [position] with size [size].
   void draw(Canvas c, Vector2 position, Vector2 size) {
     c.drawImageNine(
       sprite.image,
-      _center,
+      center,
       Rect.fromLTWH(position.x, position.y, size.x, size.y),
       _whitePaint,
     );
