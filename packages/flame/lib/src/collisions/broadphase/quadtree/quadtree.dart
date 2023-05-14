af9716e47d885d0028f546c8bd42b9d647478c1f
Lukas Klingsbo <me@lukas.fyi>
2023-05-15 00:40:29 +0200

    Fix regression of quadtree

--- packages/flame/lib/src/collisions/broadphase/quadtree/quadtree.dart
+++ packages/flame/lib/src/collisions/broadphase/quadtree/quadtree.dart
@@ -316,9 +316,8 @@ class QuadTreeNodeDebugInfo {
 
   List<QuadTreeNodeDebugInfo> get nodes {
     final list = <QuadTreeNodeDebugInfo>[this];
-    var i = 0;
-    for (final node in this.node.children) {
-      i++;
+    for (var i = 0; i < node.children.length; i++) {
+      final node = this.node.children[i];
       if (node == null) {
         continue;
       }
