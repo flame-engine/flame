bcdf84d117a5f0dafd5ca4639ded62c27a9786f4
Lukas Klingsbo <me@lukas.fyi>
2023-05-21 19:36:36 +0200

    Fix platformer tutorial

--- doc/tutorials/platformer/app/lib/overlays/main_menu.dart
+++ doc/tutorials/platformer/app/lib/overlays/main_menu.dart
@@ -58,8 +58,9 @@ class MainMenu extends StatelessWidget {
               ),
               const SizedBox(height: 20),
               const Text(
-                '''Use WASD or Arrow Keys for movement.  Space bar to jump.
-                 Collect as many stars as you can and avoid enemies!''',
+                '''Use WASD or Arrow Keys for movement.
+Space bar to jump.
+Collect as many stars as you can and avoid enemies!''',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   color: whiteTextColor,
