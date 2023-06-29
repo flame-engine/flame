17c11ea681010e2b20b17ccf23693893e8155773
Alexey Volkov <asgalex@gmail.com>
2023-06-29 12:25:26 +0300

    Update packages/flame/lib/src/sprite.dart
    Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>

--- packages/flame/lib/src/sprite.dart
+++ packages/flame/lib/src/sprite.dart
@@ -133,8 +133,8 @@ class Sprite {
 
   /// Return a new [Image] based on the [src] of the Sprite.
   ///
-  /// A sync version of [toImage] function. Read [Picture.toImageSync] for
-  /// detailed description of possible benefits in performance
+  /// A sync version of the [toImage] function. Read [Picture.toImageSync] for a
+  /// detailed description of possible benefits in performance.
   Image toImageSync() {
     final composition = ImageComposition()
       ..add(image, Vector2.zero(), source: src);
