8605920815a0c00a3efd23df429175278e7bb551
Erick Zanardo <erickzanardoo@gmail.com>
2023-10-09 16:01:03 -0300

    fix lint

--- packages/flame_tiled/test/tile_atlas_test.dart
+++ packages/flame_tiled/test/tile_atlas_test.dart
@@ -1,6 +1,5 @@
 import 'package:flame/cache.dart';
 import 'package:flame/components.dart';
-import 'package:flame/flame.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flame_tiled/src/tile_atlas.dart';
 import 'package:flutter/services.dart';
