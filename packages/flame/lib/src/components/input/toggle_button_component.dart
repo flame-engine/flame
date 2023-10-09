edaca7b91ea42b5e91cbf4c87bd0864440a2926e
Denis Gladky <de.gladkiy@gmail.com>
2023-10-09 16:02:19 +0300

    Update packages/flame/lib/src/components/input/toggle_button_component.dart
    Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>

--- packages/flame/lib/src/components/input/toggle_button_component.dart
+++ packages/flame/lib/src/components/input/toggle_button_component.dart
@@ -6,9 +6,9 @@ import 'package:flutter/foundation.dart';
 /// between the selected and not selected state, imagine for example a switch widget
 /// or a tab that can be selected.
 ///
-/// Note: You have to set the [defaultSkin],[defaultSelectedSkin]
-/// and other in [onLoad] if you are not passing it in through
-/// the constructor
+/// Note: You have to set the [defaultSkin], [defaultSelectedSkin]
+/// and other skins that you want to use in [onLoad] if you are not passed in through
+/// the constructor.
 class ToggleButtonComponent extends AdvancedButtonComponent {
   ToggleButtonComponent({
     super.onPressed,
