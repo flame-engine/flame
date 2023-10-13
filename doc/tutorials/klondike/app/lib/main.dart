cb25f7edda6e61a5958eff304ca887eab4c48318
IanWadham <iandw.au@gmail.com>
2023-10-13 11:55:46 +1100

    Revert "Fixes for issue #2798" - undo 1 change and 6 file adds.
    This reverts commit 18f71bece67fad79b71b9185dc18101f81f0df27.

--- doc/tutorials/klondike/app/lib/main.dart
+++ doc/tutorials/klondike/app/lib/main.dart
@@ -1,12 +1,11 @@
-// import 'dart:html'; // ignore: avoid_web_libraries_in_flutter
+import 'dart:html'; // ignore: avoid_web_libraries_in_flutter
 import 'package:flutter/widgets.dart';
 import 'step2/main.dart' as step2;
 import 'step3/main.dart' as step3;
 import 'step4/main.dart' as step4;
 
 void main() {
-  // var page = window.location.search ?? '';
-  var page = 'step4';
+  var page = window.location.search ?? '';
   if (page.startsWith('?')) {
     page = page.substring(1);
   }
