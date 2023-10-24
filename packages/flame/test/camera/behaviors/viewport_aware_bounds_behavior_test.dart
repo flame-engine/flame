56035cc265e5fa8849d0eb92479bafa285212aa8
Hugo Delaunay <Skyost@users.noreply.github.com>
2023-10-24 10:53:51 +0200

    Preparing to add some tests.

---
+++ packages/flame/test/camera/behaviors/viewport_aware_bounds_behavior_test.dart
@@ -0,0 +1,11 @@
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
+import 'package:flame_test/flame_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ViewportAwareBoundsBehavior', () {
+
+  });
+}
