7529c49f6a1ef3d28b6aa7e0b96855139cbec67f
Ivan <ivan.femia@gmail.com>
2023-11-06 18:07:44 -0600

    Update move_along_path_effect_test.dart

--- packages/flame/test/effects/move_along_path_effect_test.dart
+++ packages/flame/test/effects/move_along_path_effect_test.dart
@@ -195,13 +195,17 @@ void main() {
         if (i <= 15) {
           expect(component.position.x, closeTo(200 + 6 * i, 1e-10));
           expect(component.position.y, closeTo(200 - 8 * i, 1e-10));
-          expect(component.angle,
-              closeTo(-asin(0.8) + component.nativeAngle, 1e-7));
+          expect(
+            component.angle,
+            closeTo(-asin(0.8) + component.nativeAngle, 1e-7),
+          );
         } else if (i <= 35) {
           expect(component.position.x, closeTo(290 + 8 * (i - 15), 1e-10));
           expect(component.position.y, closeTo(80 + 6 * (i - 15), 1e-10));
-          expect(component.angle,
-              closeTo(asin(0.6) + component.nativeAngle, 1e-7));
+          expect(
+            component.angle,
+            closeTo(asin(0.6) + component.nativeAngle, 1e-7),
+          );
         } else {
           expect(component.position.x, closeTo(450 - 10 * (i - 35), 1e-10));
           expect(component.position.y, closeTo(200, 1e-10));
@@ -237,13 +241,17 @@ void main() {
         if (i <= 15) {
           expect(component.position.x, closeTo(200 + 6 * i, 1e-10));
           expect(component.position.y, closeTo(200 - 8 * i, 1e-10));
-          expect(component.angle,
-              closeTo(-asin(0.8) + component.nativeAngle, 1e-7));
+          expect(
+            component.angle,
+            closeTo(-asin(0.8) + component.nativeAngle, 1e-7),
+          );
         } else if (i <= 35) {
           expect(component.position.x, closeTo(290 + 8 * (i - 15), 1e-10));
           expect(component.position.y, closeTo(80 + 6 * (i - 15), 1e-10));
-          expect(component.angle,
-              closeTo(asin(0.6) + component.nativeAngle, 1e-7));
+          expect(
+            component.angle,
+            closeTo(asin(0.6) + component.nativeAngle, 1e-7),
+          );
         } else {
           expect(component.position.x, closeTo(450 - 10 * (i - 35), 1e-10));
           expect(component.position.y, closeTo(200, 1e-10));
