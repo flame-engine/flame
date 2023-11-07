cc0a0259f62364fa1a2a7ab8357f93ff5fcb15f3
Ivan <ivan.femia@gmail.com>
2023-11-06 18:01:19 -0600

    Update move_along_path_effect_test.dart

--- packages/flame/test/effects/move_along_path_effect_test.dart
+++ packages/flame/test/effects/move_along_path_effect_test.dart
@@ -83,38 +83,26 @@ void main() {
           expect(component.position.x, closeTo(200 + 6 * i, 1e-10));
           expect(component.position.y, closeTo(200 - 8 * i, 1e-10));
           expect(
-            component.angle, 
-            closeTo(
-              -asin(0.8) + component.nativeAngle,
-              1e-7,
-            ),
+            component.angle,
+            closeTo(-asin(0.8) + component.nativeAngle, 1e-7),
           );
         } else if (i <= 35) {
           expect(component.position.x, closeTo(290 + 8 * (i - 15), 1e-10));
           expect(component.position.y, closeTo(80 + 6 * (i - 15), 1e-10));
           expect(
-            component.angle, 
-            closeTo(
-              -asin(0.6) + component.nativeAngle,
-              1e-7,
-            ),
+            component.angle,
+            closeTo(asin(0.6) + component.nativeAngle, 1e-7),
           );
         } else {
           expect(component.position.x, closeTo(450 - 10 * (i - 35), 1e-10));
           expect(component.position.y, closeTo(200, 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              pi + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle, closeTo(pi + component.nativeAngle, 1e-7));
         }
         game.update(0.1);
       }
     });
 
-    testWithFlameGame('absolute oriented path w/nativeAngle', (game) async {
+    testWithFlameGame('absolute oriented path with nativeAngle', (game) async {
       final component = PositionComponent(
         position: Vector2(17, -5),
         angle: -30.5,
@@ -141,32 +129,20 @@ void main() {
           expect(component.position.x, closeTo(200 + 6 * i, 1e-10));
           expect(component.position.y, closeTo(200 - 8 * i, 1e-10));
           expect(
-            component.angle, 
-            closeTo(
-              -asin(0.8) + component.nativeAngle,
-              1e-7,
-            ),
+            component.angle,
+            closeTo(-asin(0.8) + component.nativeAngle, 1e-7),
           );
         } else if (i <= 35) {
           expect(component.position.x, closeTo(290 + 8 * (i - 15), 1e-10));
           expect(component.position.y, closeTo(80 + 6 * (i - 15), 1e-10));
           expect(
-            component.angle, 
-            closeTo(
-              -asin(0.6) + component.nativeAngle,
-              1e-7,
-            ),
+            component.angle,
+            closeTo(asin(0.6) + component.nativeAngle, 1e-7),
           );
         } else {
           expect(component.position.x, closeTo(450 - 10 * (i - 35), 1e-10));
           expect(component.position.y, closeTo(200, 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              pi + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle, closeTo(pi + component.nativeAngle, 1e-7));
         }
         game.update(0.1);
       }
@@ -219,39 +195,24 @@ void main() {
         if (i <= 15) {
           expect(component.position.x, closeTo(200 + 6 * i, 1e-10));
           expect(component.position.y, closeTo(200 - 8 * i, 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              -asin(0.8) + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle,
+              closeTo(-asin(0.8) + component.nativeAngle, 1e-7));
         } else if (i <= 35) {
           expect(component.position.x, closeTo(290 + 8 * (i - 15), 1e-10));
           expect(component.position.y, closeTo(80 + 6 * (i - 15), 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              -asin(0.6) + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle,
+              closeTo(asin(0.6) + component.nativeAngle, 1e-7));
         } else {
           expect(component.position.x, closeTo(450 - 10 * (i - 35), 1e-10));
           expect(component.position.y, closeTo(200, 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              pi + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle, closeTo(pi + component.nativeAngle, 1e-7));
         }
         game.update(0.1);
       }
     });
 
-    testWithFlameGame('non-absolute oriented path w/nativeAngle', (game) async {
+    testWithFlameGame('non-absolute oriented path with nativeAngle',
+        (game) async {
       final component = PositionComponent(
         position: Vector2.zero(),
         angle: -30.5,
@@ -276,38 +237,22 @@ void main() {
         if (i <= 15) {
           expect(component.position.x, closeTo(200 + 6 * i, 1e-10));
           expect(component.position.y, closeTo(200 - 8 * i, 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              -asin(0.8) + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle,
+              closeTo(-asin(0.8) + component.nativeAngle, 1e-7));
         } else if (i <= 35) {
           expect(component.position.x, closeTo(290 + 8 * (i - 15), 1e-10));
           expect(component.position.y, closeTo(80 + 6 * (i - 15), 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              -asin(0.6) + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle,
+              closeTo(asin(0.6) + component.nativeAngle, 1e-7));
         } else {
           expect(component.position.x, closeTo(450 - 10 * (i - 35), 1e-10));
           expect(component.position.y, closeTo(200, 1e-10));
-          expect(
-            component.angle, 
-            closeTo(
-              pi + component.nativeAngle,
-              1e-7,
-            ),
-          );
+          expect(component.angle, closeTo(pi + component.nativeAngle, 1e-7));
         }
         game.update(0.1);
       }
     });
-    
+
     test('errors', () {
       final controller = LinearEffectController(0);
       expect(
