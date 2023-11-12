a6c8554f49b727879c4e6388151cdfe3e9b503c4
IanWadham <iandw.au@gmail.com>
2023-11-12 18:19:07 +1100

    feat: Add KlondikeWorld, delete most init, buttons not working yet

--- doc/tutorials/klondike/app/lib/step5/klondike_game.dart
+++ doc/tutorials/klondike/app/lib/step5/klondike_game.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:math';
 
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
@@ -27,103 +28,34 @@ class KlondikeGame extends FlameGame {
     const Radius.circular(cardRadius),
   );
 
+  // Used when creating Random seed. Cannot use the full 2^63 in Web apps.
+  static const int maxint = 2 ^ 53 - 1;
+
   // Default is Klondike Draw 1: a button switches between Draw 1 and Draw 3.
   var _klondikeDraw = 1;
 
   int get klondikeDraw => _klondikeDraw;
 
-  final stock = StockPile(position: Vector2(cardGap, topGap));
-  final waste = WastePile(position: Vector2(cardSpaceWidth + cardGap, topGap));
-  final List<FoundationPile> foundations = [];
-  final List<TableauPile> piles = [];
-  final List<Card> cards = [];
+  final klondikeWorld = KlondikeWorld(klondikeDraw: 1);
 
   @override
   Future<void> onLoad() async {
-    await Flame.images.load('klondike-sprites.png');
-
-    for (var i = 0; i < 4; i++) {
-      foundations.add(
-        FoundationPile(
-          i,
-          checkWin,
-          position: Vector2((i + 3) * cardSpaceWidth + cardGap, topGap),
-        ),
-      );
-    }
-    for (var i = 0; i < 7; i++) {
-      piles.add(
-        TableauPile(
-          position: Vector2(
-            i * cardSpaceWidth + cardGap,
-            cardSpaceHeight + topGap,
-          ),
-        ),
-      );
-    }
-
-    for (var rank = 1; rank <= 13; rank++) {
-      for (var suit = 0; suit < 4; suit++) {
-        cards.add(Card(rank, suit));
-      }
-    }
-
-    final gameSize =
-        Vector2(7 * cardSpaceWidth + cardGap, 4 * cardSpaceHeight + topGap);
-    final gameMidX = gameSize.x / 2;
-
-    world.add(stock);
-    world.add(waste);
-    world.addAll(foundations);
-    world.addAll(piles);
-    world.addAll(cards);
-
-    addButton('New deal', gameMidX, Startup.newDeal);
-    addButton('Same deal', gameMidX + cardSpaceWidth, Startup.sameDeal);
-    addButton('Draw 1 or 3', gameMidX + 2 * cardSpaceWidth, Startup.changeDraw);
-    addButton('Have fun', gameMidX + 3 * cardSpaceWidth, Startup.haveFun);
-
-    camera.viewfinder.visibleGameSize = gameSize;
-    camera.viewfinder.position = Vector2(gameMidX, 0);
-    camera.viewfinder.anchor = Anchor.topCenter;
-
-    init(Startup.first);
+    add(klondikeWorld);
   }
 
   void init(Startup startType) {
-    assert(cards.length == 52, 'There are ${cards.length} cards: should be 52');
     if (startType == Startup.first) {
       deal(Startup.first);
-    } else {
-      stock.init();
-      waste.init();
-      foundations.forEach((foundation) => foundation.init());
-      piles.forEach((tableau) => tableau.init());
-      cards.forEach((card) => card.init());
-
-      var nMovingCards = 0;
-      for (final card in cards) {
-        if (card.isFaceUp) {
-          card.flip();
-        }
-        if ((card.position - stock.position).length > 1.0) {
-          // Move cards that are not already in the Stock Pile.
-          nMovingCards++;
-          card.doMove(
-            stock.position,
-            onComplete: () {
-              nMovingCards--;
-              if (nMovingCards == 0) {
-                deal(startType);
-              }
-            },
-          );
-        }
-      }
-    }
+    } else {}
   }
 
   void deal(Startup startType) {
+    print('Deal: start type $startType');
+    final cards = klondikeWorld.cards;
+    final tableauPiles = klondikeWorld.tableauPiles;
+    final stock = klondikeWorld.stock;
+
+    assert(cards.length == 52, 'There are ${cards.length} cards: should be 52');
     switch (startType) {
       case Startup.first:
       case Startup.newDeal:
@@ -144,16 +76,16 @@ class KlondikeGame extends FlameGame {
       for (var j = i; j < 7; j++) {
         final card = cards[cardToDeal--];
         card.doMove(
-          piles[j].position,
+          tableauPiles[j].position,
           start: nMovingCards * 0.15,
           onComplete: () {
-            piles[j].acquireCard(card);
+            tableauPiles[j].acquireCard(card);
             nMovingCards--;
             if (nMovingCards == 0) {
               var delayFactor = 0;
-              for (final pile in piles) {
+              for (final tableauPile in tableauPiles) {
                 delayFactor++;
-                pile.flipTopCard(start: delayFactor * 0.15);
+                tableauPile.flipTopCard(start: delayFactor * 0.15);
               }
             }
           },
@@ -166,23 +98,9 @@ class KlondikeGame extends FlameGame {
     }
   }
 
-  void addButton(String label, double buttonX, Startup action) {
-    final button = FlatButton(
-      label,
-      size: Vector2(cardWidth, 0.6 * topGap),
-      position: Vector2(buttonX, topGap / 2),
-      onReleased: () {
-        if (action == Startup.haveFun) {
-          letsCelebrate();
-        } else {
-          init(action);
-        }
-      },
-    );
-    world.add(button);
-  }
-
   void checkWin() {
+    final foundations = klondikeWorld.foundations;
+
     var nComplete = 0;
     for (final f in foundations) {
       if (f.isFull) {
@@ -201,6 +119,8 @@ class KlondikeGame extends FlameGame {
     // First get the device's screen-size in game co-ordinates, then get the
     // top-left of the off-screen area that will accept the scattered cards.
     // Note: The play area is anchored at TopCenter, so topLeft.y is fixed.
+
+    final cards = klondikeWorld.cards;
 
     final cameraZoom = camera.viewfinder.zoom;
     final zoomedScreen = size / cameraZoom;
@@ -282,6 +202,106 @@ class KlondikeGame extends FlameGame {
         space = length[side] + space;
       }
     }
+  }
+}
+
+class KlondikeWorld extends World with HasGameReference<KlondikeGame> {
+  KlondikeWorld({this.klondikeDraw = 1, this.seed});
+
+  final cardGap = KlondikeGame.cardGap;
+  final topGap = KlondikeGame.topGap;
+  final cardSpaceWidth = KlondikeGame.cardSpaceWidth;
+  final cardSpaceHeight = KlondikeGame.cardSpaceHeight;
+
+  // Default is Klondike Draw 1: a button switches between Draw 1 and Draw 3.
+  final int klondikeDraw;
+
+  // The seed is used to make the shuffle repeat when "Same deal" is requested.
+  int? seed;
+  late Random random;
+
+  final stock = StockPile(position: Vector2(0.0, 0.0));
+  final waste = WastePile(position: Vector2(0.0, 0.0));
+  final List<FoundationPile> foundations = [];
+  final List<TableauPile> tableauPiles = [];
+  final List<Card> cards = [];
+
+  @override
+  Future<void> onLoad() async {
+    await Flame.images.load('klondike-sprites.png');
+
+    stock.position = Vector2(cardGap, topGap);
+    waste.position = Vector2(cardSpaceWidth + cardGap, topGap);
+
+    for (var i = 0; i < 4; i++) {
+      foundations.add(
+        FoundationPile(
+          i,
+          game.checkWin,
+          position: Vector2((i + 3) * cardSpaceWidth + cardGap, topGap),
+        ),
+      );
+    }
+    for (var i = 0; i < 7; i++) {
+      tableauPiles.add(
+        TableauPile(
+          position: Vector2(
+            i * cardSpaceWidth + cardGap,
+            cardSpaceHeight + topGap,
+          ),
+        ),
+      );
+    }
+    for (var rank = 1; rank <= 13; rank++) {
+      for (var suit = 0; suit < 4; suit++) {
+        cards.add(Card(this, rank, suit));
+      }
+    }
+
+    final gameSize =
+        Vector2(7 * cardSpaceWidth + cardGap, 4 * cardSpaceHeight + topGap);
+    final gameMidX = gameSize.x / 2;
+
+    add(stock);
+    add(waste);
+    addAll(foundations);
+    addAll(tableauPiles);
+    addAll(cards);
+
+    addButton('New deal', gameMidX, Startup.newDeal);
+    addButton('Same deal', gameMidX + cardSpaceWidth, Startup.sameDeal);
+    addButton('Draw 1 or 3', gameMidX + 2 * cardSpaceWidth, Startup.changeDraw);
+    addButton('Have fun', gameMidX + 3 * cardSpaceWidth, Startup.haveFun);
+
+    game.camera.world = this;
+    game.camera.viewfinder.visibleGameSize = gameSize;
+    game.camera.viewfinder.position = Vector2(gameMidX, 0);
+    game.camera.viewfinder.anchor = Anchor.topCenter;
+
+    // This creates a random seed if one was not supplied.
+    seed ??= Random().nextInt(KlondikeGame.maxint);
+    // This creates the random generator that should be used everywhere for shuffling etc.
+    random = Random(seed);
+    // ...
+    // This would maybe not be done in this method, just showing how to pass the random class to it.
+    cards.shuffle(random);
+    game.init(Startup.first);
+  }
+
+  void addButton(String label, double buttonX, Startup action) {
+    final button = FlatButton(
+      label,
+      size: Vector2(KlondikeGame.cardWidth, 0.6 * topGap),
+      position: Vector2(buttonX, topGap / 2),
+      onReleased: () {
+        if (action == Startup.haveFun) {
+          game.letsCelebrate();
+        } else {
+          game.init(action);
+        }
+      },
+    );
+    add(button);
   }
 }
 
