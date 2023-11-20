0822aa1002091300d07a7621a19ff27f5e627899
Erick <erickzanardoo@gmail.com>
2023-11-20 17:05:53 -0300

    PR suggestions

---
+++ packages/flame/lib/debug.dart
@@ -0,0 +1,4 @@
+export 'src/components/debug/child_counter_component.dart';
+export 'src/components/debug/time_track_component.dart';
+export 'src/components/fps_component.dart';
+export 'src/components/fps_text_component.dart';
