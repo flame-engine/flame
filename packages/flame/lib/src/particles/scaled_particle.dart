3730cb1d834c73c87dc3597554039fd0f0a32bae
Lukas Klingsbo <me@lukas.fyi>
2023-12-17 18:06:00 +0100

    fix: Export `ScalingParticle` (#2928)
    The `ScalingParticle` wasn't exported, this exports it and corrects the
    dartdocs.

--- packages/flame/lib/src/particles/scaled_particle.dart
+++ packages/flame/lib/src/particles/scaled_particle.dart
@@ -3,8 +3,11 @@ import 'dart:ui';
 import 'package:flame/src/components/mixins/single_child_particle.dart';
 import 'package:flame/src/particles/curved_particle.dart';
 import 'package:flame/src/particles/particle.dart';
+import 'package:flame/src/particles/scaling_particle.dart';
 
-/// A particle which scales its child over the lifespan to the set scale.
+/// Statically scales the given child [Particle] by given [scale].
+///
+/// If you're looking to scale the child over time, consider [ScalingParticle].
 class ScaledParticle extends CurvedParticle with SingleChildParticle {
   @override
   Particle child;
