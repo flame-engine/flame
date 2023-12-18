c521e2f46a1996d935121018d6aa4a73ca69ff97
Filip Hracek <filip.hracek@gmail.com>
2023-12-18 13:51:20 +0100

    Add docs

--- packages/flame/lib/src/cache/memory_cache.dart
+++ packages/flame/lib/src/cache/memory_cache.dart
@@ -1,13 +1,15 @@
 import 'dart:collection';
 
 /// Simple class to cache values with size based eviction.
-///
 class MemoryCache<K, V> {
   final LinkedHashMap<K, V> _cache = LinkedHashMap();
   final int cacheSize;
 
   MemoryCache({this.cacheSize = 10});
 
+  /// Adds the [value] to the cache under [key].
+  ///
+  /// If that [key] is already used, updates the value.
   void setValue(K key, V value) {
     final preexisting = _cache.containsKey(key);
     _cache[key] = value;
@@ -30,11 +32,15 @@ class MemoryCache<K, V> {
     _cache.clear();
   }
 
+  /// Gets the value under [key].
   V? getValue(K key) => _cache[key];
 
+  /// Checks whether the cache has any value under [key].
   bool containsKey(K key) => _cache.containsKey(key);
 
+  /// Returns the number of values saved in this memory cache.
   int get size => _cache.length;
 
+  /// Iterates over all existing keys with values.
   Iterable<K> get keys => _cache.keys;
 }
