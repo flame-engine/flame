78380b9d3bb895e20f382c4a1227bcc11e5038b9
DevKage <33748002+ufrshubham@users.noreply.github.com>
2024-02-11 22:36:51 +0530

    feat: Add TiledObjectHealpers extension on TiledObject (#3032)
    This PR adds an extension on `TiledObject` to allow getting the object's
    `position` and `size` as `Vector2` objects.
    
    ---------
    
    Co-authored-by: Lukas Klingsbo <me@lukas.fyi>

--- packages/flame_tiled/lib/flame_tiled.dart
+++ packages/flame_tiled/lib/flame_tiled.dart
@@ -2,6 +2,7 @@ library flame_tiled;
 
 export 'package:tiled/tiled.dart';
 
+export 'src/extensions.dart';
 export 'src/flame_tsx_provider.dart';
 export 'src/renderable_tile_map.dart';
 export 'src/simple_flips.dart';
