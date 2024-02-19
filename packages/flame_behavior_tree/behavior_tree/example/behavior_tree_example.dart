740e27d9ae666288492f71eefc5c78e1c614778c
DevKage <ufrshubham@gmail.com>
2024-02-19 10:37:21 +0530

    Create behavior_tree package

---
+++ packages/flame_behavior_tree/behavior_tree/example/behavior_tree_example.dart
@@ -0,0 +1,6 @@
+import 'package:behavior_tree/behavior_tree.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/flame_behavior_tree/behavior_tree/test/behavior_tree_test.dart
@@ -0,0 +1,16 @@
+import 'package:behavior_tree/behavior_tree.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
