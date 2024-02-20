5fa175ec3285600f5cd5604ec4b8b35370acb18a
DevKage <33748002+ufrshubham@users.noreply.github.com>
2024-02-21 00:35:24 +0530

    Add Task node

---
+++ packages/flame_behavior_tree/behavior_tree/lib/src/tasks/task.dart
@@ -0,0 +1,21 @@
+import 'package:behavior_tree/behavior_tree.dart';
+
+/// The type of callback used by the [Task] node.
+typedef TaskCallback = NodeStatus Function();
+
+/// This is a leaf node that will execute the given task when ticked.
+class Task implements Node {
+  Task(this.taskCallback);
+
+  /// The callback that will be executed when the task is ticked.
+  /// It should return the status of the task.
+  final TaskCallback taskCallback;
+
+  NodeStatus _status = NodeStatus.running;
+
+  @override
+  NodeStatus get status => _status;
+
+  @override
+  void tick() => _status = taskCallback();
+}
