cfc844a3dd468778c27ab38346bc8ab68e697908
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-02-20 09:23:32 +0100

    Update packages/flame_texturepacker/lib/flame_texturepacker.dart

--- packages/flame_texturepacker/lib/flame_texturepacker.dart
+++ packages/flame_texturepacker/lib/flame_texturepacker.dart
@@ -3,8 +3,8 @@ library flame_texturepacker;
 import 'package:flame/game.dart';
 import 'package:flame_texturepacker/src/texture_packer_atlas.dart';
 
-export 'package:flame_texturepacker/src/texture_packer_sprite.dart';
 export 'package:flame_texturepacker/src/texture_packer_atlas.dart';
+export 'package:flame_texturepacker/src/texture_packer_sprite.dart';
 
 extension TexturepackerLoader on Game {
   /// Loads the specified pack file.
