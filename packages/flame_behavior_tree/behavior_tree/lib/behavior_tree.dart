d8b27c329921c8f3e8a1aa1f6135145169fe9210
DevKage <33748002+ufrshubham@users.noreply.github.com>
2024-02-21 00:40:43 +0530

    Add tests for Task node

--- packages/flame_behavior_tree/behavior_tree/lib/behavior_tree.dart
+++ packages/flame_behavior_tree/behavior_tree/lib/behavior_tree.dart
@@ -3,8 +3,9 @@ library behavior_tree;
 
 export 'src/composites/selector.dart';
 export 'src/composites/sequence.dart';
-
 export 'src/decorators/inverter.dart';
 export 'src/decorators/limiter.dart';
 
 export 'src/node.dart';
+
+export 'src/tasks/task.dart';
