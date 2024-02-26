00ab42e5a24d8d67b4bab0e38a28406bbd359eb5
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2024-02-26 14:35:05 +0100

    feat(flame_3d): export a custom flame game

--- packages/flame_3d/lib/src/game/flame_game_3d.dart
+++ packages/flame_3d/lib/src/game/flame_game_3d.dart
@@ -3,17 +3,16 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 import 'package:flame_3d/camera.dart';
 
-class FlameGame3D<W extends World3D, C extends CameraComponent3D>
-    extends FlameGame<W> {
+class FlameGame3D<W extends World3D> extends FlameGame<W> {
   FlameGame3D({
     super.children,
     W? world,
-    C? camera,
+    CameraComponent3D? camera,
   }) : super(
           world: world ?? World3D(clearColor: const Color(0xFFFFFFFF)) as W,
           camera: camera ?? CameraComponent3D(),
         );
 
   @override
-  C get camera => super.camera as C;
+  CameraComponent3D get camera => super.camera as CameraComponent3D;
 }
