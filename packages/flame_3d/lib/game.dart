776f972b5ef0c50b4cbf97a18355d26bce80a8ef
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2024-02-26 14:33:49 +0100

    feat(flame_3d): export a custom flame game

--- packages/flame_3d/lib/game.dart
+++ packages/flame_3d/lib/game.dart
@@ -1,10 +1,6 @@
-import 'package:flame/game.dart';
-import 'package:flame_3d/camera.dart';
-
 export 'package:vector_math/vector_math.dart' hide Colors;
 
+export 'src/game/flame_game_3d.dart';
 export 'src/game/notifying_quaternion.dart';
 export 'src/game/notifying_vector3.dart';
 export 'src/game/transform_3d.dart';
-
-typedef FlameGame3D<W extends World3D> = FlameGame<W>;
