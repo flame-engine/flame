e434bafb15fc486c51b43aaa9d9190b8b7e783cb
Jochum van der Ploeg <jochum@vdploeg.net>
2024-02-26 15:52:33 +0100

    feat(flame_3d): initial implementation of 3D support (#3012)
    Co-authored-by: Lukas Klingsbo <me@lukas.fyi>

---
+++ packages/flame_3d/lib/src/resources/texture/color_texture.dart
@@ -0,0 +1,20 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flame_3d/resources.dart';
+
+/// {@template color_texture}
+/// A texture that holds a single color. By default it creates a 1x1 texture.
+/// {@endtemplate}
+class ColorTexture extends Texture {
+  /// {@macro color_texture}
+  ColorTexture(Color color, {int width = 1, int height = 1})
+      : super(
+          Uint32List.fromList(
+            List.filled(width * height, color.value),
+          ).buffer.asByteData(),
+          width: width,
+          height: height,
+          format: PixelFormat.bgra8888,
+        );
+}
