3243a480fbe1fe91ec915f5e41db4a4b931c142e
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-03-01 21:40:12 +0100

    fix: Revert "feat(flame_3d): initial implementation of 3D support (#3012)"
    This reverts commit e434bafb15fc486c51b43aaa9d9190b8b7e783cb.

--- packages/flame_3d/lib/src/extensions/color.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:typed_data';
-import 'dart:ui';
-
-extension ColorExtension on Color {
-  /// Returns a Float32List that represents the color as a vector.
-  Float32List get storage =>
-      Float32List.fromList([red / 255, green / 255, blue / 255, opacity]);
-}
--- packages/flame_3d/lib/src/extensions/vector2.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flame_3d/game.dart';
-
-/// Represents an immutable [Vector2].
-typedef ImmutableVector2 = ({double x, double y});
-
-extension Vector2Extension on Vector2 {
-  /// Returns an immutable representation of the vector.
-  ImmutableVector2 get immutable => (x: x, y: y);
-}
