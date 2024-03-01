741d9384dbfea7bb692f181a7689a7b10a947ef0
Lukas Klingsbo <me@lukas.fyi>
2024-03-01 21:48:40 +0100

    fix: Revert "feat(flame_3d): initial implementation of 3D support" (#3060)
    This reverts commit e434bafb15fc486c51b43aaa9d9190b8b7e783cb (flame_3d)
    for the time being so that we can get the pipeline back up running.

--- packages/flame_3d/lib/src/extensions/vector4.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flame_3d/game.dart';
-
-/// Represents an immutable [Vector3].
-typedef ImmutableVector4 = ({double x, double y, double z, double w});
-
-extension Vector4Extension on Vector4 {
-  /// Returns an immutable representation of the vector.
-  ImmutableVector4 get immutable => (x: x, y: y, z: z, w: w);
-}
