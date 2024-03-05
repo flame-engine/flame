c92910c688f5dc4463e129132759102e7ebf2e36
Lukas Klingsbo <me@lukas.fyi>
2024-03-05 01:04:43 +0100

    feat: Initial functionality of flame_devtools (#3061)
    ![image](https://github.com/flame-engine/flame/assets/744771/4ca93f5f-369e-4644-b7fb-1d7790b962e2)
    
    This adds a structure and some basic functionality for the Flame
    devtools extension.
    
    Later I will add a pre/post-hook for publishing to Melos so that it can
    build the devtools extension before publishing (and remove the directory
    afterwards), since it isn't committed to this repository. For now one
    has to run `melos devtools-build` before publishing.
    
    ---------
    
    Co-authored-by: Renan <6718144+renancaraujo@users.noreply.github.com>
    Co-authored-by: Erick <erickzanardoo@gmail.com>

---
+++ packages/flame_devtools/lib/repository.dart
@@ -0,0 +1,55 @@
+import 'package:devtools_extensions/devtools_extensions.dart';
+
+sealed class Repository {
+  Repository._();
+
+  static Future<int> getComponentCount() async {
+    final componentCountResponse =
+        await serviceManager.callServiceExtensionOnMainIsolate(
+      'ext.flame_devtools.getComponentCount',
+    );
+    return componentCountResponse.json!['component_count'] as int;
+  }
+
+  static Future<bool> swapDebugMode() async {
+    final nextDebugMode = !(await getDebugMode());
+    await serviceManager.callServiceExtensionOnMainIsolate(
+      'ext.flame_devtools.setDebugMode',
+      args: {'debug_mode': nextDebugMode},
+    );
+    return nextDebugMode;
+  }
+
+  static Future<bool> getDebugMode() async {
+    final debugModeResponse =
+        await serviceManager.callServiceExtensionOnMainIsolate(
+      'ext.flame_devtools.getDebugMode',
+    );
+    return debugModeResponse.json!['debug_mode'] as bool;
+  }
+
+  // ignore: avoid_positional_boolean_parameters
+  static Future<bool> setPaused(bool shouldPause) async {
+    await serviceManager.callServiceExtensionOnMainIsolate(
+      'ext.flame_devtools.setPaused',
+      args: {'paused': shouldPause},
+    );
+    return shouldPause;
+  }
+
+  static Future<bool> getPaused() async {
+    final getPausedResponse =
+        await serviceManager.callServiceExtensionOnMainIsolate(
+      'ext.flame_devtools.getPaused',
+    );
+    return getPausedResponse.json!['paused'] as bool;
+  }
+
+  static Future<double> step({required double stepTime}) async {
+    final stepResponse = await serviceManager.callServiceExtensionOnMainIsolate(
+      'ext.flame_devtools.step',
+      args: {'step_time': stepTime.toString()},
+    );
+    return stepResponse.json!['step_time'] as double;
+  }
+}
