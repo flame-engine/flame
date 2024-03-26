b4913c0857f9cbde6f293ee9691b8a52af663404
Yayo Arellano <gerardo_28@hotmail.com>
2024-03-26 21:15:26 +0800

    Deprecate the function fromAtlas in favour of atlasFromAssets and atlasFromStorage

--- packages/flame_texturepacker/lib/flame_texturepacker.dart
+++ packages/flame_texturepacker/lib/flame_texturepacker.dart
@@ -9,9 +9,20 @@ export 'package:flame_texturepacker/src/texture_packer_sprite.dart';
 extension TexturepackerLoader on Game {
   /// Loads the specified pack file.
   /// Uses the parent directory of the pack file to find the page images.
+  @Deprecated('Use [atlasFromAssets] or [atlasFromStorage] instead')
   Future<TexturePackerAtlas> fromAtlas(
     String assetsPath, {
     bool fromStorage = false,
   }) async =>
       TexturePackerAtlas.load(assetsPath, fromStorage: fromStorage);
+
+  /// Loads the specified pack file from assets
+  /// Uses the parent directory of the pack file to find the page images.
+  Future<TexturePackerAtlas> atlasFromAssets(String assetsPath) async =>
+      TexturePackerAtlas.load(assetsPath);
+
+  /// Loads the specified pack file from storage
+  /// Uses the parent directory of the pack file to find the page images.
+  Future<TexturePackerAtlas> atlasFromStorage(String storagePath) async =>
+      TexturePackerAtlas.load(storagePath, fromStorage: true);
 }
