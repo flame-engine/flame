684ff1b236920e8974aedb473a405b77f861c335
Luan Nico <luanpotter27@gmail.com>
2024-09-11 23:59:09 +0200

    chore: Fix deprecated color methods [flame_3d] (#3295)
    Fix deprecated color methods

--- packages/flame_3d/lib/src/extensions/color.dart
+++ packages/flame_3d/lib/src/extensions/color.dart
@@ -3,10 +3,5 @@ import 'dart:ui';
 
 extension ColorExtension on Color {
   /// Returns a Float32List that represents the color as a vector.
-  Float32List get storage => Float32List.fromList([
-        opacity,
-        red.toDouble() / 255,
-        green.toDouble() / 255,
-        blue.toDouble() / 255,
-      ]);
+  Float32List get storage => Float32List.fromList([a, r, g, b]);
 }
