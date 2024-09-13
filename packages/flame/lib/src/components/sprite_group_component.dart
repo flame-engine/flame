763615270639d66f36be2685471b2fcd273209a8
puneet <puneetkashyap20@gmail.com>
2024-09-13 16:17:27 +0530

    replaced null check operator with null aware

--- packages/flame/lib/src/components/sprite_group_component.dart
+++ packages/flame/lib/src/components/sprite_group_component.dart
@@ -100,7 +100,7 @@ class SpriteGroupComponent<T> extends PositionComponent
 
   /// Updates the sprite for the given key.
   void updateSprite(T key, Sprite sprite) {
-    _sprites![key] = sprite;
+    _sprites?[key] = sprite;
     _resizeToSprite();
   }
 
