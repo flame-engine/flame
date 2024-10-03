3b33ca4003d188f12443830a6f16c58e668cf6ec
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-10-03 21:50:23 +0200

    fix: Don't update the sprites until onMount

--- packages/flame/lib/src/components/input/sprite_button_component.dart
+++ packages/flame/lib/src/components/input/sprite_button_component.dart
@@ -47,12 +47,16 @@ class SpriteButtonComponent extends SpriteGroupComponent<ButtonState>
 
   set button(Sprite value) {
     _button = value;
-    updateSprite(ButtonState.up, value);
+    if (isLoaded) {
+      updateSprite(ButtonState.up, value);
+    }
   }
 
   set buttonDown(Sprite value) {
     _buttonDown = value;
-    updateSprite(ButtonState.down, value);
+    if (isLoaded) {
+      updateSprite(ButtonState.down, value);
+    }
   }
 
   @override
@@ -62,7 +66,7 @@ class SpriteButtonComponent extends SpriteGroupComponent<ButtonState>
       'The button sprite has to be set either in onLoad or in the constructor',
     );
     sprites = {
-      ButtonState.up: button,
+      ButtonState.up: _button!,
       ButtonState.down: buttonDown,
     };
     super.onMount();
