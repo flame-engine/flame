7068e2ec1c0667a1bdf8b1ddabd0da53496b2b43
Erick Zanardo <erickzanardoo@gmail.com>
2024-10-31 17:37:03 -0300

    fix(flame_console): MemoryRepository can't be const

--- packages/flame_console/lib/src/repository/memory_console_repository.dart
+++ packages/flame_console/lib/src/repository/memory_console_repository.dart
@@ -3,11 +3,13 @@ import 'package:flame_console/flame_console.dart';
 /// An implementation of a [ConsoleRepository] that stores the command history
 /// in memory.
 class MemoryConsoleRepository extends ConsoleRepository {
-  const MemoryConsoleRepository({
-    List<String> commands = const [],
-  }) : _commands = commands;
+  MemoryConsoleRepository({
+    List<String>? commands,
+  }) {
+    _commands = commands ?? <String>[];
+  }
 
-  final List<String> _commands;
+  late final List<String> _commands;
 
   @override
   Future<void> addToCommandHistory(String command) async {
