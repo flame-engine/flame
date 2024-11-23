d5e7702236aa17117827501261ef198b2d271540
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-11-23 16:02:04 +0100

    feat: Add WorldRoute enable swapping worlds from the RouterComponent

--- packages/flame/lib/game.dart
+++ packages/flame/lib/game.dart
@@ -3,10 +3,11 @@
 library game;
 
 export 'src/collisions/has_collision_detection.dart';
-export 'src/components/overlay_route.dart' show OverlayRoute;
-export 'src/components/route.dart' show Route;
-export 'src/components/router_component.dart' show RouterComponent;
-export 'src/components/value_route.dart' show ValueRoute;
+export 'src/components/router/overlay_route.dart' show OverlayRoute;
+export 'src/components/router/route.dart' show Route;
+export 'src/components/router/router_component.dart' show RouterComponent;
+export 'src/components/router/value_route.dart' show ValueRoute;
+export 'src/components/router/world_route.dart' show WorldRoute;
 export 'src/extensions/vector2.dart';
 export 'src/game/flame_game.dart';
 export 'src/game/game.dart';
