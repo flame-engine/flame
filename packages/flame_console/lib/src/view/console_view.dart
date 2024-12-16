de74a93b44f442341f816a2988c854f40902ff7e
Erick <erickzanardoo@gmail.com>
2024-12-16 12:00:26 -0300

    feat: Refactoring flame_console to use terminui (#3388)
    Simplify flame_console to use `terminui` instead of implementing the
    terminal interface itself.

--- packages/flame_console/lib/src/view/console_view.dart
+++ packages/flame_console/lib/src/view/console_view.dart
@@ -1,23 +1,9 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_console/flame_console.dart';
-import 'package:flame_console/src/controller.dart';
-import 'package:flame_console/src/view/container_builder.dart';
-import 'package:flame_console/src/view/cursor_builder.dart';
-import 'package:flame_console/src/view/history_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-typedef HistoryBuilder = Widget Function(
-  BuildContext context,
-  ScrollController scrollController,
-  Widget child,
-);
-
-typedef ContainerBuilder = Widget Function(
-  BuildContext context,
-  Widget child,
-);
+import 'package:terminui/terminui.dart';
 
 /// A Console like view that can be used to interact with a game.
 ///
@@ -38,8 +24,8 @@ typedef ContainerBuilder = Widget Function(
 ///     ),
 ///   },
 /// )
-class ConsoleView<G extends FlameGame> extends StatefulWidget {
-  const ConsoleView({
+class FlameConsoleView<G extends FlameGame> extends StatefulWidget {
+  const FlameConsoleView({
     required this.game,
     required this.onClose,
     this.customCommands,
@@ -49,15 +35,13 @@ class ConsoleView<G extends FlameGame> extends StatefulWidget {
     this.cursorColor,
     this.historyBuilder,
     this.textStyle,
-    @visibleForTesting this.controller,
     super.key,
   });
 
   final G game;
-  final List<ConsoleCommand<G>>? customCommands;
+  final List<FlameConsoleCommand<G>>? customCommands;
   final VoidCallback onClose;
-  final ConsoleRepository? repository;
-  final ConsoleController? controller;
+  final TerminuiRepository? repository;
 
   final ContainerBuilder? containerBuilder;
   final WidgetBuilder? cursorBuilder;
@@ -67,7 +51,7 @@ class ConsoleView<G extends FlameGame> extends StatefulWidget {
   final TextStyle? textStyle;
 
   @override
-  State<ConsoleView> createState() => _ConsoleViewState();
+  State<FlameConsoleView> createState() => _ConsoleViewState();
 }
 
 class _ConsoleKeyboardHandler extends Component with KeyboardHandler {
@@ -82,28 +66,16 @@ class _ConsoleKeyboardHandler extends Component with KeyboardHandler {
   }
 }
 
-class _ConsoleViewState extends State<ConsoleView> {
-  late final List<ConsoleCommand> _commandList = [
-    ...ConsoleCommands.commands,
+class _ConsoleViewState extends State<FlameConsoleView> {
+  late final List<FlameConsoleCommand> _commandList = [
+    ...FlameConsoleCommands.commands,
     if (widget.customCommands != null) ...widget.customCommands!,
   ];
 
-  late final repository = widget.repository ?? MemoryConsoleRepository();
+  late final repository = widget.repository ?? MemoryTerminuiRepository();
 
-  late final Map<String, ConsoleCommand> _commandsMap = {
-    for (final command in _commandList) command.name: command,
-  };
+  late final _keyboardEventEmitter = KeyboardEventEmitter();
 
-  late final _controller = widget.controller ??
-      ConsoleController(
-        repository: repository,
-        game: widget.game,
-        scrollController: _scrollController,
-        onClose: widget.onClose,
-        commands: _commandsMap,
-      );
-
-  late final _scrollController = ScrollController();
   late final KeyboardHandler _keyboardHandler;
 
   @override
@@ -112,126 +84,31 @@ class _ConsoleViewState extends State<ConsoleView> {
 
     widget.game.add(
       _keyboardHandler = _ConsoleKeyboardHandler(
-        _controller.handleKeyEvent,
+        _keyboardEventEmitter.emit,
       ),
     );
-
-    _controller.init();
   }
 
   @override
   void dispose() {
     _keyboardHandler.removeFromParent();
-    _scrollController.dispose();
+    _keyboardEventEmitter.dispose();
 
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final cursorColor = widget.cursorColor ?? Colors.white;
-
-    final textStyle = widget.textStyle ??
-        Theme.of(context).textTheme.bodySmall?.copyWith(
-              color: Colors.white,
-            );
-
-    final historyBuilder = widget.historyBuilder ?? defaultHistoryBuilder;
-    final containerBuilder = widget.containerBuilder ?? defaultContainerBuilder;
-    final cursorBuilder = widget.cursorBuilder ?? defaultCursorBuilder;
-
-    return ValueListenableBuilder(
-      valueListenable: _controller.state,
-      builder: (context, state, _) {
-        return SizedBox(
-          height: 400,
-          width: double.infinity,
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 48,
-                child: containerBuilder(
-                  context,
-                  historyBuilder(
-                    context,
-                    _scrollController,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        for (final line in state.history)
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Text(line, style: textStyle),
-                          ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              if (state.showHistory)
-                Positioned(
-                  bottom: 48,
-                  left: 0,
-                  right: 0,
-                  child: containerBuilder(
-                    context,
-                    SizedBox(
-                      height: 168,
-                      child: Column(
-                        verticalDirection: VerticalDirection.up,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (state.commandHistory.isEmpty)
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: Text('No history', style: textStyle),
-                            ),
-                          for (var i = state.commandHistoryIndex;
-                              i >= 0 && i >= state.commandHistoryIndex - 5;
-                              i--)
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: ColoredBox(
-                                color: i == state.commandHistoryIndex
-                                    ? cursorColor.withValues(alpha: .5)
-                                    : Colors.transparent,
-                                child: Text(
-                                  state.commandHistory[i],
-                                  style: textStyle?.copyWith(
-                                    color: i == state.commandHistoryIndex
-                                        ? cursorColor
-                                        : textStyle.color,
-                                  ),
-                                ),
-                              ),
-                            ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: containerBuilder(
-                  context,
-                  Row(
-                    children: [
-                      Text(state.cmd, style: textStyle),
-                      SizedBox(width: (textStyle?.fontSize ?? 12) / 4),
-                      cursorBuilder(context),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
+    return TerminuiView(
+      onClose: widget.onClose,
+      commands: _commandList,
+      subject: widget.game,
+      keyboardEventEmitter: _keyboardEventEmitter,
+      containerBuilder: widget.containerBuilder,
+      cursorBuilder: widget.cursorBuilder,
+      cursorColor: widget.cursorColor,
+      historyBuilder: widget.historyBuilder,
+      textStyle: widget.textStyle,
     );
   }
 }
