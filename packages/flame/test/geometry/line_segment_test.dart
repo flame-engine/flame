74763573c027fd41bbfae2f803a99e303ca84fbd
Luan Nico <luanpotter27@gmail.com>
2025-01-15 10:31:57 -0500

    feat: Add direction and length getters and constructor to LineSegment (#3446)
    feat: Add direction and length getters and constructor to LineSegment

--- packages/flame/test/geometry/line_segment_test.dart
+++ packages/flame/test/geometry/line_segment_test.dart
@@ -11,5 +11,25 @@ void main() {
       final lineSegment2 = LineSegment(Vector2.all(0), Vector2(0, 2));
       expect(lineSegment2.midpoint, Vector2(0, 1));
     });
+
+    test('(to, from) and (direction, length) are equivalent', () {
+      final lineSegment1 = LineSegment(Vector2(1, 1), Vector2(2, 1));
+      expect(lineSegment1.from, Vector2(1, 1));
+      expect(lineSegment1.to, Vector2(2, 1));
+
+      expect(lineSegment1.length, 1);
+      expect(lineSegment1.direction, Vector2(1, 0));
+
+      final lineSegment2 = LineSegment.withLength(
+        start: Vector2(1, 1),
+        direction: Vector2(1, 0),
+        length: 1,
+      );
+      expect(lineSegment2.from, Vector2(1, 1));
+      expect(lineSegment2.to, Vector2(2, 1));
+
+      expect(lineSegment2.length, 1);
+      expect(lineSegment2.direction, Vector2(1, 0));
+    });
   });
 }
