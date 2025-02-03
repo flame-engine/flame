333cef74a67c74658fa781d5e3e825dcad87828d
Treorai <50612887+Treorai@users.noreply.github.com>
2025-02-03 19:33:46 -0300

    Add: Flip methods around an anchor point

--- packages/flame/lib/src/components/position_component.dart
+++ packages/flame/lib/src/components/position_component.dart
@@ -416,6 +416,25 @@ class PositionComponent extends Component
     transform.flipVertically();
   }
 
+  void flipHorizontallyAroundAnchor(Anchor anchorPoint) {
+    if (anchor.x != 0.5) {
+      final delta = (1 - 2 * anchorPoint.x) * width * transform.scale.x;
+      transform.x += delta * math.cos(transform.angle);
+      transform.y += delta * math.sin(transform.angle);
+    }
+    transform.flipHorizontally();
+  }
+
+  /// Flip the component vertically around a given anchor point.
+  void flipVerticallyAroundAnchor(Anchor anchorPoint) {
+    if (anchor.y != 0.5) {
+      final delta = (1 - 2 * anchorPoint.y) * height * transform.scale.y;
+      transform.x += -delta * math.sin(transform.angle);
+      transform.y += delta * math.cos(transform.angle);
+    }
+    transform.flipVertically();
+  }
+
   /// Whether it is currently flipped horizontally.
   bool get isFlippedHorizontally => transform.scale.x.isNegative;
 
