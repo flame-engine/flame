70ed7fcb5c570431eb8e0d5a78737dc6f98777da
Treorai <50612887+Treorai@users.noreply.github.com>
2025-02-04 22:35:40 -0300

    Fix: Correct name of the variables being checked

--- packages/flame/lib/src/components/position_component.dart
+++ packages/flame/lib/src/components/position_component.dart
@@ -418,7 +418,7 @@ class PositionComponent extends Component
 
   /// Flip the component horizontally around a given anchor point.
   void flipHorizontallyAroundAnchor(Anchor anchorPoint) {
-    if (anchor.x != 0.5) {
+    if (anchorPoint.x != 0.5) {
       final delta = (1 - 2 * anchorPoint.x) * width * transform.scale.x;
       transform.x += delta * math.cos(transform.angle);
       transform.y += delta * math.sin(transform.angle);
@@ -428,7 +428,7 @@ class PositionComponent extends Component
 
   /// Flip the component vertically around a given anchor point.
   void flipVerticallyAroundAnchor(Anchor anchorPoint) {
-    if (anchor.y != 0.5) {
+    if (anchorPoint.y != 0.5) {
       final delta = (1 - 2 * anchorPoint.y) * height * transform.scale.y;
       transform.x += -delta * math.sin(transform.angle);
       transform.y += delta * math.cos(transform.angle);
