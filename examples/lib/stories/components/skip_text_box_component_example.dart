dc5f8d7f8c24779d55d4aab42e996d9a65268eac
Liv Tan Ong <livtanong@gmail.com>
2025-02-13 14:46:00 +0800

    Replace skip_text_box_component passage with real english monologue for spell checker

--- examples/lib/stories/components/skip_text_box_component_example.dart
+++ examples/lib/stories/components/skip_text_box_component_example.dart
@@ -12,7 +12,7 @@ class SkipTextBoxComponentExample extends FlameGame {
   @override
   FutureOr<void> onLoad() {
     final textBoxComponent = TextBoxComponent(
-      text: loremIpsum,
+      text: samplePassage,
       position: Vector2(48, 48 * 2),
       boxConfig: const TextBoxConfig(
         maxWidth: 480,
@@ -29,15 +29,17 @@ class SkipTextBoxComponentExample extends FlameGame {
     ]);
   }
 
-  static const String loremIpsum = '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porttitor vel dui efficitur venenatis. Praesent justo lectus, vestibulum vel erat in, placerat pharetra enim. Proin molestie congue augue, id lacinia mi. Nulla enim urna, imperdiet a lectus quis, venenatis commodo orci. Ut vitae luctus nulla. Integer eu tellus justo. Vivamus interdum nibh eget justo auctor porttitor. Proin interdum ligula massa, ac fermentum velit posuere in. In eu lacus vel ante euismod imperdiet. Pellentesque a mauris lacus. Nullam et lacus id sem tempus faucibus. Vivamus quis lacinia arcu. Integer condimentum non velit non tristique. Donec dapibus ipsum ac ipsum eleifend, blandit luctus purus efficitur. Donec tempus et arcu eu lobortis.
+  static const String samplePassage = '''
+Look again at that dot. That's here. That's home. That's us. On it everyone you love, everyone you know, everyone you ever heard of, every human being who ever was, lived out their lives. The aggregate of our joy and suffering, thousands of confident religions, ideologies, and economic doctrines, every hunter and forager, every hero and coward, every creator and destroyer of civilization, every king and peasant, every young couple in love, every mother and father, hopeful child, inventor and explorer, every teacher of morals, every corrupt politician, every "superstar," every "supreme leader," every saint and sinner in the history of our species lived there--on a mote of dust suspended in a sunbeam.
 
-Ut vel tellus elit. Fusce id urna quis mi maximus sodales sed a lorem. Nam auctor faucibus eros sed tempor. Pellentesque non nunc blandit erat venenatis tincidunt eu vitae sapien. Fusce semper tincidunt ultricies. Vivamus sed felis et lacus lobortis pharetra. Nulla suscipit, elit a luctus faucibus, dolor orci consectetur elit, a bibendum dolor justo sit amet lectus. Pellentesque malesuada placerat tortor nec sollicitudin. Nunc eu ex consectetur, suscipit urna efficitur, congue tortor. Donec luctus vel eros et feugiat. In feugiat, nunc non pharetra pulvinar, velit nisi malesuada purus, ut blandit metus ex ac libero. Sed gravida turpis in eleifend interdum.
+The Earth is a very small stage in a vast cosmic arena. Think of the rivers of blood spilled by all those generals and emperors so that, in glory and triumph, they could become the momentary masters of a fraction of a dot. Think of the endless cruelties visited by the inhabitants of one corner of this pixel on the scarcely distinguishable inhabitants of some other corner, how frequent their misunderstandings, how eager they are to kill one another, how fervent their hatreds.
 
-Suspendisse potenti. Cras nec bibendum orci. Integer lacinia vel enim non dapibus. Maecenas eleifend mi ut sodales ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc sed felis massa. Etiam blandit sollicitudin erat, nec hendrerit tellus luctus eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer mollis et felis sed tempus. Nam in tellus tortor. Aliquam rutrum, sapien vitae lacinia sagittis, sapien purus interdum ligula, at hendrerit velit neque sit amet nisi. Aliquam nec erat eu sapien efficitur rhoncus. Maecenas condimentum elementum dui. Praesent commodo euismod commodo.
+Our posturings, our imagined self-importance, the delusion that we have some privileged position in the Universe, are challenged by this point of pale light. Our planet is a lonely speck in the great enveloping cosmic dark. In our obscurity, in all this vastness, there is no hint that help will come from elsewhere to save us from ourselves.
 
-In dapibus elementum semper. Curabitur et congue neque, tempus ullamcorper dui. Nunc ut mi pharetra, interdum nulla eu, pellentesque dolor. Vivamus ante lectus, efficitur et diam facilisis, tincidunt efficitur enim. Etiam lorem tellus, vehicula at hendrerit a, posuere sed nisl. In et sem vehicula, aliquam eros sit amet, suscipit ante. Suspendisse finibus sodales mi, eget scelerisque sem egestas non. Sed sagittis at urna ac scelerisque. Proin mollis lectus vitae nisi ullamcorper, sit amet semper sapien aliquet. Cras fringilla tellus sit amet tincidunt interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean molestie cursus aliquet. Fusce finibus lobortis est, sed iaculis nisi placerat aliquam. Phasellus mollis lorem vel turpis rutrum, fermentum euismod dui porttitor. Nullam mollis ante eu leo commodo faucibus in vitae nisi.
+The Earth is the only world known so far to harbor life. There is nowhere else, at least in the near future, to which our species could migrate. Visit, yes. Settle, not yet. Like it or not, for the moment the Earth is where we make our stand.
 
-Mauris quis convallis neque. Sed blandit dapibus libero, a euismod nunc accumsan non. Pellentesque cursus sem egestas, bibendum arcu sit amet, rhoncus nulla. Cras eleifend enim ut nisi consectetur, in dignissim lorem auctor. Proin id odio facilisis, lobortis justo quis, congue libero. Quisque scelerisque facilisis purus. Nunc turpis ex, tempor id ligula at, accumsan semper tellus. Donec aliquet vestibulum lorem ut facilisis. Donec tempus maximus tellus, nec sollicitudin arcu condimentum a. Morbi a libero eu magna dictum luctus. Mauris sed mollis orci. Duis a lacinia tellus, sed tristique tortor. Duis vestibulum nulla libero, in mollis mi iaculis sit amet.
+It has been said that astronomy is a humbling and character-building experience. There is perhaps no better demonstration of the folly of human conceits than this distant image of our tiny world. To me, it underscores our responsibility to deal more kindly with one another, and to preserve and cherish the pale blue dot, the only home we've ever known.
+
+— Carl Sagan, Pale Blue Dot, 1994
 ''';
 }
