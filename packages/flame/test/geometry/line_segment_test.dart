38362a274925f2f22d35c75b9fba765241ca0b42
Luan Nico <luanpotter27@gmail.com>
2025-04-15 19:23:04 -0400

    feat: Add helper methods on LineSegment (translate, inflate, deflate, spread)

--- packages/flame/test/geometry/line_segment_test.dart
+++ packages/flame/test/geometry/line_segment_test.dart
@@ -31,5 +31,67 @@ void main() {
       expect(lineSegment2.length, 1);
       expect(lineSegment2.direction, Vector2(1, 0));
     });
+
+    test('translate line segment', () {
+      final line = LineSegment(Vector2.all(1), Vector2.all(2));
+
+      final lineUp = line.translate(Vector2(0, -1));
+      expect(lineUp.from, Vector2(1, 0));
+      expect(lineUp.to, Vector2(2, 1));
+
+      final lineDown = line.translate(Vector2(0, 1));
+      expect(lineDown.from, Vector2(1, 2));
+      expect(lineDown.to, Vector2(2, 3));
+
+      final lineLeft = line.translate(Vector2(-1, 0));
+      expect(lineLeft.from, Vector2(0, 1));
+      expect(lineLeft.to, Vector2(1, 2));
+
+      final lineRight = line.translate(Vector2(1, 0));
+      expect(lineRight.from, Vector2(2, 1));
+      expect(lineRight.to, Vector2(3, 2));
+
+      final lineDiagonal = line.translate(Vector2(1, -1));
+      expect(lineDiagonal.from, Vector2(2, 0));
+      expect(lineDiagonal.to, Vector2(3, 1));
+    });
+
+    test('inflate and deflate', () {
+      final line = LineSegment(Vector2(1, 2), Vector2(3, 2));
+
+      final inflatedLine = line.inflate(1);
+      expect(inflatedLine.from, Vector2(0, 2));
+      expect(inflatedLine.to, Vector2(4, 2));
+
+      final deflatedLine = line.deflate(0.5);
+      expect(deflatedLine.from, Vector2(1.5, 2));
+      expect(deflatedLine.to, Vector2(2.5, 2));
+    });
+
+    test('spread', () {
+      final line = LineSegment(Vector2(0, 0), Vector2(10, 0));
+
+      final points1 = line.spread(4);
+      expect(points1.length, 4);
+      expect(points1[0], Vector2(2, 0));
+      expect(points1[1], Vector2(4, 0));
+      expect(points1[2], Vector2(6, 0));
+      expect(points1[3], Vector2(8, 0));
+
+      final points2 = line.spread(0);
+      expect(points2.length, 0);
+
+      // expect an exception with message for negative amount
+      expect(
+        () => line.spread(-1),
+        throwsA(
+          isA<ArgumentError>().having(
+            (e) => e.message,
+            'message',
+            'Amount must be non-negative',
+          ),
+        ),
+      );
+    });
   });
 }
