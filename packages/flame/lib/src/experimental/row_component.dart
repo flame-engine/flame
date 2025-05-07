1827cc4f397f6f1ba0627bc9e70b199f33ce81b5
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2025-05-07 22:42:45 +0200

    docs: Update structure and add RowComponent + ColumnComponent docs

--- packages/flame/lib/src/experimental/row_component.dart
+++ packages/flame/lib/src/experimental/row_component.dart
@@ -1,9 +1,32 @@
 import 'package:flame/src/experimental/layout_component.dart';
 import 'package:flutter/rendering.dart';
 
-/// Arranges [children] in a row, [gap] points apart.
-/// The [children] are arranged [mainAxisAlignment] along the horizontal axis,
-/// and [crossAxisAlignment] along the vertical axis.
+/// RowComponent is a layout component that arranges its children in a
+/// horizontal row.
+///
+/// The [children] are laid out along the horizontal axis, with spacing between
+/// them defined by the [gap] parameter.
+/// The alignment of the children along the horizontal axis is controlled by
+/// [mainAxisAlignment], while their alignment along the vertical axis is
+/// controlled by [crossAxisAlignment].
+///
+/// If [shrinkWrap] is set to true, the size of the row will shrink to fit its
+/// children. Otherwise, the size of the row will be determined by the [size]
+/// parameter or the size of its parent.
+///
+/// Example usage:
+/// ```dart
+/// RowComponent(
+///   gap: 10.0,
+///   mainAxisAlignment: MainAxisAlignment.center,
+///   crossAxisAlignment: CrossAxisAlignment.start,
+///   children: [
+///     TextComponent('Child 1'),
+///     TextComponent('Child 2'),
+///     TextComponent('Child 3'),
+///   ],
+/// );
+/// ```
 class RowComponent extends LayoutComponent {
   RowComponent({
     super.mainAxisAlignment = MainAxisAlignment.start,
