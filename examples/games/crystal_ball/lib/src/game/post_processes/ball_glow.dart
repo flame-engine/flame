baecb86186a1bff7f21d804e7867f894d2f9d23c
Lukas Klingsbo <me@lukas.fyi>
2025-05-26 22:00:25 +0200

    fix: PostProcessComponent should size dynamically (#3611)
    The `PostProcessComponent` now updates its size if the children list
    changes.
    This only applies if it doesn't have an explicit size.

--- examples/games/crystal_ball/lib/src/game/post_processes/ball_glow.dart
+++ examples/games/crystal_ball/lib/src/game/post_processes/ball_glow.dart
@@ -20,15 +20,16 @@ class BallGlow extends PostProcessComponent<BallGlowPostProcess>
   }) : super(postProcess: BallGlowPostProcess());
 
   @override
-  Future<void> onLoad() async {
+  Future<void> onLoad() {
     postProcess.world = game.world;
-    await super.onLoad();
+    return super.onLoad();
   }
 
   @override
   void update(double dt) {
     position = game.world.cameraTarget.position;
     size = game.size;
+    super.update(dt);
   }
 }
 
