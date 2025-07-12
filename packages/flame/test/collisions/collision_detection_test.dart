flame-engine/flame

commit e6f3d105577cd346d377aaaed42d4ceb93aec077

fix: `angleTo` and `lookAt` should consider parental transformations (#3629)
Fix `angleTo` and `lookAt` considering parental transformations, notably
the angle and flips of parents.

Closes: #3625

---------

Co-authored-by: Lukas Klingsbo <lukas.klingsbo@gmail.com>
Co-authored-by: Lukas Klingsbo <me@lukas.fyi>

packages/flame/lib/src/components/position_component.dart
@@ -1,14 +1,12 @@
 import 'dart:math' as math;
-import 'dart:ui' hide Offset;
 
-import 'package:collection/collection.dart';
 import 'package:flame/camera.dart';
+import 'package:flame/extensions.dart';
+import 'package:flame/geometry.dart';
 import 'package:flame/src/anchor.dart';
 import 'package:flame/src/components/core/component.dart';
 import 'package:flame/src/components/mixins/coordinate_transform.dart';
 import 'package:flame/src/effects/provider_interfaces.dart';
-import 'package:flame/src/extensions/offset.dart';
-import 'package:flame/src/extensions/vector2.dart';
 import 'package:flame/src/game/notifying_vector2.dart';
 import 'package:flame/src/game/transform2d.dart';
 import 'package:flame/src/rendering/decorator.dart';
@@ -148,7 +146,7 @@ class PositionComponent extends Component
   @override
   double get angle => transform.angle;
   @override
-  set angle(double a) => transform.angle = a;
+  set angle(double a) => transform.angle = a.toNormalizedAngle();
 
   /// The scale factor of this component. The scale can be different along
   /// the X and Y dimensions. A scale greater than 1 makes the component
@@ -229,21 +227,46 @@ class PositionComponent extends Component
     );
   }
 
-  /// The resulting angle after all the ancestors and the components own angle
-  /// has been applied.
+  /// The resulting angle after all the ancestors and the components own angles
+  /// and scales have been applied.
   double get absoluteAngle {
-    // TODO(spydon): take scale into consideration
-    return ancestors(includeSelf: true)
-        .whereType<ReadOnlyAngleProvider>()
-        .map((c) => c.angle)
-        .sum;
+    var angle = 0.0;
+    var totalScaleX = 1.0;
+    var totalScaleY = 1.0;
+
+    final ancestorChain = ancestors(includeSelf: true).toList(growable: false)
+      ..reverse();
+
+    for (final ancestor in ancestorChain) {
+      if (ancestor is ReadOnlyScaleProvider) {
+        final ancestorScale = (ancestor as ReadOnlyScaleProvider).scale;
+        totalScaleX *= ancestorScale.x;
+        totalScaleY *= ancestorScale.y;
+        if (ancestorScale.x.isNegative) {
+          angle *= -1;
+        }
+        if (ancestorScale.y.isNegative) {
+          angle -= math.pi - angle;
+        }
+      }
+
+      if (ancestor is ReadOnlyAngleProvider) {
+        final reflected = totalScaleX.isNegative ^ totalScaleY.isNegative;
+        final localAngle = (ancestor as ReadOnlyAngleProvider).angle;
+        angle += reflected ? -localAngle : localAngle;
+      }
+    }
+
+    return angle.toNormalizedAngle();
   }
 
   /// The resulting scale after all the ancestors and the components own scale
   /// has been applied.
-  Vector2 get absoluteScale {
-    return ancestors().whereType<PositionComponent>().fold<Vector2>(
-          scale.clone(),
+  Vector2 get absoluteScale => scale.clone()..multiply(_parentAbsoluteScale);
+
+  Vector2 get _parentAbsoluteScale {
+    return ancestors().whereType<ReadOnlyScaleProvider>().fold<Vector2>(
+          Vector2.all(1.0),
           (totalScale, c) => totalScale..multiply(c.scale),
         );
   }
@@ -362,21 +385,41 @@ class PositionComponent extends Component
   Vector2 get absoluteCenter => absolutePositionOfAnchor(Anchor.center);
 
   /// Returns the angle formed by component's orientation vector and a vector
-  /// starting at component's absolute position and ending at [target]. This
-  /// angle is measured in clockwise direction. [target] should be in absolute/world
-  /// coordinate system.
+  /// starting at component's absolute position and ending at [target]. I.e.
+  /// how much the current component need to rotate to face the target. This
+  /// angle is measured in clockwise direction. [target] should be in
+  /// absolute/world coordinate system.
   ///
   /// Uses [nativeAngle] to decide the orientation direction of the component.
   /// See [lookAt] to make the component instantly rotate towards target.
   ///
-  /// Note: If target coincides with the current component, then it is treated
-  /// as being north.
+  /// Note: If target coincides with the current component's position, then it
+  /// is treated as being north.
   double angleTo(Vector2 target) {
-    return math.atan2(
-          target.x - absolutePosition.x,
-          absolutePosition.y - target.y,
-        ) -
-        (nativeAngle + absoluteAngle);
+    final direction = target - absolutePosition;
+    if (direction.isZero()) {
+      // If the target coincides with the component's position, we treat it as
+      // being north.
+      return -nativeAngle % tau;
+    }
+
+    final parentAbsoluteScale = _parentAbsoluteScale;
+    final targetAngle = math.atan2(
+      direction.x * scale.x.sign,
+      -direction.y * scale.y.sign,
+    );
+    final angleDifference = targetAngle - absoluteAngle - nativeAngle;
+
+    final hasOddFlips = parentAbsoluteScale.x.isNegative ^
+        parentAbsoluteScale.y.isNegative ^
+        scale.x.isNegative ^
+        scale.y.isNegative;
+    final hasSelfYFlip =
+        !parentAbsoluteScale.y.isNegative && scale.y.isNegative;
+
+    final result = (hasOddFlips ? -1 : 1) * angleDifference +
+        (hasSelfYFlip ? 1 : 0) * math.pi;
+    return result.toNormalizedAngle();
   }
 
   /// Rotates/snaps the component to look at the [target].
@@ -386,7 +429,9 @@ class PositionComponent extends Component
   /// [target] should to be in absolute/world coordinate system.
   ///
   /// See also: [angleTo]
-  void lookAt(Vector2 target) => angle += angleTo(target);
+  void lookAt(Vector2 target) {
+    angle += angleTo(target);
+  }
 
   //#endregion
 

examples/lib/stories/components/look_at_example.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flame/components.dart';
@@ -7,8 +8,10 @@ import 'package:flame/game.dart';
 import 'package:flame/palette.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class LookAtExample extends FlameGame {
+class LookAtExample extends FlameGame<_TapWorld>
+    with HasKeyboardHandlerComponents {
   static const description = 'This example demonstrates how a component can be '
       'made to look at a specific target using the lookAt method. Tap anywhere '
       'to change the target point for both the choppers. '
@@ -18,8 +21,7 @@ class LookAtExample extends FlameGame {
 
   LookAtExample() : super(world: _TapWorld());
 
-  late SpriteAnimationComponent _chopper1;
-  late SpriteAnimationComponent _chopper2;
+  late List<_ChopperParent> _choppers;
 
   @override
   Color backgroundColor() => const Color.fromARGB(255, 96, 145, 112);
@@ -32,36 +34,113 @@ class LookAtExample extends FlameGame {
     );
 
     _spawnChoppers(spriteSheet);
-    _spawnInfoText();
   }
 
   void _spawnChoppers(SpriteSheet spriteSheet) {
-    // Notice now the nativeAngle is set to pi because the chopper
-    // is facing in down/south direction in the original image.
-    world.add(
-      _chopper1 = SpriteAnimationComponent(
-        nativeAngle: pi,
-        size: Vector2.all(128),
-        anchor: Anchor.center,
-        animation: spriteSheet.createAnimation(row: 0, stepTime: 0.05),
+    _choppers = [
+      // Notice now the nativeAngle is set to pi because the chopper
+      // is facing in down/south direction in the original image.
+      _ChopperParent(
+        position: Vector2(0, -200),
+        chopper: SpriteAnimationComponent(
+          nativeAngle: pi,
+          size: Vector2.all(128),
+          anchor: Anchor.center,
+          animation: spriteSheet.createAnimation(row: 0, stepTime: 0.05),
+        ),
       ),
-    );
+      // This chopper does not use correct nativeAngle, hence using
+      // lookAt on it results in the sprite pointing in incorrect
+      // direction visually.
+      _ChopperParent(
+        position: Vector2(0, 200),
+        chopper: SpriteAnimationComponent(
+          size: Vector2.all(128),
+          anchor: Anchor.center,
+          animation: spriteSheet.createAnimation(row: 0, stepTime: 0.05),
+        ),
+      ),
+    ];
+    world.addAll(_choppers);
+  }
+}
 
-    // This chopper does not use correct nativeAngle, hence using
-    // lookAt on it results in the sprite pointing in incorrect
-    // direction visually.
-    world.add(
-      _chopper2 = SpriteAnimationComponent(
-        size: Vector2.all(128),
-        anchor: Anchor.center,
-        animation: spriteSheet.createAnimation(row: 0, stepTime: 0.05),
-        position: Vector2(0, 160),
-      ),
-    );
+class _TapWorld extends World
+    with TapCallbacks, KeyboardHandler, HasGameReference<LookAtExample> {
+  final CircleComponent target = CircleComponent(
+    radius: 5,
+    anchor: Anchor.center,
+    paint: BasicPalette.black.paint(),
+  );
+
+  int _currentFlipIdx = 0;
+  final _flips = [
+    (Vector2(1, 1), Vector2(1, 1)),
+    (Vector2(1, 1), Vector2(1, -1)),
+    (Vector2(1, 1), Vector2(-1, 1)),
+    (Vector2(1, 1), Vector2(-1, -1)),
+    (Vector2(1, -1), Vector2(1, 1)),
+    (Vector2(1, -1), Vector2(1, -1)),
+    (Vector2(1, -1), Vector2(-1, 1)),
+    (Vector2(1, -1), Vector2(-1, -1)),
+    (Vector2(-1, 1), Vector2(1, 1)),
+    (Vector2(-1, 1), Vector2(1, -1)),
+    (Vector2(-1, 1), Vector2(-1, 1)),
+    (Vector2(-1, 1), Vector2(-1, -1)),
+    (Vector2(-1, -1), Vector2(1, 1)),
+    (Vector2(-1, -1), Vector2(1, -1)),
+    (Vector2(-1, -1), Vector2(-1, 1)),
+    (Vector2(-1, -1), Vector2(-1, -1)),
+  ];
+
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    if (event is KeyDownEvent) {
+      if (keysPressed.contains(LogicalKeyboardKey.keyF)) {
+        _cycleFlips();
+        return true;
+      }
+    }
+    return false;
   }
 
-  // Just displays some information. No functional contribution to the example.
-  void _spawnInfoText() {
+  @override
+  void onTapDown(TapDownEvent event) {
+    _updatePosition(event.localPosition);
+  }
+
+  void _cycleFlips() {
+    _currentFlipIdx = (_currentFlipIdx + 1) % _flips.length;
+    final nextFlip = _flips[_currentFlipIdx];
+    for (final parent in game._choppers) {
+      parent.scale = nextFlip.$1;
+      parent.chopper.scale = nextFlip.$2;
+    }
+  }
+
+  void _updatePosition(Vector2 position) {
+    if (!target.isMounted) {
+      add(target);
+    }
+    target.position = position;
+    for (final parent in game._choppers) {
+      parent.chopper.lookAt(position);
+    }
+  }
+}
+
+class _ChopperParent extends PositionComponent
+    with HasGameReference<LookAtExample> {
+  final PositionComponent chopper;
+  late TextBoxComponent textBox;
+
+  _ChopperParent({
+    required super.position,
+    required this.chopper,
+  }) : super(children: [chopper]);
+
+  @override
+  FutureOr<void> onLoad() {
     final shaded = TextPaint(
       style: TextStyle(
         color: BasicPalette.white.color,
@@ -71,43 +150,41 @@ class LookAtExample extends FlameGame {
         ],
       ),
     );
-
-    world.add(
-      TextComponent(
-        text: 'nativeAngle = pi',
-        textRenderer: shaded,
+    parent!.add(
+      textBox = TextBoxComponent(
+        text: '-',
+        position: position + Vector2(0, -150),
         anchor: Anchor.center,
-        position: _chopper1.absolutePosition + Vector2(0, -70),
+        align: Anchor.topCenter,
+        textRenderer: shaded,
+        boxConfig: const TextBoxConfig(
+          maxWidth: 600,
+        ),
       ),
     );
-
-    world.add(
-      TextComponent(
-        text: 'nativeAngle = 0',
-        textRenderer: shaded,
-        anchor: Anchor.center,
-        position: _chopper2.absolutePosition + Vector2(0, -70),
-      ),
-    );
+    return super.onLoad();
   }
-}
-
-class _TapWorld extends World with TapCallbacks {
-  final CircleComponent _targetComponent = CircleComponent(
-    radius: 5,
-    anchor: Anchor.center,
-    paint: BasicPalette.black.paint(),
-  );
 
   @override
-  void onTapDown(TapDownEvent event) {
-    if (!_targetComponent.isMounted) {
-      add(_targetComponent);
-    }
-    _targetComponent.position = event.localPosition;
-    final choppers = children.query<SpriteAnimationComponent>();
-    for (final chopper in choppers) {
-      chopper.lookAt(event.localPosition);
-    }
+  void update(double dt) {
+    final angleTo = chopper.angleTo(game.world.target.position);
+    textBox.text = '''
+      nativeAngle = ${chopper.nativeAngle.toStringAsFixed(2)}
+      angleTo = ${angleTo.toStringAsFixed(2)}
+      absoluteAngle = ${chopper.absoluteAngle.toStringAsFixed(2)}
+      absoluteScale = ${_asSigns(chopper.absoluteScale)} (${_asSigns(absoluteScale)} * ${_asSigns(chopper.scale)})
+    ''';
+  }
+
+  String _asSigns(Vector2 v) {
+    return '[${_asSign(v.x)}, ${_asSign(v.y)}]';
+  }
+
+  String _asSign(double value) {
+    return switch (value.sign) {
+      1 => '+',
+      -1 => '-',
+      _ => '0',
+    };
   }
 }

packages/flame/lib/src/extensions/double.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:flame/geometry.dart';
+
 extension DoubleExtension on double {
   /// Converts +-[infinity] to +-[maxFinite].
   /// If it is already a finite value, that is returned.
@@ -9,5 +13,17 @@ extension DoubleExtension on double {
     } else {
       return this;
     }
+  }
+
+  /// This method normalizes the angle in radians to the range [-π, π].
+  ///
+  /// If the angle is already within this range, it is returned unchanged.
+  double toNormalizedAngle() {
+    if (this >= -pi && this <= pi) {
+      return this;
+    }
+    final normalized = this % tau;
+
+    return normalized > pi ? normalized - tau : normalized;
   }
 }

commit 7e8d3a9885f77da12456b148cd1f425395a00f71

fix: Hitboxes with vertically flipped ancestor should not reflect angle for vertices (#3642)
We broke vertically flipped hitboxes in #3629, this fixes it up so that
it works like it used to.

packages/flame/lib/src/components/position_component.dart
@@ -229,7 +229,14 @@ class PositionComponent extends Component
 
   /// The resulting angle after all the ancestors and the components own angles
   /// and scales have been applied.
-  double get absoluteAngle {
+  double get absoluteAngle => _absoluteAngle();
+
+  /// The resulting angle after all the ancestors and the components own angles
+  /// and scales have been applied, but without reflecting the angle
+  /// if the component is flipped.
+  double get absoluteAngleWithoutReflection => _absoluteAngle(reflect: false);
+
+  double _absoluteAngle({bool reflect = true}) {
     var angle = 0.0;
     var totalScaleX = 1.0;
     var totalScaleY = 1.0;
@@ -245,7 +252,7 @@ class PositionComponent extends Component
         if (ancestorScale.x.isNegative) {
           angle *= -1;
         }
-        if (ancestorScale.y.isNegative) {
+        if (ancestorScale.y.isNegative && reflect) {
           angle -= math.pi - angle;
         }
       }

packages/flame/lib/src/geometry/polygon_component.dart
@@ -152,7 +152,8 @@ class PolygonComponent extends ShapeComponent {
   /// gives back the shape vectors multiplied by the size and scale
   List<Vector2> globalVertices() {
     final scale = absoluteScale;
-    final angle = absoluteAngle;
+    final shouldReverse = scale.y.isNegative ^ scale.x.isNegative;
+    final angle = absoluteAngleWithoutReflection;
     final position = absoluteTopLeftPosition;
     if (!_cachedGlobalVertices.isCacheValid<dynamic>(<dynamic>[
       position,
@@ -168,7 +169,7 @@ class PolygonComponent extends ShapeComponent {
           ..add(position)
           ..rotate(angle, center: position);
       }
-      if (scale.y.isNegative || scale.x.isNegative) {
+      if (shouldReverse) {
         // Since the list will be clockwise we have to reverse it for it to
         // become counterclockwise.
         _reverseList(_globalVertices);

packages/flame/test/collisions/collision_detection_test.dart
@@ -462,6 +462,198 @@ void main() {
         reason: 'Wrong number of intersections',
       );
     });
+
+    test('flipped rectangle intersection', () {
+      final rectangleA = RectangleComponent(
+        position: Vector2.zero(),
+        size: Vector2.all(4),
+      );
+      final rectangleB = RectangleComponent(
+        position: Vector2.all(2),
+        size: Vector2.all(4),
+      );
+      final intersections = geometry.intersections(rectangleA, rectangleB);
+      expect(
+        intersections,
+        containsAll([
+          Vector2(2, 4),
+          Vector2(4, 2),
+        ]),
+        reason: 'Missed intersections',
+      );
+      expect(
+        intersections,
+        hasLength(2),
+        reason: 'Wrong number of intersections',
+      );
+
+      rectangleB.flipVerticallyAroundCenter();
+      final flippedIntersections =
+          geometry.intersections(rectangleA, rectangleB);
+
+      expect(
+        flippedIntersections,
+        containsAll([
+          Vector2(2, 4),
+          Vector2(4, 2),
+        ]),
+        reason: 'Missed intersections',
+      );
+      expect(
+        flippedIntersections,
+        hasLength(2),
+        reason: 'Wrong number of intersections',
+      );
+
+      rectangleB.flipHorizontallyAroundCenter();
+      final doubleFlippedIntersections =
+          geometry.intersections(rectangleA, rectangleB);
+
+      expect(
+        doubleFlippedIntersections,
+        containsAll([
+          Vector2(2, 4),
+          Vector2(4, 2),
+        ]),
+        reason: 'Missed intersections',
+      );
+      expect(
+        doubleFlippedIntersections,
+        hasLength(2),
+        reason: 'Wrong number of intersections',
+      );
+    });
+
+    test('vertically flipped and angled rectangle intersections', () {
+      final rectangleA = RectangleComponent(
+        position: Vector2.all(4),
+        size: Vector2.all(4),
+        angle: tau / 4,
+        anchor: Anchor.center,
+      );
+      final rectangleB = RectangleComponent(
+        position: Vector2.all(4),
+        size: Vector2.all(4),
+      );
+      final intersections = geometry.intersections(rectangleA, rectangleB);
+      expect(
+        intersections,
+        containsAll([
+          Vector2(4, 6),
+          Vector2(6, 4),
+        ]),
+        reason: 'Missed intersections',
+      );
+      expect(
+        intersections,
+        hasLength(2),
+        reason: 'Wrong number of intersections',
+      );
+
+      rectangleB.flipVerticallyAroundCenter();
+      final flippedIntersections =
+          geometry.intersections(rectangleA, rectangleB);
+
+      expect(
+        flippedIntersections,
+        containsAll([
+          Vector2(4, 6),
+          Vector2(6, 4),
+        ]),
+        reason: 'Missed intersections',
+      );
+      expect(
+        flippedIntersections,
+        hasLength(2),
+        reason: 'Wrong number of intersections',
+      );
+
+      rectangleB.flipHorizontallyAroundCenter();
+      final doubleFlippedIntersections =
+          geometry.intersections(rectangleA, rectangleB);
+
+      expect(
+        doubleFlippedIntersections,
+        containsAll([
+          Vector2(4, 6),
+          Vector2(6, 4),
+        ]),
+        reason: 'Missed intersections',
+      );
+      expect(
+        doubleFlippedIntersections,
+        hasLength(2),
+        reason: 'Wrong number of intersections',
+      );
+    });
+
+    testWithFlameGame(
+      'horizontally flipped and angled rectangle intersections with parents',
+      (game) async {
+        final rectangleA = RectangleHitbox();
+        final rectangleB = RectangleHitbox();
+        final parentA = RectangleComponent(
+          position: Vector2(1, 0),
+          size: Vector2.all(4),
+          children: [rectangleA],
+        )..flipHorizontallyAroundCenter();
+        final parentB = RectangleComponent(
+          position: Vector2(2, 1),
+          size: Vector2.all(4),
+          children: [rectangleB],
+        );
+        await game.ensureAddAll([parentA, parentB]);
+
+        final intersections = geometry.intersections(rectangleA, rectangleB);
+        expect(
+          intersections,
+          containsAll([
+            Vector2(5, 1),
+            Vector2(2, 4),
+          ]),
+          reason: 'Missed intersections',
+        );
+        expect(
+          intersections,
+          hasLength(2),
+          reason: 'Wrong number of intersections',
+        );
+      },
+    );
+
+    testWithFlameGame(
+      'flipped and angled rectangle intersections with parents',
+      (game) async {
+        final rectangleA = RectangleHitbox();
+        final rectangleB = RectangleHitbox();
+        final parentA = RectangleComponent(
+          position: Vector2(1, 0),
+          size: Vector2.all(4),
+          children: [rectangleA],
+        )..flipVerticallyAroundCenter();
+        final parentB = RectangleComponent(
+          position: Vector2(2, 1),
+          size: Vector2.all(4),
+          children: [rectangleB],
+        );
+        await game.ensureAddAll([parentA, parentB]);
+
+        final intersections = geometry.intersections(rectangleA, rectangleB);
+        expect(
+          intersections,
+          containsAll([
+            Vector2(5, 1),
+            Vector2(2, 4),
+          ]),
+          reason: 'Missed intersections',
+        );
+        expect(
+          intersections,
+          hasLength(2),
+          reason: 'Wrong number of intersections',
+        );
+      },
+    );
   });
 
   group('Circle intersections tests', () {

packages/flame/test/extensions/vector2_test.dart
@@ -191,6 +191,12 @@ void main() {
         position.rotate(math.pi / 2, center: center);
         expect(position, closeToVector(Vector2(4.0, -3.0)));
       });
+
+      test('rotate - with center as itself', () {
+        final v = Vector2(1, 0);
+        v.rotate(0.01, center: v.clone());
+        expect(v, Vector2(1, 0));
+      });
     });
 
     group('Scaling', () {