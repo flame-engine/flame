9d96ecd4347efbbc0993214f2ff31c6bb6ff36b7
Grayson Erhard <develop@graysonerhard.com>
2025-07-13 14:52:52 -0600

    perf: make all region properties immutable

--- packages/flame_texturepacker/lib/src/model/region.dart
+++ packages/flame_texturepacker/lib/src/model/region.dart
@@ -1,18 +1,58 @@
 import 'package:flame_texturepacker/src/model/page.dart';
+import 'package:flutter/foundation.dart';
 
 /// Represents a region within the texture packer atlas.
+@immutable
 class Region {
-  late Page page;
-  late String name;
-  late double left;
-  late double top;
-  late double width;
-  late double height;
-  double offsetX = 0;
-  double offsetY = 0;
-  double originalWidth = 0;
-  double originalHeight = 0;
-  int degrees = 0;
-  bool rotate = false;
-  int index = -1;
+  /// The page in the texture pack this region belongs to.
+  late final Page page;
+
+  /// The name of the original image file, without the file's extension.
+  /// If the name ends with an underscore followed by only numbers, that part is
+  /// excluded: underscores denote special instructions to the texture packer.
+  late final String name;
+
+  /// The left position of the region in the texture atlas.
+  late final double left;
+
+  /// The top position of the region in the texture atlas.
+  late final double top;
+
+  /// The width of the image, after whitespace was removed for packing.
+  late final double width;
+
+  /// The height of the image, after whitespace was removed for packing.
+  late final double height;
+
+  /// The offset from the left of the original image to the left of the packed
+  /// image, after whitespace was removed for packing.
+  late final double offsetX;
+
+  /// The offset from the bottom of the original image to the bottom of the
+  /// packed image, after whitespace was removed for packing.
+  late final double offsetY;
+
+  /// The width of the image, before whitespace was removed and rotation was
+  /// applied for packing.
+  late final double originalWidth;
+
+  /// The height of the image, before whitespace was removed for packing.
+  late final double originalHeight;
+
+  /// The degrees the region has been rotated, counter clockwise between 0 and
+  /// 359. Most atlas region handling deals only with 0 or 90 degree rotation
+  /// (enough to handle rectangles).
+  /// More advanced texture packing may support other rotations (eg, for tightly
+  /// packing polygons).
+  late final int degrees;
+
+  /// If true, the region has been rotated 90 degrees counter clockwise.
+  late final bool rotate;
+
+  /// The number at the end of the original image file name, or -1 if none.
+  ///
+  /// When sprites are packed, if the original file name ends with a number, it
+  /// is stored as the index and is not considered as part of the sprite's name.
+  /// This is useful for keeping animation frames in order.
+  late final int index;
 }
