8650d786fa23ae0cee66e58006af13259cd433e2
Liv Tan Ong <livtanong@gmail.com>
2025-07-29 10:48:21 +0800

    PaddingComponent docstring

--- packages/flame/lib/src/experimental/padding_component.dart
+++ packages/flame/lib/src/experimental/padding_component.dart
@@ -3,6 +3,17 @@ import 'package:flame/experimental.dart';
 import 'package:flame/extensions.dart';
 import 'package:flutter/rendering.dart';
 
+/// A padding component akin to Flutter's Padding widget.
+/// Use [padding] as you would Flutter's counterpart.
+/// In general, do not set the size of this widget explicitly, because it is
+/// only designed to shrink or expand to its child's dimensions, plus padding.
+///
+/// Set the child of this component with [child]. Avoid using [add] directly on
+/// an instance of [PaddingComponent] because its behavior is undefined with
+/// multiple children. It is designed only for one child.
+///
+/// You may set [padding] as well as the [child] after the fact, and it will
+/// cause the layout to refresh.
 class PaddingComponent extends LayoutComponent {
   PaddingComponent({
     EdgeInsets? padding,
