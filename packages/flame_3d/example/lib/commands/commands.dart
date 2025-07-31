d591575263d8b13aee862efe05842001aa60f89d
Luan Nico <luanpotter27@gmail.com>
2025-07-31 12:03:10 -0400

    feat: Organize components and add destroy command to flame_3d example (#3665)
    Organize components and add destroy command to flame_3d example

--- packages/flame_3d/example/lib/commands/commands.dart
+++ packages/flame_3d/example/lib/commands/commands.dart
@@ -1,5 +1,7 @@
+import 'package:example/commands/destroy_command.dart';
 import 'package:example/commands/reset_command.dart';
 
 const customCommandProviders = [
   ResetCommand.new,
+  DestroyCommand.new,
 ];
