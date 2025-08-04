5519bebf88019a75f9f166056be327b47811fab5
Liv Tan Ong <livtanong@gmail.com>
2025-08-04 08:57:41 +0800

    cleanup

--- packages/flame/lib/src/experimental/notifying_nullable_vector_2.dart
+++ packages/flame/lib/src/experimental/notifying_nullable_vector_2.dart
@@ -5,7 +5,8 @@ class NotifyingNullableVector2 extends NullableVector2 with ChangeNotifier {
   NotifyingNullableVector2(super.x, super.y);
 
   factory NotifyingNullableVector2.fromNullableVector2(
-      NullableVector2? nullableVector2) {
+    NullableVector2? nullableVector2,
+  ) {
     return NotifyingNullableVector2(nullableVector2?.x, nullableVector2?.y);
   }
 
--- packages/flame/lib/src/experimental/padding_component.dart
+++ packages/flame/lib/src/experimental/padding_component.dart
@@ -1,4 +1,3 @@
-import 'package:flame/components.dart';
 import 'package:flame/experimental.dart';
 import 'package:flame/extensions.dart';
 import 'package:flutter/rendering.dart';
