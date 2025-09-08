cea8a01abeedea60ce4ac9ca588db16bb6f37f1f
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2025-09-08 17:14:12 +0200

    Update tests

--- packages/flame_forge2d/test/forge2d_world_test.dart
+++ packages/flame_forge2d/test/forge2d_world_test.dart
@@ -8,22 +8,40 @@ class _TestForge2DWorld extends Forge2DWorld {
 
 void main() {
   testWithFlameGame(
-    'Bodies are set to awake after remounting the world',
+    'Bodies are destroyed after world is removed when destroyBodiesOnRemove is '
+    'true',
     (game) async {
       final world = _TestForge2DWorld();
       game.add(world);
       await game.ready();
       final bodyDef = BodyDef()..type = BodyType.dynamic;
-      final body = world.createBody(bodyDef);
+      final component = BodyComponent(bodyDef: bodyDef);
+      world.add(component);
       await game.ready();
-      expect(body.isAwake, isTrue);
-      game.update(100);
-      expect(body.isAwake, isFalse);
+      final body = component.body;
 
       world.removeFromParent();
+      await game.ready();
+      expect(world.physicsWorld.bodies, isNot(contains(body)));
+    },
+  );
+
+  testWithFlameGame(
+    'Bodies are not destroyed after world is removed when '
+    'destroyBodiesOnRemove is false',
+    (game) async {
+      final world = _TestForge2DWorld()..destroyBodiesOnRemove = false;
       game.add(world);
       await game.ready();
-      expect(body.isAwake, isTrue);
+      final bodyDef = BodyDef()..type = BodyType.dynamic;
+      final component = BodyComponent(bodyDef: bodyDef);
+      world.add(component);
+      await game.ready();
+      final body = component.body;
+
+      world.removeFromParent();
+      await game.ready();
+      expect(world.physicsWorld.bodies, contains(body));
     },
   );
 }
