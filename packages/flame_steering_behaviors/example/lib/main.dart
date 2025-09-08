b426d27b1081f6fa17d7307c7ec34be185cdf2b3
Oscar Martin <martinm.oscar@gmail.com>
2025-09-08 12:43:17 +0200

    chore: separate flame_steering_behaviors PRs

--- packages/flame_steering_behaviors/example/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:example/src/example_game.dart';
-import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(GameWidget(game: ExampleGame()));
-}
