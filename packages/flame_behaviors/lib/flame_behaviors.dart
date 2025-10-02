d2a1b6b8d933130645338cea3691fafc234ad661
Oscar <martinm.oscar@gmail.com>
2025-10-02 16:06:32 +0200

    Update packages/flame_behaviors/lib/flame_behaviors.dart
    Co-authored-by: Erick <erickzanardoo@gmail.com>

--- packages/flame_behaviors/lib/flame_behaviors.dart
+++ packages/flame_behaviors/lib/flame_behaviors.dart
@@ -1,5 +1,5 @@
 /// Flame Behaviors applies separation of concerns to game logic in the form of
-/// Entities and Behaviors, built by Very Good Ventures.
+/// Entities and Behaviors, originally built by Very Good Ventures.
 library;
 
 export 'src/behaviors/behaviors.dart';
