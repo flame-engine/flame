c219b29250931442b7829eda683ec1bb638792b7
lunxinfeng <892452936@qq.com>
2025-12-15 20:47:23 +0800

    fix import in riverpod 3.0

--- packages/flame_studio/lib/src/core/component_tree.dart
+++ packages/flame_studio/lib/src/core/component_tree.dart
@@ -3,6 +3,7 @@ import 'package:flame/game.dart';
 import 'package:flame_studio/src/core/game_controller.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 final componentTreeProvider =
     StateNotifierProvider<ComponentTreeObserver, ComponentTreeState>((ref) {
--- packages/flame_studio/lib/src/core/game_controller.dart
+++ packages/flame_studio/lib/src/core/game_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 final gameControllerProvider =
     StateNotifierProvider<_GameController, _GameState>(
