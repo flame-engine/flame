979366210571ec38a629b66161a589a741278699
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-02-12 21:34:02 +0100

    Fix experimental usage

--- packages/flame/lib/src/effects/glow_effect.dart
+++ packages/flame/lib/src/effects/glow_effect.dart
@@ -2,14 +2,12 @@ import 'dart:ui';
 
 import 'package:flame/effects.dart';
 import 'package:flame/src/effects/provider_interfaces.dart';
-import 'package:meta/meta.dart';
 
 /// Change the MaskFilter on Paint of a component over time.
 ///
 /// This effect applies incremental changes to the MaskFilter on Paint of a
 /// component and requires that any other effect or update logic applied to the
 /// same component also used incremental updates.
-@experimental
 class GlowEffect extends Effect with EffectTarget<PaintProvider> {
   GlowEffect(
     this.strength,
