ce1d42adfb93ebd2856f001fc624da6f9ea22643
Jochum van der Ploeg <jochum@vdploeg.net>
2026-03-23 08:23:40 +0100

    feat(flame_3d): Add culling scenario and implement AABB frustum culling logic(#3868)
    Add AABB frustum culling support.

--- packages/flame_3d/lib/src/model/model_component.dart
+++ packages/flame_3d/lib/src/model/model_component.dart
@@ -20,10 +20,8 @@ class ModelComponent extends Object3D {
     super.scale,
   });
 
-  Aabb3 get aabb => _aabb
-    ..setFrom(model.aabb)
-    ..transform(transformMatrix);
-  final Aabb3 _aabb = Aabb3();
+  @override
+  Aabb3? computeLocalAabb() => model.aabb;
 
   @override
   void bind(GraphicsDevice device) {
@@ -35,8 +33,8 @@ class ModelComponent extends Object3D {
 
       final mesh = node.node.mesh;
       if (mesh != null) {
-        device.jointsInfo.jointTransformsPerSurface = node.jointTransforms;
-        world.device
+        device
+          ..jointsInfo.jointTransformsPerSurface = node.jointTransforms
           ..model.setFrom(
             worldTransformMatrix.multiplied(node.combinedTransform),
           )
