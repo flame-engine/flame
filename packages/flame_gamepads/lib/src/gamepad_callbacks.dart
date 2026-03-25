d3fe2de72ea37a2bcfa59e04870ad4752f59e58f
Lisette Linse <lisette.linse@gmail.com>
2026-03-25 22:01:00 +0100

    doc: Add dartdoc to GamepadCallbacks

--- packages/flame_gamepads/lib/src/gamepad_callbacks.dart
+++ packages/flame_gamepads/lib/src/gamepad_callbacks.dart
@@ -4,6 +4,11 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:gamepads/gamepads.dart';
 
+/// This mixin can be added to a [Component] allowing it to receive
+/// normalized gamepad events while the component is mounted.
+///
+/// If this is added to multiple components that are concurrently
+/// mounted, all of them will receive the event.
 mixin GamepadCallbacks on Component {
   void onGamepadEvent(NormalizedGamepadEvent event) {}
 
