2565396c35df9e21518e83e1c6b6bde8673103dc
Jochum van der Ploeg <jochum@flutterflow.io>
2026-03-25 15:09:09 +0100

    refactor(flame_3d)!: Allow LightComponents to be nested and make Materials decide themselves how lights should be used

--- packages/flame_3d/lib/src/resources/material/spatial_material.dart
+++ packages/flame_3d/lib/src/resources/material/spatial_material.dart
@@ -82,7 +82,29 @@ class SpatialMaterial extends Material {
   }
 
   void _applyLights(GraphicsDevice device) {
-    device.lightingInfo.apply(fragmentShader);
+    final lights = device.lights;
+
+    // Apply ambient light (at most one, fallback to default).
+    final ambient =
+        lights.map((e) => e.source).whereType<AmbientLight>().firstOrNull ??
+        AmbientLight();
+    fragmentShader
+      ..setColor('AmbientLight.color', ambient.color)
+      ..setFloat('AmbientLight.intensity', ambient.intensity);
+
+    // Apply point lights.
+    final points = lights.where((e) => e.source is PointLight);
+
+    // NOTE: using floats because Android GLES does not support integer uniforms
+    // Refer to https://github.com/flutter/engine/pull/55329
+    fragmentShader.setFloat('Lights.numLights', points.length.toDouble());
+
+    for (final (index, light) in points.indexed) {
+      fragmentShader
+        ..setVector3('Lights.positions[$index]', light.position)
+        ..setColor('Lights.colors[$index]', light.source.color)
+        ..setFloat('Lights.intensities[$index]', light.source.intensity);
+    }
   }
 
   static const _maxJoints = 16;
