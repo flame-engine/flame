1737e52af21cb2d5097917f0f2b9dd81d2f85ee7
Jochum van der Ploeg <jochum@vdploeg.net>
2026-05-08 12:18:09 +0200

    fix(flame_3d): Fix material on `Line3D`

--- packages/flame_3d/test/components/line_3d_test.dart
+++ packages/flame_3d/test/components/line_3d_test.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flame_3d/components.dart';
 import 'package:flame_3d/core.dart';
+import 'package:flame_3d/resources.dart';
 import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -11,7 +12,7 @@ void main() {
       final line = Line3D.generate(
         start: Vector3(0, -1, 0),
         end: Vector3(0, 1, 0),
-        color: const Color(0xFFFFFFFF),
+        material: UnlitMaterial(),
       );
 
       expect(
@@ -24,7 +25,7 @@ void main() {
       final line = Line3D.generate(
         start: Vector3(-1, 0, 0),
         end: Vector3(1, 0, 0),
-        color: const Color(0xFFFFFFFF),
+        material: UnlitMaterial(),
       );
 
       expect(
