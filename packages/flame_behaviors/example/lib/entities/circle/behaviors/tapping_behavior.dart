98e65544152253a3facaae04e4a48780fa73f8a0
Luan Nico <luanpotter27@gmail.com>
2026-08-03 10:34:09 -0400

    refactor!: Remove events handled property (#3976)
    Remove events handled property, continuing the [events master
    plan](https://docs.google.com/document/d/1nBUup9QCPioVwWL1zs79z1hWF882tAYfNywFMdye2Qc/edit?pli=1&tab=t.0).
    The behaviour is kept entirely on the `continuePropagation` field.

--- packages/flame_behaviors/example/lib/entities/circle/behaviors/tapping_behavior.dart
+++ packages/flame_behaviors/example/lib/entities/circle/behaviors/tapping_behavior.dart
@@ -4,9 +4,11 @@ import 'package:flame_behaviors_example/entities/entities.dart';
 
 /// This behavior ensures that SpawningBehavior of the game does not spawn
 /// anything when we click on a circle (for dragging).
+///
+/// It does so simply by existing: the tap is delivered to this behavior, and
+/// since it does not set `continuePropagation`, it never reaches the game-level
+/// SpawningBehavior underneath.
 class TappingBehavior extends TappableBehavior<Circle> {
   @override
-  void onTapDown(TapDownEvent event) {
-    event.handled = true;
-  }
+  void onTapDown(TapDownEvent event) {}
 }
